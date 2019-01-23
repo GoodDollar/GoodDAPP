1676fb16d0b4808883e5b4633a8c5e8cce574523
Your Name <dminones@gmail.com>
2019-01-23 18:00:26 -0300

    Improving tab look and feel

--- src/components/appNavigation/AppNavigation.js
+++ src/components/appNavigation/AppNavigation.js
@@ -2,62 +2,87 @@
 import { extend } from "lodash";
 import React, { Component } from "react";
 import GoodWallet from "../../lib/wallet/GoodWallet";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 import { createSwitchNavigator } from "@react-navigation/core";
 import { Link } from "@react-navigation/web";
 
-import Rewards from "./Rewards"
-import BuySell from "./BuySell"
-import Dashboard from "./Dashboard"
-import Donate from "./Donate"
+import Rewards from "./Rewards";
+import BuySell from "./BuySell";
+import Dashboard from "./Dashboard";
+import Donate from "./Donate";
 
 type AppNavigationState = {
-  pubkey: string,
-  email?: string,
-  phone?: string,
-  name?: string,
-  smsValidated?: boolean
+  pubkey: string
 };
 
-const AppNavigator = createSwitchNavigator({
-  Rewards,
-  BuySell,
-  Dashboard,
-  Donate
-}, {
-  initialRouteName: "Dashboard"
-});
+type AppNavigationProps = {
+  navigation: any
+};
+
+const AppNavigator = createSwitchNavigator(
+  {
+    Rewards,
+    BuySell,
+    Dashboard,
+    Donate
+  },
+  {
+    initialRouteName: "Dashboard"
+  }
+);
+
+type TabButtonProps = {
+  text: string,
+  routeName: string
+};
+const TabButton = (props: TabButtonProps) => {
+  return (
+    <Link routeName={props.routeName}>
+      <Image
+        source="https://facebook.github.io/react-native/img/opengraph.png"
+        style={styles.tabIcon}
+      />
+      <Text style={styles.tabView}>{props.text}</Text>
+    </Link>
+  );
+};
 
 class AppNavigation extends React.Component<
-  { navigation: any },
+  AppNavigationProps,
   AppNavigationState
 > {
   static router = AppNavigator.router;
-  state = {
-    pubkey: GoodWallet.account
-  };
-  constructor(props) {
+
+  constructor(props: AppNavigationProps) {
     super(props);
     console.log(props);
   }
 
-  //TODO: Change div and links for proper tabviews
   render() {
     return (
       <React.Fragment>
-        <div>
-          <Link routeName="Rewards">Rewards</Link>
-          {" - "}
-          <Link routeName="BuySell">BuySell</Link>
-          {" - "}
-          <Link routeName="Donate">Donate</Link>
-          {" - "}
-          <Link routeName="Dashboard">Dashboard</Link>
-        </div>
+        <View style={styles.tabView}>
+          <TabButton routeName="Rewards" text="Rewards" />
+          <TabButton routeName="BuySell" text="BuySell" />
+          <TabButton routeName="Donate" text="Donate" />
+          <TabButton routeName="Dashboard" text="Dashboard" />
+        </View>
         <AppNavigator navigation={this.props.navigation} />
       </React.Fragment>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tabButton: { color: "#d2d2d2", alignItems: "center", textAlign: "center" },
+  tabView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    height: "60px",
+    backgroundColor: "#909090",
+    color: "#d2d2d2"
+  },
+  tabIcon: { width: 40, height: 40 }
+});
 export default AppNavigation;

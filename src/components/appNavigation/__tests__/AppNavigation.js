4e3adbd6a92a40bac2a685a5bdfd0beef9fa2d40
Your Name <dminones@gmail.com>
2019-01-25 16:58:31 -0300

    AppNavigation Snapshot added

--- src/components/appNavigation/__tests__/AppNavigation.js
+++ src/components/appNavigation/__tests__/AppNavigation.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import AppNavigation from '../AppNavigation'
 import { createSwitchNavigator } from '@react-navigation/core'
-import { createBrowserApp, Link } from '@react-navigation/web'
+import { createBrowserApp } from '@react-navigation/web'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
 describe('AppNavigation', () => {
   it('renders without errors', () => {
-    const Router = createSwitchNavigator({ AppNavigation })
-    const WebRouter = createBrowserApp(Router)
+    const WebRouter = createBrowserApp(createSwitchNavigator({ AppNavigation }))
     const tree = renderer.create(<WebRouter />)
     expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it.only('matches snapshot', () => {
+    const WebRouter = createBrowserApp(createSwitchNavigator({ AppNavigation }))
+    const component = renderer.create(<WebRouter />)
+    expect(component.toJSON()).toMatchSnapshot()
   })
 })

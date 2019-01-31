905929ccae42ff3910dd63bd09bfadb4716e7c1e
fernandomg <fernando.greco@gmail.com>
2019-01-31 14:49:56 -0300

    Fix types in components.js

--- src/components/signup/components.js
+++ src/components/signup/components.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { View } from 'react-native'
 import { IconButton, Button } from 'react-native-paper'
 
-export const BackButton = ({ back, ...other }) => (
+export const BackButton = ({ back, ...other }: { back: Function, other: Array<any> }) => (
   <IconButton style={{ marginTop: '15px' }} icon="arrow-back" size={20} onPress={() => back()} {...other} />
 )
 export const ContinueButton = (props: { valid: boolean, handleSubmit: () => any }) => (
@@ -18,6 +18,6 @@ export const ContinueButton = (props: { valid: boolean, handleSubmit: () => any 
     Continue
   </Button>
 )
-export const Wrapper = props => (
+export const Wrapper = (props: any) => (
   <View style={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center' }} {...props} />
 )

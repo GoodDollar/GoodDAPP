410f4ad8ba1a84e32ebc9abfc91e8b84d82e9842
Your Name <dminones@gmail.com>
2019-02-08 16:27:28 -0300

    Dashboard tests

--- src/components/dashboard/__tests__/Dashboard.js
+++ src/components/dashboard/__tests__/Dashboard.js
@@ -7,28 +7,30 @@ import { createBrowserApp } from '@react-navigation/web'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-describe.skip('Dashboard', () => {
+const routes = {
+  Dashboard,
+  Donate
+}
+
+const AppNavigator = createSwitchNavigator(routes)
+class AppNavigation extends React.Component<AppNavigationProps, AppNavigationState> {
+  static router = AppNavigator.router
+
+  render() {
+    return <AppNavigator navigation={this.props.navigation} screenProps={{ routes }} />
+  }
+}
+
+describe('Dashboard', () => {
   it('renders without errors', () => {
-    const routes = {
-      Dashboard,
-      Donate
-    }
+    const WebRouter = createBrowserApp(createSwitchNavigator({ AppNavigation }))
 
-    const AppNavigator = createBrowserApp(createSwitchNavigator(routes))
-    class AppNavigation extends React.Component<AppNavigationProps, AppNavigationState> {
-      static router = AppNavigator.router
-
-      render() {
-        return <AppNavigator navigation={this.props.navigation} screenProps={{ routes }} />
-      }
-    }
-
-    const tree = renderer.create(<AppNavigation />)
+    const tree = renderer.create(<WebRouter />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const WebRouter = createBrowserApp(createSwitchNavigator({ Dashboard }))
+    const WebRouter = createBrowserApp(createSwitchNavigator({ AppNavigation }))
 
     const component = renderer.create(<WebRouter />)
     const tree = component.toJSON()
@@ -36,9 +38,9 @@ describe.skip('Dashboard', () => {
   })
 
   it('matches snapshot when changing tab', () => {
-    const WebRouter = createBrowserApp(createSwitchNavigator({ Dashboard }))
+    const WebRouter = createBrowserApp(createSwitchNavigator({ AppNavigation }))
     const component = renderer.create(<WebRouter />)
-    const [tabsView] = component.toJSON()
+    const [tabsView] = component.toJSON().children
     const [tabButton] = tabsView.children
     tabButton.props.onClick(new Event('fakeEvent'))
     const tree = component.toJSON()

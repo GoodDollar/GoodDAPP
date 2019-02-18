39e9cad4138c0684a0506578883ebe6a77c93370
Hadar Rottenberg <hadarro@etoro.com>
2019-02-18 18:44:50 +0200

    WIP: wait for wallet before rendering route

--- src/components/AppSwitch.js
+++ src/components/AppSwitch.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { SceneView } from '@react-navigation/core'
 import goodWallet from '../lib/wallet/GoodWallet'
 import goodWalletLogin from '../lib/login/GoodWalletLogin'
+import API from '../lib/API/api'
+
 import logger from '../lib/logger/pino-logger'
 
 type LoadingProps = {
@@ -23,12 +25,19 @@ const TIMEOUT = 1000
  * The main app route. Here we decide where to go depending on the user's credentials status
  */
 class AppSwitch extends React.Component<LoadingProps, {}> {
+  state = {
+    activeKey: 'Splash'
+  }
   /**
    * Triggers the required actions before navigating to any app's page
    * @param {LoadingProps} props
    */
   constructor(props: LoadingProps) {
     super(props)
+    const { navigation } = this.props
+    this.savedActiveKey = navigation.state.routes[navigation.state.index].key
+  }
+  componentWillMount() {
     this.checkAuthStatus()
   }
 
@@ -48,7 +57,10 @@ class AppSwitch extends React.Component<LoadingProps, {}> {
     const isLoggedIn = credsOrError.jwt !== undefined
 
     if (isLoggedIn && isCitizen) {
-      this.props.navigation.navigate('AppNavigation')
+      log.info('to AppNavigation', this.savedActiveKey)
+      // this.props.navigation.navigate('AppNavigation')
+      // this.props.navigation.navigate(this.activeKey)
+      this.setState({ activeKey: this.savedActiveKey })
     } else {
       const { jwt } = credsOrError
 
@@ -64,9 +76,8 @@ class AppSwitch extends React.Component<LoadingProps, {}> {
   }
 
   render() {
-    const { descriptors, navigation } = this.props
-    const activeKey = navigation.state.routes[navigation.state.index].key
-    const descriptor = descriptors[activeKey]
+    const { descriptors } = this.props
+    const descriptor = descriptors[this.state.activeKey]
     return <SceneView navigation={descriptor.navigation} component={descriptor.getComponent()} />
   }
 }

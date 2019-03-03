309f8d98570ce73ec1dc45ac0b07ac01fafec4d4
Your Name <dminones@gmail.com>
2019-03-03 12:52:34 -0300

    Added continue button to send address via typing address

--- src/components/dashboard/Send.js
+++ src/components/dashboard/Send.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Wrapper, TopBar, Section, IconButton } from '../common'
-import { StyleSheet } from 'react-native'
+import { Wrapper, TopBar, Section, IconButton, CustomButton } from '../common'
+import { StyleSheet, View } from 'react-native'
 import { TextInput } from 'react-native-paper'
 import { useScreenState } from '../appNavigation/stackNavigation'
 
@@ -28,6 +28,15 @@ const GenerateLinkButton = ({ screenProps, to }) => (
   />
 )
 
+const ContinueButton = ({ screenProps, to }) => (
+  <CustomButton
+    onPress={() => screenProps.push('Amount', { to, nextRoutes: ['Reason', 'SendQRSummary'] })}
+    mode="contained"
+  >
+    Continue
+  </CustomButton>
+)
+
 const Send = props => {
   const [screenState, setScreenState] = useScreenState(props.screenProps)
 
@@ -36,12 +45,17 @@ const Send = props => {
     <Wrapper>
       <TopBar />
       <Section style={styles.bottomSection}>
-        <Section.Title>TO WHO?</Section.Title>
-        <TextInput onChangeText={text => setScreenState({ to: text })} value={to} />
-        <Section.Row>
-          <ScanQRButton screenProps={props.screenProps} />
-          <GenerateLinkButton screenProps={props.screenProps} to={to} />
-        </Section.Row>
+        <View style={styles.topContainer}>
+          <Section.Title>TO WHO?</Section.Title>
+          <TextInput onChangeText={text => setScreenState({ to: text })} value={to} />
+          <Section.Row>
+            <ScanQRButton screenProps={props.screenProps} />
+            <GenerateLinkButton screenProps={props.screenProps} to={to} />
+          </Section.Row>
+        </View>
+        <View style={styles.bottomContainer}>
+          <ContinueButton screenProps={props.screenProps} to={to} />
+        </View>
       </Section>
     </Wrapper>
   )
@@ -57,6 +71,9 @@ const styles = StyleSheet.create({
     alignItems: 'baseline'
   },
   bottomSection: {
+    flex: 1
+  },
+  topContainer: {
     flex: 1
   }
 })

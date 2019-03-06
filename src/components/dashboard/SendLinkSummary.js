d28e7421ab6ccdc27ee9f1caeef43cf52402c4a1
Your Name <dminones@gmail.com>
2019-03-06 10:53:05 -0300

    Catching error on link separatelly so we can revert transaction

--- src/components/dashboard/SendLinkSummary.js
+++ src/components/dashboard/SendLinkSummary.js
@@ -28,43 +28,60 @@ const SendLinkSummary = (props: AmountProps) => {
     setDialogData({ visible: false })
   }
 
+  const showDialogError = e => {
+    console.log({ e })
+    setDialogData({ visible: true, title: 'Error', message: e.message })
+    setLoading(false)
+  }
+
   const { amount, reason, to } = screenState
 
-  const generateLink = async () => {
+  const generateLinkAndSend = async () => {
     setLoading(true)
+    let generateLinkResponse
     try {
       // Generate link deposit
-      const { sendLink, receipt } = await goodWallet.generateLink(amount)
-
-      // Save transaction
-      const transactionEvent: TransactionEvent = {
-        id: receipt.blockHash,
-        date: new Date().toString(),
-        type: 'send',
-        data: {
-          to,
-          reason,
-          amount,
-          sendLink,
-          receipt
-        }
-      }
-      await UserStorage.updateFeedEvent(transactionEvent)
-
-      // Cheking events are being stored
-      const events = await UserStorage.feed.get('2019-03-01').decrypt()
-      console.log({ events, transactionEvent })
-
-      // Send email if to is email
-      if (to && isEmail(to)) {
-        await API.sendLinkByEmail(to, sendLink)
-      }
-
-      // Show confirmation
-      screenProps.push('SendConfirmation', { sendLink })
+      generateLinkResponse = await goodWallet.generateLink(amount)
     } catch (e) {
-      setDialogData({ visible: true, title: 'Error', message: e.message })
-      setLoading(false)
+      showDialogError(e)
+    }
+
+    if (generateLinkResponse) {
+      try {
+        // Generate link deposit
+        const { sendLink, receipt } = generateLinkResponse
+
+        // Save transaction
+        const transactionEvent: TransactionEvent = {
+          id: receipt.blockHash,
+          date: new Date().toString(),
+          type: 'send',
+          data: {
+            to,
+            reason,
+            amount,
+            sendLink,
+            receipt
+          }
+        }
+        await UserStorage.updateFeedEvent(transactionEvent)
+
+        // Cheking events are being stored
+        // FIXME: Remove this since is only to check that is working
+        const events = await UserStorage.feed.get('2019-03-01').decrypt()
+        console.log({ events, transactionEvent })
+
+        // Send email if to is email
+        if (to && isEmail(to)) {
+          await API.sendLinkByEmail(to, sendLink)
+        }
+
+        // Show confirmation
+        screenProps.push('SendConfirmation', { sendLink })
+      } catch (e) {
+        // TODO: cash out generated link if either transaction log save or email could not be send
+        showDialogError(e)
+      }
     }
   }
   return (
@@ -86,7 +103,7 @@ const SendLinkSummary = (props: AmountProps) => {
             <BackButton mode="text" screenProps={screenProps} style={{ flex: 1 }}>
               Cancel
             </BackButton>
-            <CustomButton mode="contained" onPress={generateLink} style={{ flex: 2 }} loading={loading}>
+            <CustomButton mode="contained" onPress={generateLinkAndSend} style={{ flex: 2 }} loading={loading}>
               Next
             </CustomButton>
           </View>

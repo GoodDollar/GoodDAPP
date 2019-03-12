10bbac49b92dcc2cf722a6fc8bf08ba0856f2aff
Your Name <dminones@gmail.com>
2019-03-12 14:16:19 -0300

    Removing extra Container component on tests

--- src/components/dashboard/__tests__/SendConfirmation.js
+++ src/components/dashboard/__tests__/SendConfirmation.js
@@ -1,30 +1,18 @@
 import React from 'react'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
-
-import GDStore from '../../../lib/undux/GDStore'
 import { getWebRouterComponentWithMocks } from './__util__'
-
-const { Container } = GDStore
 
 describe('SendConfirmation', () => {
   it('renders without errors', () => {
     const SendConfirmation = getWebRouterComponentWithMocks('../SendConfirmation')
-    const tree = renderer.create(
-      <Container>
-        <SendConfirmation />
-      </Container>
-    )
+    const tree = renderer.create(<SendConfirmation />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
     const SendConfirmation = getWebRouterComponentWithMocks('../SendConfirmation')
-    const component = renderer.create(
-      <Container>
-        <SendConfirmation />
-      </Container>
-    )
+    const component = renderer.create(<SendConfirmation />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })

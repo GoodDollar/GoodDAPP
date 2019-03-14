af9091d82594093f7ab6f2a724cc4a7df1a2183a
Your Name <dminones@gmail.com>
2019-03-14 10:08:20 -0300

    Adding flow types

--- src/components/common/InputGoodDollar.js
+++ src/components/common/InputGoodDollar.js
@@ -1,13 +1,18 @@
+// @flow
+
 import React from 'react'
 import { TextInput } from 'react-native-paper'
 import { weiToMask, maskToWei } from '../../lib/wallet/utils'
 
-const InputGoodDollar = props => {
+type Props = {
+  onChangeWei: number => void,
+  wei: number
+}
+
+const InputGoodDollar = (props: Props) => {
   return (
     <TextInput
       {...props}
-      step="0.01"
-      placeholder="0"
       value={weiToMask(props.wei)}
       onChangeText={text => {
         props.onChangeWei(maskToWei(text))
--- src/lib/wallet/utils.js
+++ src/lib/wallet/utils.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { MaskService } from 'react-native-masked-text'
 const DECIMALS = 2
 
@@ -9,19 +11,20 @@ const maskSettings = {
   suffixUnit: ''
 }
 
-export const weiToGd = wei => wei * Math.pow(0.1, DECIMALS)
-export const gdToWei = gd => gd * Math.pow(10, DECIMALS)
+export const weiToGd = (wei: number): number => wei * Math.pow(0.1, DECIMALS)
+export const gdToWei = (gd: number): number => gd * Math.pow(10, DECIMALS)
 
-const getComposedSettings = (settings = {}) => {
+const getComposedSettings = (settings?: {} = {}): {} => {
   const { showUnits, ...restSettings } = settings
   const customSettings = { suffixUnit: showUnits ? ' GD' : undefined }
   return { ...maskSettings, ...restSettings, ...customSettings }
 }
 
-export const toMask = (gd, settings) => {
+export const toMask = (gd: number, settings?: {}): string => {
   return MaskService.toMask('money', gd, getComposedSettings(settings))
 }
-export const toRawValue = (masked, settings) => MaskService.toRawValue('money', masked, getComposedSettings(settings))
+export const toRawValue = (masked: string, settings?: {}): number =>
+  MaskService.toRawValue('money', masked, getComposedSettings(settings))
 
-export const weiToMask = (wei, settings) => toMask(weiToGd(wei), settings)
-export const maskToWei = (mask, settings) => gdToWei(toRawValue(mask, settings))
+export const weiToMask = (wei: number, settings?: {}): string => toMask(weiToGd(wei), settings)
+export const maskToWei = (mask: string, settings?: {}): number => gdToWei(toRawValue(mask, settings))

ad1ddf4fc98d356b684446e35b0da1363e48c458
fernandomg <fernando.greco@gmail.com>
2019-03-15 09:14:49 -0300

    Add `loading` prop for `CustomDialog`

--- src/components/common/CustomDialog.js
+++ src/components/common/CustomDialog.js
@@ -8,9 +8,11 @@ type DialogProps = {
   title?: string,
   message?: string,
   dismissText?: string,
-  onDismiss?: () => void
+  onDismiss?: () => void,
+  loading?: boolean
 }
-const CustomDialog = ({ visible, title, message, dismissText, onDismiss }: DialogProps) => (
+
+const CustomDialog = ({ visible, title, message, dismissText, onDismiss, loading = false }: DialogProps) => (
   <Portal>
     <Dialog visible={visible} onDismiss={onDismiss} dismissable={true}>
       <Dialog.Title>{title}</Dialog.Title>
@@ -18,7 +20,9 @@ const CustomDialog = ({ visible, title, message, dismissText, onDismiss }: Dialo
         <Paragraph>{message}</Paragraph>
       </Dialog.Content>
       <Dialog.Actions>
-        <CustomButton onPress={onDismiss}>{dismissText || 'Done'}</CustomButton>
+        <CustomButton onPress={onDismiss} disabled={loading} loading={loading}>
+          {dismissText || 'Done'}
+        </CustomButton>
       </Dialog.Actions>
     </Dialog>
   </Portal>

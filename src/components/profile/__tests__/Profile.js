dd13753ee2adfbeff9b501a0aa228f2f50b55411
Your Name <dminones@gmail.com>
2019-03-19 15:14:28 -0300

    fixed test

--- src/components/profile/__tests__/Profile.js
+++ src/components/profile/__tests__/Profile.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import Profile from '../Profile'
-import { createSwitchNavigator } from '@react-navigation/core'
-import { createBrowserApp } from '@react-navigation/web'
+import { getWebRouterComponentWithMocks } from '../../dashboard/__tests__/__util__'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
 describe('Profile', () => {
   it('renders without errors', () => {
-    const WebRouter = createBrowserApp(createSwitchNavigator({ Profile }))
+    const Profile = getWebRouterComponentWithMocks('../../profile/Profile')
 
-    const tree = renderer.create(<WebRouter />)
+    const tree = renderer.create(<Profile />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const WebRouter = createBrowserApp(createSwitchNavigator({ Profile }))
-
-    const component = renderer.create(<WebRouter />)
+    const Profile = getWebRouterComponentWithMocks('../../profile/Profile')
+    const component = renderer.create(<Profile />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })

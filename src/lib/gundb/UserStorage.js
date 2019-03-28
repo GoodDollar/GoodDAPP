1823261185004273e376fa656bed1a2032e80e26
fernandomg <fernando.greco@gmail.com>
2019-03-28 16:08:48 -0300

    Fix profile picture editing

--- src/lib/gundb/UserStorage.js
+++ src/lib/gundb/UserStorage.js
@@ -171,7 +171,8 @@ class UserStorage {
     const profileSettings = {
       fullName: { defaultPrivacy: 'public' },
       email: { defaultPrivacy: 'masked' },
-      mobile: { defaultPrivacy: 'masked' }
+      mobile: { defaultPrivacy: 'masked' },
+      avatar: { defaultPrivacy: 'public' }
     }
 
     const getPrivacy = async field => {

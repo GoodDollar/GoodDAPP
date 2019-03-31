957dde74bd3d1859a3125d912556812278c9b305
hadarbmdev <hadarbmdev@gmail.com>
2019-03-31 14:06:36 +0300

    more tests

--- src/lib/wallet/__tests__/GoodWallet.js
+++ src/lib/wallet/__tests__/GoodWallet.js
@@ -2,6 +2,7 @@ import goodWallet from '../GoodWallet'
 
 describe('Wallet Initialization', () => {
   let wallet
+  const numOfAcoounts = 10
 
   beforeEach(() => {
     wallet = goodWallet
@@ -10,5 +11,24 @@ describe('Wallet Initialization', () => {
   it(`should initialize wallet property`, () => {
     expect(wallet).toBeDefined()
     expect(wallet).not.toBeNull()
+    expect(wallet.wallet).toBeDefined()
+    expect(wallet.wallet).not.toBeNull()
+    expect(wallet.account).toBeDefined()
+    expect(wallet.account).not.toBeNull()
+    expect(wallet.accounts).toBeDefined()
+    expect(wallet.accounts).not.toBeNull()
+    expect(wallet.accounts.length).toEqual(numOfAcoounts)
+    expect(wallet.networkId).toBeDefined()
+    expect(wallet.networkId).not.toBeNull()
+    expect(wallet.identityContract).toBeDefined()
+    expect(wallet.identityContract).not.toBeNull()
+    expect(wallet.claimContract).toBeDefined()
+    expect(wallet.claimContract).not.toBeNull()
+    expect(wallet.tokenContract).toBeDefined()
+    expect(wallet.tokenContract).not.toBeNull()
+    expect(wallet.reserveContract).toBeDefined()
+    expect(wallet.reserveContract).not.toBeNull()
+    expect(wallet.oneTimePaymentLinksContract).toBeDefined()
+    expect(wallet.oneTimePaymentLinksContract).not.toBeNull()
   })
 })

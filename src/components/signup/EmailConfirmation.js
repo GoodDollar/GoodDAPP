30244c90fb69fc1d7d86e1906364202d71919c15
fernandomg <fernando.greco@gmail.com>
2019-04-08 17:52:13 -0300

    Refactor `EmailConfirmation`
    - Use hooks
    - process `validation` param value if available

--- src/components/signup/EmailConfirmation.js
+++ src/components/signup/EmailConfirmation.js
@@ -1,38 +1,68 @@
 // @flow
-import React from 'react'
-import { Wrapper, Title, Description, LinkButton, NextButton } from './components'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { normalize } from 'react-native-elements'
 
+import { useWrappedApi } from '../../lib/API/useWrappedApi'
+import { useWrappedUserStorage } from '../../lib/gundb/useWrappedStorage'
+import logger from '../../lib/logger/pino-logger'
+import { Description, LinkButton, NextButton, Title, Wrapper } from './components'
+
 type Props = {
-  screenProps: any
+  screenProps: any,
+  navigation: any
 }
-type State = {}
-export default class EmailConfirmation extends React.Component<Props, State> {
-  handleSubmit = () => {
-    this.props.screenProps.doneCallback({ isEmailConfirmed: true })
+
+const log = logger.child({ from: 'EmailConfirmation' })
+
+const EmailConfirmation = ({ navigation, screenProps }: Props) => {
+  const [processing, setProcessing] = useState(false)
+  const API = useWrappedApi()
+  const userStorage = useWrappedUserStorage()
+
+  useEffect(() => {
+    const { params } = navigation.state
+
+    const validateEmail = async () => {
+      setProcessing(true)
+      const globalProfile = await userStorage.getProfile()
+      await API.verifyEmail({ code: params.validation })
+
+      screenProps.doneCallback({ ...globalProfile, isEmailConfirmed: true })
+    }
+
+    if (params && params.validation) {
+      validateEmail().then(() => setProcessing(false))
+    }
+  }, [])
+
+  const handleResend = async () => {
+    setProcessing(true)
+    await API.sendVerificationEmail(screenProps.data)
+    setProcessing(false)
   }
-  render() {
-    return (
-      <Wrapper
-        handleSubmit={this.handleSubmit}
-        footerComponent={props => {
-          return (
-            <React.Fragment>
-              <Description style={styles.description}>{'Please click the link to approve the email'}</Description>
-              <NextButton valid={true} handleSubmit={props.handleSubmit}>
-                Open your email app
-              </NextButton>
-              <LinkButton onPress={() => console.log('Link button')}>Send mail again</LinkButton>
-            </React.Fragment>
-          )
-        }}
-      >
-        <Description>{"We've sent an email to:"}</Description>
-        <Title>{this.props.screenProps.data.email}</Title>
-      </Wrapper>
-    )
+
+  const handleSubmit = () => {
+    log.info('opening email client...')
   }
+
+  return (
+    <Wrapper
+      handleSubmit={handleSubmit}
+      footerComponent={props => (
+        <React.Fragment>
+          <Description style={styles.description}>{'Please click the link to approve the email'}</Description>
+          <NextButton valid={true} handleSubmit={props.handleSubmit} loading={processing}>
+            Open your email app
+          </NextButton>
+          <LinkButton onPress={() => !processing && handleResend()}>Send mail again</LinkButton>
+        </React.Fragment>
+      )}
+    >
+      <Description>{"We've sent an email to:"}</Description>
+      <Title>{screenProps.data.email}</Title>
+    </Wrapper>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -40,3 +70,5 @@ const styles = StyleSheet.create({
     marginBottom: normalize(20)
   }
 })
+
+export default EmailConfirmation

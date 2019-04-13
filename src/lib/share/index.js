cf0fc5d85a91eaca23f634f043d1f6f688985435
fernandomg <fernando.greco@gmail.com>
2019-04-13 16:10:04 -0300

    Create method to validate GD receive link
    - created readReceiveLink
    - added tests
    - extended Config with env variable

--- src/lib/share/index.js
+++ src/lib/share/index.js
@@ -1,5 +1,8 @@
 // @flow
 import { decode, encode, isMNID } from 'mnid'
+import isURL from 'validator/lib/isURL'
+
+import Config from '../../config/config'
 
 export function generateCode(address: string, networkId: number, amount: number) {
   const mnid = encode({ address, network: `0x${networkId.toString(16)}` })
@@ -9,6 +12,8 @@ export function generateCode(address: string, networkId: number, amount: number)
 
 export function readCode(code: string) {
   const [mnid, value] = code.split('|')
+
+  console.log({ code })
 
   if (!isMNID(mnid)) {
     return null
@@ -22,4 +27,23 @@ export function readCode(code: string) {
     address,
     amount: amount ? amount : undefined
   }
+}
+
+/**
+ * Parses the read ReceiveGDLink from QR Code.
+ * If not valid, returns null.
+ * If valid, returns the ReceiveGDLink.
+ * @param {string} link - receive GD Link
+ * @returns {string|null} - {link|null}
+ */
+export function readReceiveLink(link: string) {
+  // checks that the link has the expected strings in it
+  const isValidReceiveLink = [Config.publicUrl, 'receiveLink', 'reason'].every(v => link.indexOf(v) !== -1)
+  const isUrlOptions = Config.env === 'development' ? { require_tld: false } : {}
+
+  if (!isURL(link, isUrlOptions) || !isValidReceiveLink) {
+    return null
+  }
+
+  return link
 }

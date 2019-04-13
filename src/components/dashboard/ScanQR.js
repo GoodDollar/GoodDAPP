b6c9f873f0183608e2fc51187ba77375f9304c16
fernandomg <fernando.greco@gmail.com>
2019-04-13 19:07:37 -0300

    Modify ScanQR component to support 'receive' scans also

--- src/components/dashboard/ScanQR.js
+++ src/components/dashboard/ScanQR.js
@@ -4,13 +4,15 @@ import QRCodeScanner from 'react-native-qrcode-scanner'
 
 import { NETWORK_ID } from '../../lib/constants/network'
 import logger from '../../lib/logger/pino-logger'
-import { readCode } from '../../lib/share'
+import { extractQueryParams, readCode, readReceiveLink } from '../../lib/share'
 import { CustomDialog, Section, TopBar, Wrapper } from '../common'
+import Withdraw from './Withdraw'
 
 const log = logger.child({ from: 'ScanQR.web' })
 
 const ScanQR = ({ screenProps }) => {
   const [dialogData, setDialogData] = useState({ visible: false })
+  const [withdrawParams, setWithdrawParams] = useState({ receiveLink: '', reason: '' })
 
   const dismissDialog = () => {
     setDialogData({ visible: false })
@@ -24,19 +26,35 @@ const ScanQR = ({ screenProps }) => {
         log.info({ code })
 
         if (code === null) {
-          throw new Error('Invalid QR Code.')
-        }
+          const url = readReceiveLink(data)
 
-        const { networkId, address, amount } = code
+          log.info({ url })
 
-        if (networkId !== NETWORK_ID.FUSE) {
-          throw new Error('Invalid network. Switch to Fuse.')
-        }
-
-        if (!amount) {
-          screenProps.push('Amount', { to: address, nextRoutes: ['Reason', 'SendQRSummary'] })
+          if (url === null) {
+            throw new Error('Invalid QR Code.')
+          } else {
+            const { receiveLink, reason } = extractQueryParams(url)
+            setWithdrawParams({ receiveLink, reason })
+          }
         } else {
-          screenProps.push('SendQRSummary', { to: address, amount, reason: 'From QR with Amount' })
+          const { networkId, address, amount } = code
+
+          if (networkId !== NETWORK_ID.FUSE) {
+            throw new Error('Invalid network. Switch to Fuse.')
+          }
+
+          if (!amount) {
+            screenProps.push('Amount', { to: address, nextRoutes: ['Reason', 'SendQRSummary'] })
+          } else {
+            // FIXME: nextRoute -> foo
+            // It was added as a workaround to the restrictions in the SendQRSummary.shouldNavigateToComponent
+            screenProps.push('SendQRSummary', {
+              to: address,
+              amount,
+              reason: 'From QR with Amount',
+              nextRoutes: ['foo']
+            })
+          }
         }
       } catch (e) {
         log.error({ e })
@@ -54,6 +72,7 @@ const ScanQR = ({ screenProps }) => {
         </Section.Row>
       </Section>
       <CustomDialog onDismiss={dismissDialog} {...dialogData} />
+      {withdrawParams.receiveLink ? <Withdraw params={withdrawParams} onSuccess={screenProps.pop} /> : null}
     </Wrapper>
   )
 }
--- src/components/dashboard/ScanQR.web.js
+++ src/components/dashboard/ScanQR.web.js
@@ -4,10 +4,11 @@ import QrReader from 'react-qr-reader'
 
 import { NETWORK_ID } from '../../lib/constants/network'
 import logger from '../../lib/logger/pino-logger'
-import { readCode } from '../../lib/share'
-import { CustomDialog, Section, TopBar, Wrapper } from '../common'
-import { wrapFunction } from '../../lib/undux/utils/wrapper'
+import { extractQueryParams, readCode, readReceiveLink } from '../../lib/share'
 import GDStore from '../../lib/undux/GDStore'
+import { wrapFunction } from '../../lib/undux/utils/wrapper'
+import { Section, TopBar, Wrapper } from '../common'
+import Withdraw from './Withdraw'
 
 const QR_DEFAULT_DELAY = 300
 
@@ -15,7 +16,9 @@ const log = logger.child({ from: 'ScanQR.web' })
 
 const ScanQR = ({ screenProps }) => {
   const [qrDelay, setQRDelay] = useState(QR_DEFAULT_DELAY)
+  const [withdrawParams, setWithdrawParams] = useState({ receiveLink: '', reason: '' })
   const store = GDStore.useStore()
+
   const onDismissDialog = () => setQRDelay(QR_DEFAULT_DELAY)
 
   const handleScan = async data => {
@@ -26,21 +29,35 @@ const ScanQR = ({ screenProps }) => {
         log.info({ code })
 
         if (code === null) {
-          throw new Error('Invalid QR Code.')
-        }
+          const url = readReceiveLink(data)
 
-        const { networkId, address, amount } = code
+          log.info({ url })
 
-        if (networkId !== NETWORK_ID.FUSE) {
-          throw new Error('Invalid network. Switch to Fuse.')
-        }
-
-        if (!amount) {
-          screenProps.push('Amount', { to: address, nextRoutes: ['Reason', 'SendQRSummary'] })
+          if (url === null) {
+            throw new Error('Invalid QR Code.')
+          } else {
+            const { receiveLink, reason } = extractQueryParams(url)
+            setWithdrawParams({ receiveLink, reason })
+          }
         } else {
-          // FIXME: nextRoute -> foo
-          // It was added as a workaround to the restrictions in the SendQRSummary.shouldNavigateToComponent
-          screenProps.push('SendQRSummary', { to: address, amount, reason: 'From QR with Amount', nextRoutes: ['foo'] })
+          const { networkId, address, amount } = code
+
+          if (networkId !== NETWORK_ID.FUSE) {
+            throw new Error('Invalid network. Switch to Fuse.')
+          }
+
+          if (!amount) {
+            screenProps.push('Amount', { to: address, nextRoutes: ['Reason', 'SendQRSummary'] })
+          } else {
+            // FIXME: nextRoute -> foo
+            // It was added as a workaround to the restrictions in the SendQRSummary.shouldNavigateToComponent
+            screenProps.push('SendQRSummary', {
+              to: address,
+              amount,
+              reason: 'From QR with Amount',
+              nextRoutes: ['foo']
+            })
+          }
         }
       } catch (e) {
         log.error({ e })
@@ -55,19 +72,22 @@ const ScanQR = ({ screenProps }) => {
   }
 
   return (
-    <Wrapper>
-      <TopBar hideBalance={true} push={screenProps.push} />
-      <Section style={styles.bottomSection}>
-        <Section.Row>
-          <QrReader
-            delay={qrDelay}
-            onError={handleError}
-            onScan={wrapFunction(handleScan, store, { onDismiss: onDismissDialog })}
-            style={{ width: '100%' }}
-          />
-        </Section.Row>
-      </Section>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <TopBar hideBalance={true} push={screenProps.push} />
+        <Section style={styles.bottomSection}>
+          <Section.Row>
+            <QrReader
+              delay={qrDelay}
+              onError={handleError}
+              onScan={wrapFunction(handleScan, store, { onDismiss: onDismissDialog })}
+              style={{ width: '100%' }}
+            />
+          </Section.Row>
+        </Section>
+      </Wrapper>
+      {withdrawParams.receiveLink ? <Withdraw params={withdrawParams} onSuccess={screenProps.pop} /> : null}
+    </>
   )
 }
 

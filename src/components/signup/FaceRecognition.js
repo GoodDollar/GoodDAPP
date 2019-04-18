a986fc2017f22518fab595b6242b3100972cc4cb
hadarbmdev <hadarbmdev@gmail.com>
2019-04-18 16:54:21 +0300

    Basic structure for Liveness Test

--- src/components/signup/FaceRecognition.js
+++ src/components/signup/FaceRecognition.js
@@ -3,14 +3,21 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import { Wrapper, Title, Description } from './components'
 import { normalize } from 'react-native-elements'
-
+import { wrapFunction } from '../../lib/undux/utils/wrapper' // use for error handling
 type Props = {
   screenProps: any
 }
 type State = {}
 export default class FaceRecognition extends React.Component<Props, State> {
   handleSubmit = () => {
-    this.props.screenProps.doneCallback({})
+    // Show LivelinessCapture.tx UI.
+    /*wrapFunction( // see in ReceieveByQR.web line 61 the usage.
+      // ... 
+    )*/
+    // Show face reco client.
+    this.props.screenProps.doneCallback({}) // call it when the face recognition is over successfully
+
+    // if something fails
   }
 
   render() {

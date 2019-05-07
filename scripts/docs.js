68d2122211d9b337cc63725e40c5bca44c877602
Your Name <dminones@gmail.com>
2019-05-07 18:56:03 -0300

    removing unnecesary code

--- scripts/docs.js
+++ scripts/docs.js
@@ -2,13 +2,6 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'test';
-process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
-
-//"docs": "documentation build src -f md -o docs/code.md --shallow"
-
 console.log("Generating docs...")
 const excludedFolders = ['__tests__', '__util__']
 

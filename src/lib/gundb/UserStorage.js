71101291a3d484ef112ae232ab1c5120b45fd098
sirpy <hadarr@gmail.com>
2019-05-07 11:48:02 +0300

    Update src/lib/gundb/UserStorage.js

--- src/lib/gundb/UserStorage.js
+++ src/lib/gundb/UserStorage.js
@@ -595,7 +595,6 @@ export class UserStorage {
       const searchField = 'by' + (isMobilePhone(address) ? 'mobile' : isEmail(address) ? 'email' : 'walletAddress')
       const profileToShow = gun
         .rootAO(`users/${searchField}`)
-        .get(searchField)
         .get(address)
         .get('profile')
 

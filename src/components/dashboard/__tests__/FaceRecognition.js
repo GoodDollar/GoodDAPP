699c2a9650b672f7787d832e822fbe474af2d96c
Kevin Bardi <bardi.kevin@gmail.com>
2019-05-07 10:25:02 -0300

    Fixes unit test with case sensitive folder

--- src/components/dashboard/__tests__/FaceRecognition.js
+++ src/components/dashboard/__tests__/FaceRecognition.js
@@ -5,13 +5,13 @@ import renderer from 'react-test-renderer'
 
 describe('FaceRecognition', () => {
   it('renders without errors', () => {
-    const FaceRecognition = getWebRouterComponentWithMocks('../faceRecognition/FaceRecognition')
+    const FaceRecognition = getWebRouterComponentWithMocks('../FaceRecognition/FaceRecognition')
     const tree = renderer.create(<FaceRecognition />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const FaceRecognition = getWebRouterComponentWithMocks('../faceRecognition/FaceRecognition')
+    const FaceRecognition = getWebRouterComponentWithMocks('../FaceRecognition/FaceRecognition')
     const component = renderer.create(<FaceRecognition />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()

2bfd4c77fa236d59c21f88a0834cf188e716e8aa
fernandomg <fernando.greco@gmail.com>
2019-05-08 10:00:07 -0300

    Store creation block after user verification

--- src/components/signup/SignupState.js
+++ src/components/signup/SignupState.js
@@ -103,6 +103,11 @@ const Signup = ({ navigation, screenProps }: { navigation: any, screenProps: any
           // saved to the `state`
           await API.addUser(state)
           await API.verifyUser({})
+
+          // Stores creationBlock number into 'lastBlock' feed's node
+          const creationBlock = (await goodWallet.getBlockNumber()).toString()
+          await userStorage.saveLastBlockNumber(creationBlock)
+
           const destinationPath = store.get('destinationPath')
           // top wallet of new user
           // wait for the topping to complete to be able to withdraw

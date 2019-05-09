957661b3d7f2db3edc44ecbb53d237381be1388c
Your Name <dminones@gmail.com>
2019-05-09 15:09:07 -0300

    Adding webpack config in prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -256,6 +256,15 @@ module.exports = {
         include: paths.appSrc
       },
       {
+        test: /postMock.html$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      },
+      {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.

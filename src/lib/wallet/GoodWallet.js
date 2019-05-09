2ecadcf6127d6287439e53b97df4b072c0a0fd9e
fernandomg <fernando.greco@gmail.com>
2019-05-09 17:12:40 -0300

    Use `generateShareLink` function in GoodWallet

--- src/lib/wallet/GoodWallet.js
+++ src/lib/wallet/GoodWallet.js
@@ -10,6 +10,7 @@ import { BN, toBN } from 'web3-utils'
 
 import Config from '../../config/config'
 import logger from '../../lib/logger/pino-logger'
+import { generateShareLink } from '../share'
 import WalletFactory from './WalletFactory'
 import abiDecoder from 'abi-decoder'
 
@@ -395,7 +396,14 @@ export class GoodWallet {
 
     log.info({ amount })
 
-    const sendLink = `${Config.publicUrl}/AppNavigation/Dashboard/Home?receiveLink=${generatedString}&reason=${reason}`
+    const sendLink = generateShareLink('send', {
+      receiveLink: generatedString,
+      reason
+    })
+
+    if (!sendLink) {
+      throw new Error(`Link couldn't be generated`)
+    }
 
     const onTransactionHash = events.onTransactionHash({ sendLink, generatedString })
     const receipt = await this.sendTransaction(transferAndCall, { onTransactionHash }, { gas })

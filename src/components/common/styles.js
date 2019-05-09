4e2c2de831720d25b728e1d42b2bd4b9d302980b
Your Name <dminones@gmail.com>
2019-05-09 12:29:51 -0300

    Adding Webview component and using it in Auth

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,7 +1,6 @@
 import { normalize } from 'react-native-elements'
 
 export const fontStyle = {
-  fontFamily: 'Helvetica, "sans-serif"',
   color: '#555',
   fontSize: normalize(18),
   textAlign: 'center'
---
+++ src/components/webView/WebViewScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import WebView from 'WebView'
+
+const WebViewScreen = props => {
+  return <WebView source={{ uri: props.source }} style={{ flex: 1 }} />
+}
+
+export default WebViewScreen
+
+export const createWebViewScreen = (source, title) => {
+  const CurrentWebView = () => <WebViewScreen source={source} />
+  CurrentWebView.navigationOptions = {
+    title
+  }
+  return CurrentWebView
+}

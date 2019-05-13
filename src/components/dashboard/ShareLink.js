5e5c6f9a12b8e74b590a92178bfa35454f5cde6e
fernandomg <fernando.greco@gmail.com>
2019-05-13 15:53:52 -0300

    Implement native share link in receive screens
    If not on mobile, link is copied to clipboard
    (probably requires a different layout to provide the user with the option to copy)

--- src/components/dashboard/ShareLink.js
+++ src/components/dashboard/ShareLink.js
@@ -1,13 +1,32 @@
+// @flow
+import { isMobile } from 'mobile-device-detect'
 import React, { useCallback } from 'react'
+import { Clipboard } from 'react-native'
 
 import logger from '../../lib/logger/pino-logger'
 import { CustomButton as Button } from '../common'
 
 const log = logger.child({ from: 'ShareLink' })
 
-const ShareLink = ({ children, ...props }) => {
-  const link = props.link
-  const share = useCallback(() => log.warn(link))
+type Props = {
+  children: any,
+  link: string,
+  props: any
+}
+
+const ShareLink = ({ children, link, ...props }: Props) => {
+  const share = useCallback(() => {
+    if (isMobile) {
+      navigator.send({
+        title: 'Sending GD via Good Dollar App',
+        text: 'To send me GD open:',
+        url: link
+      })
+    } else {
+      Clipboard.setString(link)
+      log.info('Receive link copied', { link })
+    }
+  })
 
   return (
     <Button mode="contained" dark={true} onPress={share} {...props}>

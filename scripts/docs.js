fd81589bf8bbfd1ef4c35ba758d7fcb0ff3338ae
Your Name <dminones@gmail.com>
2019-05-15 10:28:27 -0300

    Removing empty docs

--- scripts/docs.js
+++ scripts/docs.js
@@ -5,6 +5,42 @@ const DELETE_EMPTY_DOCS = false
 console.log("Generating docs...")
 const excludedFolders = ['__tests__', '__util__']
 
+const getEmptyFileString = (function () {
+    let emptyFile
+    return async function () {
+        return new Promise((resolve, reject) => {
+            if(emptyFile) resolve(emptyFile)
+
+            exec(`documentation build ./fakepath -f md --shallow`, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error)
+                }
+                if(stdout) {
+                    resolve(stdout)
+                }
+            });            
+        })
+    }
+  })();
+
+/**
+ * Removes all empty docs and returns a list with actual documentation
+ * @param {[string]} docs
+ */
+async function removeEmptyDocs(docs) {
+    const emptyDoc = await getEmptyFileString()
+    const actualDocs = docs.filter(docPath => {
+        const data = fs.readFileSync(docPath);
+        const doc = data.toString()
+
+        return doc != emptyDoc 
+    })
+
+    const toRemove = docs.filter(doc => !actualDocs.includes(doc))
+    toRemove.forEach(toRemove => fs.unlinkSync(toRemove))
+    return actualDocs
+}
+
 function promiseFromChildProcess(child) {
     return new Promise(function (resolve, reject) {
         child.addListener("error", reject);
@@ -24,6 +60,7 @@ async function generateDocs(baseFolder, deep){
     async function doGenerateDocs(folder, deep) {
         const files = fs.readdirSync(folder);
         const filesToProcess = files.filter(fileName => !excludedFolders.includes(fileName))
+
         const results = await Promise.all(filesToProcess.map(async file => {
             const filePath = `${folder}/${file}`;
             //generate recursively for all subfolders when reaching last level
@@ -31,8 +68,9 @@ async function generateDocs(baseFolder, deep){
                 const outputFolder = `${folder.replace(baseFolder,'docs')}/`
                 await execPromise(`mkdir -p ${outputFolder}`)
 
-                const toExec = `documentation build ${filePath} -f md -o ${outputFolder+file}.md --shallow`
-                return execPromise(toExec).then(r => filePath)
+                const mdFile = `${outputFolder+file}.md`
+                const toExec = `documentation build ${filePath} -f md -o ${mdFile} --shallow`
+                return execPromise(toExec).then(r => mdFile)
             }
             //generate docs only for direct source files for non last level directory
             else if(fs.lstatSync(filePath).isDirectory())
@@ -40,13 +78,14 @@ async function generateDocs(baseFolder, deep){
                 const outputFolder = `${folder.replace(baseFolder,'docs')}/`
                 await execPromise(`mkdir -p ${outputFolder}`)
                 //adding '*.js' causes documentation to not be generated for sub folders
-                const toExec = `documentation build ${filePath}/*.js -f md -o ${outputFolder+file}.md --shallow`                
-                const childrenDocs = await doGenerateDocs(filePath, deep-1)
-                console.log({filePath, childrenDocs})
+                const mdFile = `${outputFolder+file}.md`
+                const toExec = `documentation build ${filePath}/*.js -f md -o ${mdFile} --shallow`                
+                const childrenDocs = await doGenerateDocs(filePath, deep-1).then(removeEmptyDocs)
+                console.log({filePath, mdFile, childrenDocs})
                 await execPromise(toExec)
                 //TODO: add children folder docs to TOC
                 addTOC(`${outputFolder+file}.md`,childrenDocs)
-                return filePath
+                return { mdFile, childrenDocs }
             }
 
             return Promise.resolve(false)
@@ -55,7 +94,8 @@ async function generateDocs(baseFolder, deep){
     }
 
     await execPromise('rm -rf docs/*')
-    await doGenerateDocs(baseFolder, deep)
+    const childrenDocs = await doGenerateDocs(baseFolder, deep)
+    console.log({childrenDocs})
     if(DELETE_EMPTY_DOCS)
       await execPromise("find ./docs -name '*.md'  -size 118c -delete")
 }

01f082383fd36cc79f6d8475b125970e1eec77fa
Hadar <hadarr@gmail.com>
2019-05-18 23:53:36 +0300

    add: throttle feed rereads to store

--- src/lib/undux/utils/feed.js
+++ src/lib/undux/utils/feed.js
@@ -1,13 +1,13 @@
 // @flow
 import type { Store } from 'undux'
-
+import throttle from 'lodash/throttle'
 import userStorage from '../../gundb/UserStorage'
 import pino from '../../logger/pino-logger'
 const logger = pino.child({ from: 'feeds' })
 
 export const PAGE_SIZE = 10
 
-export const getInitialFeed = async (store: Store) => {
+const getInitial = async (store: Store) => {
   const currentScreen = store.get('currentScreen')
   store.set('currentScreen')({ ...currentScreen, loading: true })
   const feeds = await userStorage
@@ -17,10 +17,13 @@ export const getInitialFeed = async (store: Store) => {
   store.set('feeds')(feeds)
 }
 
-export const getNextFeed = async (store: Store) => {
+const getNext = async (store: Store) => {
   const currentFeeds = store.get('feeds')
   const newFeeds = await userStorage.getFormattedEvents(PAGE_SIZE, false)
   if (newFeeds.length > 0) {
     store.set('feeds')([...currentFeeds, ...newFeeds])
   }
 }
+
+export const getInitialFeed = throttle(getInitial, 1000, { leading: true })
+export const getNextFeed = throttle(getNext, 1000, { leading: true })

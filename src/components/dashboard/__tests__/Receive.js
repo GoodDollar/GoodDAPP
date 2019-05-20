5319d4e4225d81eef3c1ba6e67eec561dbb2bc73
fernandomg <fernando.greco@gmail.com>
2019-05-20 11:09:29 -0300

    Add snapshot tests for `Receive` component

---
+++ src/components/dashboard/__tests__/Receive.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer'
+
+import { getWebRouterComponentWithMocks } from './__util__'
+
+describe('Receive', () => {
+  it('renders without errors', () => {
+    const Receive = getWebRouterComponentWithMocks('../Receive')
+    const tree = renderer.create(<Receive />)
+    expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it('matches snapshot', () => {
+    const Receive = getWebRouterComponentWithMocks('../Receive')
+    const component = renderer.create(<Receive />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})

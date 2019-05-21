5e6138d24d3592c73716da8cfb27dbe598900167
Hadar Rottenberg <hadar@gooddollar.org>
2019-05-21 10:11:11 +0300

    fix: test with mnemonic

--- src/components/dashboard/__tests__/Receive.js
+++ src/components/dashboard/__tests__/Receive.js
@@ -2,19 +2,25 @@ import React from 'react'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-import { getWebRouterComponentWithMocks } from './__util__'
-
+let getWebRouterComponentWithMocks
 describe('Receive', () => {
+  beforeAll(async () => {
+    localStorage.setItem(
+      'GD_USER_MNEMONIC',
+      'burger must derive wrong dry unaware reopen laptop acoustic report slender scene'
+    )
+    getWebRouterComponentWithMocks = require('./__util__').getWebRouterComponentWithMocks
+  })
   it('renders without errors', () => {
     const Receive = getWebRouterComponentWithMocks('../Receive')
     const tree = renderer.create(<Receive />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
-  // it('matches snapshot', () => {
-  //   const Receive = getWebRouterComponentWithMocks('../Receive')
-  //   const component = renderer.create(<Receive />)
-  //   const tree = component.toJSON()
-  //   expect(tree).toMatchSnapshot()
-  // })
+  it('matches snapshot', () => {
+    const Receive = getWebRouterComponentWithMocks('../Receive')
+    const component = renderer.create(<Receive />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })

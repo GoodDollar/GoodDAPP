9e3f1612e167a677028110d287ec5f8867371912
Hadar Rottenberg <hadar@gooddollar.org>
2019-06-10 17:28:26 +0300

    add: inspectlet identifier

--- src/init.js
+++ src/init.js
@@ -4,17 +4,20 @@ import goodWallet from './lib/wallet/GoodWallet'
 import userStorage from './lib/gundb/UserStorage'
 import Config from './config/config'
 declare var amplitude
+declare var __insp
 export const init = async () => {
   return await Promise.all([goodWallet.ready, userStorage.ready]).then(([wallet, storage]) => {
     global.wallet = goodWallet
+    const identifier = goodWallet.getAccountForType('login')
     if (global.Rollbar && Config.env !== 'test')
       global.Rollbar.configure({
         payload: {
           person: {
-            id: goodWallet.getAccountForType('login')
+            id: identifier
           }
         }
       })
-    amplitude.getInstance().setUserId(goodWallet.getAccountForType('login'))
+    amplitude.getInstance().setUserId(identifier)
+    __insp.push(['identify', identifier])
   })
 }

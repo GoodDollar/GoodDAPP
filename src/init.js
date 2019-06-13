2f1d25c7b62d1c6d6638b1ce03be9768f4ef8a82
hadarbmdev <hadarbmdev@gmail.com>
2019-06-13 12:03:49 +0300

    add: merge PR #193 to production

--- src/init.js
+++ src/init.js
@@ -1,15 +1,17 @@
 //@flow
+// eslint-disable-next-line no-unused-vars
 import initGunDB from './lib/gundb/gundb'
 import goodWallet from './lib/wallet/GoodWallet'
 import userStorage from './lib/gundb/UserStorage'
 import Config from './config/config'
 declare var amplitude
+
 declare var __insp
-export const init = async () => {
-  return await Promise.all([goodWallet.ready, userStorage.ready]).then(([wallet, storage]) => {
+export const init = () => {
+  return Promise.all([goodWallet.ready, userStorage.ready]).then(([wallet, storage]) => {
     global.wallet = goodWallet
     const identifier = goodWallet.getAccountForType('login')
-    if (global.Rollbar && Config.env !== 'test')
+    if (global.Rollbar && Config.env !== 'test') {
       global.Rollbar.configure({
         payload: {
           person: {
@@ -17,6 +19,7 @@ export const init = async () => {
           }
         }
       })
+    }
     amplitude.getInstance().setUserId(identifier)
     __insp.push(['identify', identifier])
   })

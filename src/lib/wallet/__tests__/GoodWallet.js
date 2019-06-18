43c25e5104e3bc49196ba575ad0319ab49d4ee29
fernandomg <fernando.greco@gmail.com>
2019-06-18 16:55:55 -0300

    Add tests for `pollForEvents`

--- src/lib/wallet/__tests__/GoodWallet.js
+++ src/lib/wallet/__tests__/GoodWallet.js
@@ -1,3 +1,4 @@
+import { BN } from 'web3-utils'
 import goodWallet from '../GoodWallet'
 
 const httpProviderMock = jest.fn().mockImplementation(() => {
@@ -13,15 +14,100 @@ beforeAll(() => {
   jest.resetAllMocks()
 })
 
-describe('Wallet Creation', () => {
-  it(`should create wallet property`, () => {
-    goodWallet.ready.then(() => {
-      const { wallet } = goodWallet
+describe('Polling for events', () => {
+  it(`should stop polling after cancel returns true`, () => {
+    // Given
+    const contract = {
+      getPastEvents() {
+        return Promise.resolve([])
+      }
+    }
 
-      expect(wallet).toBeDefined()
-      expect(wallet).not.toBeNull()
-      expect(wallet.wallet).toBeDefined()
-      expect(wallet.wallet).not.toBeNull()
+    return new Promise(async resolve => {
+      // When
+      await goodWallet.ready
+      goodWallet.setBlockNumber(new BN(1))
+      goodWallet.pollForEvents(
+        { event: 'Transfer', contract },
+        () => {},
+        () => {
+          goodWallet.setBlockNumber(goodWallet.blockNumber.add(new BN(1)))
+          const shouldCancel = goodWallet.blockNumber > 7
+
+          if (shouldCancel) {
+            resolve(undefined)
+          }
+
+          return shouldCancel
+        },
+        1
+      )
+    }).then(() => {
+      // Then
+      expect(goodWallet.blockNumber.toString()).toEqual('8')
+    })
+  })
+
+  it(`should fail if no callback is provided`, () => {
+    // Given
+    const contract = {
+      getPastEvents() {
+        return Promise.resolve([])
+      }
+    }
+
+    return new Promise(async (resolve, reject) => {
+      await goodWallet.ready
+
+      // When
+      try {
+        await goodWallet.pollForEvents({ event: 'Transfer', contract })
+      } catch (e) {
+        reject(e)
+      }
+    }).catch(error => {
+      // Then
+      expect(error.message).toBe('callback must be provided')
+    })
+  })
+
+  it(`should fail if no event is provided`, () => {
+    // Given
+    const contract = {
+      getPastEvents() {
+        return Promise.resolve([])
+      }
+    }
+
+    return new Promise(async (resolve, reject) => {
+      await goodWallet.ready
+
+      // When
+      try {
+        await goodWallet.pollForEvents({ contract }, () => {})
+      } catch (e) {
+        reject(e)
+      }
+    }).catch(error => {
+      // Then
+      expect(error.message).toBe('event must be provided')
+    })
+  })
+
+  it(`should fail if no contract is provided`, () => {
+    return new Promise(async (resolve, reject) => {
+      // Given
+      await goodWallet.ready
+
+      // When
+      try {
+        await goodWallet.pollForEvents({ event: 'Transfer' }, () => {})
+      } catch (e) {
+        reject(e)
+      }
+    }).catch(error => {
+      // Then
+      expect(error.message).toBe('contract object must be provided')
     })
   })
 })
@@ -30,26 +116,24 @@ describe('Wallet Initialization', () => {
   it(`should initialize wallet property`, () => {
     const numOfAcoounts = 10
 
-    goodWallet.ready.then(() => {
-      const { wallet } = goodWallet
-
-      expect(wallet.account).toBeDefined()
-      expect(wallet.account).not.toBeNull()
-      expect(wallet.accounts).toBeDefined()
-      expect(wallet.accounts).not.toBeNull()
-      expect(wallet.accounts.length).toEqual(numOfAcoounts)
-      expect(wallet.networkId).toBeDefined()
-      expect(wallet.networkId).not.toBeNull()
-      expect(wallet.identityContract).toBeDefined()
-      expect(wallet.identityContract).not.toBeNull()
-      expect(wallet.claimContract).toBeDefined()
-      expect(wallet.claimContract).not.toBeNull()
-      expect(wallet.tokenContract).toBeDefined()
-      expect(wallet.tokenContract).not.toBeNull()
-      expect(wallet.reserveContract).toBeDefined()
-      expect(wallet.reserveContract).not.toBeNull()
-      expect(wallet.oneTimePaymentLinksContract).toBeDefined()
-      expect(wallet.oneTimePaymentLinksContract).not.toBeNull()
+    return goodWallet.ready.then(() => {
+      expect(goodWallet.account).toBeDefined()
+      expect(goodWallet.account).not.toBeNull()
+      expect(goodWallet.accounts).toBeDefined()
+      expect(goodWallet.accounts).not.toBeNull()
+      expect(goodWallet.accounts.length).toEqual(numOfAcoounts)
+      expect(goodWallet.networkId).toBeDefined()
+      expect(goodWallet.networkId).not.toBeNull()
+      expect(goodWallet.identityContract).toBeDefined()
+      expect(goodWallet.identityContract).not.toBeNull()
+      expect(goodWallet.claimContract).toBeDefined()
+      expect(goodWallet.claimContract).not.toBeNull()
+      expect(goodWallet.tokenContract).toBeDefined()
+      expect(goodWallet.tokenContract).not.toBeNull()
+      expect(goodWallet.reserveContract).toBeDefined()
+      expect(goodWallet.reserveContract).not.toBeNull()
+      expect(goodWallet.oneTimePaymentLinksContract).toBeDefined()
+      expect(goodWallet.oneTimePaymentLinksContract).not.toBeNull()
     })
   })
 
@@ -57,5 +141,16 @@ describe('Wallet Initialization', () => {
     await goodWallet.ready
     expect(goodWallet.getAccountForType('gd')).toBe(goodWallet.accounts[0].address)
     expect(goodWallet.getAccountForType('zoomId')).toBe(goodWallet.accounts[5].address)
+  })
+})
+
+describe('Wallet Creation', () => {
+  it(`should create wallet property`, () => {
+    return goodWallet.ready.then(() => {
+      expect(goodWallet).toBeDefined()
+      expect(goodWallet).not.toBeNull()
+      expect(goodWallet.wallet).toBeDefined()
+      expect(goodWallet.wallet).not.toBeNull()
+    })
   })
 })

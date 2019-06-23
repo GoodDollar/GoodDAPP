687124c4bc28f9a29a7e0b0bdcf87a804d50c27f
hadarbmdev <hadarbmdev@gmail.com>
2019-06-23 13:36:34 +0300

    tests: try to fix "ZoomSDK" not defined error on tests only

--- src/components/dashboard/__tests__/FaceRecognition.js
+++ src/components/dashboard/__tests__/FaceRecognition.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import zoomSdkLoader from '../FaceRecognition/ZoomSdkLoader'
+import * as ZoomSDK from '../FaceRecognition/FaceRecognition'
+
 import { getWebRouterComponentWithMocks } from './__util__'
 
 // Note: test renderer must be required after react-native.
-
 describe('FaceRecognition Rendering', () => {
-  beforeAll(async () => {
+  beforeAll(async done => {
     await zoomSdkLoader.load()
+    window.ZoomSDK = ZoomSDK
+    done()
   })
   it('renders without errors', () => {
     const FaceRecognition = getWebRouterComponentWithMocks('../FaceRecognition/FaceRecognition')

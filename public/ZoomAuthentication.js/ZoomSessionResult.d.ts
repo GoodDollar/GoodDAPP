f8d99c4d313af72758e37c106e8ad9b292fbd012
hadarbmdev <hadarbmdev@gmail.com>
2019-06-23 10:41:50 +0300

    add: update zoom resources folder

---
+++ public/ZoomAuthentication.js/ZoomSessionResult.d.ts
@@ -0,0 +1,11 @@
+export interface ZoomSessionResult {
+    faceMetrics: {
+        auditTrail: string[];
+    };
+    sessionData: any;
+    facemap: any;
+    sessionId: string;
+    status: string;
+    [key: string]: string | boolean | any | number;
+}
+//# sourceMappingURL=ZoomSessionResult.d.ts.map

9c0545366281ec9458bd89d99acbcd5aa374bbdd
fernandomg <fernando.greco@gmail.com>
2019-06-26 20:48:00 -0300

    Update EditProfile to redirect to Edit/View Avatar
    - removed `.web` extension, as crop component is no longer used

--- src/components/profile/EditProfile.js
+++ src/components/profile/EditProfile.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { CustomButton, Section, UserAvatar, Wrapper } from '../common'
@@ -11,11 +12,13 @@ const TITLE = 'Edit Profile'
 const EditProfile = props => {
   const store = GDStore.useStore()
   const wrappedUserStorage = useWrappedUserStorage()
+  const { screenProps } = props
 
   const [profile, setProfile] = useState(store.get('profile'))
   const [saving, setSaving] = useState()
   const [errors, setErrors] = useState({})
   const [showErrorDialog] = useErrorDialog()
+
   useEffect(() => {
     wrappedUserStorage.getPrivateProfile(profile).then(setProfile)
   }, [profile.fullName])
@@ -34,13 +37,14 @@ const EditProfile = props => {
       .finally(r => {
         setSaving(false)
       })
-    props.screenProps.pop()
+    screenProps.pop()
   }
+
   return (
     <Wrapper>
       <Section style={styles.section}>
         <Section.Row style={styles.centered}>
-          <UserAvatar onChange={setProfile} editable={true} profile={profile} />
+          <UserAvatar profile={profile} onPress={() => screenProps.push(`${profile.avatar ? 'View' : 'Edit'}Avatar`)} />
           <CustomButton
             disabled={saving}
             loading={saving}

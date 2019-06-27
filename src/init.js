ca853c672c7b19cee96c08cb60a24c8bddad9eac
Kevin Bardi <bardi.kevin@gmail.com>
2019-06-27 10:28:48 -0300

    Update broken init after merge

--- src/init.js
+++ src/init.js
@@ -1,6 +1,5 @@
 //@flow
-// eslint-disable-next-line no-unused-vars
-import initGunDB from './lib/gundb/gundb'
+import './lib/gundb/gundb'
 import goodWallet from './lib/wallet/GoodWallet'
 import userStorage from './lib/gundb/UserStorage'
 import { initAnalytics } from './lib/analytics/analytics'
@@ -8,18 +7,6 @@ import { initAnalytics } from './lib/analytics/analytics'
 export const init = () => {
   return Promise.all([goodWallet.ready, userStorage.ready]).then(async () => {
     global.wallet = goodWallet
-    const identifier = goodWallet.getAccountForType('login')
-    const emailOrId = (await userStorage.getProfileFieldValue('email')) || identifier
-    if (global.Rollbar && Config.env !== 'test') {
-      global.Rollbar.configure({
-        payload: {
-          person: {
-            id: emailOrId
-          }
-        }
-      })
-    }
-    global.FS && FS.identify(emailOrId, {})
-    global.amplitude && amplitude.getInstance().setUserId(emailOrId)
+    await initAnalytics(goodWallet, userStorage)
   })
 }

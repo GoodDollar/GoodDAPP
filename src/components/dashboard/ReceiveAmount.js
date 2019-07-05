11a0c033b42ae17c95bc816b02d078d031a5237f
Your Name <dminones@gmail.com>
2019-07-05 13:45:26 -0300

    Matching receiveamount styles

--- src/components/dashboard/ReceiveAmount.js
+++ src/components/dashboard/ReceiveAmount.js
@@ -1,6 +1,9 @@
 // @flow
 import React, { useEffect, useMemo, useState } from 'react'
+import { StyleSheet } from 'react-native'
 import { isMobile } from 'mobile-device-detect'
+import normalize from 'react-native-elements/src/helpers/normalizeText'
+import { withTheme } from 'react-native-paper'
 import { useErrorDialog } from '../../lib/undux/utils/dialog'
 
 import goodWallet from '../../lib/wallet/GoodWallet'
@@ -8,6 +11,8 @@ import { generateCode, generateReceiveShareObject } from '../../lib/share'
 import { BigGoodDollar, CopyButton, CustomButton, Section, Wrapper } from '../common'
 import DoneButton from '../common/buttons/DoneButton'
 import { BackButton, useScreenState } from '../appNavigation/stackNavigation'
+import logger from '../../lib/logger/pino-logger'
+const log = logger.child({ from: 'ReceiveAmount' })
 
 export type ReceiveProps = {
   screenProps: any,
@@ -16,7 +21,7 @@ export type ReceiveProps = {
 
 const RECEIVE_TITLE = 'Receive G$'
 
-const ReceiveAmount = ({ screenProps }: ReceiveProps) => {
+const ReceiveAmount = ({ screenProps, ...props }: ReceiveProps) => {
   const { account, networkId } = goodWallet
   const [screenState] = useScreenState(screenProps)
   const [showErrorDialog] = useErrorDialog()
@@ -26,6 +31,7 @@ const ReceiveAmount = ({ screenProps }: ReceiveProps) => {
 
   const code = useMemo(() => generateCode(account, networkId, amount, reason), [account, networkId, amount, reason])
   const share = useMemo(() => generateReceiveShareObject(code), [code])
+  const styles = getStylesFromProps(props)
 
   const shareAction = async () => {
     try {
@@ -55,30 +61,22 @@ const ReceiveAmount = ({ screenProps }: ReceiveProps) => {
   return (
     <Wrapper>
       <Section justifyContent="space-between" grow>
-        <Section.Title>Summary</Section.Title>
-        <Section.Stack>
-          <Section.Row>
+        <Section.Stack grow>
+          <Section.Title>Summary</Section.Title>
+        </Section.Stack>
+        <Section.Stack grow justifyContent="flex-start">
+          <Section.Row style={styles.tableRow}>
             <Section.Text>From:</Section.Text>
             <Section.Text>{fromWho}</Section.Text>
           </Section.Row>
-          <Section.Row>
+          <Section.Row style={styles.tableRow}>
             <Section.Text>Amount:</Section.Text>
             <BigGoodDollar number={amount} />
           </Section.Row>
-          <Section.Row>
+          <Section.Row style={styles.tableRow}>
             <Section.Text>For:</Section.Text>
             <Section.Text>{reason}</Section.Text>
           </Section.Row>
-          {/* <Section.Stack>
-            <Section.Text style={[styles.lowerSecondaryText]}>This QR code requests exactly</Section.Text>
-            <Section.Text style={styles.addressSection}>
-              <Text style={styles.url}>{share.url}</Text>
-            </Section.Text>
-            <Section.Text>
-              <BigGoodDollar style={styles.centered} number={amount} />
-            </Section.Text>
-            <Section.Text>{reason ? reason : null}</Section.Text>
-          </Section.Stack> */}
         </Section.Stack>
         {confirmed ? (
           <Section.Stack>
@@ -111,4 +109,28 @@ ReceiveAmount.shouldNavigateToComponent = props => {
   return !!screenState.nextRoutes && screenState.amount
 }
 
-export default ReceiveAmount
+export default withTheme(ReceiveAmount)
+
+const getStylesFromProps = props => {
+  const { theme } = props
+  log.debug({ theme })
+  return StyleSheet.create({
+    qrCode: {
+      padding: normalize(16),
+      borderColor: theme.colors.primary,
+      borderWidth: 1,
+      borderRadius: normalize(5)
+    },
+    qrWrapper: {
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    tableRow: {
+      borderBottomColor: theme.colors.placeholder,
+      borderBottomWidth: normalize(2),
+      borderBottomStyle: 'solid',
+      marginTop: theme.defaultMargin * 2,
+      alignItems: 'baseline'
+    }
+  })
+}

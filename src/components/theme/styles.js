c5a8aeec4d6269eb881e9e63d4434666fa241f44
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-07-08 15:14:34 -0300

    (remove) unused code

--- src/components/theme/styles.js
+++ src/components/theme/styles.js
@@ -1,34 +1,6 @@
 import DefaultTheme from 'react-native-paper/src/styles/DefaultTheme'
 import normalize from 'react-native-elements/src/helpers/normalizeText'
 
-// export const colors = {
-//   blue: '#006EA0',
-//   darkBlue: '#0C263D',
-//   darkGray: '#42454A',
-//   gray50Percent: '#CBCBCB',
-//   green: '#00C3AE',
-//   lightGray: '#EEE',
-//   orange: '#F8AF40',
-//   primary: '#00AFFF',
-//   purple: '#9F6A9D',
-//   red: '#FA6C77',
-//   text: '#222'
-// }
-
-// export const paddings = {
-//   mainContainerPadding: normalize(8)
-// }
-
-// export const fontStyle = {
-//   color: '#555',
-//   fontSize: normalize(18),
-//   textAlign: 'center'
-// }
-
-// export const scrollableContainer = {
-//   flexGrow: 1
-// }
-
 export const theme = {
   ...DefaultTheme,
   colors: {

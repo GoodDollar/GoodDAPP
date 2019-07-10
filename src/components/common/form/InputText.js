983bdbf51fa686609783cae485e5fabd2936756f
Your Name <dminones@gmail.com>
2019-07-10 20:20:01 -0300

    Adding tests and improving style. Updating snapshots

--- src/components/common/form/InputText.js
+++ src/components/common/form/InputText.js
@@ -1,20 +1,23 @@
 // @flow
 import React from 'react'
-import { TextInput } from 'react-native-paper'
+import { TextInput } from 'react-native'
 import normalize from 'react-native-elements/src/helpers/normalizeText'
 import { withStyles } from '../../../lib/styles'
 
 const InputText = (props: any) => {
-  return <TextInput {...props} />
+  return <TextInput style={props.styles.input} {...props} />
 }
 
 const getStylesFromProps = ({ theme }) => {
   return {
     input: {
       ...theme.fontStyle,
-      fontSize: normalize(24),
-      fontWeight: '500',
-      textTransform: 'uppercase'
+      fontStyle: theme.fonts.slab,
+      color: theme.colors.darkGray,
+      borderBottomStyle: 'solid',
+      borderBottomWidth: normalize(1),
+      padding: theme.sizes.default,
+      borderBottomColor: theme.colors.darkGray
     }
   }
 }

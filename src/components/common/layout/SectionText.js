d5194b5e13e82f47cd935b855d6d4129c5ff4b55
fernandomg <fernando.greco@gmail.com>
2019-07-11 13:53:09 -0300

    Simplify Section.Text to inherit Text props and avoid overrides

--- src/components/common/layout/SectionText.js
+++ src/components/common/layout/SectionText.js
@@ -1,21 +1,10 @@
 // @flow
 import React from 'react'
-import normalize from 'react-native-elements/src/helpers/normalizeText'
 import { withStyles } from '../../../lib/styles'
 import Text from '../view/Text'
 
 const SectionText = (props: any) => {
-  const { styles } = props
-  return <Text {...props} style={[styles.text, props.style]} />
+  return <Text {...props} />
 }
 
-const getStylesFromProps = ({ theme }) => {
-  return {
-    text: {
-      ...theme.fontStyle,
-      fontSize: normalize(14)
-    }
-  }
-}
-
-export default withStyles(getStylesFromProps)(SectionText)
+export default withStyles()(SectionText)

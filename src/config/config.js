ecfbfd2f2f988f192566ca36bf5959683497ae32
Hadar <hadarr@gmail.com>
2019-07-20 23:16:34 +0300

    add: move analytics go gtm

--- src/config/config.js
+++ src/config/config.js
@@ -7,37 +7,40 @@ const Config = {
   infuraKey: process.env.REACT_APP_INFURA_KEY,
   network: process.env.REACT_APP_NETWORK || 'fuse',
   zoomLicenseKey: process.env.REACT_APP_ZOOM_LICENSE_KEY,
+  amplitudeKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
+  rollbarKey: process.env.REACT_APP_ROLLBAR_API_KEY,
   httpWeb3provider: process.env.REACT_APP_WEB3_RPC,
   recaptcha: '6LeOaJIUAAAAAKB3DlmijMPfX2CBYsve3T2MwlTd',
-  skipEmailVerification: process.env.REACT_APP_SKIP_EMAIL_VERIFICATION === "true",
-  skipMobileVerification: process.env.REACT_APP_SKIP_MOBILE_VERIFICATION === "true",
-  throwSaveProfileErrors: !process.env.REACT_APP_THROW_SAVE_PROFILE_ERRORS || process.env.REACT_APP_THROW_SAVE_PROFILE_ERRORS === "true",
+  skipEmailVerification: process.env.REACT_APP_SKIP_EMAIL_VERIFICATION === 'true',
+  skipMobileVerification: process.env.REACT_APP_SKIP_MOBILE_VERIFICATION === 'true',
+  throwSaveProfileErrors:
+    !process.env.REACT_APP_THROW_SAVE_PROFILE_ERRORS || process.env.REACT_APP_THROW_SAVE_PROFILE_ERRORS === 'true',
   ethereum: {
     '42': {
       network_id: 42,
       web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://kovan.infura.io/v3/',
-      websocketWeb3Provider: 'wss://kovan.infura.io/ws'
+      websocketWeb3Provider: 'wss://kovan.infura.io/ws',
     },
     '3': {
       network_id: 3,
       web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://ropsten.infura.io/v3/',
-      websocketWeb3Provider: 'wss://ropsten.infura.io/ws'
+      websocketWeb3Provider: 'wss://ropsten.infura.io/ws',
     },
     '121': {
       network_id: 121,
       web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://rpc.fuse.io/',
-      websocketWeb3Provider: 'wss://explorer-node.fuse.io/ws'
+      websocketWeb3Provider: 'wss://explorer-node.fuse.io/ws',
     },
     '4447': {
       network_id: 4447,
       web3Transport: 'HttpProvider',
       httpWeb3provider: 'http://localhost:9545/',
-      websocketWeb3Provider: 'ws://localhost:9545/ws'
-    }
-  }
+      websocketWeb3Provider: 'ws://localhost:9545/ws',
+    },
+  },
 }
 
 // Forcing value as number, if not MNID encoder/decoder may fail
--- src/lib/analytics/analytics.js
+++ src/lib/analytics/analytics.js
@@ -2,26 +2,42 @@
 import logger from '../../lib/logger/pino-logger'
 import Config from '../../config/config'
 
-let Amplitude, FS
+let Amplitude, FS, Rollbar
 const log = logger.child({ from: 'analytics' })
 
 export const initAnalytics = async (goodWallet: GoodWallet, userStorage: UserStorage) => {
   const identifier = goodWallet.getAccountForType('login')
   const emailOrId = (await userStorage.getProfileFieldValue('email')) || identifier
-  if (global.Rollbar && Config.env !== 'test') {
+  if (global.Rollbar && Config.rollbarKey) {
+    Rollbar = global.Rollbar
     global.Rollbar.configure({
+      accessToken: Config.rollbarKey,
+      captureUncaught: true,
+      captureUnhandledRejections: true,
       payload: {
+        environment: Config.env,
         person: {
           id: emailOrId,
         },
       },
     })
   }
-  FS = window.FS
-  Amplitude = window.amplitude && window.amplitude.getInstance()
-  FS && FS.identify(emailOrId, {})
-  Amplitude && Amplitude.setUserId(emailOrId)
-  log.debug('Initialized analytics:', { Amplitude: Amplitude !== undefined, FS: FS !== undefined })
+
+  if (global.Amplitude && Config.amplitudeKey) {
+    Amplitude = global.amplitude.getInstance()
+    Amplitude.init(Config.amplitudeKey)
+    Amplitude && Amplitude.setUserId(emailOrId)
+  }
+
+  if (global.FS) {
+    FS = window.FS
+    FS.identify(emailOrId, {})
+  }
+  log.debug('Initialized analytics:', {
+    Amplitude: Amplitude !== undefined,
+    FS: FS !== undefined,
+    Rollbar: Rollbar !== undefined,
+  })
 }
 
 export const fireEvent = (event: string, data: {} = {}) => {

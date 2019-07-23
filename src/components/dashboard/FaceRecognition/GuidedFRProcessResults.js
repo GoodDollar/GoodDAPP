f1fddc3232aa0a8ed1151f63b8a4f96c90cbbed5
sirpy <hadarr@gmail.com>
2019-07-23 09:51:36 +0300

    add: improve error display (#281)
    * add: improve error display
    
    * add: FR events
    
    * add: FR events

--- src/components/dashboard/FaceRecognition/GuidedFRProcessResults.js
+++ src/components/dashboard/FaceRecognition/GuidedFRProcessResults.js
@@ -4,6 +4,8 @@ import { ActivityIndicator, Image, StyleSheet, View } from 'react-native'
 import { Text } from 'react-native-paper'
 import normalize from 'react-native-elements/src/helpers/normalizeText'
 import find from 'lodash/find'
+import findKey from 'lodash/findKey'
+import mapValues from 'lodash/mapValues'
 import { CustomButton, Section } from '../../common'
 import logger from '../../../lib/logger/pino-logger'
 import goodWallet from '../../../lib/wallet/GoodWallet'
@@ -13,6 +15,7 @@ import Check from '../../../assets/Icons - Success - White.svg'
 import Cross from '../../../assets/Icons - Close X - White.svg'
 import LookingGood from '../../../assets/LookingGood.svg'
 import GDStore from '../../../lib/undux/GDStore'
+import { fireEvent } from '../../../lib/analytics/analytics'
 
 const log = logger.child({ from: 'GuidedFRProcessResults' })
 
@@ -38,7 +41,7 @@ const FRStep = ({ title, isActive, status, isProcessFailed, paddingBottom }) => 
     </View>
   )
 }
-const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigation, isWhitelisted }: any) => {
+const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigation, isAPISuccess }: any) => {
   const store = GDStore.useStore()
   const { fullName } = store.get('profile')
 
@@ -48,12 +51,17 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
     isEnrolled: undefined,
     isLive: undefined,
     isWhitelisted: undefined,
+    isProfileSaved: undefined,
   })
 
   const updateProgress = data => {
     log.debug('updating progress', { data })
 
     // let explanation = ''
+    let failedFR = findKey(data, (v, k) => v === false)
+    if (failedFR) {
+      fireEvent(`FR_Failed`, { failedFR })
+    }
     setStatus({ ...processStatus, ...data })
 
     // log.debug('analyzed data,', { processStatus })
@@ -80,7 +88,7 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
 
   // let [showDialogWithData] = useDialog()
   let gun = userStorage.gun
-  log.debug({ sessionId, isWhitelisted })
+  log.debug({ sessionId, isAPISuccess })
 
   useEffect(() => {
     log.debug('subscriping to gun updates:', { sessionId })
@@ -112,6 +120,7 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
   const gotoSupport = () => {
     navigation.push('Support')
   }
+
   useEffect(() => {
     //done save profile and call done callback
     if (processStatus.isWhitelisted) {
@@ -119,15 +128,19 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
     }
   }, [processStatus.isWhitelisted])
 
-  //API call finished, so it will pass isWhitelisted to us
-  //this is a backup incase the gundb messaging doesnt work
-  const gunOK = find(processStatus, (v, k) => v !== undefined)
-  if (gunOK === undefined && isWhitelisted) {
-    processStatus.isWhitelisted = true
-    saveProfileAndDone()
-  } else if (gunOK === undefined && isWhitelisted === false) {
-    processStatus.isWhitelisted = false
-  }
+  useEffect(() => {
+    if (isAPISuccess === undefined) {
+      return
+    }
+
+    //API call finished, so it will pass isWhitelisted to us
+    //this is a backup incase the gundb messaging doesnt work
+    const gunOK = find(processStatus, (v, k) => v !== undefined)
+    if (gunOK === undefined) {
+      const newStatus = mapValues(processStatus, v => false)
+      setStatus({ ...newStatus, isWhitelisted: isAPISuccess, useAPIResult: true })
+    }
+  }, [isAPISuccess])
 
   const isProcessFailed =
     processStatus.isNotDuplicate === false ||
@@ -156,7 +169,9 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
     ) : null
 
   let helpText
-  if (processStatus.isNotDuplicate === false) {
+  if (processStatus.useAPIResult && isAPISuccess === false) {
+    helpText = 'Something went wrong, please try again...'
+  } else if (processStatus.isNotDuplicate === false) {
     helpText = (
       <View>
         <Text style={styles.textHelp}>
@@ -182,7 +197,7 @@ const GuidedFRProcessResults = ({ profileSaved, sessionId, retry, done, navigati
     helpText =
       'We could not verify you are a living person. Funny hu? please make sure:\n\n\
 A. Center your webcam\n\
-B. Ensure camera is at eye level\n\
+B. Camera is at eye level\n\
 C. Light your face evenly'
   } else if (isProcessFailed) {
     helpText = 'Something went wrong, please try again...'
@@ -223,6 +238,7 @@ C. Light your face evenly'
             <FRStep
               title={'Checking liveness'}
               isActive={
+                isProcessFailed ||
                 isProcessSuccess ||
                 (processStatus.isNotDuplicate !== undefined && processStatus.isNotDuplicate === true)
               }
@@ -231,14 +247,20 @@ C. Light your face evenly'
             />
             <FRStep
               title={'Validating identity'}
-              isActive={isProcessSuccess || (processStatus.isLive !== undefined && processStatus.isLive === true)}
+              isActive={
+                isProcessFailed ||
+                isProcessSuccess ||
+                (processStatus.isLive !== undefined && processStatus.isLive === true)
+              }
               status={isProcessSuccess || processStatus.isWhitelisted}
               isProcessFailed={isProcessFailed}
             />
             <FRStep
               title={'Updating profile'}
               isActive={
-                isProcessSuccess || (processStatus.isWhitelisted !== undefined && processStatus.isWhitelisted === true)
+                isProcessFailed ||
+                isProcessSuccess ||
+                (processStatus.isWhitelisted !== undefined && processStatus.isWhitelisted === true)
               }
               status={isProcessSuccess || processStatus.isProfileSaved}
               isProcessFailed={isProcessFailed}

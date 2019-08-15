0a9cb7a59d01272f052b50cca0f0c901227f74ac
fernandomg <fernando.greco@gmail.com>
2019-08-15 15:23:38 -0300

    Update `CustomButton` text color implementatio

--- src/components/common/buttons/CustomButton.js
+++ src/components/common/buttons/CustomButton.js
@@ -59,14 +59,15 @@ const mapPropsToStyles = ({ theme }) => ({
 })
 
 const TextContent = withStyles(mapPropsToStyles)(
-  ({ children, dark, uppercase, styles, textStyle }: TextContentProps) => {
+  ({ children, color, dark, uppercase, styles, textStyle }: TextContentProps) => {
     if (typeof children === 'string') {
+      // if set to dark, then text will be white.
+      // if 'color' is specified, use the color for the text
+      // if not, then button will be using 'primary' color
+      const textColor = (dark && 'white') || color || 'primary'
+
       return (
-        <Text
-          color={dark && 'white'}
-          textTransform={uppercase ? 'uppercase' : 'none'}
-          style={[styles.buttonText, textStyle]}
-        >
+        <Text color={textColor} textTransform={uppercase ? 'uppercase' : 'none'} style={[styles.buttonText, textStyle]}>
           {children}
         </Text>
       )
@@ -127,7 +128,7 @@ const CustomButton = (props: ButtonProps) => {
       {icon && (!iconAlignment || iconAlignment === 'left') && (
         <IconButton icon={icon} theme={theme} dark={dark} size={iconSize} style={styles.leftIcon} />
       )}
-      <TextContent dark={dark} uppercase={uppercase} textStyle={textStyle}>
+      <TextContent dark={dark} uppercase={uppercase} textStyle={textStyle} color={buttonProps.color}>
         {children}
       </TextContent>
       {icon && iconAlignment === 'right' && (

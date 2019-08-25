43e0a0f10b196667628c7d81e9dec1c3fa6b431f
Fernando <fernando.greco@gmail.com>
2019-08-25 17:21:45 -0300

    (Feature) Scroll-to-top button (#474)
    * Create ScrollToTopButton
    
    * Implement `ScrollToTopButton` inside scrollable List
    
    * Update snapshot

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -207,6 +207,7 @@ const Dashboard = props => {
             setHeaderLarge(newHeaderLarge)
           }
         }}
+        headerLarge={headerLarge}
       />
       {currentFeed && (
         <FeedModalList

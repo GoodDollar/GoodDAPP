b3b9bbd2de38e6ec56107559300cce69beb9b8e2
fernandomg <fernando.greco@gmail.com>
2019-08-29 15:27:20 -0300

    Update profile after user is registered

--- src/lib/login/checkAuthStatus.js
+++ src/lib/login/checkAuthStatus.js
@@ -7,7 +7,9 @@ export const checkAuthStatus = async () => {
   // when wallet is ready perform login to server (sign message with wallet and send to server)
   const [credsOrError, isCitizen]: any = await Promise.all([goodWalletLogin.auth(), goodWallet.isCitizen()])
 
-  const isLoggedIn = credsOrError.jwt !== undefined && (await userStorage.getProfileField('registered'))
+  const isAuthorized = credsOrError.jwt !== undefined
+  const isRegistered = (await userStorage.getProfileFieldValue('registered')) !== undefined
+  const isLoggedIn = isAuthorized && isRegistered
   const isLoggedInCitizen = isLoggedIn && isCitizen
 
   return {
--- src/lib/undux/effects/profile.js
+++ src/lib/undux/effects/profile.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Effects, Store } from 'undux'
 import userStorage from '../../gundb/UserStorage'
+import { checkAuthStatus } from '../../login/checkAuthStatus'
 import type { State } from '../GDStore'
 
 /**
@@ -10,6 +11,28 @@ import type { State } from '../GDStore'
 const withProfile: Effects<State> = (store: Store) => {
   store.on('isLoggedIn').subscribe(isLoggedIn => {
     if (!isLoggedIn) {
+      // Listen to 'registered' changes on gundb
+      // This is done due to time restrains.
+      // After signup flow, 'registered' is not yet set before it's queried by `checkAuthStatus`, which leads to `isLoggedIn === false`.
+      // After 'registered' value is set, we re-check the auth-status, getting a `isLoggedIn === true`
+      userStorage.profile.get('registered').on(async () => {
+        const isRegistered = await userStorage.getProfileFieldValue('registered')
+
+        if (isRegistered) {
+          // if user is registered re-check the auth-status
+          const { isLoggedIn } = await checkAuthStatus()
+
+          if (isLoggedIn) {
+            // if in fact it's logged in, we update its value in the store...
+            store.set('isLoggedIn')(isLoggedIn)
+
+            // ... and stop listening for 'registered' changes
+            userStorage.profile.get('registered').off()
+          }
+        }
+      })
+
+      // do nothing else until store's `isLoggedIn === true`
       return
     }
 

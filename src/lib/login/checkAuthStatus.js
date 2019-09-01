90ba96c7673e700c742e08e8605bb04449dec960
Hadar Rottenberg <hadar@gooddollar.org>
2019-09-01 11:17:32 +0300

    add: remove isLoggedIn dependency on 'registered' field

--- src/lib/login/checkAuthStatus.js
+++ src/lib/login/checkAuthStatus.js
@@ -1,15 +1,13 @@
 //@flow
 import goodWallet from '../wallet/GoodWallet'
 import goodWalletLogin from '../login/GoodWalletLogin'
-import userStorage from '../gundb/UserStorage'
 
 export const checkAuthStatus = async () => {
   // when wallet is ready perform login to server (sign message with wallet and send to server)
   const [credsOrError, isCitizen]: any = await Promise.all([goodWalletLogin.auth(), goodWallet.isCitizen()])
 
   const isAuthorized = credsOrError.jwt !== undefined
-  const isRegistered = (await userStorage.getProfileFieldValue('registered')) !== undefined
-  const isLoggedIn = isAuthorized && isRegistered
+  const isLoggedIn = isAuthorized
   const isLoggedInCitizen = isLoggedIn && isCitizen
 
   return {

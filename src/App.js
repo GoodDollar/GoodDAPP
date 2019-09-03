cb66f10b1cec432243657c7f9cef84f6d7ab775e
Your Name <dminones@gmail.com>
2019-09-03 17:07:05 -0300

    adding add web library

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Platform, SafeAreaView, StyleSheet } from 'react-native'
 import PaperProvider from 'react-native-paper/src/core/Provider'
+import AddToHomescreen from 'react-add-to-homescreen'
 import { theme } from './components/theme/styles'
 import SimpleStore from './lib/undux/SimpleStore'
 import RouterSelector from './RouterSelector'
@@ -12,6 +13,11 @@ const App = () => {
   // onRecaptcha = (token: string) => {
   //   userStorage.setProfileField('recaptcha', token, 'private')
   // }
+  const handleAddToHomescreenClick = () => {
+    alert(`
+      1. Open Share menu
+      2. Tap on "Add to Home Screen" button`)
+  }
 
   return (
     <SimpleStore.Container>
@@ -22,6 +28,7 @@ const App = () => {
             <LoadingIndicator />
             {/* <ReCaptcha sitekey={Config.recaptcha} action="auth" verifyCallback={this.onRecaptcha} /> */}
             <RouterSelector />
+            <AddToHomescreen onAddToHomescreenClick={handleAddToHomescreenClick} />
           </React.Fragment>
         </SafeAreaView>
       </PaperProvider>

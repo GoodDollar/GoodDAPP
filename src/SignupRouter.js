153201d91f1bd55e19329d12b79d3d0a115b9cc3
StanislavShevchenko <stas246978@gmail.com>
2019-09-03 12:10:28 +0300

    User should see new signin screen magilink explanation (#517)
    * User should verify his email by copy pasting a code
    
    * Fixing by comments and new recover screen #497
    
    * New Recover screen
    
    * returns of the incomingMnemonic to support the option of the mnemonic as parameter from the url
    
    * use getDesignRelativeHeight and not use normalize

--- src/SignupRouter.js
+++ src/SignupRouter.js
@@ -4,12 +4,14 @@ import { createSwitchNavigator } from '@react-navigation/core'
 
 import { Platform } from 'react-native'
 import Signup from './components/signup/SignupState'
+import SigninInfo from './components/signin/SigninInfo'
 import Auth from './components/auth/Auth'
 
 const router = createSwitchNavigator(
   {
     Auth,
     Signup,
+    SigninInfo,
   },
   {
     initialRouteName: 'Auth',

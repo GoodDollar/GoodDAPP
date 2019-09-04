29910bbdd402317d8047f64eb3eba1a000c52960
Hadar Rottenberg <hadar@gooddollar.org>
2019-09-04 21:55:33 +0300

    add: default websocketprovider for web3js

--- src/config/config.js
+++ src/config/config.js
@@ -11,7 +11,7 @@ const Config = {
   amplitudeKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
   rollbarKey: process.env.REACT_APP_ROLLBAR_API_KEY,
   httpWeb3provider: process.env.REACT_APP_WEB3_RPC,
-  web3TransportProvider: process.env.REACT_APP_WEB3_TRANSPORT_PROVIDER,
+  web3TransportProvider: process.env.REACT_APP_WEB3_TRANSPORT_PROVIDER || 'WebSocketProvider',
   recaptcha: '6LeOaJIUAAAAAKB3DlmijMPfX2CBYsve3T2MwlTd',
   skipEmailVerification: process.env.REACT_APP_SKIP_EMAIL_VERIFICATION === 'true',
   skipMobileVerification: process.env.REACT_APP_SKIP_MOBILE_VERIFICATION === 'true',
@@ -22,25 +22,26 @@ const Config = {
   ethereum: {
     '42': {
       network_id: 42,
-      web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://kovan.infura.io/v3/',
       websocketWeb3Provider: 'wss://kovan.infura.io/ws',
     },
     '3': {
       network_id: 3,
-      web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://ropsten.infura.io/v3/',
       websocketWeb3Provider: 'wss://ropsten.infura.io/ws',
     },
     '121': {
       network_id: 121,
-      web3Transport: 'HttpProvider',
       httpWeb3provider: 'https://rpc.fuse.io/',
       websocketWeb3Provider: 'wss://explorer-node.fuse.io/ws',
     },
+    '122': {
+      network_id: 122,
+      httpWeb3provider: 'https://rpc.fusenet.io/',
+      websocketWeb3Provider: 'wss://explorer-node.fusenet.io/ws',
+    },
     '4447': {
       network_id: 4447,
-      web3Transport: 'HttpProvider',
       httpWeb3provider: 'http://localhost:9545/',
       websocketWeb3Provider: 'ws://localhost:9545/ws',
     },

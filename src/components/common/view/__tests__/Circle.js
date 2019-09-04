b5fc71a84f2b31e3248fc9e7430f90ca00a32deb
StanislavShevchenko <stas246978@gmail.com>
2019-09-05 01:15:36 +0300

    add test and update snapshots

---
+++ src/components/common/view/__tests__/Circle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Circle from '../Circle'
+import { withThemeProvider } from '../../../../__tests__/__util__'
+
+// Note: test renderer must be required after react-native.
+
+describe('Circle', () => {
+  const WrappedCircle = withThemeProvider(Circle)
+  
+  it('matches snapshot', () => {
+    const component = renderer.create(<WrappedCircle number={10}> Test text</WrappedCircle>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})

2362825856ca6e27585b65fffb81f3c46663b3c7
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-09-06 13:08:02 +0300

    fix "test with PaymentCancel event"

--- src/components/dashboard/__tests__/GoodWallet.js
+++ src/components/dashboard/__tests__/GoodWallet.js
@@ -4,7 +4,7 @@ import { gdToWei } from '../../../lib/wallet/utils'
 import adminWallet from './__util__/AdminWallet'
 
 describe('GoodWalletShare/ReceiveTokens', () => {
-  jest.setTimeout(60000)
+  jest.setTimeout(90000)
 
   const amount = gdToWei(0.1)
   const reason = 'Test_Reason'
@@ -24,6 +24,9 @@ describe('GoodWalletShare/ReceiveTokens', () => {
 
     await adminWallet.whitelistUser(testWallet.account, 'did:gd')
     await adminWallet.whitelistUser(testWallet2.account, 'did:gd')
+
+    await adminWallet.topWallet(testWallet.account)
+    await adminWallet.topWallet(testWallet2.account)
   })
 
   it('should emit `transfer` event filtered by `to` block', async () => {
@@ -41,16 +44,11 @@ describe('GoodWalletShare/ReceiveTokens', () => {
       expect(event[0].event).toBe('Transfer')
     })
 
-    await adminWallet.topWallet(testWallet.account)
-
     await testWallet.claim()
   })
 
   it('should emit `PaymentWithdraw` and `transfer` event filtered by `from` block', async () => {
     const lastBlock = await testWallet.getBlockNumber()
-
-    await adminWallet.topWallet(testWallet.account)
-    await adminWallet.topWallet(testWallet2.account)
 
     const linkData = testWallet.generateLink(amount, reason, () => {})
 
@@ -69,19 +67,17 @@ describe('GoodWalletShare/ReceiveTokens', () => {
     testWallet2.withdraw(linkData.hashedCode)
   })
 
-  it('should emit `PaymentCancel` event', async () => {
-    const lastBlock = await testWallet.getBlockNumber()
+  it('should emit `PaymentCancel` event', () => {
+    const linkData = testWallet.generateLink(amount, reason, async () => {
+      const lastBlock = await testWallet.getBlockNumber()
 
-    await adminWallet.topWallet(testWallet.account)
+      testWallet.listenTxUpdates(lastBlock, ({ toBlock, event }) => {
+        expect(event).toBeTruthy()
+        expect(event.event).toBe('PaymentCancel')
+        expect(toBlock).toBeTruthy()
+      })
 
-    const linkData = testWallet.generateLink(amount, reason, () => {})
-
-    testWallet.listenTxUpdates(lastBlock, ({ toBlock, event }) => {
-      expect(event).toBeTruthy()
-      expect(event.event).toBe('PaymentCancel')
-      expect(toBlock).toBeTruthy()
+      testWallet.cancelOTL(linkData.hashedCode)
     })
-
-    testWallet.cancelOTL(linkData.hashedCode)
   })
 })

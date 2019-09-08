bb997d720bab071dbf8d107b17c6744e9e9d4923
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2019-09-08 08:45:08 +0300

    PR #428 user from W3 login flow (#478)
    * #428 - make registration flow
    
    * #428 - make registration flow
    
    * #428 - make registration flow
    
    * #429 - User already opened wallet not through web3
    
    * #428 - User can click on W3 link to login to DAPP
    
    * #428 - User can click on W3 link to login to DAPP. Changes required in PR by sirpy
    
    * add: verification of web3 user email on server (#59)
    
    * refactor: "get base64 from image url through fetch library"
    
    * refactor: "move checkWeb3Email request to goToPhone section"
    
    * add: "log error in case if W3 email verification failed"
    
    * fix "add data:image prefix for avatar base64 string"
    
    * fix "update snapshots"
    
    * add "add test for InvalidW3TokenError component render"
    
    * add "add test for InvalidW3TokenError component render"
    
    * refactor: "add missed comma"
    
    * refactor: "indents"
    
    * add: "new tests to pass coverage"
    
    * add: "new tests to pass coverage"
    
    * refactor: "use axios for get base64 image"
    
    * fix: "remove await for methods which result is not used"
    
    * fix: "remove cross-fetch lib"
    
    * fix: "web3transport configs"
    
    * fix: "fix Payment Cancel GoodWallet test"
    
    * refactor "test"
    
    * fix "test with PaymentCancel event"

--- src/components/signin/Mnemonics.js
+++ src/components/signin/Mnemonics.js
@@ -28,6 +28,8 @@ const Mnemonics = ({ screenProps, navigation, styles }) => {
   const [errorMessage, setErrorMessage] = useState()
   const [showErrorDialog, hideDialog] = useErrorDialog()
 
+  AsyncStorage.removeItem('web3Token')
+
   const handleChange = (mnemonics: string) => {
     log.info({ mnemonics })
     const splitted = mnemonics.split(' ')
@@ -126,11 +128,21 @@ const Mnemonics = ({ screenProps, navigation, styles }) => {
     return [exists, exists && (await userStorage.getProfileFieldDisplayValue('fullName'))]
   }
 
+  const web3HasWallet = get(navigation, 'state.params.web3HasWallet')
+
   return (
     <Section grow={5} style={styles.wrapper}>
       <Section.Stack grow style={styles.instructions} justifyContent="space-around">
         <Text fontWeight="medium" fontSize={22}>
           {'Please enter your\n12-word pass phrase:'}
+        </Text>
+        {web3HasWallet && (
+          <Text color="gray80Percent" fontSize={14}>
+            Looks like you already have a wallet. Please recover it to continue
+          </Text>
+        )}
+        <Text color="gray80Percent" fontSize={14}>
+          You can copy-paste it from your backup email
         </Text>
       </Section.Stack>
       <Section.Stack grow={4} justifyContent="space-between">

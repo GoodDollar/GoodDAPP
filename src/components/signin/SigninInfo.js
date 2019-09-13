2a59a857ce01cf8782b59f955d20cf834c57cc58
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-09-13 14:38:46 +0300

    refactor "merge - after merge fixes"

--- src/components/signin/SigninInfo.js
+++ src/components/signin/SigninInfo.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { AsyncStorage Image, TouchableOpacity, View } from 'react-native'
+import { AsyncStorage, Image, TouchableOpacity, View } from 'react-native'
 import Section from '../common/layout/Section'
 import Circle from '../common/view/Circle'
 import Wrapper from '../common/layout/Wrapper'
@@ -11,14 +11,6 @@ import illustration from '../../assets/Signin/illustration.svg'
 import { getDesignRelativeHeight } from '../../lib/utils/sizes'
 import { createStackNavigator } from '../appNavigation/stackNavigation'
 
-type Props = {
-  navigation: any,
-  screenProps: {
-    push: Function,
-  },
-  styles: any,
-}
-
 Image.prefetch(illustration)
 
 const Signin = props => {
@@ -26,66 +18,59 @@ const Signin = props => {
 
   AsyncStorage.removeItem('web3Token')
 
-  const handleSignIn = () => {
-    props.navigation.navigate('Auth')
-  }
-
   const handleRecover = () => {
     props.navigation.navigate('Recover')
   }
 
-  render() {
-    const { styles } = this.props
-    return (
-      <Section.Stack grow justifyContent="flex-start">
-        <Wrapper backgroundColor="#fff" style={styles.mainWrapper}>
-          <Image source={illustration} style={styles.illustration} resizeMode="contain" />
-          <Section.Row alignItems="center" justifyContent="center" style={styles.row}>
-            <View style={styles.bottomContainer}>
-              <Text fontWeight="medium" fontSize={22} fontFamily="Roboto">
-                {'To sign in\n please follow this steps:'}
-              </Text>
-              <Text fontSize={14} color="gray80Percent" fontFamily="Roboto">
-                {`(works from any device or platform)`}
-              </Text>
-            </View>
-          </Section.Row>
-          <Section.Row alignItems="center" justifyContent="center" style={styles.row}>
-            <View style={styles.bottomContainer}>
-              <Section.Text style={styles.blockCircle}>
-                <Circle number={1}>Go to your email</Circle>
-                <Circle number={2}>
-                  Find{' '}
-                  <Text fontWeight="bold" style={styles.text} fontFamily="Roboto">
-                    GoodDollar magic mail
-                  </Text>
-                </Circle>
-                <Circle number={3}>
-                  Click the{' '}
-                  <Text fontWeight="bold" style={styles.text} fontFamily="Roboto">
-                    magic link
-                  </Text>
-                </Circle>
-              </Section.Text>
-            </View>
-          </Section.Row>
-        </Wrapper>
+  return (
+    <Section.Stack grow justifyContent="flex-start">
+      <Wrapper backgroundColor="#fff" style={styles.mainWrapper}>
+        <Image source={illustration} style={styles.illustration} resizeMode="contain" />
         <Section.Row alignItems="center" justifyContent="center" style={styles.row}>
-          <TouchableOpacity onPress={this.handleRecover}>
-            <Section.Text
-              fontWeight="medium"
-              style={styles.textBottom}
-              textDecorationLine="underline"
-              fontSize={14}
-              color="primary"
-            >
-              Or, recover from pass phrase
-            </Section.Text>
-          </TouchableOpacity>
+          <View style={styles.bottomContainer}>
+            <Text fontWeight="medium" fontSize={22} fontFamily="Roboto">
+              {'To sign in\n please follow this steps:'}
+            </Text>
+            <Text fontSize={14} color="gray80Percent" fontFamily="Roboto">
+              {`(works from any device or platform)`}
+            </Text>
+          </View>
         </Section.Row>
-      </Section.Stack>
-    )
-  }
+        <Section.Row alignItems="center" justifyContent="center" style={styles.row}>
+          <View style={styles.bottomContainer}>
+            <Section.Text style={styles.blockCircle}>
+              <Circle number={1}>Go to your email</Circle>
+              <Circle number={2}>
+                Find{' '}
+                <Text fontWeight="bold" style={styles.text} fontFamily="Roboto">
+                  GoodDollar magic mail
+                </Text>
+              </Circle>
+              <Circle number={3}>
+                Click the{' '}
+                <Text fontWeight="bold" style={styles.text} fontFamily="Roboto">
+                  magic link
+                </Text>
+              </Circle>
+            </Section.Text>
+          </View>
+        </Section.Row>
+      </Wrapper>
+      <Section.Row alignItems="center" justifyContent="center" style={styles.row}>
+        <TouchableOpacity onPress={handleRecover}>
+          <Section.Text
+            fontWeight="medium"
+            style={styles.textBottom}
+            textDecorationLine="underline"
+            fontSize={14}
+            color="primary"
+          >
+            Or, recover from pass phrase
+          </Section.Text>
+        </TouchableOpacity>
+      </Section.Row>
+    </Section.Stack>
+  )
 }
 
 const getStylesFromProps = ({ theme }) => {

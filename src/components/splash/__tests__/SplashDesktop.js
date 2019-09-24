4730269f2e8d0906bc3209261abd3618666e18b8
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-09-24 11:50:24 +0300

    fix: splashDesctop test

--- src/components/splash/__tests__/SplashDesktop.js
+++ src/components/splash/__tests__/SplashDesktop.js
@@ -5,14 +5,17 @@ import ImportedSplash from '../SplashDesktop'
 
 const SplashDesktop = withThemeProvider(ImportedSplash)
 
+const emptyHandler = () => {}
+const mockUrl = 'http://localhost:8000'
+
 describe('SplashDesktop', () => {
   it('renders without errors', () => {
-    const tree = renderer.create(<SplashDesktop />)
+    const tree = renderer.create(<SplashDesktop onContinue={emptyHandler} urlForQR={mockUrl} />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const component = renderer.create(<SplashDesktop />)
+    const component = renderer.create(<SplashDesktop onContinue={emptyHandler} urlForQR={mockUrl} />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })

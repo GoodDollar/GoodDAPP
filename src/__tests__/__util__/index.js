0e35239c95313bebe588d8debd5c0b4b483444c8
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-09-25 23:24:43 +0300

    merge

--- src/__tests__/__util__/index.js
+++ src/__tests__/__util__/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { theme as defaultTheme } from '../../components/theme/styles'
 import { StoresWrapper } from '../../lib/undux/utils/storeswrapper.js'
+import SimpleStore from '../../lib/undux/SimpleStore'
 
 export const withThemeProvider = (Component, theme = defaultTheme) => props => (
   <PaperProvider theme={theme}>
@@ -9,4 +10,10 @@ export const withThemeProvider = (Component, theme = defaultTheme) => props => (
       <Component {...props} />
     </StoresWrapper>
   </PaperProvider>
+)
+
+export const withSimpleStateProvider = (Component, theme = defaultTheme) => props => (
+  <SimpleStore.Container>
+    <Component {...props} />
+  </SimpleStore.Container>
 )

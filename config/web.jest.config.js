87b698390c578d36b28a43dc31175ffd8e6c6ca6
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-10-08 13:45:04 +0300

    fix: fix test to pass build

--- config/web.jest.config.js
+++ config/web.jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     '<rootDir>/config/initTest.js',
   ],
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/UserStorage.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}'
   ],
   testEnvironment: 'jsdom',

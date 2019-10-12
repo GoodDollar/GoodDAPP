796a7e0fbb090d0684277000528a9fcb7c84c8fc
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2019-10-12 21:03:47 +0300

    502 user should verify his changed email or mobile (#680)
    * add: add posibility to change email and phone number for user with verification
    
    * add: save new data to user profile. add success popup on success save
    
    * add: add test for new screens, update snapshots
    
    * fix: fix width of buttons
    
    * fix: update snapshots
    
    * fix: save new phone/email with the same privacy as it was before. Delete success popup dialog on sms verification screen
    
    * fix: after merge fix
    
    * fix: after merge fix
    
    * fix: remove unnecessary data from api requests
    
    * refactor: remove re-verification endpoints
    
    * refactor: fix button text
    
    * fix: revert changes with SmsForm component. Use the same SmsForm and EmailConfirmation components for re-verify email/phone as SignUp Container does
    
    * fix: fix test and update snapshots

---
+++ src/components/profile/VerifyEditCode.js
@@ -0,0 +1,98 @@
+// @flow
+import React from 'react'
+import _get from 'lodash/get'
+import logger from '../../lib/logger/pino-logger'
+import { getDesignRelativeHeight } from '../../lib/utils/sizes'
+import { withStyles } from '../../lib/styles'
+import userStorage from '../../lib/gundb/UserStorage'
+import EmailConfirmation from '../signup/EmailConfirmation'
+import SmsForm from '../signup/SmsForm'
+
+const log = logger.child({ from: 'Verify Edit Code' })
+
+const VerifyEditCode = props => {
+  const { navigation } = props
+  const field = _get(navigation, 'state.params.field')
+  const content = _get(navigation, 'state.params.content')
+  let fieldToSave
+  let retryFunctionName
+  let RenderComponent
+
+  switch (field) {
+    case 'phone':
+      fieldToSave = 'mobile'
+      retryFunctionName = 'sendNewOTP'
+      RenderComponent = SmsForm
+      break
+
+    case 'email':
+    default:
+      fieldToSave = 'email'
+      retryFunctionName = 'sendVerificationForNewEmail'
+      RenderComponent = EmailConfirmation
+      break
+  }
+
+  log.info('Received params', {
+    field,
+    content,
+  })
+
+  const handleSubmit = async () => {
+    const privacy = await userStorage.getFieldPrivacy(fieldToSave)
+    await userStorage.setProfileField(fieldToSave, content, privacy)
+
+    navigation.navigate('Profile')
+  }
+
+  return (
+    <RenderComponent
+      screenProps={{
+        retryFunctionName: retryFunctionName,
+        doneCallback: handleSubmit,
+        data: {
+          [fieldToSave]: content,
+        },
+      }}
+    />
+  )
+}
+
+VerifyEditCode.navigationOptions = {
+  title: 'Edit Profile',
+}
+
+const getStylesFromProps = ({ theme }) => ({
+  informativeParagraph: {
+    margin: '1em',
+  },
+  buttonWrapper: {
+    alignContent: 'stretch',
+    flexDirection: 'column',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  button: {
+    justifyContent: 'center',
+    width: '100%',
+    height: 60,
+  },
+  row: {
+    marginVertical: theme.sizes.defaultDouble,
+  },
+  errorStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.red,
+    color: theme.colors.red,
+  },
+  container: {
+    minHeight: getDesignRelativeHeight(200),
+    height: getDesignRelativeHeight(200),
+  },
+  bottomContent: {
+    marginTop: 'auto',
+    marginBottom: theme.sizes.defaultDouble,
+  },
+})
+
+export default withStyles(getStylesFromProps)(VerifyEditCode)

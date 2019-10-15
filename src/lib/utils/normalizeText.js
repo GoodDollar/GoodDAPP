3525b79338a45fc44483100fced698e3ae042227
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-10-15 14:05:31 +0300

    fix: apply fixes disscused in slack channel

--- src/lib/utils/normalizeText.js
+++ src/lib/utils/normalizeText.js
@@ -1,12 +1,18 @@
 import { Dimensions } from 'react-native'
+import { isBrowser, isTablet } from 'mobile-device-detect'
 
 const { height, width } = Dimensions.get('window')
 
 const DESIGN_WIDTH = 360
 const DESIGN_HEIGHT = 640 - 24
 
-const windowHeight = height > DESIGN_HEIGHT ? DESIGN_HEIGHT : height
-const windowWidth = width > DESIGN_WIDTH ? DESIGN_WIDTH : width
+const MAX_WIDTH = 475
+const MAX_HEIGHT = 844
+
+const isDesktopOrTablet = isBrowser || isTablet
+
+const windowHeight = isDesktopOrTablet && height > MAX_HEIGHT ? MAX_HEIGHT : height
+const windowWidth = isDesktopOrTablet && width > MAX_WIDTH ? MAX_WIDTH : width
 
 const CURRENT_RESOLUTION = Math.sqrt(windowHeight * windowHeight + windowWidth * windowWidth)
 const DESIGN_RESOLUTION = Math.sqrt(DESIGN_HEIGHT * DESIGN_HEIGHT + DESIGN_WIDTH * DESIGN_WIDTH)
@@ -14,9 +20,9 @@ const DESIGN_RESOLUTION = Math.sqrt(DESIGN_HEIGHT * DESIGN_HEIGHT + DESIGN_WIDTH
 const RESOLUTIONS_PROPORTION = CURRENT_RESOLUTION / DESIGN_RESOLUTION
 
 export default size => {
-  if (RESOLUTIONS_PROPORTION < 1 && size <= 14) {
+  if (RESOLUTIONS_PROPORTION < 1 && size > 16) {
     return size
   }
 
-  return RESOLUTIONS_PROPORTION * size
+  return size
 }

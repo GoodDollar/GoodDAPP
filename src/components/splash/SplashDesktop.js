741f6129e7fa79b4bff225575c47a15457c04982
StanislavShevchenko <stas246978@gmail.com>
2019-10-30 22:14:49 +0200

    Update "Go to mobile" Splash screen: Design and text (#780)

--- src/components/splash/SplashDesktop.js
+++ src/components/splash/SplashDesktop.js
@@ -6,7 +6,6 @@ import { getDesignRelativeHeight } from '../../lib/utils/sizes'
 import CustomButton from '../common/buttons/CustomButton'
 import Wrapper from '../common/layout/Wrapper'
 import Section from '../common/layout/Section'
-import Config from '../../config/config'
 import QRCode from '../common/view/QRCode'
 
 //minimize delay <Image> has over web <img>
@@ -19,13 +18,13 @@ const SplashDesktop = ({ onContinue, urlForQR }) => (
       <View style={styles.backgroundWaves} />
       <Section.Stack style={styles.content} grow justifyContent="space-between">
         <Section.Text fontSize={22} color="darkBlue">
-          {`For Best Experience\nplease scan and continue\non your mobile device.`}
+          <Section.Text fontSize={22} fontWeight="black" color="darkBlue">
+            {`Welcome to GoodDollar Wallet\n`}
+          </Section.Text>
+          {`For best experience\nplease scan and continue\non your mobile device.`}
         </Section.Text>
         <QRCode value={urlForQR} size={150} qrStyles={styles.qrStyles} />
         <Image source={goodDollarImage} style={styles.goodDollar} resizeMode="contain" />
-        <Section.Text fontSize={22} color="darkBlue">
-          {`V${Config.version}`}
-        </Section.Text>
         <CustomButton mode="outlined" color="white" style={styles.buttonContinue} onPress={onContinue}>
           Continue on Web
         </CustomButton>

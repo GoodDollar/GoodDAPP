5c331f1615a1826b8c2f43229d30b852b1dad13f
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-05 11:44:21 +0200

    refactor: unused logger

--- src/init.js
+++ src/init.js
@@ -3,7 +3,6 @@ import _pick from 'lodash/pick'
 import './lib/gundb/gundb'
 import goodWallet from './lib/wallet/GoodWallet'
 import userStorage from './lib/gundb/UserStorage'
-import logger from './lib/logger/pino-logger'
 import isWebApp from './lib/utils/isWebApp'
 import { APP_OPEN, fireEvent, initAnalytics } from './lib/analytics/analytics'
 import { extractQueryParams } from './lib/share'
@@ -11,7 +10,7 @@ import { extractQueryParams } from './lib/share'
 export const init = () => {
   return Promise.all([goodWallet.ready, userStorage.ready]).then(async () => {
     global.wallet = goodWallet
-    await initAnalytics(goodWallet, userStorage, logger)
+    await initAnalytics(goodWallet, userStorage)
 
     const params = extractQueryParams(window.location.href)
     const source = Object.keys(_pick(params, ['web3', 'paymentCode', 'code'])).pop() || 'none'

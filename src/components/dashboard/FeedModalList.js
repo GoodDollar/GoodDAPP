7372bcf359c8e000eb8cc5b24a6e53931bb3d915
StanislavShevchenko <stas246978@gmail.com>
2019-11-05 12:43:55 +0200

    testing scroll

--- src/components/dashboard/FeedModalList.js
+++ src/components/dashboard/FeedModalList.js
@@ -93,14 +93,17 @@ const FeedModalList = ({
   const feeds = data && data instanceof Array && data.length ? data : undefined
   return (
     <Portal>
-      <View style={[styles.horizontalContainer, { opacity: 1 }]}>
+      <View style={[styles.horizontalContainer, { opacity: loading ? 0 : 1 }]}>
         <FlatList
           onScroll={({ nativeEvent }) => {
             console.log('****************************************')
-            console.log({nativeEvent, offset})
+            console.log({nativeEvent})
+            console.log('-------------------------------')
+            console.log('contentOffset', Math.abs(offset - nativeEvent.contentOffset.x))
+            console.log('offset', offset)
             console.log('****************************************')
             // when nativeEvent contentOffset reaches target offset setLoading to false, we stopped scrolling
-            if (nativeEvent.contentOffset.x === offset) {
+            if (Math.abs(offset - nativeEvent.contentOffset.x) < 5) {
               setLoading(false)
             }
           }}

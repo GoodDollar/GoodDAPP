7a07ff9f42b8cc2ce748c1a908ff6e87390168c5
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-06 15:08:52 +0200

    add: update support & faq iframes

--- src/components/webView/webViewInstances.js
+++ src/components/webView/webViewInstances.js
@@ -1,9 +1,11 @@
+import Config from '../../config/config'
 import { createIframe } from './iframe.web'
+
 export const TermsOfUse = createIframe('https://community.gooddollar.org/dappterms/', 'Terms of Use')
 export const PrivacyPolicy = createIframe('https://community.gooddollar.org/pp/', 'Privacy Policy')
 export const PrivacyArticle = createIframe(
   'https://medium.com/gooddollar/gooddollar-identity-pillar-balancing-identity-and-privacy-part-i-face-matching-d6864bcebf54',
   'Privacy And Identity'
 )
-export const Support = createIframe('https://community.gooddollar.org/support/', 'Feedback & Support')
-export const FAQ = createIframe('https://community.gooddollar.org/faq/', 'FAQ')
+export const Support = createIframe('https://community.gooddollar.org/support-iframe/', 'Feedback & Support')
+export const FAQ = createIframe(`https://community.gooddollar.org/faq-${Config.isEToro ? 'etoro' : 'iframe'}`, 'FAQ')

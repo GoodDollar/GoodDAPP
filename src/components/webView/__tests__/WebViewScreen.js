9cb9815fc8167725c6fca51ae7506b9a50c6f182
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-11-06 10:39:42 +0200

    fix: fix tests to bass travis buiuld, update snapshots

--- src/components/webView/__tests__/WebViewScreen.js
+++ src/components/webView/__tests__/WebViewScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import WebViewScreen, { createWebViewScreen } from '../WebViewScreen'
+import SimpleStore from '../../../lib/undux/SimpleStore'
 import { PrivacyPolicy, TermsOfUse } from '../webViewInstances'
 
 // Note: test renderer must be required after react-native.
@@ -33,23 +34,39 @@ describe('WebViewScreen', () => {
 
 describe('WebViewScreen Instances', () => {
   it('TermsOfUse renders without errors', () => {
-    const tree = renderer.create(<TermsOfUse />)
+    const tree = renderer.create(
+      <SimpleStore.Container>
+        <TermsOfUse />
+      </SimpleStore.Container>
+    )
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('TermsOfUse matches snapshot', () => {
-    const component = renderer.create(<TermsOfUse />)
+    const component = renderer.create(
+      <SimpleStore.Container>
+        <TermsOfUse />
+      </SimpleStore.Container>
+    )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   it('PrivacyPolicy renders without errors', () => {
-    const tree = renderer.create(<PrivacyPolicy />)
+    const tree = renderer.create(
+      <SimpleStore.Container>
+        <PrivacyPolicy />
+      </SimpleStore.Container>
+    )
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('PrivacyPolicy matches snapshot', () => {
-    const component = renderer.create(<PrivacyPolicy />)
+    const component = renderer.create(
+      <SimpleStore.Container>
+        <PrivacyPolicy />
+      </SimpleStore.Container>
+    )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })

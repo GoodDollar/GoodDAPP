af8ba8cfc11903109a8e61cdea450dc5d5828c0d
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2019-11-08 12:03:11 +0200

    (Feature) #807 navigating back from rewards market should be replaced with exit icon (#840)
    * add: update svg for marketplace icon
    
    * fix: remove back button from rewards tab, add back to wallet button to rewards tab
    
    * fix: remove back button from market tab, add back to wallet button to market tab
    
    * fix: implement rewards/marketplace page titles with Section.Text component to be adaptive across diferent screen sizes
    
    * fix: fix positioning of elements in navbar for marketplace/reward tabs
    
    * fix: update snapshots
    
    * fix: fix the fontWeight of market/rewards tab title
    
    * fix: wallet svg
    
    * fix: remove default true value for market prop in config file
    
    * fix: update snapshots
    
    * fix: add new tests to pass build

--- src/components/dashboard/Rewards.web.js
+++ src/components/dashboard/Rewards.web.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { TouchableOpacity, View } from 'react-native'
 import IframeResizer from 'iframe-resizer-react'
 import { isIOS } from 'mobile-device-detect'
+import { Appbar } from 'react-native-paper'
 import userStorage from '../../lib/gundb/UserStorage'
 import Config from '../../config/config'
 import logger from '../../lib/logger/pino-logger'
 import SimpleStore from '../../lib/undux/SimpleStore'
+import WalletSVG from '../common/view/WalletSvg'
+import Section from '../common/layout/Section'
 
 const log = logger.child({ from: 'RewardsTab' })
 
@@ -49,7 +53,40 @@ const RewardsTab = props => {
   )
 }
 
-RewardsTab.navigationOptions = {
-  title: 'Rewards',
+const navBarStyles = {
+  wrapper: {
+    position: 'relative',
+  },
+  title: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+  },
+  walletIcon: {
+    position: 'absolute',
+    right: 5,
+    bottom: -5,
+  },
 }
+
+const NavigationBar = navigate => (
+  <Appbar.Header dark style={navBarStyles.wrapper}>
+    <View style={{ width: 48 }} />
+    <Appbar.Content />
+    <Section.Text color="white" fontWeight="medium" style={navBarStyles.title}>
+      {'REWARDS'}
+    </Section.Text>
+    <Appbar.Content />
+    <TouchableOpacity onPress={() => navigate('Home')} style={navBarStyles.walletIcon}>
+      <WalletSVG />
+    </TouchableOpacity>
+  </Appbar.Header>
+)
+
+RewardsTab.navigationOptions = ({ navigation }) => {
+  return {
+    navigationBar: () => NavigationBar(navigation.navigate),
+  }
+}
+
 export default RewardsTab

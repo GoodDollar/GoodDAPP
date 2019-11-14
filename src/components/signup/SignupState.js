c235c1ab2319c98c2a6307ac99b28f823c2ed384
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-14 15:08:06 +0200

    refactor: send magiclink in done. use dynamic imported userstorage

--- src/components/signup/SignupState.js
+++ src/components/signup/SignupState.js
@@ -13,7 +13,6 @@ import SimpleStore from '../../lib/undux/SimpleStore'
 import { useDialog } from '../../lib/undux/utils/dialog'
 import { showSupportDialog } from '../common/dialogs/showSupportDialog'
 import { getUserModel, type UserModel } from '../../lib/gundb/UserModel'
-import userStorage from '../../lib/gundb/UserStorage'
 import Config from '../../config/config'
 import { fireEvent } from '../../lib/analytics/analytics'
 import type { SMSRecord } from './SmsForm'
@@ -394,6 +393,12 @@ const Signup = ({ navigation, screenProps }: { navigation: any, screenProps: any
       } finally {
         setLoading(false)
       }
+    } else if (nextRoute && nextRoute.key === 'MagicLinkInfo') {
+      const { userStorage } = await ready
+      API.sendMagicLinkByEmail(userStorage.getMagicLink(), newState.fullName)
+        .then(r => log.info('magiclink sent'))
+        .catch(e => log.error('failed sendMagicLinkByEmail', e.message, e))
+      return navigateWithFocus(nextRoute.key)
     } else if (nextRoute) {
       return navigateWithFocus(nextRoute.key)
     }
@@ -425,9 +430,6 @@ const Signup = ({ navigation, screenProps }: { navigation: any, screenProps: any
     }
 
     if (curRoute && curRoute.key === 'MagicLinkInfo') {
-      API.sendMagicLinkByEmail(userStorage.getMagicLink(), state.fullName).catch(e =>
-        log.error('failed sendMagicLinkByEmail', e.message, e)
-      )
       setShowNavBarGoBackButton(false)
     }
 

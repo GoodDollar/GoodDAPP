3bf8dcb7daebfebbf707eede1258e3d882066909
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-11-19 15:13:11 +0200

    fix: add test for ViewAvatar Component, update snapshots

---
+++ src/components/profile/__tests__/ViewAvatar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { withThemeProvider } from '../../../__tests__/__util__'
+import ViewAvatar from '../ViewAvatar'
+
+describe('ViewAvatar', () => {
+  const WrappedViewAvatar = withThemeProvider(ViewAvatar)
+
+  it('renders without errors', () => {
+    const tree = renderer.create(<WrappedViewAvatar />)
+    expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it('matches snapshot', () => {
+    const component = renderer.create(<WrappedViewAvatar />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})

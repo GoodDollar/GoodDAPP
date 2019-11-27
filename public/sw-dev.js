b059106da4afca76620baf556a925595df8110eb
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-27 23:42:54 +0200

    add: correct service worker for testing update

--- public/sw-dev.js
+++ public/sw-dev.js
@@ -3,16 +3,21 @@
 // In the production build, this file is replaced with an actual service worker
 // file that will precache your site's local assets.
 // See https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-
-self.addEventListener('install', () => self.skipWaiting())
-
+const v = 1
+self.addEventListener('install', () => {})
+self.addEventListener('message', event => {
+  console.log('service worker self message', { event })
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
 self.addEventListener('activate', () => {
-  self.clients.matchAll({ type: 'window' }).then(windowClients => {
-    for (let windowClient of windowClients) {
-      // Force open pages to refresh, so that they have a chance to load the
-      // fresh navigation response from the local dev server.
-      windowClient.navigate(windowClient.url)
-    }
-  })
+  // self.clients.matchAll({ type: 'window' }).then(windowClients => {
+  //   for (let windowClient of windowClients) {
+  //     // Force open pages to refresh, so that they have a chance to load the
+  //     // fresh navigation response from the local dev server.
+  //     windowClient.navigate(windowClient.url)
+  //   }
+  // })
 })
 self.addEventListener('fetch', () => {})

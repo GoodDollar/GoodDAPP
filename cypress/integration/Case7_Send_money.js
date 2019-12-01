8bd789c393a9fdfb91f939166d9c53170f5ef39d
Hadar Rottenberg <hadar@gooddollar.org>
2019-12-01 17:53:11 +0200

    add: optimize case 7 no waits

--- cypress/integration/Case7_Send_money.js
+++ cypress/integration/Case7_Send_money.js
@@ -14,7 +14,6 @@ describe('Test case 7: Ability to send money', () => {
     LoginPage.mnemonicsInput.type(Cypress.env('mainAccountMnemonics'))
     LoginPage.recoverWalletButton.click()
     LoginPage.yayButton.click()
-    cy.wait(7000)
     HomePage.sendButton.click()
     SendMoneyPage.nameInput.type('another person')
     SendMoneyPage.nextButton.click()
@@ -22,19 +21,15 @@ describe('Test case 7: Ability to send money', () => {
     SendMoneyPage.nextButton.click()
     SendMoneyPage.messageInput.type('test message')
     SendMoneyPage.nextButton.click()
-    cy.wait(7000)
     SendMoneyPage.confirmButton.click()
-    cy.wait(4000)
     SendMoneyPage.copyLinkButton.click()
-    cy.wait(3000)
     SendMoneyPage.doneButton.should('be.visible')
     SendMoneyPage.doneButton.invoke('attr', 'data-url').then(sendMoneyUrl => {
       var moneyLink = sendMoneyUrl
       var pattern = /(?:http[s]?:\/\/)[^\s[",><]*/gim
       var validMoneyLnk = moneyLink.match(pattern)
-      cy.wait(3000)
+
       //  SendMoneyPage.doneButton.click();
-      cy.wait(4000)
       cy.clearLocalStorage()
       cy.clearCookies()
       StartPage.open()
@@ -47,17 +42,11 @@ describe('Test case 7: Ability to send money', () => {
       LoginPage.yayButton.click()
       HomePage.claimButton.should('be.visible')
       HomePage.moneyAmountDiv.invoke('text').then(moneyBefore => {
-        cy.contains('Claim').should('be.visible')
         cy.log('Money before sending: ' + moneyBefore)
         cy.visit(validMoneyLnk.toString())
-        cy.wait(20000)
-        cy.visit(Cypress.env('baseUrl') + '/AppNavigation/Dashboard/Home')
-        cy.wait(7000)
-        HomePage.moneyAmountDiv.invoke('text').then(moneyAfter => {
-          cy.wait(3000)
-          cy.log('Money after sending: ' + moneyAfter)
-          expect(Math.round(10 * (Number(moneyBefore) + 0.01))).to.be.equal(Math.round(10 * Number(moneyAfter)))
-        })
+        //wait for blockchain payment
+        cy.contains('Claim').should('be.visible')
+        HomePage.moneyAmountDiv.invoke('text').should('eq', (+moneyBefore + 0.01).toFixed(2))
       })
     })
   })

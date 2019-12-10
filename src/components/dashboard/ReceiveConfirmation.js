f4944a27b32b499306202dd2e7806bc8907d5e5d
StanislavShevchenko <stas246978@gmail.com>
2019-12-10 11:50:09 +0200

    remove unused send code

--- src/components/dashboard/ReceiveConfirmation.js
+++ src/components/dashboard/ReceiveConfirmation.js
@@ -3,12 +3,7 @@ import React, { useMemo } from 'react'
 import { isMobile } from 'mobile-device-detect'
 import { fireEvent } from '../../lib/analytics/analytics'
 import GDStore from '../../lib/undux/GDStore'
-import {
-  generateReceiveShareObject,
-  generateReceiveShareText,
-  generateSendShareObject,
-  generateSendShareText,
-} from '../../lib/share'
+import { generateReceiveShareObject, generateReceiveShareText } from '../../lib/share'
 import BigGoodDollar from '../common/view/BigGoodDollar'
 import QRCode from '../common/view/QRCode'
 import Section from '../common/layout/Section'
@@ -18,7 +13,7 @@ import TopBar from '../common/view/TopBar'
 
 import { useScreenState } from '../appNavigation/stackNavigation'
 import { withStyles } from '../../lib/styles'
-import { ACTION_RECEIVE, navigationOptions } from './utils/sendReceiveFlow'
+import { navigationOptions } from './utils/sendReceiveFlow'
 
 export type ReceiveProps = {
   screenProps: any,
@@ -30,22 +25,13 @@ const ReceiveConfirmation = ({ screenProps, styles, ...props }: ReceiveProps) =>
   const profile = GDStore.useStore().get('profile')
   const [screenState] = useScreenState(screenProps)
   const { amount, code, reason, counterPartyDisplayName } = screenState
-  const { params } = props.navigation.state
-
   const share = useMemo(() => {
     if (isMobile && navigator.share) {
-      return params.action === ACTION_RECEIVE
-        ? generateReceiveShareObject(code, amount, counterPartyDisplayName, profile.fullName)
-        : generateSendShareObject(code, amount, counterPartyDisplayName, profile.fullName)
+      return generateReceiveShareObject(code, amount, counterPartyDisplayName, profile.fullName)
     }
-
-    return params.action === ACTION_RECEIVE
-      ? {
-          url: generateReceiveShareText(code, amount, counterPartyDisplayName, profile.fullName),
-        }
-      : {
-          url: generateSendShareText(code, amount, counterPartyDisplayName, profile.fullName),
-        }
+    return {
+      url: generateReceiveShareText(code, amount, counterPartyDisplayName, profile.fullName),
+    }
   }, [code])
 
   return (
@@ -56,10 +42,10 @@ const ReceiveConfirmation = ({ screenProps, styles, ...props }: ReceiveProps) =>
           <QRCode value={code} />
         </Section.Stack>
         <Section.Stack grow justifyContent="center" alignItems="center">
-          <Section.Text style={styles.textRow}>{ACTION_RECEIVE ? 'Request exactly' : 'Send exactly'}</Section.Text>
+          <Section.Text style={styles.textRow}>{'Request exactly'}</Section.Text>
           {counterPartyDisplayName && (
             <Section.Text style={styles.textRow}>
-              {ACTION_RECEIVE ? 'From: ' : 'To: '}
+              {'From: '}
               <Section.Text fontSize={18}>{counterPartyDisplayName}</Section.Text>
             </Section.Text>
           )}

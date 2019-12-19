f1a9705a0f892f392f3b269d95899c7f78ec248a
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2019-12-19 17:22:23 +0200

    add: Implement the ReceiveConfirmation screen according new design

--- src/components/dashboard/ReceiveConfirmation.js
+++ src/components/dashboard/ReceiveConfirmation.js
@@ -40,28 +40,22 @@ const ReceiveConfirmation = ({ screenProps, styles, ...props }: ReceiveProps) =>
 
   return (
     <Wrapper>
-      <TopBar push={screenProps.push} hideBalance />
+      <TopBar push={screenProps.push} />
       <Section justifyContent="space-between" grow>
         <Section.Stack style={styles.qrCode}>
           <QRCode value={urlForQR} />
         </Section.Stack>
         <Section.Stack grow justifyContent="center" alignItems="center">
           <Section.Text style={styles.textRow}>{'Request exactly'}</Section.Text>
-          {counterPartyDisplayName && (
-            <Section.Text style={styles.textRow}>
-              {'From: '}
-              <Section.Text fontSize={18}>{counterPartyDisplayName}</Section.Text>
-            </Section.Text>
-          )}
           {amount && (
             <BigGoodDollar
               number={amount}
-              color="primary"
-              bigNumberProps={{ fontSize: 24 }}
+              color="green"
+              bigNumberProps={{ fontSize: 36, lineHeight: 24, fontWeight: 'bold', fontFamily: 'Roboto Slab' }}
               bigNumberUnitProps={{ fontSize: 14 }}
             />
           )}
-          <Section.Text style={styles.textRow}>{reason}</Section.Text>
+          {!!reason && <Section.Text style={styles.textRow}>For {reason}</Section.Text>}
         </Section.Stack>
         <Section.Stack>
           <ShareButton

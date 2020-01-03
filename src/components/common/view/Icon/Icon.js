3dba1de4733fead4722db5be0f90572158d4d624
Mathias Morales <mathias@Mac-Mini.local>
2020-01-03 10:20:07 -0300

    minimal adjust

--- src/components/common/view/Icon/Icon.js
+++ src/components/common/view/Icon/Icon.js
@@ -1,22 +1,22 @@
 // @flow
 import React from 'react'
-// import { withTheme } from 'react-native-paper'
-// import { createIconSetFromFontello } from 'react-native-vector-icons'
-// import fontelloConfig from './config.json'
-// // import './index.css'
-//
-// const Icon = createIconSetFromFontello(fontelloConfig)
-//
-// type IconProps = {
-//   name: string,
-//   color?: string,
-//   size?: number,
-//   style?: {},
-//   theme: Object,
-// }
-//
-// export default withTheme(({ theme, color, size, ...props }: IconProps) => (
-//   <Icon size={size || 16} color={theme.colors[color] || color || theme.colors.primary} {...props} />
-// ))
+import { withTheme } from 'react-native-paper'
+import { createIconSetFromFontello } from 'react-native-vector-icons'
+import fontelloConfig from './config.json'
+// import './index.css'
 
-export default () => null
+const Icon = createIconSetFromFontello(fontelloConfig)
+
+type IconProps = {
+  name: string,
+  color?: string,
+  size?: number,
+  style?: {},
+  theme: Object,
+}
+
+export default withTheme(({ theme, color, size, ...props }: IconProps) => (
+  <Icon size={size || 16} color={theme.colors[color] || color || theme.colors.primary} {...props} />
+))
+
+// export default () => null

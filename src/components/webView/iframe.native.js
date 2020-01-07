8049b247caa652ef00fdab5335121b6e1a5d8231
Mathias Morales <mathias@Mac-Mini.local>
2020-01-07 15:26:10 -0300

    webview installed

---
+++ src/components/webView/iframe.native.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import SimpleStore from '../../lib/undux/SimpleStore'
+import { getMaxDeviceHeight } from '../../lib/utils/Orientation'
+import { WebView } from 'react-native-webview'
+
+const wHeight = getMaxDeviceHeight()
+
+export const createIframe = (src, title) => {
+  const IframeTab = props => {
+    const store = SimpleStore.useStore()
+
+    const isLoaded = () => {
+      store.set('loadingIndicator')({ loading: false })
+    }
+
+    useEffect(() => {
+      store.set('loadingIndicator')({ loading: true })
+    }, [])
+
+    //this is for our external pages like privacy policy, etc.. they dont require iframeresizer to work ok on ios <13
+    return (
+      <WebView
+        title={title}
+        seamless
+        frameBorder="0"
+        onLoad={isLoaded}
+        src={src}
+        width="100%"
+        height="100%"
+        style={{ height: wHeight }}
+      />
+    )
+  }
+  IframeTab.navigationOptions = {
+    title,
+  }
+  return IframeTab
+}

598bc9f006949bae959d8b6a8e44d172a4201256
Nicolás Castellanos <imnicastelo@gmail.com>
2020-01-08 00:19:58 -0300

    clean unnecesary changes

---
+++ config/initTest.js
@@ -0,0 +1,17 @@
+import initGunDB from '../src/lib/gundb/gundb'
+let webcrypto = require ('node-webcrypto-ossl')
+let crypto = new webcrypto()
+const { TextEncoder, TextDecoder } = require('text-encoding', 1)
+
+if (typeof window !== 'undefined') {
+    if (typeof HTMLCanvasElement !== 'undefined') {
+    // taken from https://stackoverflow.com/questions/48828759/jest-and-jsdom-error-with-canvas
+      HTMLCanvasElement.prototype.getContext = () => {
+        // return whatever getContext has to return
+      };
+    }
+    window.matchMedia = () => ({ matches: true });
+    window.crypto = crypto
+    window.TextDecoder = TextDecoder
+    window.TextEncoder = TextEncoder
+}

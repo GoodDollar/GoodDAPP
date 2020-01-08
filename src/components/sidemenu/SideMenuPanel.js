6a08a0c048cfa5956bd52f8855e1397df3059eb9
StanislavShevchenko <stas246978@gmail.com>
2020-01-08 11:59:05 +0200

    refactor: remove custom size, update icons

--- src/components/sidemenu/SideMenuPanel.js
+++ src/components/sidemenu/SideMenuPanel.js
@@ -92,7 +92,6 @@ const getMenuItems = ({ API, hideSidemenu, showDialog, navigation, store, theme 
         icon: 'add',
         name: 'Add App Icon',
         hidden: !installPrompt && !isMobileSafari,
-        size: 18,
         action: () => {
           addAppIcon({ API, showDialog, store, theme })
           hideSidemenu()
@@ -153,7 +152,6 @@ const getMenuItems = ({ API, hideSidemenu, showDialog, navigation, store, theme 
       {
         icon: 'privacy-policy',
         name: 'Privacy Policy',
-        size: 18,
         action: () => {
           navigation.navigate('PP')
           hideSidemenu()

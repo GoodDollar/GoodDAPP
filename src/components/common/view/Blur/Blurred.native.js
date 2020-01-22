a99a6c9b7965f16fcd593a0362b6ef01fcf9d840
Nicolás Castellanos <imnicastelo@gmail.com>
2020-01-22 08:06:44 -0300

    Enable blur on Android

--- src/components/common/view/Blur/Blurred.native.js
+++ src/components/common/view/Blur/Blurred.native.js
@@ -1,17 +1,41 @@
-import React, { useRef } from 'react'
-// eslint-disable-next-line import/named
+import React, { useRef, useState, useEffect } from 'react'
 import { BlurView } from '@react-native-community/blur'
-import { View } from 'react-native'
+import { View, findNodeHandle, StyleSheet } from 'react-native'
 
 const Blurred = props => {
-  const viewRef = useRef({})
+  const viewRef = useRef()
+  const [viewRefNode, setViewRefNode] = useState()
+
+  useEffect(() => {
+    const node = findNodeHandle(viewRef.current)
+    setViewRefNode(node)
+  }, [])
+
   return (
-    <>
-      <View ref={viewRef.current} style={props.style}>
+    <View style={props.style}>
+      <View ref={viewRef} style={[styles.container, styles.fullView]}>
         {props.children}
+        {props.blur && <View style={[styles.fullView, styles.opacityView]} />}
       </View>
-      {props.blur && <BlurView style={props.style} viewRef={viewRef.current} blurType="dark" blurAmount={10} />}
-    </>
+      {props.blur && <BlurView style={styles.fullView} viewRef={viewRefNode} blurType="light" blurAmount={32} />}
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  opacityView: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  container: {
+    backgroundColor: '#fff',
+  },
+  fullView: {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: 'absolute',
+  },
+})
+
 export default Blurred

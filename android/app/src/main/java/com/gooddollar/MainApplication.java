792d15e268022c81c65b3b9ef934839aace022df
alexeykosinski <alexey.kosinski@nordwhale.com>
2020-01-29 17:56:12 +0200

    fix android build

--- android/app/src/main/java/com/gooddollar/MainApplication.java
+++ android/app/src/main/java/com/gooddollar/MainApplication.java
@@ -4,13 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.airbnb.android.react.lottie.LottiePackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =

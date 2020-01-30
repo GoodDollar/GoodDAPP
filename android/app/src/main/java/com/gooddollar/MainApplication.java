a7dc350268d22a065c68a1856e80c4c1b2719c6c
alexeykosinski <alexey.kosinski@nordwhale.com>
2020-01-30 17:35:52 +0200

    run ios latest lottie

--- android/app/src/main/java/com/gooddollar/MainApplication.java
+++ android/app/src/main/java/com/gooddollar/MainApplication.java
@@ -1,14 +1,13 @@
 package com.gooddollar;
-
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.lottie.LottiePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
@@ -24,9 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-           packages.add(new LottiePackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new LottiePackage());
           return packages;
         }
 

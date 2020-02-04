fdcac998123ce5146a544ca8fa8a1950e8abd31e
Nicolás Castellanos <imnicastelo@gmail.com>
2020-02-04 09:01:35 -0300

    add bugsnag for native app

--- native.js
+++ native.js
@@ -1,9 +1,17 @@
 import { AppRegistry } from 'react-native'
 import React from  'react'
+import { Client, Configuration } from 'bugsnag-react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 import 'react-native-gesture-handler'
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Config from './src/config/config'
+
+if (Config.bugsnagKey) {
+  const config = new Configuration(Config.bugsnagKey)
+  config.appVersion = require('./package.json').version
+  new Client(config)
+}
 
 const DeApp  = () => (
   <SafeAreaProvider>

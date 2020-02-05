89c8733082718f670a3a33f556cd825756619384
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-02-05 17:43:22 -0300

    Adjust PhoneInputs for web

--- src/components/common/form/EditPhoneInput/EditPhoneInput.web.js
+++ src/components/common/form/EditPhoneInput/EditPhoneInput.web.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import PhoneInput from 'react-phone-number-input'
 import './PhoneInput.css'
+import { View } from 'react-native'
 
 const EditPhoneInput = props => {
-  return <PhoneInput {...props} />
+  const hasWrapper = props.nativeID ? (
+    <View nativeID={props.nativeID}>
+      <PhoneInput {...props} />
+    </View>
+  ) : (
+    <PhoneInput {...props} />
+  )
+  return hasWrapper
 }
 
 export default EditPhoneInput
--- src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
+++ src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
@@ -5,5 +5,5 @@ import EditPhoneInput from '../../common/form/EditPhoneInput/EditPhoneInput'
 import './PhoneNumberInput.css'
 
 export default props => {
-  return <EditPhoneInput {...props} />
+  return <EditPhoneInput nativeID="SignUpInput" {...props} />
 }

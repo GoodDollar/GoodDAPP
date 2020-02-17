110909e06d01c3978cf6c58896047327ee65cb82
Nicolás Castellanos <imnicastelo@gmail.com>
2020-02-17 20:14:51 -0300

    add mising handlers

--- src/components/profile/PhoneInputProfile/PhoneInputProfile.web.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import EditPhoneInput from '../../common/form/EditPhoneInput/EditPhoneInput'
-import './PhoneInput.css'
-
-export default props => {
-  return <EditPhoneInput id="profile_input" {...props} />
-}

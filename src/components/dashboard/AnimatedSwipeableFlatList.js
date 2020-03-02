0ba35c43cc0d230a839511c9947816c7588a7727
Nicolas Castellanos <imnicastelo@gmail.com>
2020-03-02 01:17:37 -0300

    Update AnimatedSwipeableFlatList.js

--- src/components/dashboard/AnimatedSwipeableFlatList.js
+++ src/components/dashboard/AnimatedSwipeableFlatList.js
@@ -1,3 +1,8 @@
+/**
+ * Creating Animated component in a separate file because of errors with
+ * Animated.createAnimatedComponent and Hot Module Replacement while reloading.
+ */
+
 import { SwipeableFlatList } from 'react-native-swipeable-lists'
 import { Animated } from 'react-native'
 

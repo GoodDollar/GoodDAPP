ac00640ac603cc23af96c5699a90a4df812ef00e
alexeykosinski <alexey.kosinski@nordwhale.com>
2020-03-04 15:50:58 +0200

    fix

--- src/components/common/animations/JumpingPeople/index.js
+++ src/components/common/animations/JumpingPeople/index.js
@@ -1,26 +1,44 @@
 import React from 'react'
 import Lottie from 'lottie-react-native'
 import { Platform, View } from 'react-native'
+import { getScreenHeight, getScreenWidth } from '../../../../lib/utils/Orientation'
 
 import animationData from './data.json'
 
+const getMarginTop = () => {
+  const width = getScreenWidth()
+  if (width < 600) {
+    return -width / 1.3
+  }
+  return -width / 2.5
+}
+
 const styles = {
   android: {
+    position: 'absolute',
     width: '100%',
+    marginTop: -getScreenHeight() / 25,
   },
   ios: {
+    position: 'absolute',
     width: '100%',
+    marginTop: -getScreenHeight() / 25,
   },
   web: {
     width: '100%',
+    position: 'absolute',
+    marginTop: getMarginTop(),
   },
 }
 const stylesBlock = {
   android: {
     height: 300,
   },
-  ios: {},
+  ios: {
+    height: 300,
+  },
   web: {
+    width: '100%',
     height: 100,
   },
 }

5e7fe194145aff5495a96cd7ca463d7f71390ed0
alexeykosinski <alexey.kosinski@nordwhale.com>
2020-03-05 16:45:33 +0200

    hot fix splash

--- src/components/common/animations/Logo/index.js
+++ src/components/common/animations/Logo/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import Lottie from 'lottie-react-native'
 import { isMobile } from 'mobile-device-detect'
 import { getScreenHeight, getScreenWidth } from '../../../../lib/utils/Orientation'
-import { getAnimationData } from '../../../../lib/utils/lottie'
-const { animationData, imageAssetsFolder } = getAnimationData('Logo', require('./data'))
+import animationData from './data.json'
 
 class Logo extends React.Component {
   render() {
     return (
       <Lottie
-        imageAssetsFolder={imageAssetsFolder}
         enableMergePathsAndroidForKitKatAndAbove={true}
         autoPlay={true}
         source={animationData}

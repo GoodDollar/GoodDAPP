0d1d48bf04380b184e0a470ec746c9bfb1f1bffb
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-03-10 12:45:39 -0300

    adjust android permissions

--- android/app/src/main/java/com/gooddollar/MainApplication.java
+++ android/app/src/main/java/com/gooddollar/MainApplication.java
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           packages.add(new LottiePackage());
+          packages.add(new ReactNativeContacts());
           return packages;
         }
 
@@ -46,13 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
-  }
-
-  @Override
-  protected List<ReactPackage> getPackages() {
-    return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new ReactNativeContacts());
   }
 
   /**

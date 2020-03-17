c4f195b62e7527664466242226cb6b006e8354cb
Nicolas Castellanos <imnicastelo@gmail.com>
2020-03-17 03:14:43 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      'module:metro-react-native-babel-preset', 'module:react-native-dotenv'
-    ],
+    presets: [['module:metro-react-native-babel-preset']],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],

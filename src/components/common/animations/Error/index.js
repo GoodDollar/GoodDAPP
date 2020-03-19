bdba00514f37352d5ff14d89ed1fbc89678d84e1
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-03-19 11:10:25 +0200

    fix: fix error animation data.json and title text for ios devices. fix styles for different platforms

--- src/components/common/animations/Error/index.js
+++ src/components/common/animations/Error/index.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 import Lottie from 'lottie-react-native'
 import { getAnimationData } from '../../../../lib/utils/lottie'
 const { animationData, imageAssetsFolder } = getAnimationData('Error', require('./data'))
 
+const styles = {
+  android: {
+    flex: 1,
+    width: 200,
+    paddingVertical: 50,
+    marginHorizontal: 'auto',
+    marginBottom: 10,
+  },
+  ios: {
+    flex: 1,
+    width: '100%',
+    paddingVertical: 50,
+    marginHorizontal: 'auto',
+    marginBottom: 10,
+  },
+  web: {
+    width: 200,
+    paddingVertical: 20,
+    marginHorizontal: 'auto',
+  },
+}
+
 class Error extends React.Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          width: 200,
-          paddingVertical: 20,
-          marginHorizontal: 'auto',
-        }}
-      >
+      <View style={Platform.select(styles)}>
         <Lottie
           imageAssetsFolder={imageAssetsFolder}
           enableMergePathsAndroidForKitKatAndAbove={true}

5b3ec46319edfaeb88b502bd3231a95d74144c73
Hadar Rottenberg <hadar@gooddollar.org>
2020-03-22 21:58:12 +0200

    fix: tabsview test

--- src/components/appNavigation/__tests__/TabsView.js
+++ src/components/appNavigation/__tests__/TabsView.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer'
 import TabsView from '../TabsView'
 import Dashboard from '../../dashboard/Dashboard'
 import Profile from '../../profile/Profile'
+import { StoresWrapper } from '../../../lib/undux/utils/storeswrapper.js'
 
 // Note: test renderer must be required after react-native.
 
@@ -17,12 +18,20 @@ describe('TabsView', () => {
   }
 
   it('renders without errors', () => {
-    const tree = renderer.create(<TabsView routes={routes} />)
+    const tree = renderer.create(
+      <StoresWrapper>
+        <TabsView routes={routes} />
+      </StoresWrapper>
+    )
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const component = renderer.create(<TabsView routes={routes} />)
+    const component = renderer.create(
+      <StoresWrapper>
+        <TabsView routes={routes} />
+      </StoresWrapper>
+    )
     expect(component.toJSON()).toMatchSnapshot()
   })
 })

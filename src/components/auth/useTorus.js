fc7a429059c09d5d4c3991757aa4b0ff6409f36a
Hadar Rottenberg <hadar@gooddollar.org>
2020-03-24 13:55:57 +0200

    fix: typo

--- src/components/auth/useTorus.js
+++ src/components/auth/useTorus.js
@@ -5,24 +5,24 @@ import logger from '../../lib/logger/pino-logger'
 
 const log = logger.child({ from: 'AuthTorus' })
 
-export const torusGoogle = new TorusSdk({
-  typeOfLogin: 'google',
-  verifier: 'google-gooddollar',
-  GOOGLE_CLIENT_ID: config.googleClientId,
+const sdkOptions = {
   proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
   network: 'ropsten', // details for test net
   redirect_uri: `${config.publicUrl}/torus/redirect`,
   enableLogging: config.env === 'development',
+}
+export const torusGoogle = new TorusSdk({
+  typeOfLogin: 'google',
+  verifier: 'google-gooddollar',
+  GOOGLE_CLIENT_ID: config.googleClientId,
+  ...sdkOptions,
 })
 
 export const torusFacebook = new TorusSdk({
   typeOfLogin: 'facebook',
   verifier: 'facebook-gooddollar',
   FACEBOOK_APP_ID: config.facebookAppId,
-  proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
-  network: 'ropsten', // details for test net
-  redirect_uri: `${config.publicUrl}/torus/redirect`,
-  enableLogging: config.env === 'development',
+  ...sdkOptions,
 })
 
 export const useTorusServiceWorker = () => {
@@ -30,7 +30,7 @@ export const useTorusServiceWorker = () => {
 
   const registerTorusWorker = async () => {
     try {
-      const registration = await navigator.serviceWorker.register(`${config.publicUrl}/torus//sw.js`)
+      const registration = await navigator.serviceWorker.register(`${config.publicUrl}/torus/sw.js`)
       log.debug('torus service worker registerd', { registration })
       setServiceWorker(registration)
     } catch (e) {

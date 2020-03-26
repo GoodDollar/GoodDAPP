a54d36da89207d087da7d643c0d0016db9039858
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-03-26 15:53:05 +0200

    fix: fix ServiceWorker folder svg files to look god on native apps and web

--- src/components/dashboard/ServiceWorkerUpdatedDialog.js
+++ src/components/dashboard/ServiceWorkerUpdatedDialog.js
@@ -7,7 +7,7 @@ import UpdateVersionSVG from '../../assets/updateversion.svg'
 
 const dialogStyles = ({ theme }) => ({
   image: {
-    textAlign: 'center',
+    alignItems: 'center',
     height: getDesignRelativeHeight(134),
     width: getDesignRelativeHeight(191),
     marginBottom: getDesignRelativeHeight(25),
@@ -16,7 +16,7 @@ const dialogStyles = ({ theme }) => ({
     paddingTop: getDesignRelativeHeight(32),
     paddingHorizontal: getDesignRelativeWidth(5),
     display: 'flex',
-    alignItems: ' center',
+    alignItems: 'center',
   },
   title: {
     width: '100%',

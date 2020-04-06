fa5f67ae9ed9a06bfa8302c6c92dacb8bf44b748
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-04-06 19:05:05 +0300

    More comments in useZoomSDK  inline, explaining methods and explaining inside methods what is happening

--- src/components/dashboard/FaceVerification/hooks/useZoomSDK.js
+++ src/components/dashboard/FaceVerification/hooks/useZoomSDK.js
@@ -5,65 +5,131 @@ import Config from '../../../../config/config'
 import ZoomAuthentication from '../../../../lib/zoom/ZoomAuthentication'
 import useMountedState from '../../../../lib/hooks/useMountedState'
 
+// Zoom SDK reference
 const sdk = ZoomAuthentication.ZoomSDK
 
-export const { ZoomSDKStatus, getFriendlyDescriptionForZoomSDKStatus } = sdk
+export const {
+  // SDK initialization status codes enum
+  ZoomSDKStatus,
 
+  // Helper function, returns full description
+  // for SDK initialization status specified
+  getFriendlyDescriptionForZoomSDKStatus,
+} = sdk
+
+/**
+ * ZoomSDK initialization hook
+ *
+ * @param {object} config Configuration
+ * @property {() => void} config.onInitialized - SDK initialized callback
+ * @property {() => void} config.onError - SDK error callback
+ *
+ * @return {object} result Public hook API
+ * @property {typeof ZoomSDK} result.sdk - ZoomSDK reference
+ * @property {boolean} result.isInitialized - Initialization state flag
+ * @property {Error | null} result.initError - Initialization error (if happened)
+ */
 export default ({ onInitialized = noop, onError = noop }) => {
+  // Initialization state flag
   const [isInitialized, setInitialized] = useState(false)
-  const [initError, setInitError] = useState(null)
-  const mountedStateRef = useMountedState()
-  const onInitializedRef = useRef(onInitialized)
-  const onErrorRef = useRef(onError)
 
+  // Initialization error state variable
+  const [initError, setInitError] = useState(null)
+
+  // Component mounted flag ref, it needs to check
+  // is component still mounted before update it's state
+  // This check is needed as onInitialized/onError could
+  // perform redirects which unmounts component
+  const mountedStateRef = useMountedState()
+
+  // Configuration callbacks refs
+  const onInitializedRef = useRef(null)
+  const onErrorRef = useRef(null)
+
+  // updating callbacks references on config changes
   useEffect(() => {
+    onInitializedRef.current = onInitialized
+    onErrorRef.current = onError
+  }, [onInitialized, onError])
+
+  // performing initialization attempt on component mounted
+  // this callback should be ran once, so we're using refs
+  // to access actual initialization / error callbacks
+  useEffect(() => {
+    // current ZoomSDK initialization status code
     let sdkStatus = sdk.getStatus()
 
+    // Helper for handle exceptions
     const handleException = exception => {
+      // executing current onError callback
       onErrorRef.current(exception)
 
+      // if component is still mounted -
       if (mountedStateRef.current) {
+        // updating initError state variable
         setInitError(exception)
       }
     }
 
+    // Helper for handle SDK status code changes
     const handleSdkStatus = () => {
+      // Checking is ZoomSDK successfullt initialized
       const sdkInitialized = ZoomSDKStatus.Initialized === sdkStatus
 
+      // if not initialized - throwing an Error
       if (!sdkInitialized) {
+        // retrieving full description from status code
         const exception = new Error(getFriendlyDescriptionForZoomSDKStatus(sdkStatus))
 
+        // adding status code as error's object property
         exception.code = sdkStatus
+
+        // handling an error
         handleException(exception)
         return
       }
 
+      // otherwise - executing onInitialized callback
       onInitializedRef.current()
-      setInitialized(sdkInitialized)
+
+      // and updating isInitialized state flag
+      // (if component is still mounted)
+      if (mountedStateRef.current) {
+        setInitialized(sdkInitialized)
+      }
     }
 
+    // checking the first retrieved status code
+    // if we already tried to initialize SDK,
+    // then just handling last attempt status
     if (ZoomSDKStatus.NeverInitialized !== sdkStatus) {
       handleSdkStatus()
       return
     }
 
-    // Set a the directory path for other ZoOm Resources.
+    // otherwise, performing initialization:
+
+    // 1. setting a the directory path for other ZoOm Resources.
     sdk.setResourceDirectory('/zoom/resources')
 
-    // Set the directory path for required ZoOm images.
+    // 2. setting the directory path for required ZoOm images.
     sdk.setImagesDirectory('/zoom/images')
 
     try {
-      // Initialize ZoOm and configure the UI features.
+      // 3. initializing ZoOm and configuring the UI features.
       sdk.initialize(Config.zoomLicenseKey, () => {
+        // updating & handling ZoomSDK status code
         sdkStatus = sdk.getStatus()
         handleSdkStatus()
       })
     } catch (exception) {
+      // handling initialization exceptions
+      // (some of them could be thrown during initialize() call)
       handleException(exception)
     }
   }, [])
 
+  // exposing public hook API
   return {
     sdk,
     isInitialized,
--- src/components/dashboard/FaceVerification/index.web.js
+++ src/components/dashboard/FaceVerification/index.web.js
@@ -4,6 +4,8 @@ import { pick } from 'lodash'
 import useZoomSDK from './hooks/useZoomSDK'
 import useZoomVerification, { ZoomSessionStatus } from './hooks/useZoomVerification'
 
+// All Zoom session result codes could be thrown if the
+// camera isn't available or no camera access was confirmed
 const cameraIssuesStatusCodes = Object.values(
   pick(
     ZoomSessionStatus,
@@ -31,6 +33,8 @@ const cameraIssuesStatusCodes = Object.values(
 )
 
 const FaceVerification = ({ screenProps }) => {
+  // Redirects to the error screen, passing exception
+  // object and allowing to show/hide retry button (hides it by default)
   const showErrorScreen = useCallback(
     (error, allowRetry = false) => {
       screenProps.navigateTo('FaceVerificationError', { error, allowRetry })
@@ -38,28 +42,45 @@ const FaceVerification = ({ screenProps }) => {
     [screenProps]
   )
 
+  /**
+   * ZoomSDK session completition handler
+   * @param {boolean} isSuccess
+   * @param {ZoomSDK.ZoomSessionResult} Session result object
+   * @param {string} lastMessage Full description of the last
+   * session status or success/error message got from server
+   */
   const completionHandler = useCallback(
     (isSuccess, { status }, lastMessage) => {
+      // preparing error object
       const exception = new Error(lastMessage)
 
+      // if session was successfull - returning sucess to the caller
       if (isSuccess) {
         screenProps.navigateTo('Home')
         return
       }
 
+      // if error was caused by the camera issues - marking
+      // error as 'NotAllowedError'. It's needed for ErrorScreen
+      // component could display that something is wrong with the camera
       if (cameraIssuesStatusCodes.includes(status)) {
         exception.name = 'NotAllowedError'
       }
 
+      // handling error
       showErrorScreen(exception, true)
     },
     [screenProps, showErrorScreen]
   )
 
+  // Using zoom verification hook, passing completion callback
   const { startVerification } = useZoomVerification({
     onComplete: completionHandler,
   })
 
+  // using zoom sdk initialization hook
+  // starting verification once sdk sucessfully initializes
+  // on error redirecting to the error screen
   useZoomSDK({
     onInitialized: startVerification,
     onError: showErrorScreen,

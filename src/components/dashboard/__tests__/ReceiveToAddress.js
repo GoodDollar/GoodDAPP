2a5462abfbe59004b0b2fd24ca5f556f8a7db9ae
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-04-16 09:54:28 +0300

    fix: fix tests

--- src/components/dashboard/__tests__/ReceiveToAddress.js
+++ src/components/dashboard/__tests__/ReceiveToAddress.js
@@ -8,14 +8,14 @@ import { getWebRouterComponentWithMocks } from './__util__'
 const address = '0x0000000000000000000000000000000000000000'
 
 describe('ReceiveToAddress', () => {
+  const ReceiveToAddress = getWebRouterComponentWithMocks('../ReceiveToAddress', { address })
+
   it('renders without errors', () => {
-    const ReceiveToAddress = getWebRouterComponentWithMocks('../ReceiveToAddress')
     const tree = renderer.create(<ReceiveToAddress address={address} />)
     expect(tree.toJSON()).toBeTruthy()
   })
 
   it('matches snapshot', () => {
-    const ReceiveToAddress = getWebRouterComponentWithMocks('../ReceiveToAddress')
     const component = renderer.create(<ReceiveToAddress address={address} />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()

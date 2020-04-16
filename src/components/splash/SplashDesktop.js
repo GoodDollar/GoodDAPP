0bef20995d7d3bd5b9530d130e2504c78266ab9a
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-04-16 15:54:01 +0300

    fix: fix splash background wave pattern rendering

--- src/components/splash/SplashDesktop.js
+++ src/components/splash/SplashDesktop.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, ImageBackground, StyleSheet } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 import goodDollarImage from '../../assets/Splash/goodDollar.svg'
 import wavePattern from '../../assets/splashWaves.svg'
 import { getDesignRelativeHeight } from '../../lib/utils/sizes'
@@ -15,26 +15,20 @@ Image.prefetch(wavePattern)
 const SplashDesktop = ({ onContinue, urlForQR }) => (
   <Wrapper style={styles.wrapper}>
     <Section style={styles.container}>
-      <ImageBackground
-        source={wavePattern}
-        imageStyle={styles.wavesImage}
-        style={styles.backgroundWaves}
-        resizeMode="repeat"
-      >
-        <Section.Stack style={styles.content} grow justifyContent="space-between">
-          <Section.Text fontSize={22} color="darkBlue">
-            <Section.Text fontSize={22} fontWeight="black" color="darkBlue">
-              {'Welcome to GoodDollar Wallet\n'}
-            </Section.Text>
-            {'For best experience\nplease scan and continue\non your mobile device.'}
+      <View style={styles.waves} />
+      <Section.Stack style={styles.content} grow justifyContent="space-between">
+        <Section.Text fontSize={22} color="darkBlue">
+          <Section.Text fontSize={22} fontWeight="black" color="darkBlue">
+            {'Welcome to GoodDollar Wallet\n'}
           </Section.Text>
-          <QRCode value={urlForQR} size={150} qrStyles={styles.qrStyles} />
-          <Image source={goodDollarImage} style={styles.goodDollar} resizeMode="contain" />
-          <CustomButton mode="outlined" color="white" style={styles.buttonContinue} onPress={onContinue}>
-            Continue on Web
-          </CustomButton>
-        </Section.Stack>
-      </ImageBackground>
+          {'For best experience\nplease scan and continue\non your mobile device.'}
+        </Section.Text>
+        <QRCode value={urlForQR} size={150} qrStyles={styles.qrStyles} />
+        <Image source={goodDollarImage} style={styles.goodDollar} resizeMode="contain" />
+        <CustomButton mode="outlined" color="white" style={styles.buttonContinue} onPress={onContinue}>
+          Continue on Web
+        </CustomButton>
+      </Section.Stack>
     </Section>
   </Wrapper>
 )
@@ -55,13 +49,12 @@ const styles = StyleSheet.create({
     transform: [{ rotateY: '180deg' }],
     flex: 1,
   },
-  backgroundWaves: {
+  waves: {
     position: 'absolute',
     width: '100%',
     height: '100%',
-    padding: 10,
-  },
-  wavesImage: {
+    backgroundImage: `url(${wavePattern})`,
+    backgroundRepeat: 'repeat-y',
     opacity: 0.1,
   },
   content: {

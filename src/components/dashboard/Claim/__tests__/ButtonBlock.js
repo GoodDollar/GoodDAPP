18ee68b956fcbc48be9af28b65037e0615bc38ea
StanislavShevchenko <stas246978@gmail.com>
2020-04-16 11:44:28 +0300

    added new tests buttonClaim and added new snapshots

---
+++ src/components/dashboard/Claim/__tests__/ButtonBlock.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { withThemeProvider } from '../../../../__tests__/__util__'
+import ButtonBlock from '../ButtonBlock'
+
+const styles = {
+  stylesmainContainer: null,
+  btnBlock: null,
+}
+
+describe('ButtonBlock', () => {
+  const WrappedClaimButton = withThemeProvider(ButtonBlock)
+
+  it('renders without errors', () => {
+    const tree = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it('matches snapshot for non-citizen', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={false}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('matches snapshot for citizen without entitlement', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('matches snapshot for citizen with entitlement', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        entitlement={100}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/components/dashboard/Claim/__tests__/PhaseOne.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { withThemeProvider } from '../../../../__tests__/__util__'
+import PhaseOne from '../PhaseOne'
+
+const claimedToday = { amount: 1, people: 1 }
+const styles = {
+  stylesmainContainer: null,
+}
+describe('PhaseOne', () => {
+  const WrappedClaimButton = withThemeProvider(PhaseOne)
+
+  it('renders without errors', () => {
+    const tree = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        claimedToday={claimedToday}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it('matches snapshot for non-citizen', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={false}
+        claimedToday={claimedToday}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('matches snapshot for citizen without entitlement', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        claimedToday={claimedToday}
+        entitlement={0}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('matches snapshot for citizen with entitlement', () => {
+    const component = renderer.create(
+      <WrappedClaimButton
+        styles={styles}
+        isCitizen={true}
+        entitlement={100}
+        claimedToday={claimedToday}
+        nextClaim="00:10:00"
+        loading={false}
+        onPress={() => null}
+      />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})

18c87d689e62f9016a73a0b0968ba5f776095c1a
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-04-23 14:08:25 +0300

    fix: fix tests

--- src/components/sidemenu/__tests__/SideMenuPanel.js
+++ src/components/sidemenu/__tests__/SideMenuPanel.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import userStorage from '../../../lib/gundb/UserStorage'
 import SideMenuPanel from '../SideMenuPanel'
 import { withThemeProvider } from '../../../__tests__/__util__'
 
 const SideMenuPanelTheme = withThemeProvider(SideMenuPanel)
 
-describe('SideMenuPanel', () => {
+describe('SideMenuPanel', async () => {
+  await userStorage.ready
+
   it('renders without errors', () => {
     const tree = renderer.create(<SideMenuPanelTheme />)
     expect(tree.toJSON()).toBeTruthy()

b0d5b2639d6dd43fc64efa181eba64657263565f
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-04-23 13:29:35 +0300

    revert: torusEnabled

--- src/config/config.js
+++ src/config/config.js
@@ -21,7 +21,7 @@ const Config = {
   network: process.env.REACT_APP_NETWORK || 'fuse',
   market: process.env.REACT_APP_MARKET === 'true' || isEToro,
   marketUrl: process.env.REACT_APP_MARKET_URL || 'https://etoro.paperclip.co',
-  torusEnabled: process.env.REACT_APP_USE_TORUS === 'true' || true,
+  torusEnabled: process.env.REACT_APP_USE_TORUS === 'true',
   enableSelfCustody: process.env.REACT_APP_ENABLE_SELF_CUSTODY === 'true',
   googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
   facebookAppId: process.env.REACT_APP_FACEBOOK_APP_ID,

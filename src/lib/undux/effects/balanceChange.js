39d1f8b8deb9e7947431eb9a55983225729c6deb
sirpy <hadar@gooddollar.org>
2020-04-24 16:57:13 +0300

    #1653 feed card custom action (#1658)
    * add: custom action to feed card
    
    * fix: handle store is null issues
    
    * fix: snapshots
    
    * PR fixes
    
    * Typo fix
    
    Co-authored-by: Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>

--- src/lib/undux/effects/balanceChange.js
+++ src/lib/undux/effects/balanceChange.js
@@ -1,14 +1,18 @@
 // @flow
 import type { Effects, Store } from 'undux'
-
+import { isNull } from 'lodash'
 import { initTransferEvents } from '../../undux/utils/account'
 import type { State } from '../GDStore'
 import logger from '../../logger/pino-logger'
 
-const log = logger.child({ from: 'undux/utils/balance' })
+const log = logger.child({ from: 'undux/effects/balance' })
 
 const withBalanceChange: Effects<State> = (store: Store) => {
   store.on('isLoggedIn').subscribe(isLoggedIn => {
+    if (isNull(store) || isNull(store.storeSnapshot)) {
+      return log.warn('withBalanceChange failed', 'received store is null')
+    }
+
     const balanceUpdate = store.get('balanceUpdate')
     log.debug('subscribing to balance changes', isLoggedIn, store, balanceUpdate)
     if (!balanceUpdate && isLoggedIn) {

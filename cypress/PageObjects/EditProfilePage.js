1a8827de382bef6c8b6c8aed7e34b1b766d9a3d1
YuryAnanyev <64954641+YuryAnanyev@users.noreply.github.com>
2020-05-08 22:26:09 +0300

    updates some files

--- cypress/PageObjects/EditProfilePage.js
+++ cypress/PageObjects/EditProfilePage.js
@@ -1,77 +1,81 @@
 /* eslint-disable no-undef */
 class EditProfilePage {
-  get nameInput() {
-    return cy.get('input[placeholder="Choose a Username"]', { timeout: 10000 })
-  }
+    get nameInput() {
+        return cy.get('input[placeholder="Choose a Username"]', { timeout: 10000 })
+    }
 
-  get phoneInput() {
-    return cy.get('#signup_phone', { timeout: 10000 })
-  }
+    get phoneInput() {
+        return cy.get('#signup_phone', { timeout: 10000 })
+    }
 
-  get emailInput() {
-    return cy.get('input[placeholder="Add your Email"]', { timeout: 10000 })
-  }
+    get emailInput() {
+        return cy.get('input[placeholder="Add your Email"]', { timeout: 10000 })
+    }
 
-  get backButton() {
-    return cy.get('[role="button"]').eq(0)
-  }
+    get backButton() {
+        return cy.get('[role="button"]').eq(0)
+    }
 
-  get avatarDiv() {
-    return cy.get('[data-focusable=true]', { timeout: 10000 }).eq(2)
-  }
+    get avatarDiv() {
+        return cy.get('[data-focusable=true]', { timeout: 10000 }).eq(2)
+    }
 
-  get saveButton() {
-    return cy.get('[role="button"]').eq(2)
-  }
+    get saveButton() {
+        return cy.xpath('/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]')
+            //return cy.get('[role="button"]').eq(2)
+    }
 
-  get pageHeader() {
-    return cy.get('h1[role=heading]', { timeout: 10000 })
-  }
+    get saveButtonText() {
+        return cy.contains('SAVE')
+    }
 
-  get saveAvatarButton() {
-    return cy.contains('Save')
-  }
+    get pageHeader() {
+        return cy.get('h1[role=heading]', { timeout: 10000 })
+    }
 
-  get clearAvatarButton() {
-    return cy.get('[dir="auto"]').eq(4)
-  }
+    get saveAvatarButton() {
+        return cy.contains('Save')
+    }
 
-  get uploadedAvatar() {
-    return cy.get('img[src*=data]', { timeout: 10000 })
-  }
+    get clearAvatarButton() {
+        return cy.get('[dir="auto"]').eq(4)
+    }
 
-  get wrongEmailErrorDiv() {
-    return cy.xpath('//*[@id="root"]/div[1]/div/div/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]', {
-      timeout: 10000,
-    })
-  }
+    get uploadedAvatar() {
+        return cy.get('img[src*=data]', { timeout: 10000 })
+    }
 
-  waitForEditProfilePageDisplayed() {
-    cy.contains('Edit Profile').should('be.visible')
-  }
+    get wrongEmailErrorDiv() {
+        return cy.xpath('//*[@id="root"]/div[1]/div/div/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]', {
+            timeout: 10000,
+        })
+    }
 
-  fillUserPhone(phoneNumber) {
-    const phoneInput = cy.get('#signup_phone', { timeout: 10000 }).should('contains.value', '+38')
-    phoneInput.focus().clear(), { timeout: 1000 }
-    phoneInput.clear().type(phoneNumber), { delay: 400 }
-    phoneInput.focus().blur()
-  }
+    waitForEditProfilePageDisplayed() {
+        cy.contains('Edit Profile').should('be.visible')
+    }
 
-  fillUserEmail(userEmail) {
-    const emailInput = cy.get('input[placeholder="Add your Email"]', { timeout: 10000 }).should('contains.value', 'com')
-    emailInput.focus().clear(), { timeout: 1000 }
-    emailInput.clear().type(userEmail), { delay: 400 }
-    emailInput.focus().blur()
-  }
+    fillUserPhone(phoneNumber) {
+        const phoneInput = cy.get('#signup_phone', { timeout: 10000 }).should('contains.value', '+38')
+        phoneInput.focus().clear(), { timeout: 1000 }
+        phoneInput.clear().type(phoneNumber), { delay: 400 }
+        phoneInput.focus().blur()
+    }
 
-  fillUserName(userName) {
-    const nameInput = cy
-      .get('input[placeholder="Choose a Username"]', { timeout: 10000 })
-      .should('contains.value', 'And')
-    nameInput.focus().clear(), { timeout: 1000 }
-    nameInput.clear().type(userName), { delay: 400 }
-    nameInput.focus().blur()
-  }
+    fillUserEmail(userEmail) {
+        const emailInput = cy.get('input[placeholder="Add your Email"]', { timeout: 10000 }).should('contains.value', 'com')
+        emailInput.focus().clear(), { timeout: 1000 }
+        emailInput.clear().type(userEmail), { delay: 400 }
+        emailInput.focus().blur()
+    }
+
+    fillUserName(userName) {
+        const nameInput = cy.get('input[placeholder="Choose a Username"]', { timeout: 10000 })
+            //.should('contains.value', 'anme')
+        nameInput.focus().clear(), { timeout: 1000 }
+        nameInput.clear().type(userName), { delay: 400 }
+        nameInput.focus().blur()
+    }
 }
 
 export default new EditProfilePage()
--- cypress/PageObjects/HomePage.js
+++ cypress/PageObjects/HomePage.js
@@ -1,76 +1,78 @@
 /* eslint-disable no-undef */
 class HomePage {
-  get rewardsButton() {
-    return cy.get('[data-testid="rewards_tab"]')
-  }
+    get rewardsButton() {
+        return cy.get('[data-testid="rewards_tab"]')
+    }
 
-  get inviteTab() {
-    return cy.get('[data-testid="invite_tab"]')
-  }
+    get inviteTab() {
+        return cy.get('[data-testid="invite_tab"]')
+    }
 
-  get goodmarketTab() {
-    return cy.get('[data-testid="goodmarket_tab"]')
-  }
+    get goodmarketTab() {
+        return cy.get('[data-testid="goodmarket_tab"]')
+    }
 
-  get supportTab() {
-    return cy.get('[data-testid="support_tab"]')
-  }
+    /*get supportTab() {
+      return cy.get('[data-testid="support_tab"]')
+    }*/
 
-  get optionsButton() {
-    return cy.get('[data-testid="burger_button"]')
-  }
+    get optionsButton() {
+        return cy.get('[data-testid="burger_button"]')
+    }
 
-  get magicLink() {
-    return cy.get('span').contains('EMAIL ME THE MAGIC LINK')
-  }
+    get magicLink() {
+        return cy.get('span').contains('EMAIL ME THE MAGIC LINK')
+    }
 
-  get sendButton() {
-    return cy.contains('Send')
-  }
+    get sendButton() {
+        return cy.contains('Send')
+    }
 
-  get claimButton() {
-    return cy.contains('Claim')
-  }
+    get claimButton() {
+        return cy.contains('Claim')
+    }
 
-  get receiveButton() {
-    return cy.contains('Receive')
-  }
+    get receiveButton() {
+        return cy.contains('Receive')
+    }
 
-  get options() {
-    return cy.get('[data-testid="close_burger_button"] + div > div', { timeout: 10000 })
-  }
+    get options() {
+        return cy.get('[data-testid="close_burger_button"] + div > div', { timeout: 10000 })
+    }
 
-  get profileAvatar() {
-    return cy.get('img[alt]', { timeout: 10000 }).eq(0)
-  }
+    get profileAvatar() {
+        return cy.get('img[alt]', { timeout: 10000 })
+            //return cy.get('img[alt]', { timeout: 10000 }).eq(0)
+    }
 
-  get closeOptionsButton() {
-    return cy.get('[dir=auto]', { timeout: 10000 }).eq(0)
-  }
+    get closeOptionsButton() {
+        return cy.get('[dir=auto]', { timeout: 10000 }).eq(0)
+    }
 
-  get moneyAmountDiv() {
-    return cy.get('[data-testid="amount_value"] div:nth-child(1)', { timeout: 10000 })
-  }
+    get moneyAmountDiv() {
+        return cy.get('[data-testid="amount_value"] div:nth-child(1)', { timeout: 10000 })
+    }
 
-  // get moneyAmountDiv() {
-  //   return cy.get('[data-testid=amount_value]', { timeout: 10000 })
-  // }
+    // get moneyAmountDiv() {
+    //   return cy.get('[data-testid=amount_value]', { timeout: 10000 })
+    // }
 
-  get welcomeFeed() {
-    return cy.contains('Start claiming free G$')
-  }
+    get welcomeFeed() {
+        return cy.contains('Claim free G$ coins daily')
+            //return cy.contains('Start claiming free G$')
+    }
 
-  get deleteAccountButton() {
-    return cy.contains('Delete wallet')
-  }
+    get deleteAccountButton() {
+        return cy.contains('Delete wallet')
+    }
 
-  get confirmDeletionButton() {
-    return cy.get('span').contains('Delete')
-  }
+    get confirmDeletionButton() {
+        return cy.get('span').contains('Delete')
+    }
 
-  waitForHomePageDisplayed() {
-    cy.contains('Claim').should('be.visible')
-  }
+    waitForHomePageDisplayed() {
+        cy.contains('Claim').should('be.visible')
+    }
 }
 
 export default new HomePage()

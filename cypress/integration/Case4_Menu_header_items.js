1129a21af9cf590231f24f0f14e6e343f7c083b4
YuryAnanyev <64954641+YuryAnanyev@users.noreply.github.com>
2020-05-12 21:09:04 +0300

    Update autotests (#1810)
    * update cases: 1-4
    
    * update test account
    
    * updates some files
    
    * update case5, fixed on comments.
    
    update case5, fixed on comments.
    update cypress.json
    
    * remove all extra whitespac

--- cypress/integration/Case4_Menu_header_items.js
+++ cypress/integration/Case4_Menu_header_items.js
@@ -8,7 +8,7 @@ import InvitePage from '../PageObjects/InvitePage'
 describe('Test case 4: Ability to send support request and subscribe', () => {
   beforeEach('authorization', () => {
     StartPage.open()
-    StartPage.continueOnWebButton.click()
+    //StartPage.continueOnWebButton.click()
     StartPage.signInButton.click()
     LoginPage.recoverFromPassPhraseLink.click()
     LoginPage.pageHeader.should('contain', 'Recover')
@@ -20,58 +20,60 @@ describe('Test case 4: Ability to send support request and subscribe', () => {
 
   it('Check is items are displayed at topbar', () => {
     HomePage.inviteTab.should('be.visible')
-    HomePage.goodmarketTab.should('be.visible')
-    HomePage.supportTab.should('be.visible')
+    //HomePage.goodmarketTab.should('be.visible')
+    //HomePage.supportTab.should('be.visible')
   })
 
   it('Check "Invite" tab', () => {
     HomePage.inviteTab.should('be.visible')
-    cy.contains('-0.01G$')
     HomePage.inviteTab.click()
-    InvitePage.pageHeader.should('contain', 'REWARDS')
+    //cy.contains('G$10')
+    InvitePage.pageHeader.should('contain', 'Invite')
     InvitePage.iframe.should('be.visible')
     InvitePage.iframe
       .then(iframe => new Promise(resolve => setTimeout(() => resolve(iframe), 8500)))
       .then(iframe => {
         const body = iframe.contents().find('body')
 
-        cy.wrap(body.find(InvitePage.centerTextDiv)).should(
-          'contain',
-          'Rewards are given to members who help our network to grow. Invite friends or complete tasks to earn more GoodDollars'
-        )
+        cy.wrap(body.find(InvitePage.centerTextDiv)).should('contain', 'Invite 3 friends to secure')
         cy.wrap(body.find(InvitePage.inviteFriendsDiv)).should('contain', 'Invite Friends')
       })
   })
 
   it('Check support page', () => {
-    cy.contains('Andrew Second')
-    HomePage.supportTab.click()
-    SupportPage.pageHeader.should('contain', 'Feedback & Support')
+    //cy.contains('Andrew Second')
+    //HomePage.supportTab.click()
+    HomePage.optionsButton.click()
+    cy.contains('Support & FAQ').click()
+    SupportPage.pageHeader.should('contain', 'Support & FAQ')
     SupportPage.iframe.should('be.visible')
     SupportPage.iframe
       .then(iframe => new Promise(resolve => setTimeout(() => resolve(iframe), 7500)))
       .then(iframe => {
         const body = iframe.contents().find('body')
 
-        cy.wrap(body.find(SupportPage.helpFormEmail)).should('be.visible')
-        cy.wrap(body.find(SupportPage.helpFormTextArea)).should('be.visible')
+        cy.wrap(body.find(SupportPage.search)).should('be.visible')
+        cy.wrap(body.find(SupportPage.topics)).should('be.visible')
+        cy.wrap(body.find(SupportPage.ask)).should('be.visible')
+        // cy.wrap(body.find(SupportPage.helpFormEmail)).should('be.visible')
+        // cy.wrap(body.find(SupportPage.helpFormTextArea)).should('be.visible')
 
-        cy.wrap(body.find(SupportPage.helpFormFirstName)).should('be.visible')
-        cy.wrap(body.find(SupportPage.helpFormLastName)).should('be.visible')
-        cy.wrap(body.find(SupportPage.submitHelpFormButton)).should('be.visible')
-        cy.wrap(body.find(SupportPage.helpFormFirstName))
-          .focus()
-          .type('Andrew', { delay: 200 })
-        cy.wrap(body.find(SupportPage.helpFormLastName))
-          .focus()
-          .type('Lebowski', { delay: 200 })
-        cy.wrap(body.find(SupportPage.helpFormEmail))
-          .focus()
-          .clear()
-          .type('andrey.holenkov@qatestlab.eu')
-        cy.wrap(body.find(SupportPage.helpFormTextArea))
-          .focus()
-          .type('Test message')
+        // cy.wrap(body.find(SupportPage.helpFormFirstName)).should('be.visible')
+        // cy.wrap(body.find(SupportPage.helpFormLastName)).should('be.visible')
+        // cy.wrap(body.find(SupportPage.submitHelpFormButton)).should('be.visible')
+        // cy.wrap(body.find(SupportPage.helpFormFirstName))
+        //     .focus()
+        //     .type('Andrew', { delay: 200 })
+        // cy.wrap(body.find(SupportPage.helpFormLastName))
+        //     .focus()
+        //     .type('Lebowski', { delay: 200 })
+        // cy.wrap(body.find(SupportPage.helpFormEmail))
+        //     .focus()
+        //     .clear()
+        //     .type('andrey.holenkov@qatestlab.eu')
+        // cy.wrap(body.find(SupportPage.helpFormTextArea))
+        //     .focus()
+        //     .type('Test message')
         // cy.wrap(body.find(SupportPage.submitHelpFormButton)).click()
 
         // cy.wrap(body.find(SupportPage.helpFormSuccessMessage)).should(

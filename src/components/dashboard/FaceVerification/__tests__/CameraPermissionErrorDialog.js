73a3c484d4ec490c7c7c4123665a9882e1548e00
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-05-25 17:16:51 +0300

    fix: add tests for new components

---
+++ src/components/dashboard/FaceVerification/__tests__/CameraPermissionErrorDialog.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { withThemeProvider } from '../../../../__tests__/__util__'
+import ImportedCameraPermissionErrorDialog from '../components/CameraPermissionErrorDialog'
+
+const ErrorScreen = withThemeProvider(ImportedCameraPermissionErrorDialog)
+
+const screenState = {
+  isValid: false,
+  error: {
+    name: 'test',
+    error: 'test',
+    message: 'test',
+  },
+  allowRetry: true,
+}
+
+describe('FaceVerificationDuplicateError', () => {
+  it('renders without errors', () => {
+    const tree = renderer.create(<ErrorScreen screenProps={{ screenState }} />)
+    expect(tree.toJSON()).toBeTruthy()
+  })
+
+  it('matches snapshot', () => {
+    const component = renderer.create(<ErrorScreen screenProps={{ screenState }} />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})

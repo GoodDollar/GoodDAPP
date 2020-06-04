129c63826c0094e5635d2880ba7b23ad5e7ee321
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-06-04 17:08:23 +0300

    refactor: PermissionsAPI.web.js

--- src/lib/permissions/PermissionsAPI.web.js
+++ src/lib/permissions/PermissionsAPI.web.js
@@ -1,17 +1,112 @@
-class PermissionsAPIWeb {
-  async getCameraPermissionStatus() {
-    // Fetching the list of input/output media devices
-    const listOfMediaDevices = navigator.mediaDevices ? await navigator.mediaDevices.enumerateDevices : []
+import logger from '../logger/pino-logger'
 
-    // Search for video input device (i.e. camera)
-    const videoDevice = listOfMediaDevices.find(device => device.kind === 'videoinput') || {}
+const log = logger.child({ from: 'PermissionsAPI' })
 
-    // if label field is not empty or null - it means camera access is granted
-    // so returning label field as boolean value
-    return Boolean(videoDevice.label)
+const PermissionsEnum = {
+  CAMERA: 'camera',
+  CLIPBOARD: 'clipboard',
+}
+const PermissionStatusesEnum = {
+  GRANTED: 'granted',
+  DENIED: 'denied',
+  PROMPT: 'prompt', // denied, but means that browser will ask for permission when user will try to use its functionality
+}
+
+export default new class PermissionsAPIWeb {
+  /*
+   * The main method which should be used to get status of specific permission kind
+   * @param {string} kind
+   * @return Promise<string> - one of PermissionStatusesEnum
+   */
+  query(kind) {
+    switch (kind) {
+      case PermissionsEnum.CAMERA:
+        return this._getCameraPermissionStatus()
+
+      case PermissionsEnum.CLIPBOARD:
+        return this._getClipboardPermissionStatus()
+
+      default:
+        return false
+    }
   }
 
-  async getClipboardPermissionStatus() {
+  /*
+   * Private method used to verify if Browser's PermissionAPI is available and get permission status by provided kind
+   * @private
+   * @param {string} kind
+   * @return (undefined - Browser's PermissionAPI is not available) or (Promise<string> - one of PermissionStatusesEnum)
+   */
+  async _getStatusByBrowsersPermissionAPI(kind) {
+    // get Browser's PermissionAPI object
+    const permissionsApi = navigator.permissions
+
+    // check if Browser's PermissionsAPI is available in current browser
+    if (permissionsApi) {
+      // if Browser's PermissionsAPI i available then fetch permission status by received kind
+      // query method will return PermissionStatus object
+      const permissionStatusObj = await permissionsApi.query({ name: kind })
+
+      // fetch and return permission status from 'PermissionStatus.state' field
+      return permissionStatusObj.state
+    }
+  }
+
+  /*
+   * Private method used to get camera permission status
+   * @private
+   * @return Promise<string> - one of PermissionStatusesEnum
+   */
+  async _getCameraPermissionStatus() {
+    // get camera permission status by Browser's PermissionsAPI
+    const browserPermissionStatus = await this._getStatusByBrowsersPermissionAPI('camera')
+
+    // check if value exists
+    if (browserPermissionStatus) {
+      // return if exists
+      return browserPermissionStatus
+    }
+
+    // get camera status by requesting a video stream manually
+    return this._getCameraPermissionStatusByRequestingStream()
+  }
+
+  /*
+   * Private method used to get camera permission status by manually request video stream
+   * Used when Browser's PermissionsAPI is not available in current browser
+   * @private
+   * @return Promise<string> - one of [PermissionStatusesEnum.DENIED, PermissionStatusesEnum.GRANTED]
+   */
+  async _getCameraPermissionStatusByRequestingStream() {
+    // fetching the getUserMedia method from navigator
+    const getUserMedia = navigator.mediaDevices.getUserMedia
+
+    // verify if getUserMedia is available
+    if (!getUserMedia) {
+      // make log - getUserMedia is not supported
+      log.warn('getUserMedia() is not supported by this browser')
+
+      // return denied as local video stream is not supported in this browser
+      return PermissionStatusesEnum.DENIED
+    }
+
+    try {
+      // requesting video stream to verify its available
+      await getUserMedia()
+    } catch (e) {
+      // make log of failed video stream request
+      log.warn('getUserMedia failed:', e.message, e)
+
+      // return denied for failure cases
+      return PermissionStatusesEnum.DENIED
+    }
+
+    // if the code reached this lines - it mean that video stream request is successful and permission is allowed
+    // return granted status
+    return PermissionStatusesEnum.GRANTED
+  }
+
+  async _getClipboardPermissionStatus() {
     try {
       // trying to read data from clipboard
       await navigator.clipboard.read()
@@ -25,6 +120,4 @@ class PermissionsAPIWeb {
     // in case error did not appear - clipboard access granted
     return true
   }
-}
-
-export default new PermissionsAPIWeb()
+}()

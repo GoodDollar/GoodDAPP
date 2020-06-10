c52f43068dfdff40e84d27a81259c22a77002f5e
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-06-10 14:58:43 +0300

    Update Zoom version, move native module to the external package, adjust ZoomSDK.native (#1988)

--- ios/creaternwapp/ZoomAuthenticationModule/Protocols/ProcesingDelegate.swift
+++
@@ -1,8 +0,0 @@
-// Helpful interfaces and enums
-
-import ZoomAuthentication
-
-protocol ProcessingDelegate: class {
-  func onProcessingComplete(isSuccess: Bool, zoomSessionResult: ZoomSessionResult?, zoomSessionMessage: String?)
-  func onSessionTokenError()
-}

3ba24e59fa91a3dd8eb908cdcccc76bd6ad60489
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-06-11 11:17:34 +0300

    fix: remove safari clipboard paste popup

--- src/lib/hooks/useClipboard.js
+++ src/lib/hooks/useClipboard.js
@@ -1,12 +1,8 @@
-import React, { useCallback } from 'react'
+import { useCallback } from 'react'
 import { Image, Platform } from 'react-native'
 
-import ExplanationDialog from '../../components/common/dialogs/ExplanationDialog'
 import illustration from '../../assets/ClipboardPermissionError.svg'
 
-import { useDialog } from '../undux/utils/dialog'
-
-import { isSafari } from '../utils/platform'
 import Clipboard from '../utils/Clipboard'
 import logger from '../logger/pino-logger'
 
@@ -16,17 +12,7 @@ if (Platform.OS === 'web') {
   Image.prefetch(illustration)
 }
 
-const NoClipboardAccessConfirmedInSafari = () => (
-  <ExplanationDialog
-    title="Please confirm paste from clipboard"
-    text={`Click on the 'Paste' context menu item next time`}
-    imageSource={illustration}
-  />
-)
-
 const useClipboard = (logger = log) => {
-  const [showDialog] = useDialog()
-
   const setString = useCallback(async content => {
     try {
       await Clipboard.setString(content)
@@ -47,18 +33,7 @@ const useClipboard = (logger = log) => {
 
       return text
     } catch (exception) {
-      const { name, message } = exception
-
-      if (isSafari && 'NotAllowedError' === name) {
-        // Safari shows context menu with 'paste' menu item each time we trying to paste from clipboard
-        // if user closes this menu by clicking outside, 'NotAllowedError' will be thrown
-        // in this case we'll show user an special popup with explanations what he should to do in Safari to paste
-        showDialog({
-          content: <NoClipboardAccessConfirmedInSafari />,
-          isMinHeight: false,
-          type: 'error',
-        })
-      }
+      const { message } = exception
 
       log.warn('Paste from clipboard failed', message, exception)
     }

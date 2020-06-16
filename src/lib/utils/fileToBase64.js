70f700283c7fa7cc59a9b6ec308cf4fb5695eab9
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-06-16 11:07:27 +0300

    refactor: delete fileToBase64.js

--- src/lib/utils/fileToBase64.js
+++
@@ -1,7 +0,0 @@
-export default file =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => resolve(reader.result)
-    reader.onerror = error => reject(error)
-  })

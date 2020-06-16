1a3872111c8e822bd857ab629b8c711208ce5355
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-06-16 15:31:25 +0300

    suppress GUN warnings

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -75,6 +75,9 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
 // It is focused on developer experience and fast rebuilds.
 // The production configuration is different and lives in a separate file.
 module.exports = {
+  stats: {
+    warningsFilter: [/critical dependency:/i],
+  },
   mode: 'development',
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebook/create-react-app/issues/343
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -102,6 +102,9 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
 // It compiles slowly and is focused on producing a fast and minimal bundle.
 // The development configuration is different and lives in a separate file.
 module.exports = {
+  stats: {
+    warningsFilter: [/critical dependency:/i],
+  },
   mode: 'production',
   // Don't attempt to continue if there are any errors.
   bail: true,

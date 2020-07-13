a7b553a980dcbb5bb57c0503dd45e83b5648bb82
YuryAnanyev <yury.ananyev@qatestlab.eu>
2020-07-13 17:28:28 +0300

    #2149
    (Modify cypress tests for approve users in the claim queue in the certain tests cases only)

--- cypress/integration/Case07_Send_money.js
+++ cypress/integration/Case07_Send_money.js
@@ -74,18 +74,23 @@ describe('Test case 7: Ability to send money', () => {
       LoginPage.recoverWalletButton.click()
       LoginPage.yayButton.click()
 
-      
-      // face verification
       HomePage.claimButton.click()
+
+      cy.request('POST', 'https://good-server.herokuapp.com/admin/queue', [ { password: 'password' }, { allow: 1 }, ])
+      // .then(response => { expect(response.body).to.have.property('ok', 0) })
+
       SendMoneyPage.dailyClaimText.should('be.visible')
       SendMoneyPage.claimButton.click()
-      // SendMoneyPage.verifyButton.click()
+      SendMoneyPage.verifyButton.should('be.visible')
+      SendMoneyPage.verifyButton.click()
+
+      // face verification
       // cy.wait(5000)
       // SendMoneyPage.readyButton.should('be.visible')
       // SendMoneyPage.readyButton.click()
+
       LoginPage.yayButton.click()
       cy.contains('1G$').should('be.visible')
-      
 
       HomePage.sendButton.click()
       SendMoneyPage.nameInput.type('another person')
@@ -101,33 +106,35 @@ describe('Test case 7: Ability to send money', () => {
       //get link from clipboard
       // cy.task('getClipboard').then(sendMoneyUrl => {
 
-      cy.get('[data-testid*="http"]').invoke('attr', 'data-testid').then(sendMoneyUrl => {
-        cy.log(sendMoneyUrl)
-        const moneyLink = sendMoneyUrl
-        const pattern = /(?:http[s]?:\/\/)[^\s[",><]*/gim
-        const validMoneyLnk = moneyLink.match(pattern)
-        cy.log(validMoneyLnk)
-        SendMoneyPage.doneButton.click()
-        cy.clearLocalStorage()
-        cy.clearCookies()
-        StartPage.open()
-        StartPage.signInButton.click()
-        LoginPage.recoverFromPassPhraseLink.click()
-        LoginPage.pageHeader.should('contain', 'Recover')
-        LoginPage.mnemonicsInput.type(Cypress.env('additionalAccountMnemonics'))
-        LoginPage.recoverWalletButton.click()
-        LoginPage.yayButton.click()
-        HomePage.claimButton.should('be.visible')
-        HomePage.moneyAmountDiv.invoke('text').then(moneyBefore => {
-          cy.log('Money before sending: ' + moneyBefore)
-          cy.visit(validMoneyLnk.toString())
+      cy.get('[data-testid*="http"]')
+        .invoke('attr', 'data-testid')
+        .then(sendMoneyUrl => {
+          cy.log(sendMoneyUrl)
+          const moneyLink = sendMoneyUrl
+          const pattern = /(?:http[s]?:\/\/)[^\s[",><]*/gim
+          const validMoneyLnk = moneyLink.match(pattern)
+          cy.log(validMoneyLnk)
+          SendMoneyPage.doneButton.click()
+          cy.clearLocalStorage()
+          cy.clearCookies()
+          StartPage.open()
+          StartPage.signInButton.click()
+          LoginPage.recoverFromPassPhraseLink.click()
+          LoginPage.pageHeader.should('contain', 'Recover')
+          LoginPage.mnemonicsInput.type(Cypress.env('additionalAccountMnemonics'))
+          LoginPage.recoverWalletButton.click()
+          LoginPage.yayButton.click()
+          HomePage.claimButton.should('be.visible')
+          HomePage.moneyAmountDiv.invoke('text').then(moneyBefore => {
+            cy.log('Money before sending: ' + moneyBefore)
+            cy.visit(validMoneyLnk.toString())
 
-          //wait for blockchain payment
-          cy.contains('Claim').should('be.visible')
-          HomePage.moneyAmountDiv.invoke('text').should('eq', (Number(moneyBefore) + 0.01).toFixed(2))
-          SendMoneyPage.yayButton.click()
+            //wait for blockchain payment
+            cy.contains('Claim').should('be.visible')
+            HomePage.moneyAmountDiv.invoke('text').should('eq', (Number(moneyBefore) + 0.01).toFixed(2))
+            SendMoneyPage.yayButton.click()
+          })
         })
-      })
     })
   })
 })

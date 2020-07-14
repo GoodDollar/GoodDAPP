d613209e999e478280a474c63015b04250b48ec2
YuryAnanyev <yury.ananyev@qatestlab.eu>
2020-07-14 16:57:39 +0300

    some fix and add #2158
    some fix and added checking Invite page

--- cypress/integration/Case07_Send_money.js
+++ cypress/integration/Case07_Send_money.js
@@ -5,64 +5,6 @@ import HomePage from '../PageObjects/HomePage'
 import SendMoneyPage from '../PageObjects/SendMoneyPage'
 
 describe('Test case 7: Ability to send money', () => {
-  // it('Send test money to new acc', () => {
-  //   cy.clearLocalStorage()
-  //   cy.clearCookies()
-  //   StartPage.open()
-  //   StartPage.signInButton.click()
-  //   LoginPage.recoverFromPassPhraseLink.click()
-  //   LoginPage.pageHeader.should('contain', 'Recover')
-  //   LoginPage.mnemonicsInput.type(Cypress.env('additionalAccountMnemonics'))
-  //   LoginPage.recoverWalletButton.click()
-  //   LoginPage.yayButton.click()
-  //   HomePage.claimButton.should('be.visible')
-  //   HomePage.sendButton.click()
-
-  //   SendMoneyPage.nameInput.type('Name Surname')
-  //   SendMoneyPage.nextButton.click()
-  //   SendMoneyPage.moneyInput.type('0.01')
-  //   SendMoneyPage.nextButton.click()
-  //   SendMoneyPage.messageInput.type('test money')
-  //   SendMoneyPage.nextButton.click()
-  //   SendMoneyPage.confirmButton.click()
-  //   SendMoneyPage.copyLinkButton.click()
-  //   SendMoneyPage.doneButton.should('be.visible')
-
-  //   //get link from clipboard
-  //   //cy.task('getClipboard').then(sendMoneyUrl => {
-
-  //     cy.get('[data-testid*="http"]').invoke('attr', 'data-testid').then(sendMoneyUrl => {
-  //     cy.log(sendMoneyUrl)
-  //     const moneyLink = sendMoneyUrl
-  //     const pattern = /(?:http[s]?:\/\/)[^\s[",><]*/gim
-  //     const validMoneyLnk = moneyLink.match(pattern)
-  //     cy.log(validMoneyLnk)
-  //     SendMoneyPage.doneButton.click()
-  //     cy.clearLocalStorage()
-  //     cy.clearCookies()
-  //     cy.readFile('cypress/fixtures/userMnemonicSave.txt').then(mnemonic => {
-  //       StartPage.open()
-  //       StartPage.signInButton.click()
-  //       LoginPage.recoverFromPassPhraseLink.click()
-  //       LoginPage.pageHeader.should('contain', 'Recover')
-  //       LoginPage.mnemonicsInput.type(mnemonic)
-  //       LoginPage.recoverWalletButton.click()
-  //       LoginPage.yayButton.click()
-
-  //       HomePage.claimButton.should('be.visible')
-  //       HomePage.moneyAmountDiv.invoke('text').then(moneyBefore => {
-  //         cy.log('Money before sending: ' + moneyBefore)
-  //         cy.visit(validMoneyLnk.toString())
-
-  //         cy.contains('Claim').should('be.visible')
-  //         HomePage.moneyAmountDiv.invoke('text').should('eq', (Number(moneyBefore) + 0.01).toFixed(2))
-  //         SendMoneyPage.yayButton.click()
-  //       })
-  //     })
-  //   })
-  // })
-
-  // test send with verification
   it('User is able to send money', () => {
     localStorage.clear()
     cy.readFile('../GoodDAPP/cypress/fixtures/userMnemonicSave.txt').then(mnemonic => {
@@ -73,11 +15,13 @@ describe('Test case 7: Ability to send money', () => {
       LoginPage.mnemonicsInput.type(mnemonic)
       LoginPage.recoverWalletButton.click()
       LoginPage.yayButton.click()
-
-      HomePage.claimButton.click()
-
-      cy.request('POST', 'https://good-server.herokuapp.com/admin/queue', [ { password: 'password' }, { allow: 1 }, ])
-      // .then(response => { expect(response.body).to.have.property('ok', 0) })
+      HomePage.claimButton.click().invoke('text').then(text => {
+        cy.log(text)
+        if (text == 'Queue') {
+          const urlRequest = Cypress.env('postUrlRequest')
+          const bodyRequest = Cypress.env('postBodyRequest')
+          cy.request('POST', urlRequest, bodyRequest)
+        }
 
       SendMoneyPage.dailyClaimText.should('be.visible')
       SendMoneyPage.claimButton.click()
@@ -90,7 +34,7 @@ describe('Test case 7: Ability to send money', () => {
       // SendMoneyPage.readyButton.click()
 
       LoginPage.yayButton.click()
-      cy.contains('1G$').should('be.visible')
+      cy.contains('G$').should('be.visible')
 
       HomePage.sendButton.click()
       SendMoneyPage.nameInput.type('another person')

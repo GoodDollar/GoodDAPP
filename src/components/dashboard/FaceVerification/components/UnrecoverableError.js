cc2440b93cac05b3f6c1d628bbb4110eda1a391c
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-07-15 17:20:36 +0300

    fix: show reload dialog if preload error occurred

--- src/components/dashboard/FaceVerification/components/UnrecoverableError.js
+++ src/components/dashboard/FaceVerification/components/UnrecoverableError.js
@@ -11,6 +11,7 @@ import useOnPress from '../../../../lib/hooks/useOnPress'
 import { isMobileOnly } from '../../../../lib/utils/platform'
 import logger from '../../../../lib/logger/pino-logger'
 
+import showReloadDialog from '../utils/showReoadDialog'
 import { getDesignRelativeHeight, getDesignRelativeWidth } from '../../../../lib/utils/sizes'
 import { withStyles } from '../../../../lib/styles'
 import illustration from '../../../../assets/FRUnrecoverableError.svg'
@@ -31,6 +32,7 @@ const UnrecoverableError = ({ styles, exception, screenProps }) => {
   const [, hideDialog, showErrorDialog] = useDialog()
 
   const sdkStatus = get(exception, 'code')
+  const isPreloadIssue = get(exception, 'message', '').includes('preload was not completed')
   const isLicenseIssue = [InvalidDeviceLicenseKeyIdentifier, LicenseExpiredOrInvalid].includes(sdkStatus)
 
   const onContactSupport = useOnPress(() => screenProps.navigateTo('Support'), [screenProps])
@@ -39,6 +41,12 @@ const UnrecoverableError = ({ styles, exception, screenProps }) => {
   useEffect(() => {
     if (!isLicenseIssue) {
       fireEvent(FV_TRYAGAINLATER)
+      return
+    }
+
+    if (isPreloadIssue) {
+      log.error('FaceVerification failed', get(exception, 'message', ''), exception, { dialogShown: true })
+      showReloadDialog()
       return
     }
 

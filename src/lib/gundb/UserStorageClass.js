4eeae40dd4738329c4787e0e6d56041038ffad8d
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-07-15 10:28:40 +0300

    (Bug) When creating a new account, phone and email are not added to the profile (#2164)
    * add retry userStorage.init() and setProfile(), show error dialog if they're failing again, display app reload button if userStorage initilization failed
    
    * remove unneded rxjs stuff arbound userStorage initialization. if we don't need defer here, everything could be done with simple Promise's .catch()
    
    * fix backgroundInit, add retryInit
    
    * remove unneeded async from backgroundInit

--- src/lib/gundb/UserStorageClass.js
+++ src/lib/gundb/UserStorageClass.js
@@ -374,8 +374,10 @@ export class UserStorage {
     username: true,
   }
 
-  //trusted GoodDollar user indexes
+  // trusted GoodDollar user indexes
   trust = {}
+
+  ready: Promise<boolean> = null
 
   /**
    * Clean string removing blank spaces and special characters, and converts to lower case
@@ -456,22 +458,8 @@ export class UserStorage {
   constructor(wallet: GoodWallet, gun: Gun) {
     this.gun = gun || defaultGun
     this.wallet = wallet
-    this.ready = this.wallet.ready
-      .then(() => this.init())
-      .then(() => logger.debug('userStorage initialized.'))
-      .catch(e => {
-        let logLevel = 'error'
 
-        if (e.message && e.message.includes('Wrong user or password')) {
-          logLevel = 'warn'
-        }
-
-        logger[logLevel]('Error initializing UserStorage', e.message, e, {
-          account: this.wallet.account,
-        })
-
-        return false
-      })
+    this.backgroundInit()
   }
 
   gunAuth(username: string, password: string): Promise<any> {
@@ -630,6 +618,43 @@ export class UserStorage {
     await Promise.all([this.startSystemFeed(), this.initTokens()])
 
     return true
+  }
+
+  // eslint-disable-next-line require-await
+  async retryInit(): Promise<boolean> {
+    this.ready = null
+
+    this.backgroundInit()
+    return this.ready
+  }
+
+  backgroundInit(): void {
+    const { ready, wallet } = this
+
+    if (ready) {
+      return
+    }
+
+    this.ready = (async () => {
+      try {
+        await wallet.ready
+        const isReady = await this.init()
+
+        logger.debug('userStorage initialized.')
+        return isReady
+      } catch (exception) {
+        let logLevel = 'error'
+        const { account } = wallet
+        const { message } = exception
+
+        if (message && message.includes('Wrong user or password')) {
+          logLevel = 'warn'
+        }
+
+        logger[logLevel]('Error initializing UserStorage', message, exception, { account })
+        throw exception
+      }
+    })()
   }
 
   async initTokens() {

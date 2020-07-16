5e7f39ecf3fcd3c3931bf05e56aedc591fbd5928
Hadar Rottenberg <hadar@gooddollar.org>
2020-07-16 22:46:26 +0300

    add: verify user exists via api also in authtorus

--- src/components/auth/torus/AuthTorus.js
+++ src/components/auth/torus/AuthTorus.js
@@ -31,6 +31,7 @@ import { getDesignRelativeHeight, getDesignRelativeWidth } from '../../../lib/ut
 import { isSmallDevice } from '../../../lib/utils/mobileSizeDetect'
 import normalizeText from '../../../lib/utils/normalizeText'
 import { isBrowser } from '../../../lib/utils/platform'
+import { userExists } from '../../../lib/login/userExists'
 import useTorus from './hooks/useTorus'
 
 Image.prefetch(illustration)
@@ -64,7 +65,7 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
       log.debug('reinitializing wallet and storage with new user')
       goodWallet.init()
       await goodWallet.ready
-      await userStorage.init()
+      userStorage.init()
     }
 
     //for QA
@@ -106,8 +107,9 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
         if (torusUser == null) {
           torusUser = await torusSDK.triggerLogin(provider)
         }
-
-        identifyOnUserSignup(torusUser.email)
+        if (provider !== 'auth0-pwdless-sms') {
+          identifyOnUserSignup(torusUser.email)
+        }
 
         const curSeed = await AsyncStorage.getItem(GD_USER_MASTERSEED)
         const curMnemonic = await AsyncStorage.getItem(GD_USER_MNEMONIC)
@@ -134,17 +136,20 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
       }
 
       try {
-        const { userStorage, source } = await ready(replacing)
-        const userExists = await userStorage.userAlreadyExist()
-        log.debug('checking userAlreadyExist', { userExists })
+        const { exists, fullName } = await userExists()
+
+        // const userExists = await userStorage.userAlreadyExist()
+        log.debug('checking userAlreadyExist', { exists, fullName })
 
         //user exists reload with dashboard route
-        if (userExists) {
+        if (exists) {
           fireEvent(SIGNIN_TORUS_SUCCESS, { provider })
           await AsyncStorage.setItem(IS_LOGGED_IN, true)
           store.set('isLoggedIn')(true)
           return
         }
+
+        const { source } = await ready(replacing)
 
         //user doesnt exists start signup
         fireEvent(SIGNUP_STARTED, { source, provider })

69ad737e0cdf216013fb915c15de8dca5d026384
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-07-20 14:15:05 +0300

    (Chore) Iframes should load faster (#2182)
    * add proxy iframe's DOMContentLoaded via postMessage. listen to proxied events, keeping onLoad as fallback
    
    * final fixes
    
    * refactor iframe manager to use evenemitter3
    
    * update snippet to support RN VebView
    
    * Merge branch 'master' into 2130-iframes-should-load-faster

--- src/components/webView/iframe.web.js
+++ src/components/webView/iframe.web.js
@@ -1,33 +1,35 @@
 import React, { useEffect } from 'react'
 import { TouchableOpacity, View } from 'react-native'
 import { Appbar } from 'react-native-paper'
+
 import Section from '../common/layout/Section'
 import Icon from '../common/view/Icon'
-import SimpleStore from '../../lib/undux/SimpleStore'
+
 import { getMaxDeviceHeight } from '../../lib/utils/Orientation'
+import useLoadingIndicator from '../../lib/hooks/useLoadingIndicator'
+import IframeManager from './iframe.manager'
 
 const wHeight = getMaxDeviceHeight()
 
 export const createIframe = (src, title, backToWallet = false, backToRoute = 'Home') => {
   const IframeTab = props => {
-    const store = SimpleStore.useStore()
-
-    const isLoaded = () => {
-      store.set('loadingIndicator')({ loading: false })
-    }
+    const [showLoading, hideLoading] = useLoadingIndicator()
 
     useEffect(() => {
-      store.set('loadingIndicator')({ loading: true })
+      showLoading()
+      IframeManager.addListener(src, hideLoading)
+
+      return () => IframeManager.removeListener(src, hideLoading)
     }, [])
 
-    //this is for our external pages like privacy policy, etc.. they dont require iframeresizer to work ok on ios <13
+    // this is for our external pages like privacy policy, etc.. they dont require iframeresizer to work ok on ios <13
     return (
       <iframe
         allowFullScreen
         title={title}
         seamless
         frameBorder="0"
-        onLoad={isLoaded}
+        onLoad={hideLoading}
         src={src}
         width="100%"
         height="100%"

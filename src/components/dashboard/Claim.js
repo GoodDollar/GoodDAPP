0ab1effd3f45f443fa277ec114c15f2601dc73f8
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2020-07-22 14:41:15 +0300

    () #2181 G$ claim amount: need to add Thousands Separators (#2201)
    * fix: format claim amount with comma separator
    
    * Apply suggestions from code review
    
    Co-authored-by: serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>

--- src/components/dashboard/Claim.js
+++ src/components/dashboard/Claim.js
@@ -13,7 +13,7 @@ import SimpleStore from '../../lib/undux/SimpleStore'
 import { useDialog } from '../../lib/undux/utils/dialog'
 import wrapper from '../../lib/undux/utils/wrapper'
 import { openLink } from '../../lib/utils/linking'
-import { formatWithSIPrefix } from '../../lib/utils/formatNumber'
+import { formatWithThousandsSeparator, formatWithSIPrefix } from '../../lib/utils/formatNumber'
 import API from '../../lib/API/api'
 import { weiToGd } from '../../lib/wallet/utils'
 import { getDesignRelativeHeight, getDesignRelativeWidth } from '../../lib/utils/sizes'
@@ -293,6 +293,8 @@ const Claim = props => {
 
   const handleFaceVerification = () => screenProps.push('FaceVerificationIntro', { from: 'Claim' })
 
+  const claimAmountFormatter = useCallback(value => formatWithThousandsSeparator(weiToGd(value)), [])
+
   return (
     <WrapperClaim>
       <Section.Stack style={styles.mainContainer} justifyContent="space-between">
@@ -306,7 +308,7 @@ const Claim = props => {
                 <Section.Text color="#0C263D" style={styles.amountBlockTitle} fontWeight="bold" fontFamily="Roboto">
                   <BigGoodDollar
                     number={entitlement}
-                    formatter={weiToGd}
+                    formatter={claimAmountFormatter}
                     fontFamily="Roboto"
                     bigNumberProps={{
                       fontFamily: 'Roboto',

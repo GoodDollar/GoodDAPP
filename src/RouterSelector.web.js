b80c804117f7b70c3de4e2ec724bfaef5c0dbd87
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-07-22 12:36:15 +0300

    for approute we dont require waiting until splash animation is over

--- src/RouterSelector.web.js
+++ src/RouterSelector.web.js
@@ -97,7 +97,6 @@ let AppRouter = React.lazy(async () => {
       .then(({ init }) => init())
       .then(() => log.debug('storage and wallet ready')),
 
-    delay(animationDuration),
   ])
 
   log.debug('router ready')

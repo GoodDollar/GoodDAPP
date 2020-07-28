f4b0c9758032197acb1ff6a217d3d4a328c9c5d9
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-07-28 17:37:18 -0300

    update: revert userpropertiesclass refactor

--- src/lib/gundb/UserPropertiesClass.js
+++ src/lib/gundb/UserPropertiesClass.js
@@ -1,5 +1,5 @@
 // @flow
-import { defaults, once } from 'lodash'
+
 import { REGISTRATION_METHOD_SELF_CUSTODY } from '../constants/login'
 
 /**
@@ -41,39 +41,20 @@ export default class UserProperties {
   ]
 
   /**
-   * a gun node referring to gun.user().get('properties')
+   * a gun node referring tto gun.user().get('properties')
    * @instance {UserProperties}
    */
-  node: Gun
+  gun: Gun
 
-  /** @private */
-  props: {}
+  data: {}
 
-  get ready(): Promise {
-    return this.loadInBackground()
-  }
-
-  /** @private */
-  loadInBackground = once(async () => {
-    let props
-    const { constructor, node } = this
-    const { defaultProperties } = constructor
-
-    try {
-      props = await node.decrypt()
-    } catch {
-      props = {}
-    }
-
-    props = defaults(props, defaultProperties)
-    this.props = props
-
-    return props
-  })
-
-  constructor(propsNode: Gun) {
-    this.node = propsNode
-    this.loadInBackground()
+  constructor(propertiesGun: Gun) {
+    this.gun = propertiesGun
+    this.ready = this.gun
+      .decrypt()
+      .catch(_ => {})
+      .then(_ => Object.assign({}, UserProperties.defaultProperties, _))
+      .then(_ => (this.data = _))
   }
 
   /**
@@ -81,22 +62,21 @@ export default class UserProperties {
    *
    * @param {string} field
    * @param {string} value
-   * @returns {Promise<boolean>}
+   * @returns {Promise<void>}
    */
-  // eslint-disable-next-line require-await
   async set(field: string, value: any) {
-    return this.updateAll({ [field]: value })
+    await this.updateAll({ [field]: value })
+
+    return true
   }
 
   /**
    * Return property values
    * @param field
-   * @returns {any}
+   * @returns {Promise<any>}
    */
   get(field: string) {
-    const { props } = this
-
-    return props[field]
+    return this.data[field]
   }
 
   /**
@@ -104,49 +84,16 @@ export default class UserProperties {
    * @returns {{}}
    */
   getAll() {
-    const { props } = this
-
-    return { ...props }
+    return this.data
   }
 
   /**
-   * Sets all properties from the object passed
-   * Warning! This method rewrites all props, if you need
-   * just update a few props - use updateProps()
-   *
-   * @param {object} newProps Object to set properties from
+   * Set value to multiple properties
    */
-  async setAll(newProps: object): Promise<boolean> {
-    const { node } = this
-    const props = { ...newProps }
-
-    await node.secret(props)
-    this.props = props
+  async updateAll(data: { [string]: any }): Promise<boolean> {
+    Object.assign(this.data, data)
+    await this.gun.secret(this.data)
 
     return true
-  }
-
-  /**
-   * Updates props by merging them with the object passed
-   *
-   * @param {object} updateProps Object to merge properties with
-   */
-  // eslint-disable-next-line require-await
-  async updateAll(updateProps: object): Promise<boolean> {
-    const { props } = this
-    const updatedProps = { ...props, ...updateProps }
-
-    return this.setAll(updatedProps)
-  }
-
-  /**
-   * Resets props to the default ones
-   * Warning! This method rewrites all props
-   */
-  // eslint-disable-next-line require-await
-  async reset() {
-    const { defaultProperties } = this.constructor
-
-    return this.setAll(defaultProperties)
   }
 }

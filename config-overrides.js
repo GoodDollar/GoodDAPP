c561e6e98e9fb35459f8ea91e086f538f32837f7
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-08-20 13:43:55 +0300

    fix: configure react-native branch to use latest webpack configs from master branch

--- config-overrides.js
+++ config-overrides.js
@@ -1,133 +1,14 @@
-const path = require('path');
-const {
-  override,
-  addBundleVisualizer,
-  addWebpackAlias,
-  babelInclude,
-  addExternalBabelPlugins,
-  addBabelPresets,
-  fixBabelImports,
-  babelExclude,
-  useEslintRc
-} = require("customize-cra");
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const assign = require('lodash/assign')
 
-const lodashWebpackPluginOverride = (config) => {
-  config.plugins.unshift(
-    new LodashModuleReplacementPlugin({
-      collections: true,
-      paths: true,
-      flattening: true,
-      currying: true,
-      placeholder: true,
-      caching: true,
-    }),
-  )
-
-  return config
-}
-
-// SVGR is an tool that converts your SVG files into React components that you can use directly in JXS.
-const svgrOverride = config => {
-  let loaders = config.module.rules[2].oneOf
-  loaders.splice(loaders.length - 1, 0, {
-    test: /\.svg$/,
-    use: [{
-      loader: '@svgr/webpack',
-      options: {
-        template: function defaultTemplate({ template }, opts, { imports, interfaces, componentName, props, jsx, exports }) {
-          const plugins = ['jsx']
-          let exportLoadedFileAsUrl = ''
-
-          if (opts.state.caller.previousExport) {
-            exportLoadedFileAsUrl = opts.state.caller.previousExport.replace('default', 'const url =')
-          }
-
-          if (opts.typescript) {
-            plugins.push('typescript')
-          }
-
-          const typeScriptTpl = template.smart({ plugins })
-
-          return typeScriptTpl.ast`${imports}
-            ${interfaces}
-            function ${componentName}(${props}) {
-              return ${jsx};
-            }
-            ${exportLoadedFileAsUrl}
-            export default ${componentName}
-            `
-        }
-      }
-    }, {
-      loader: 'file-loader'
-    }],
-  })
-
-  return config
-}
+let configType = process.env === 'production' ? 'prod' : 'dev'
+const webpackConfig = require(`./config/webpack.config.${configType}`)
 
 module.exports = {
-  webpack: override(
-    fixBabelImports('module-resolver', {
-      alias: {
-        '^react-native$': 'react-native-web',
-        '^lottie-react-native$': 'react-native-web-lottie',
-        '^react-native-linear-gradient$': 'react-native-web-linear-gradient',
-      },
-    }),
+  webpack: (config) => {
+    assign(config, webpackConfig)
 
-    svgrOverride,
-
-    babelInclude([
-      path.resolve('src'),
-      path.resolve('node_modules/react-navigation'),
-      path.resolve('node_modules/@react-navigation'),
-      path.resolve('node_modules/react-native-paper'),
-      path.resolve('node_modules/react-native-safe-area-view'),
-      path.resolve('node_modules/react-native-vector-icons/lib'),
-      path.resolve('node_modules/react-native-gesture-handler'),
-      path.resolve('node_modules/react-native-keyboard-aware-scroll-view'),
-      path.resolve('node_modules/react-native-animatable'),
-      path.resolve('node_modules/react-native-card-flip'),
-    ]),
-
-    babelExclude([
-      path.resolve('node_modules/@react-navigation/core/lib/module/utils'),
-      path.resolve('node_modules/@react-navigation/native/dist/utils'),
-      path.resolve('node_modules/@react-navigation/web'),
-      path.resolve('node_modules/react-native-card-flip/node_modules'),
-      path.resolve('src/lib/zoom'),
-    ]),
-
-    useEslintRc(),
-
-    addWebpackAlias({
-      'react-native-linear-gradient': 'react-native-web-linear-gradient',
-      'react-native': 'react-native-web',
-      'lottie-react-native': 'react-native-web-lottie',
-    }),
-
-    ...addExternalBabelPlugins(
-      "lodash",
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-object-rest-spread"
-    ),
-
-    ...addBabelPresets(
-      ['@babel/preset-env', {
-        modules: false,
-        targets: {
-          node: 4,
-        }
-      }],
-      "@babel/preset-flow"
-    ),
-
-    lodashWebpackPluginOverride,
-
-    addBundleVisualizer({}, true),
-  ),
+    return config
+  },
 
   jest: function(config) {
     config.transformIgnorePatterns = [

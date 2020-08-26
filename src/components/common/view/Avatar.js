9d8e4dbb69e49d3a79c52692eaba5a534f583560
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-08-26 16:17:26 -0300

    Fix avatar

--- src/components/common/view/Avatar.js
+++ src/components/common/view/Avatar.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { Platform, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-paper'
 import unknownProfile from '../../../assets/unknownProfile.svg'
 import { withStyles } from '../../../lib/styles'
@@ -14,23 +14,29 @@ import { withStyles } from '../../../lib/styles'
  * @param {Number} [props.size=34]
  * @returns {React.Node}
  */
-const CustomAvatar = ({ styles, style, source, onPress, size, imageSize, children, ...avatarProps }) => (
-  <TouchableOpacity
-    activeOpacity={1}
-    disabled={!onPress}
-    onPress={onPress}
-    style={[styles.avatarContainer, { width: size, height: size, borderRadius: size / 2 }, style]}
-    underlayColor="#fff"
-  >
-    <Avatar.Image
-      size={imageSize || size - 2}
-      source={{ uri: source || unknownProfile }}
-      style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-      {...avatarProps}
-    />
-    {children}
-  </TouchableOpacity>
-)
+const CustomAvatar = ({ styles, style, source, onPress, size, imageSize, children, ...avatarProps }) => {
+  const imageSource = Platform.select({
+    web: { uri: source || unknownProfile },
+    default: require('../../../../public/favicon-96x96.png'),
+  })
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      disabled={!onPress}
+      onPress={onPress}
+      style={[styles.avatarContainer, { width: size, height: size, borderRadius: size / 2 }, style]}
+      underlayColor="#fff"
+    >
+      <Avatar.Image
+        size={imageSize || size - 2}
+        source={imageSource}
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+        {...avatarProps}
+      />
+      {children}
+    </TouchableOpacity>
+  )
+}
 
 CustomAvatar.defaultProps = {
   size: 42,

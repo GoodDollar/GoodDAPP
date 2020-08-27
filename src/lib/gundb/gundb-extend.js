0f92082c2a3db41182681b98f6808d1e188c460d
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-08-27 19:47:21 +0300

    - add retry in gundb-extend

--- src/lib/gundb/gundb-extend.js
+++ src/lib/gundb/gundb-extend.js
@@ -1,151 +1,149 @@
-import { assign, identity, isFunction } from 'lodash'
+import { assign, identity, noop, once } from 'lodash'
 
 import Gun from '@gooddollar/gun'
 import SEA from '@gooddollar/gun/sea'
 import '@gooddollar/gun/lib/load'
 
-import { delay } from '../utils/async'
+import { delay, retry } from '../utils/async'
 
 /**
  * extend gundb SEA with decrypt to match ".secret"
  * @module
  */
-const gunExtend = (() => {
-  const { chain, User } = Gun
+const { chain, User } = Gun
 
-  assign(chain, {
-    /**
-     * fix gun issue https://github.com/amark/gun/issues/855
-     */
-    // eslint-disable-next-line require-await
-    async then(callback, wait = 200) {
-      const readPromise = new Promise(resolve => this.once(resolve, { wait }))
+// eslint-disable-next-line require-await
+const promisify = async callback =>
+  new Promise((resolve, reject) => {
+    const onAck = once(ack => (ack.err ? reject : resolve)(ack))
 
-      if (!isFunction(callback)) {
-        return readPromise
-      }
-
-      return readPromise.then(callback)
-    },
-
-    /**
-     * it returns a promise with the first result from a peer
-     * @param {any} data Data to put onto Gun's node
-     * @param {function} callback Function to be executed once write operation done
-     * @returns {Promise<ack>}
-     */
-    // eslint-disable-next-line require-await
-    async putAck(data, cb) {
-      var gun = this
-
-      let promise = new Promise((res, rej) => gun.put(data, ack => (ack.err ? rej(ack) : res(ack))))
-      return promise.then(cb || identity)
-    },
-
-    onThen(cb = undefined, opts = {}) {
-      opts = Object.assign({ wait: 2000, default: undefined }, opts)
-      let gun = this
-      const onPromise = new Promise((res, rej) => {
-        gun.on((v, k, g, ev) => {
-          ev.off()
-
-          //timeout if value is undefined
-          if (v !== undefined) {
-            res(v)
-          }
-        })
-      })
-      let oncePromise = new Promise(function(res, rej) {
-        gun.once(
-          v => {
-            //timeout if value is undefined
-            if (v !== undefined) {
-              res(v)
-            }
-          },
-          { wait: opts.wait },
-        )
-      })
-      const res = Promise.race([onPromise, oncePromise, delay(opts.wait + 1000, opts.default)]).catch(_ => undefined)
-      return cb ? res.then(cb) : res
-    },
+    callback(onAck)
   })
 
-  assign(User.prototype, {
-    /**
-     * saves encrypted and returns a promise with the first result from a peer
-     * @returns {Promise<ack>}
-     */
-    secretAck(data, cb) {
-      var gun = this,
-        callback =
-          cb ||
-          function(ctx) {
-            return ctx
-          }
-      let promise = new Promise((res, rej) => gun.secret(data, ack => (ack.err ? rej(ack) : res(ack))))
-      return promise.then(callback)
-    },
+assign(chain, {
+  /**
+   * fix gun issue https://github.com/amark/gun/issues/855
+   */
+  // eslint-disable-next-line require-await
+  async then(callback = null, wait = 200) {
+    const readPromise = new Promise(resolve => this.once(resolve, { wait }))
 
-    /**
-     * returns the decrypted value
-     * @returns {Promise<any>}
-     */
-    decrypt(cb) {
-      var gun = this,
-        user = gun.back(-1).user(),
-        pair = user.pair(),
-        path = ''
-      gun.back(function(at) {
-        if (at.is) {
-          return
+    return readPromise.then(callback || identity)
+  },
+
+  /**
+   * it returns a promise with the first result from a peer
+   * @param {any} data Data to put onto Gun's node
+   * @param {function} callback Function to be executed once write operation done
+   * @returns {Promise<ack>}
+   */
+  // eslint-disable-next-line require-await
+  async putAck(data, callback = null) {
+    const writePromise = retry(() => promisify(onAck => this.put(data, onAck)), 1)
+
+    return writePromise.then(callback || identity)
+  },
+
+  onThen(callback = null, options = null) {
+    const { wait = 2000, default: defaultValue } = options || {}
+
+    const onPromise = new Promise(resolve =>
+      this.on((v, k, g, ev) => {
+        ev.off()
+
+        // timeout if value is undefined
+        if (v !== undefined) {
+          resolve(v)
         }
-        path += at.get || ''
-      })
-      return (async () => {
-        let sec = await user
-          .get('trust')
-          .get(pair.pub)
-          .get(path)
-          .then()
-        sec = await SEA.decrypt(sec, pair)
-        return gun
-          .then(async data => {
-            if (data == null) {
-              return data
-            }
-            if (!sec) {
-              return Promise.reject('decrypting key missing for ' + path)
-            }
-            let decrypted = await SEA.decrypt(data, sec)
-            return decrypted
-          })
-          .then(cb)
-      })()
-    },
+      }),
+    )
 
-    /**
-     * restore a user from saved credentials
-     * this bypasses the user/password which is slow because of pbkdf2 iterations
-     * this is based on Gun.User.prototype.auth act.g in original sea.js
-     */
-    restore(credentials) {
-      var gun = this,
-        cat = gun._,
-        root = gun.back(-1)
-      const pair = credentials.sea
-      var user = root._.user,
-        at = user._
-      var upt = at.opt
-      at = user._ = root.get('~' + pair.pub)._
-      at.opt = upt
+    const oncePromise = new Promise(resolve =>
+      this.once(
+        v => {
+          // timeout if value is undefined
+          if (v !== undefined) {
+            resolve(v)
+          }
+        },
+        { wait },
+      ),
+    )
 
-      // add our credentials in-memory only to our root user instance
-      user.is = credentials.is
-      at.sea = pair
-      cat.ing = false
-    },
-  })
-})()
+    return Promise.race([onPromise, oncePromise, delay(wait + 1000, defaultValue)])
+      .then(callback || identity)
+      .catch(noop)
+  },
+})
 
-export default gunExtend
+assign(User.prototype, {
+  /**
+   * saves encrypted and returns a promise with the first result from a peer
+   * @returns {Promise<ack>}
+   */
+  // eslint-disable-next-line require-await
+  secretAck(data, callback = null) {
+    const encryptPromise = retry(() => promisify(onAck => this.secret(data, onAck)), 1)
+
+    return encryptPromise.then(callback || identity)
+  },
+
+  /**
+   * returns the decrypted value
+   * @returns {Promise<any>}
+   */
+  decrypt(cb) {
+    var gun = this,
+      user = gun.back(-1).user(),
+      pair = user.pair(),
+      path = ''
+    gun.back(function(at) {
+      if (at.is) {
+        return
+      }
+      path += at.get || ''
+    })
+    return (async () => {
+      let sec = await user
+        .get('trust')
+        .get(pair.pub)
+        .get(path)
+        .then()
+      sec = await SEA.decrypt(sec, pair)
+      return gun
+        .then(async data => {
+          if (data == null) {
+            return data
+          }
+          if (!sec) {
+            return Promise.reject('decrypting key missing for ' + path)
+          }
+          let decrypted = await SEA.decrypt(data, sec)
+          return decrypted
+        })
+        .then(cb || identity)
+    })()
+  },
+
+  /**
+   * restore a user from saved credentials
+   * this bypasses the user/password which is slow because of pbkdf2 iterations
+   * this is based on Gun.User.prototype.auth act.g in original sea.js
+   */
+  restore(credentials) {
+    var gun = this,
+      cat = gun._,
+      root = gun.back(-1)
+    const pair = credentials.sea
+    var user = root._.user,
+      at = user._
+    var upt = at.opt
+    at = user._ = root.get('~' + pair.pub)._
+    at.opt = upt
+
+    // add our credentials in-memory only to our root user instance
+    user.is = credentials.is
+    at.sea = pair
+    cat.ing = false
+  },
+})

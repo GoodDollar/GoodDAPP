0218e3b5fa8efd9b3c04b84f332c60f6652d6baa
yaroslav-fedyshyn-nordwhale <49862004+yaroslav-fedyshyn-nordwhale@users.noreply.github.com>
2020-08-30 10:18:17 +0300

    fix: remove close button from loading dialog when torus login in process (#2346)

--- src/components/auth/torus/AuthTorus.js
+++ src/components/auth/torus/AuthTorus.js
@@ -103,13 +103,14 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
   const signupFacebook = () => handleSignUp('facebook')
   const signupAuth0 = loginType => handleSignUp(loginType === 'email' ? 'auth0-pwdless-email' : 'auth0-pwdless-sms')
 
-  const showLoadingDialog = success => {
+  const showLoadingDialog = () => {
     showDialog({
-      image: success ? undefined : <LoadingIcon />,
+      image: <LoadingIcon />,
       loading: true,
       message: 'Please wait\nThis might take a few seconds...',
       showButtons: false,
       title: `PREPARING\nYOUR WALLET`,
+      showCloseButtons: false,
     })
   }
 
@@ -150,7 +151,8 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
           torusUser = await AsyncStorage.getItem('TorusTestUser').then(JSON.parse)
         }
 
-        showLoadingDialog(false)
+        showLoadingDialog()
+
         if (torusUser == null) {
           torusUser = await torusSDK.triggerLogin(provider)
         }

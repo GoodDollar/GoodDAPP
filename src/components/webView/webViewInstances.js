5f72167d8c38a575d0e3be06fcab20de34e0fc99
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-08-31 15:00:26 +0300

    fix: use direct dashboard link for the statistics redirect, open link with appropriate fn

--- src/components/webView/webViewInstances.js
+++ src/components/webView/webViewInstances.js
@@ -7,7 +7,7 @@ const { isEToro, dashboardUrl, isPhaseOne } = Config
 
 const tou = isPhaseOne ? 'tou1' : 'tou'
 const faq = `faq${isEToro ? '-etoro' : ''}`
-export const dashboardLink = trimEnd(dashboardUrl, ' #/')
+const dashboard = trimEnd(dashboardUrl, ' #/')
 
 export const PrivacyPolicyAndTerms = createIframe(`https://community.gooddollar.org/${tou}/`, 'Privacy Policy & Terms')
 export const PrivacyPolicy = createIframe(`https://community.gooddollar.org/${tou}/#privacy-policy`, 'Privacy Policy')
@@ -15,5 +15,5 @@ export const PrivacyPolicy = createIframe(`https://community.gooddollar.org/${to
 export const Support = createIframe('https://help.gooddollar.org', ' Help & Feedback', true)
 export const SupportForUnsigned = createIframe('https://help.gooddollar.org', ' Help & Feedback', true, 'Login')
 
-export const Statistics = createIframe(`${dashboardLink}/admin/dashboard`, 'Statistics')
+export const Statistics = createIframe(`${dashboard}/admin/dashboard`, 'Statistics')
 export const FAQ = createIframe(`https://community.gooddollar.org/${faq}`, 'FAQ')

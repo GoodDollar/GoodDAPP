e9fd30275eb62b1a6b03d746ab339f8171357a38
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-09-01 18:13:38 -0300

    update: replace of ios & android folders

---
+++ android/app/src/main/java/com/gooddollar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gooddollar;
+
+import com.facebook.react.ReactActivity;
+import io.branch.rnbranch.*;
+import android.content.Intent;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "GoodDollar";
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    RNBranchModule.initSession(getIntent().getData(), this);
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
+  }
+}

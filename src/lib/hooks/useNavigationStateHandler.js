8c40485a373f75f12c62f8d6370323c7e33a6639
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-09-03 15:34:07 +0300

    fix: skip hiding success withdraw dialog if route is not changed

--- src/lib/hooks/useNavigationStateHandler.js
+++ src/lib/hooks/useNavigationStateHandler.js
@@ -1,26 +1,38 @@
 import { useCallback } from 'react'
 import { noop } from 'lodash'
-import SimpleStore, { useCurriedSetters } from '../undux/SimpleStore.js'
+import { useCurriedSetters } from '../undux/SimpleStore.js'
 import { useDialog } from '../undux/utils/dialog'
 import { fireEventFromNavigation } from '../analytics/analytics'
 
+const checkSameRoute = (prevNav, nav, prevIndexes = [], indexes = []) => {
+  const prevIndex = prevNav.index
+  const index = nav.index
+
+  if (prevIndex >= 0 && index >= 0) {
+    prevIndexes.push(prevIndex)
+    indexes.push(index)
+  } else {
+    return prevIndexes.join('') === indexes.join('')
+  }
+
+  return checkSameRoute(prevNav.routes[prevIndex], nav.routes[index], prevIndexes, indexes)
+}
+
 export default (options = {}) => {
   const [, hideDialog] = useDialog()
-  const store = SimpleStore.useStore()
-  const skipNavigationPopupHiding = store.get('skipNavigationPopupHiding')
-  const [setCurrentFeed, setSideMenu, setSkipNavigationPopupHiding] = useCurriedSetters([
-    'currentFeed',
-    'sidemenu',
-    'skipNavigationPopupHiding',
-  ])
+  const [setCurrentFeed, setSideMenu] = useCurriedSetters(['currentFeed', 'sidemenu'])
   const { resetFeed = true, resetMenu = true, resetPopups = true, fireEvent = true, onChange = noop } = options
 
   return useCallback(
     (prevNav, nav, action) => {
-      if (resetPopups && !skipNavigationPopupHiding) {
+      // do not clean the dialog state if the route is not changed
+      const isSameRoute = checkSameRoute(prevNav, nav)
+      if (isSameRoute) {
+        return
+      }
+
+      if (resetPopups) {
         hideDialog()
-      } else {
-        setSkipNavigationPopupHiding(false)
       }
 
       if (resetFeed) {

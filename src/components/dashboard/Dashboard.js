2264539a472459e4a41003208c7d210620c14bef
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-09-04 12:15:56 +0300

    do not clean the dialog state if the route is not changed

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -523,7 +523,7 @@ const Dashboard = props => {
 
         if (transactionHash) {
           fireEvent('WITHDRAW')
-          hideDialog()
+
           showDialog({
             title: 'Payment Link Processed Successfully',
             image: <SuccessIcon />,

a1c28f338ce2143ed923488e58193e2711b235d8
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-09-04 14:35:22 -0300

    fix claim steps

--- src/components/dashboard/Claim.js
+++ src/components/dashboard/Claim.js
@@ -393,6 +393,7 @@ const Claim = props => {
                   fontFamily: 'Roboto',
                   fontSize: regularFontSize,
                   color: 'black',
+                  lineHeight: 22,
                 }}
                 bigNumberUnitProps={{
                   fontFamily: 'Roboto',

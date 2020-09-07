1a68c40b4bc62de7ee747a7c488cdf4c9127fb31
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-09-07 17:43:41 +0300

    fix unit tests

--- src/lib/utils/sizes.js
+++ src/lib/utils/sizes.js
@@ -1,21 +1,8 @@
 import { theme } from '../../components/theme/styles'
 import { getScreenHeight, getScreenWidth, isPortrait } from './orientation'
 
-// have moved the following two functions here to break the circle dependency
-// components/theme/styles -> lib/utls/normalizeText -> lib/utils/orientation -> components/theme/styles
-export const getMaxDeviceWidth = () => {
-  const width = getScreenWidth()
-  const { maxWidthForTabletAndDesktop } = theme.sizes
-
-  return Math.min(width, maxWidthForTabletAndDesktop)
-}
-
-export const getMaxDeviceHeight = () => {
-  const height = getScreenHeight()
-  const { maxHeightForTabletAndDesktop } = theme.sizes
-
-  return Math.min(height, maxHeightForTabletAndDesktop)
-}
+const DESIGN_WIDTH = 360
+const DESIGN_HEIGHT = 640 - 24
 
 /**
  * Receives a size matching the designs baseSize and converts to dp on the current device
@@ -32,33 +19,57 @@ const getDesignRelativeSize = (size, isMax = true, baseSize, currentSize) => {
   return calculatedSize
 }
 
-const DESIGN_WIDTH = 360
+// had to re-defined those utils set as an class instance
+// to allow mocking getMaxDeviceWidth/Height.
+// otherwise there are used as closures and couldn't be overrided
+const sizes = new class {
+  // have moved the following two functions here to break the circle dependency
+  // components/theme/styles -> lib/utls/normalizeText -> lib/utils/orientation -> components/theme/styles
+  getMaxDeviceWidth() {
+    const width = getScreenWidth()
+    const { maxWidthForTabletAndDesktop } = theme.sizes
 
-/**
- * Receives a width matching the designs width and converts to dp on the current device
- * @param {number} width
- * @param {boolean} isMax: should or shouldnt use Math.min
- */
-export const getDesignRelativeWidth = (width, isMax = true) => {
-  const screenWidth = isPortrait() ? getMaxDeviceWidth() : getMaxDeviceHeight()
+    return Math.min(width, maxWidthForTabletAndDesktop)
+  }
 
-  return getDesignRelativeSize(width, isMax, DESIGN_WIDTH, screenWidth)
-}
+  getMaxDeviceHeight() {
+    const height = getScreenHeight()
+    const { maxHeightForTabletAndDesktop } = theme.sizes
 
-const DESIGN_HEIGHT = 640 - 24
+    return Math.min(height, maxHeightForTabletAndDesktop)
+  }
 
-/**
- * Receives a height matching the designs height and converts to dp on the current device
- * @param {number} height
- * @param {boolean} isMax: should or shouldnt use Math.min
- */
-export const getDesignRelativeHeight = (height, isMax = true) => {
-  const screenHeight = isPortrait() ? getMaxDeviceHeight() : getMaxDeviceWidth()
+  /**
+   * Receives a width matching the designs width and converts to dp on the current device
+   * @param {number} width
+   * @param {boolean} isMax: should or shouldnt use Math.min
+   */
+  getDesignRelativeWidth(width, isMax = true) {
+    const { getMaxDeviceWidth, getMaxDeviceHeight } = sizes
+    const screenWidth = isPortrait() ? getMaxDeviceWidth() : getMaxDeviceHeight()
 
-  return getDesignRelativeSize(height, isMax, DESIGN_HEIGHT, screenHeight)
-}
+    return getDesignRelativeSize(width, isMax, DESIGN_WIDTH, screenWidth)
+  }
 
-// eslint-disable-next-line
-export const measure = async view => new Promise(resolve =>
-  view.measure((x, y, width) => resolve({ x, y, width }))
-)
+  /**
+   * Receives a height matching the designs height and converts to dp on the current device
+   * @param {number} height
+   * @param {boolean} isMax: should or shouldnt use Math.min
+   */
+  getDesignRelativeHeight(height, isMax = true) {
+    const { getMaxDeviceWidth, getMaxDeviceHeight } = sizes
+    const screenHeight = isPortrait() ? getMaxDeviceHeight() : getMaxDeviceWidth()
+
+    return getDesignRelativeSize(height, isMax, DESIGN_HEIGHT, screenHeight)
+  }
+
+  // eslint-disable-next-line
+  async measure (view) {
+    return new Promise(resolve => view.measure((x, y, width) => resolve({ x, y, width })))
+  }
+}()
+
+// backward compatibility expors
+export const { getMaxDeviceWidth, getMaxDeviceHeight, getDesignRelativeWidth, getDesignRelativeHeight, measure } = sizes
+
+export default sizes

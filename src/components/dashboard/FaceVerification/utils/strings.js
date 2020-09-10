c7ee73dea83c209a2a3906cd229679f76120b99a
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-09-10 12:26:45 +0300

    fix: add custom liveness best practices text on zoom's native retry screen

--- src/components/dashboard/FaceVerification/utils/strings.js
+++ src/components/dashboard/FaceVerification/utils/strings.js
@@ -1,1 +1,2 @@
 export const zoomResultSuccessMessage = `You’re a beautiful\n& unique snowflake!`
+export const zoomRetryInstructionMessage1 = `Just like a passport picture, keep your head straight, eyes at camera level, and good, even lighting •`

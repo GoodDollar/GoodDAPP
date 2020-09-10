73e84dd99b6a61bb607da6293de99b627f8427ce
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-09-10 14:04:07 +0300

    fix: change text

--- src/components/dashboard/FaceVerification/utils/strings.js
+++ src/components/dashboard/FaceVerification/utils/strings.js
@@ -1,2 +1,2 @@
 export const zoomResultSuccessMessage = `You’re a beautiful\n& unique snowflake!`
-export const zoomRetryInstructionMessage1 = `Just like a passport picture, keep your head straight, eyes at camera level, and good, even lighting •`
+export const zoomRetryInstructionMessage1 = `Just like a passport picture, keep your eyes at camera level, and good, even lighting •`

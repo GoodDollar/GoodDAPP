d80b31e0a38bc2e6be0f9213baba7e8bce95e3d5
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-09-15 17:15:43 -0300

    modify SignupRouter

--- src/SignupRouter.js
+++ src/SignupRouter.js
@@ -14,13 +14,15 @@ import './components/appNavigation/blurFx.css'
 import SimpleStore from './lib/undux/SimpleStore.js'
 import { getOriginalScreenHeight } from './lib/utils/orientation'
 import useNavigationStateHandler from './lib/hooks/useNavigationStateHandler'
+import Welcome from './components/auth/login/WelcomeScreen'
 
 // import IOSWebAppSignIn from './components/signin/IOSWebAppSignIn'
 
-const initialRouteName = 'Auth' // isMobileSafari && isWebApp ? 'IOSWebAppSignIn' : 'Auth'
+const initialRouteName = 'Welcome' // isMobileSafari && isWebApp ? 'IOSWebAppSignIn' : 'Auth'
 const AuthType = Config.torusEnabled ? AuthTorus : Auth
 
 const routes = {
+  Welcome,
   Auth: AuthType,
   Signup,
   InvalidW3TokenError,

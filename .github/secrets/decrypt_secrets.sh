5a5ad946cbe83ac3c9e0eec0d749db9d04731ef0
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-09-23 00:07:36 -0300

    update: uuid to find profile & added new set of certs

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -6,7 +6,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/9e082bc9-c641-4185-9bd8-e8504fa13a53.mobileprovision
 
 echo "Creating keychain"
 security create-keychain -p "" build.keychain

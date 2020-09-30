8a37029ac1724d98e1074e70458d3db4245f65a0
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-09-29 23:45:49 -0300

    add: output logs for ios workflow

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -23,3 +23,5 @@ security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+echo "Keychain results"
+security find-identity -p codesigning -v

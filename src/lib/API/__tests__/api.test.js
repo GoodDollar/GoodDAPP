eec7a252038c12d55faefa74f5f822a4337178d6
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-10-01 18:42:22 +0300

    fix getErrorMessage, add unit tests

---
+++ src/lib/API/__tests__/api.test.js
@@ -0,0 +1,31 @@
+import { defaultErrorMessage, getErrorMessage } from '../api'
+
+describe('API', () => {
+  describe('getErrorMessage', () => {
+    const message = 'Test error message'
+
+    it('should parse string', () => expect(getErrorMessage(message)).toEqual(message))
+
+    it('should parse JS error', () => {
+      const error = new Error(message)
+
+      expect(getErrorMessage(error)).toEqual(message)
+    })
+
+    it('should parse { ok, error } shape', () => {
+      const error = { ok: 0, error: message }
+
+      expect(getErrorMessage(error)).toEqual(message)
+    })
+
+    it('should parse { ok, message } shape', () => {
+      const error = { ok: 0, message }
+
+      expect(getErrorMessage(error)).toEqual(message)
+    })
+
+    it('should return default message in other cases', () => {
+      ;[null, undefined, new Error(), {}].forEach(error => expect(getErrorMessage(error)).toEqual(defaultErrorMessage))
+    })
+  })
+})

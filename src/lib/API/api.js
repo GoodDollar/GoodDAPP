7543cea11c9c463915987c7858eb139f2c1fb985
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-10-01 22:19:33 +0300

    [RN] (Bug) 2470 Error message from server not logged correctly (#2526)
    * fix getErrorMessage, add unit tests
    
    * add log axios response in /user/add

--- src/lib/API/api.js
+++ src/lib/API/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 import type { $AxiosXHR, AxiosInstance, AxiosPromise } from 'axios'
-import { identity, isError, isPlainObject, isString, throttle } from 'lodash'
+import { identity, isObject, isString, throttle } from 'lodash'
 
 import AsyncStorage from '../utils/asyncStorage'
 import Config from '../../config/config'
@@ -30,22 +30,28 @@ export type UserRecord = NameRecord &
     username?: string,
   }
 
+export const defaultErrorMessage = 'Unexpected error happened during api call'
+
 export const getErrorMessage = apiError => {
-  let message
+  let errorMessage
 
   if (isString(apiError)) {
-    message = apiError
-  } else if (isError(apiError)) {
-    message = apiError.message
-  } else if (isPlainObject(apiError)) {
-    message = apiError.error || apiError.message
+    errorMessage = apiError
+  } else if (isObject(apiError)) {
+    // checking all cases:
+    // a) JS Error - will have .message property
+    // b) { ok: 0, message: 'Error message' } shape
+    // c) { ok: 0, error: 'Error message' } shape
+    const { message, error } = apiError
+
+    errorMessage = message || error
   }
 
-  if (!message) {
-    message = 'Unexpected error happened during api call'
+  if (!errorMessage) {
+    errorMessage = defaultErrorMessage
   }
 
-  return message
+  return errorMessage
 }
 
 /**
@@ -148,14 +154,10 @@ export class APIService {
    * `/user/add` post api call
    * @param {UserRecord} user
    */
-  addUser = throttle((user: UserRecord): AxiosPromise<any> => {
-    //-skipRegistrationStep ONLY FOR TESTING  delete this condition aftere testing
-    return this.client.post(
-      '/user/add',
-      { user, skipRegistrationStep: global.skipRegistrationStep },
-      { withCredentials: true }, //we need also the cookies for utm
-    )
-  }, 1000)
+  addUser = throttle(
+    (user: UserRecord): AxiosPromise<any> => this.client.post('/user/add', { user }, { withCredentials: true }),
+    1000,
+  )
 
   /**
    * `/user/delete` post api call

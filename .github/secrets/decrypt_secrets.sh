f2679a5001e5833ed7e196b6f029fe34a658180f
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-10-01 23:19:12 -0300

    Revert "update: ios build flow to use full appcenter"
    This reverts commit aae7f99bd635721a4092f5ba7d4cb6af3cba2ec7.

---
+++ .github/secrets/decrypt_secrets.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Decrypting files"
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/GoodDollar\ Ad\ Hoc.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+echo "List profiles - before"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "Move profiles"
+cp ./.github/secrets/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "List profiles - after"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+
+echo "Creating keychain"
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PROFILE_KEY" -A
+
+echo "Installing in keychain"
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+echo "Keychain results"
+security find-identity -p codesigning -v

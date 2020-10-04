bf21e8b7514cb39f1ef6599df79e457075338819
sirpy <hadarr@gmail.com>
2020-10-04 16:02:53 +0300

    add: gun node probs

--- src/lib/gundb/gundb.js
+++ src/lib/gundb/gundb.js
@@ -6,19 +6,21 @@ import '@gooddollar/gun/lib/store'
 import '@gooddollar/gun/lib/rindexed'
 import '@gooddollar/gun/nts'
 
-import { assign } from 'lodash'
+import { assign, findLastIndex } from 'lodash'
 
 import './gundb-extend'
 import Config from '../../config/config'
 import logger from '../logger/pino-logger'
 
-const { gunPublicUrl, forcePeer } = Config
+const { gunPublicUrl, forcePeer, peersProb } = Config
 const { RindexedDB } = window || {}
 const gunOptions = []
 const peers = gunPublicUrl.split(',')
-let peer = peers.length > 1 && Math.random() <= 0.1 ? peers[1] : peers[0]
+const prob = Math.random()
+const peerIndex = findLastIndex(peersProb, x => x >= prob)
+let peer = peers[peerIndex] || peers[0]
 if (forcePeer) {
-  peer = peers[1]
+  peer = peers[forcePeer]
 }
 if (process.env.NODE_ENV !== 'test') {
   gunOptions.push({
@@ -27,9 +29,9 @@ if (process.env.NODE_ENV !== 'test') {
   })
 }
 
-const gun = Gun(...gunOptions)
+let gun = Gun(...gunOptions)
 
-logger.debug('Initialized gundb', { gunOptions })
+logger.debug('Initialized gundb', { gunOptions, peers, prob, peerIndex })
 assign(global, { gun })
 
 export default gun

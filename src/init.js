322313a71d40f63f42ff5458716f8347fa39e0df
sirpy <hadarr@gmail.com>
2020-10-07 00:10:53 +0300

    add: app_open event also for non registered

--- src/init.js
+++ src/init.js
@@ -1,11 +1,6 @@
 //@flow
-import { pick } from 'lodash'
-import { Platform } from 'react-native'
 import goodWallet from './lib/wallet/GoodWallet'
 import userStorage from './lib/gundb/UserStorage'
-import isWebApp from './lib/utils/isWebApp'
-import { APP_OPEN, fireEvent, initAnalytics } from './lib/analytics/analytics'
-import { extractQueryParams } from './lib/share'
 import { setUserStorage, setWallet } from './lib/undux/SimpleStore'
 import logger from './lib/logger/pino-logger'
 
@@ -14,34 +9,21 @@ const log = logger.child({ from: 'init' })
 let initialized = false
 
 // userStorage.ready already awaits for goodwallet
-export const init = () =>
-  userStorage.ready.then(async () => {
-    let source = 'none'
-    log.debug('wallet and storage ready, initializing analytics', { initialized })
+export const init = async () => {
+  await userStorage.ready
+  log.debug('wallet and storage ready, initializing analytics', { initialized })
 
-    if (initialized === false) {
-      global.wallet = goodWallet
+  if (initialized === false) {
+    global.wallet = goodWallet
 
-      // set wallet to simple storage so we can use it in InternetConnection
-      setWallet(goodWallet)
+    // set wallet to simple storage so we can use it in InternetConnection
+    setWallet(goodWallet)
 
-      // set userStorage to simple storage
-      setUserStorage(userStorage)
+    // set userStorage to simple storage
+    setUserStorage(userStorage)
 
-      await initAnalytics()
-      log.debug('analytics has been initialized')
+    initialized = true
+  }
 
-      // FIXME RN INAPPLINKS
-      if (Platform.OS === 'web') {
-        const params = extractQueryParams(window.location.href)
-
-        source = document.referrer.match(/^https:\/\/(www\.)?gooddollar\.org/) == null ? source : 'web3'
-        source = Object.keys(pick(params, ['inviteCode', 'paymentCode', 'code'])).pop() || source
-      }
-
-      fireEvent(APP_OPEN, { source, isWebApp })
-      initialized = true
-    }
-
-    return { goodWallet, userStorage, source }
-  })
+  return { goodWallet, userStorage }
+}

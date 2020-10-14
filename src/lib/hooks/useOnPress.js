7eb4c61952bb81598e8e9f414fb6f862264078f1
Mathias Morales <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-10-14 09:14:40 -0300

    [RN] misc issues related to Mnemonic. (#2576)
    * fixed buttons
    
    * fixed useOnPress hook
    
    * issue with AssynStorage.clear
    
    * remove eslint disable
    
    * fixed useOnPress
    
    * fix useOnPress
    
    * fixed buttons and, save version
    
    Co-authored-by: Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>

--- src/lib/hooks/useOnPress.js
+++ src/lib/hooks/useOnPress.js
@@ -1,24 +1,34 @@
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
+import { isNil } from 'lodash'
 
-import { debounce, identity } from 'lodash'
+const useOnPress = (callback, deps = []) =>
+  useCallback(
+    event => {
+      if (event) {
+        event.preventDefault()
+      }
 
-const debounceWrapper = callback => debounce(callback, 500, { leading: true, trailing: false })
+      return callback(event)
+    },
+    [callback, ...deps],
+  )
 
-const useOnPressHook = (callback, deps = [], options = {}) => {
-  const { debounce = false } = options
-  const memoizedCallback = useCallback(callback, deps)
-  const wrapper = debounce ? debounceWrapper : identity
+export const useDebouncedOnPress = (callback, deps = []) => {
+  const nextInvokationRef = useRef(null)
 
-  return useCallback(
-    wrapper(event => {
-      event.preventDefault()
+  return useOnPress(
+    event => {
+      const nextInvokation = nextInvokationRef.current
+      const currentTs = Date.now()
 
-      return memoizedCallback(event)
-    }),
-    [memoizedCallback],
+      if (isNil(nextInvokation) || currentTs >= nextInvokation) {
+        callback(event)
+      }
+
+      nextInvokationRef.current = currentTs + 500
+    },
+    [callback, ...deps],
   )
 }
 
-export const useDebouncedOnPress = (callback, deps = []) => useOnPressHook(callback, deps, { debounce: true })
-
-export default (callback, deps = []) => useOnPressHook(callback, deps)
+export default useOnPress

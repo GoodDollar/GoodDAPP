3668169dadd6d47fbec5f77ba821d4f225a7821a
MathiasMoralesDixtra <59056508+MathiasMoralesDixtra@users.noreply.github.com>
2020-10-16 13:19:30 -0300

    improve solution

--- src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
+++ src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
@@ -2,13 +2,6 @@
 
 import React from 'react'
 import PhoneNumberInput from '../../common/form/PhoneNumberInput/PhoneNumberInput'
-import Section from '../../common/layout/Section'
 import './PhoneNumberInput.css'
 
-export default props => {
-  return (
-    <Section.Stack className="signup_phone_input">
-      <PhoneNumberInput id="signup_phone" {...props} />
-    </Section.Stack>
-  )
-}
+export default props => <PhoneNumberInput id="signup_phone" {...props} />

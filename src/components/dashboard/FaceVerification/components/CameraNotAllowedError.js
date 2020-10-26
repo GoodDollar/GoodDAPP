638cd56bb209949d53ea075a166b17adb24601dd
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-10-26 13:29:53 +0200

    restore temporary disabled feature

--- src/components/dashboard/FaceVerification/components/CameraNotAllowedError.js
+++ src/components/dashboard/FaceVerification/components/CameraNotAllowedError.js
@@ -19,16 +19,38 @@ const CameraNotAllowedError = ({ onRetry, exception = {} }) => {
   const [showDialog] = useDialog()
 
   useEffect(() => {
+    const buttons = []
+    let errorMessage = "We can't find your camera.."
+    let title = `Please connect yours\nor\ntry a different device`
+    let text = null
+
+    if (!cameraDoesNotExist) {
+      // temporary disabling this feature
+      // do not remove this block
+      /*buttons.push({
+          text: 'How to do that?',
+          mode: 'text',
+          // explanation dialog auto dismisses popup
+          // so no dismiss callback is passed to the action
+          action: () => {
+            // do some
+            onRetry()
+          },
+      })*/
+
+      errorMessage = 'We can’t access your camera...'
+      title = 'Please enable camera permission'
+      text = 'Change it via your device settings'
+    }
+
     showDialog({
       content: (
         <ExplanationDialog
-          errorMessage={cameraDoesNotExist ? "We can't find your camera.." : 'We can’t access your camera...'}
-          title={
-            cameraDoesNotExist ? `Please connect yours\nor\ntry a different device` : 'Please enable camera permission'
-          }
-          text={cameraDoesNotExist ? null : 'Change it via your device settings'}
+          errorMessage={errorMessage}
+          title={title}
+          text={text}
           imageSource={illustration}
-          buttons={[]}
+          buttons={buttons}
         />
       ),
       type: 'error',

085df344c074e251e9948dfe00eaaa545ff70da1
Mariano Amado <28148619+MarianoAmado@users.noreply.github.com>
2020-10-26 17:02:24 -0300

    Switch to another device styles: * Fixed spacing between items & container paddings * Fixed image width * Improved sizes on desktop browser * using theme's values when possible

--- src/components/dashboard/FaceVerification/components/SwitchToAnotherDevice.js
+++ src/components/dashboard/FaceVerification/components/SwitchToAnotherDevice.js
@@ -6,7 +6,7 @@ import Separator from '../../../common/layout/Separator'
 import { CustomButton, Section, Wrapper } from '../../../common'
 
 import useOnPress from '../../../../lib/hooks/useOnPress'
-import { isMobileOnly } from '../../../../lib/utils/platform'
+import { isBrowser } from '../../../../lib/utils/platform'
 
 import { getDesignRelativeHeight, getDesignRelativeWidth } from '../../../../lib/utils/sizes'
 import { withStyles } from '../../../../lib/styles'
@@ -23,20 +23,24 @@ const SwitchToAnotherDevice = ({ styles, displayTitle, exception, screenProps })
   return (
     <Wrapper>
       <View style={styles.topContainer}>
-        <Section style={styles.descriptionContainer} justifyContent="space-evenly">
+        <Section style={styles.descriptionContainer} justifyContent="space-between">
           <Section.Title fontWeight="medium" textTransform="none" color="red">
             {displayTitle}
             {',\nPlease try to switch\nto another device'}
           </Section.Title>
-          <Image source={illustration} resizeMode="contain" style={styles.errorImage} />
-          <Section style={styles.errorSection}>
+          <Image
+            source={illustration}
+            resizeMode="contain"
+            style={[styles.errorImage, isBrowser ? styles.browserSizing : {}]}
+          />
+          <Section style={[styles.errorSection, isBrowser ? styles.browserSizing : {}]}>
             <Separator width={2} />
             <View style={styles.descriptionWrapper}>
               <Text color="primary" fontSize={18} lineHeight={25}>
                 {'Sometimes, switching to a\ndifferent device is a good solution.'}
               </Text>
               <Text color="primary" fontWeight="bold" fontSize={18} lineHeight={25}>
-                Sorry about that… :)
+                {'Sorry about that… :)'}
               </Text>
             </View>
             <Separator width={2} />
@@ -53,7 +57,6 @@ const SwitchToAnotherDevice = ({ styles, displayTitle, exception, screenProps })
 const getStylesFromProps = ({ theme }) => ({
   topContainer: {
     alignItems: 'center',
-    justifyContent: 'space-evenly',
     display: 'flex',
     backgroundColor: theme.colors.surface,
     height: '100%',
@@ -61,36 +64,35 @@ const getStylesFromProps = ({ theme }) => ({
     flexGrow: 1,
     flexShrink: 0,
     paddingBottom: getDesignRelativeHeight(theme.sizes.defaultDouble),
-    paddingLeft: getDesignRelativeWidth(theme.sizes.default),
-    paddingRight: getDesignRelativeWidth(theme.sizes.default),
     paddingTop: getDesignRelativeHeight(theme.sizes.defaultDouble),
-    borderRadius: 5,
+    borderRadius: theme.sizes.borderRadius,
   },
   errorImage: {
     height: getDesignRelativeHeight(146, false),
-    marginTop: isMobileOnly ? getDesignRelativeHeight(32) : 0,
-    marginBottom: isMobileOnly ? getDesignRelativeHeight(40) : 0,
   },
   descriptionContainer: {
     flex: 1,
     marginBottom: 0,
-    paddingBottom: getDesignRelativeHeight(theme.sizes.defaultDouble),
-    paddingLeft: getDesignRelativeWidth(theme.sizes.default),
-    paddingRight: getDesignRelativeWidth(theme.sizes.default),
-    paddingTop: getDesignRelativeHeight(theme.sizes.default),
+    paddingBottom: getDesignRelativeHeight(44),
+    paddingHorizontal: getDesignRelativeWidth(theme.sizes.defaultQuadruple),
+    paddingTop: isBrowser ? getDesignRelativeHeight(22) : 0,
     width: '100%',
   },
   action: {
     width: '100%',
+    paddingHorizontal: getDesignRelativeWidth(theme.sizes.default),
   },
   errorSection: {
-    paddingBottom: 0,
-    paddingTop: 0,
+    paddingVertical: 0,
+    paddingHorizontal: 0,
     marginBottom: 0,
   },
   descriptionWrapper: {
-    paddingTop: getDesignRelativeHeight(25),
-    paddingBottom: getDesignRelativeHeight(25),
+    paddingVertical: getDesignRelativeHeight(isBrowser ? theme.sizes.defaultDouble : theme.sizes.default),
+  },
+  browserSizing: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
 })
 

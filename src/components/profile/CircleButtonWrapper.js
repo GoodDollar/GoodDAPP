8d38357468f2d92a1067e245824a50dc34bee9c6
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2020-10-29 12:12:53 +0200

    fix useOnPress -  return empty value if callback is empty

--- src/components/profile/CircleButtonWrapper.js
+++ src/components/profile/CircleButtonWrapper.js
@@ -1,8 +1,12 @@
 // @flow
 import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
-import { withStyles } from '../../lib/styles'
+
 import { Icon, Text } from '../common'
+
+import useOnPress from '../../lib/hooks/useOnPress'
+
+import { withStyles } from '../../lib/styles'
 
 const CircleButtonWrapper = ({
   label,
@@ -15,22 +19,26 @@ const CircleButtonWrapper = ({
   iconName,
   iconColor,
   iconSize,
-}) => (
-  <View style={containerStyle}>
-    <TouchableOpacity
-      cursor={disabled ? 'inherit' : 'pointer'}
-      onPress={disabled ? undefined : onPress}
-      style={[styles.button, style]}
-    >
-      <Icon color={iconColor} size={iconSize} name={iconName} />
-    </TouchableOpacity>
-    {!!label && (
-      <Text fontSize={10} fontWeight="500" lineHeight={11} color="white" style={[styles.label, labelStyles]}>
-        {label}
-      </Text>
-    )}
-  </View>
-)
+}) => {
+  const onIconPress = useOnPress(onPress)
+
+  return (
+    <View style={containerStyle}>
+      <TouchableOpacity
+        cursor={disabled ? 'inherit' : 'pointer'}
+        onPress={!disabled && onIconPress}
+        style={[styles.button, style]}
+      >
+        <Icon color={iconColor} size={iconSize} name={iconName} />
+      </TouchableOpacity>
+      {label && (
+        <Text fontSize={10} fontWeight="500" lineHeight={11} color="white" style={[styles.label, labelStyles]}>
+          {label}
+        </Text>
+      )}
+    </View>
+  )
+}
 
 CircleButtonWrapper.defaultProps = {
   iconColor: '#fff',

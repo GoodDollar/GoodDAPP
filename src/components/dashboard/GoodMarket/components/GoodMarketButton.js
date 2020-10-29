84c9603026189592de1dfb2584b3fb3278971e85
Mariano Amado <28148619+MarianoAmado@users.noreply.github.com>
2020-10-29 15:55:26 -0300

    [RN] GoodMarketButton fix: * Fixed button alignment on native * Increasing paddingBottom on ios to prevent text from showing behind 'gestures/home bar'

--- src/components/dashboard/GoodMarket/components/GoodMarketButton.js
+++ src/components/dashboard/GoodMarket/components/GoodMarketButton.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useRef } from 'react'
-import { Animated, Easing, TouchableOpacity } from 'react-native'
+import { Animated, Easing, TouchableOpacity, View } from 'react-native'
 import { noop } from 'lodash'
 
 import Icon from '../../../common/view/Icon'
@@ -11,6 +11,7 @@ import { useDialog } from '../../../../lib/undux/utils/dialog'
 import { fireEvent, GOTO_MARKET_POPUP } from '../../../../lib/analytics/analytics'
 import { withStyles } from '../../../../lib/styles'
 import { getDesignRelativeWidth } from '../../../../lib/utils/sizes'
+import { isIOSNative } from '../../../../lib/utils/platform'
 import GoodMarketDialog from './GoodMarketDialog'
 
 export const marketAnimationDuration = 1500
@@ -58,24 +59,30 @@ const GoodMarketButton = ({ styles, hidden = false }) => {
   }
 
   return (
-    <TouchableOpacity onPress={onButtonClicked} style={styles.marketButton}>
-      <Icon name="goodmarket" size={36} color="white" />
-    </TouchableOpacity>
+    <View style={styles.btnContainer}>
+      <TouchableOpacity onPress={onButtonClicked} style={styles.marketButton}>
+        <Icon name="goodmarket" size={36} color="white" />
+      </TouchableOpacity>
+    </View>
   )
 }
 
 const getStylesFromProps = ({ theme }) => ({
+  btnContainer: {
+    position: 'absolute',
+    right: '50%',
+    left: '50%',
+    bottom: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
   marketButton: {
     backgroundColor: theme.colors.primary,
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingVertical: 8,
+    paddingTop: theme.sizes.default,
+    paddingBottom: isIOSNative ? theme.sizes.defaultDouble : theme.sizes.default,
     width: getDesignRelativeWidth(140),
-    position: 'absolute',
-    right: 0,
-    left: 0,
-    bottom: 0,
-    marginHorizontal: 'auto',
     borderTopRightRadius: 22,
     borderTopLeftRadius: 22,
   },

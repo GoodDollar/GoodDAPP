7c34342f8961f74740af121369db4d0d674e8203
Mariano <28148619+MarianoAmado@users.noreply.github.com>
2020-11-04 16:34:47 -0300

    Fix Signup phone form: (#2699)
    * removed div, using Section.Stack again but without className
    * Changed the way of overriding PhoneNumberInput css by adding the className in the component

--- src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
+++ src/components/signup/PhoneNumberInput/PhoneNumberInput.web.js
@@ -4,4 +4,4 @@ import React from 'react'
 import PhoneNumberInput from '../../common/form/PhoneNumberInput/PhoneNumberInput'
 import './PhoneNumberInput.css'
 
-export default props => <PhoneNumberInput id="signup_phone" {...props} />
+export default props => <PhoneNumberInput className="signup_phone_input" id="signup_phone" {...props} />

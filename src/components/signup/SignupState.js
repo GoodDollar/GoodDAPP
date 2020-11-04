627378248413b76a99bc2d49dc8069df837f4613
sirpy <hadarr@gmail.com>
2020-11-04 10:55:29 +0200

    fix: empty catch

--- src/components/signup/SignupState.js
+++ src/components/signup/SignupState.js
@@ -471,9 +471,9 @@ const Signup = ({ navigation }: { navigation: any, screenProps: any }) => {
   //check if email/mobile was used to register before and offer user to login instead
   const checkExisting = useCallback(
     async searchBy => {
-      const { exists, fullName, provider: foundOtherProvider } = await userExists(searchBy).catch(() => {
-        // eslint-disable-next-line no-unused-expressions
-        false
+      const { exists, fullName, provider: foundOtherProvider } = await userExists(searchBy).catch(e => {
+        log.warn('userExists check failed:', e.message, e)
+        return { exists: false }
       })
 
       log.debug('checking userAlreadyExist', { exists, fullName, foundOtherProvider })

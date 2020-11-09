3b3a4a14b5dd91659083dd3011428e1ef0a13839
Mariano Amado <28148619+MarianoAmado@users.noreply.github.com>
2020-11-09 17:12:24 -0300

    fix blurred dialog background on the screens above stack navigation (merge commit from #2711)

--- src/components/appNavigation/stackNavigation.js
+++ src/components/appNavigation/stackNavigation.js
@@ -242,6 +242,7 @@ class AppView extends Component<AppViewProps, AppViewState> {
     const { descriptors, navigation, navigationConfig, screenProps: incomingScreenProps, store } = this.props
     const activeKey = navigation.state.routes[navigation.state.index].key
     const descriptor = descriptors[activeKey]
+
     const {
       title,
       navigationBar: NavigationBar,
@@ -249,6 +250,7 @@ class AppView extends Component<AppViewProps, AppViewState> {
       backButtonHidden,
       disableScroll,
     } = descriptor.options
+
     const screenProps = {
       ...incomingScreenProps,
       navigationConfig,
@@ -260,17 +262,19 @@ class AppView extends Component<AppViewProps, AppViewState> {
       screenState: this.state.currentState,
       setScreenState: this.setScreenState,
     }
+
     log.info('stackNavigation Render: FIXME rerender', descriptor, activeKey)
+
     const Component = this.getComponent(descriptor.getComponent(), { screenProps })
     const pageTitle = title || activeKey
     const open = store.get('sidemenu').visible
-    const { visible: dialogVisible } = (store.get('currentScreen') || {}).dialogData || {}
-    const currentFeed = store.get('currentFeed')
+
     const menu = (
       <SafeAreaView style={styles.safeArea}>
         <SideMenuPanel navigation={navigation} />
       </SafeAreaView>
     )
+
     return (
       <React.Fragment>
         {open && (
@@ -284,7 +288,7 @@ class AppView extends Component<AppViewProps, AppViewState> {
             />
           </View>
         )}
-        <Blurred style={fullScreenContainer} blur={open || dialogVisible || currentFeed}>
+        <Blurred whenSideMenu>
           {!navigationBarHidden &&
             (NavigationBar ? (
               <NavigationBar />
@@ -304,20 +308,6 @@ class AppView extends Component<AppViewProps, AppViewState> {
   }
 }
 
-const fullScreen = {
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-  position: 'absolute',
-}
-const fullScreenContainer = {
-  ...fullScreen,
-  display: 'flex',
-  flexGrow: 1,
-  flexDirection: 'column',
-}
-
 const styles = StyleSheet.create({
   scrollView: {
     display: 'flex',
@@ -329,7 +319,11 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   sideMenuContainer: {
-    ...fullScreen,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: 'absolute',
     transform: [
       {
         translateX: Platform.select({
--- src/components/common/view/Blur/Blurred.web.js
+++ src/components/common/view/Blur/Blurred.web.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import './blurFx.css'
+import React, { useMemo } from 'react'
+import { View } from 'react-native'
+import { isAndroid } from 'mobile-device-detect'
+import { get } from 'lodash'
 
-const Blurred = props => (
-  <div style={props.style} className={props.blur ? 'blurFx' : ''}>
-    {props.children}
-  </div>
-)
+import SimpleStore from '../../../../lib/undux/SimpleStore.js'
 
-export default Blurred
+import { withStyles } from '../../../../lib/styles'
+import { getOriginalScreenHeight } from '../../../../lib/utils/orientation'
+
+const Blurred = ({ styles, children, whenDialog = false, whenSideMenu = false }) => {
+  const store = SimpleStore.useStore()
+
+  const blurStyles = useMemo(() => {
+    const { fullScreen, blurFx } = styles
+    const isPopupShown = get(store.get('currentScreen'), 'dialogData.visible', false)
+    const isSideNavShown = get(store.get('sidemenu'), 'visible', false)
+    const isMobileKbdShown = !!store.get('isMobileKeyboardShown')
+    const isFeedPopupShown = !!store.get('currentFeed')
+
+    const minHeight = isAndroid && isMobileKbdShown ? getOriginalScreenHeight() : 480
+    const isDialogShown = isPopupShown || isFeedPopupShown
+    const stylesList = [{ minHeight }, fullScreen]
+
+    if ((whenDialog && isDialogShown) || (whenSideMenu && isSideNavShown)) {
+      stylesList.push(blurFx)
+    }
+
+    return stylesList
+  }, [whenDialog, whenSideMenu, styles, store])
+
+  return <View style={blurStyles}>{children}</View>
+}
+
+const getStylesFromProps = () => ({
+  fullScreen: {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: 'absolute',
+    display: 'flex',
+    flexGrow: 1,
+    flexDirection: 'column',
+  },
+  blurFx: {
+    // TODO: native style
+    filter: 'blur(24px) brightness(0.8) opacity(0.5)',
+  },
+})
+
+export default withStyles(getStylesFromProps)(Blurred)

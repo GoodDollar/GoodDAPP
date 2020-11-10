505ee894be069a897f2f4485f312ec467aec76dd
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-11-10 13:13:36 +0200

    (UI BUG) Export wallet - Address pop up - The background should be blurred (as per design) (#2711)
    * fix blurred dialog background on the screens above stack navigation
    
    * - update snapshots
    
    * update snapshots

--- src/components/common/view/Blurred.web.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Blurred = props => (
-  <div style={props.style} className={props.blur ? 'blurFx' : ''}>
-    {props.children}
-  </div>
-)
-
-export default Blurred

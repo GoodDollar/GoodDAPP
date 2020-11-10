60349efd91d2d6823fff831df5c02900d80d112b
sirpy <hadarr@gmail.com>
2020-11-10 17:42:21 +0200

    fix: #2708 dont always use identifier

--- src/lib/login/userExists.js
+++ src/lib/login/userExists.js
@@ -3,9 +3,13 @@ import { default as API } from '../API/api'
 import { GoodWallet } from '../wallet/GoodWalletClass'
 
 export const userExists = async ({ mnemonics, privateKey, email, mobile }): Promise<any> => {
-  const wallet = new GoodWallet({ mnemonic: mnemonics || privateKey })
-  await wallet.ready
-  const identifier = wallet.getAccountForType('login')
+  const walletSeed = mnemonics || privateKey
+  let identifier
+  if (walletSeed) {
+    const wallet = new GoodWallet({ mnemonic: walletSeed })
+    await wallet.ready
+    identifier = wallet.getAccountForType('login')
+  }
   const { data } = await API.userExistsCheck({ identifier, email, mobile })
 
   return data

4006869fef36c34bfd75e8b45889853f068f881c
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-11-11 00:48:56 -0300

    add: check to not wrapp with hot when running unit tests

--- src/lib/utils/hotReloadComponent.js
+++ src/lib/utils/hotReloadComponent.js
@@ -1,10 +1,13 @@
+import Config from '../../config/config'
 import { isWeb } from './platform'
+
+const runningTests = Config.env === 'test'
 
 /*
   Hot reloads components for web-ONLY as react-native already has built-in hot refresher
 */
 const hotReloadComponent = Component => {
-  if (isWeb) {
+  if (isWeb && !runningTests) {
     const { hot } = require('react-hot-loader/root')
     return hot(Component)
   }

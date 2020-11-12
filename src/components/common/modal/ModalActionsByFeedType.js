11da2e0db6e8b10c5ff873279e7b5ea920f978e4
sirpy <hadarr@gmail.com>
2020-11-12 13:29:42 +0200

    add: dont use gdstore for inviteCode

--- src/components/common/modal/ModalActionsByFeedType.js
+++ src/components/common/modal/ModalActionsByFeedType.js
@@ -36,8 +36,8 @@ const ModalActionsByFeedType = ({ theme, styles, item, handleModalClose, navigat
   const { canShare, generateSendShareObject, generateSendShareText, generateShareLink } = useNativeSharing()
 
   const store = GDStore.useStore()
-  const inviteCode = store.get('inviteCode')
   const _handleModalClose = useCallback(handleModalClose)
+  const inviteCode = userStorage.userProperties.get('inviteCode')
   const { fullName: currentUserName } = store.get('profile')
 
   const [cancellingPayment, setCancellingPayment] = useState(false)

a55829eb5fa6fbd9751ee6b71c4aa0b29ac28831
sirpy <hadarr@gmail.com>
2020-11-22 09:23:47 +0200

    fix: show signup error message

--- src/components/auth/torus/AuthTorus.web.js
+++ src/components/auth/torus/AuthTorus.web.js
@@ -145,12 +145,10 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
         } else {
           log.error('torus login failed', e.message, e, { dialogShown: true })
         }
-
-        showErrorDialog('We were unable to complete the signup. Please try again.')
       }
       return { torusUser, replacing }
     },
-    [showErrorDialog, torusSDK],
+    [torusSDK],
   )
 
   const showLoadingDialog = () => {
@@ -219,7 +217,7 @@ const AuthTorus = ({ screenProps, navigation, styles, store }) => {
     showLoadingDialog()
     const { torusUser, replacing } = await getTorusUser(provider)
     if (torusUser == null) {
-      hideDialog()
+      showErrorDialog('We were unable to complete the signup. Please try again.')
       return
     }
 

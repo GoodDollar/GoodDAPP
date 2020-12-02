2e0c6ada19dfc631b6eab29d1c407f8b6b4d1096
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2020-12-03 00:14:51 +0200

    Update FormatDate.js

--- src/lib/utils/FormatDate.js
+++ src/lib/utils/FormatDate.js
@@ -1,28 +1,18 @@
 // @flow
 import { filter } from 'lodash'
-import moment from 'moment'
-import { NativeModules, Platform } from 'react-native'
 
 /**
  * format date util function
- * * DO NOT use .toLocaleString to format dates as it has limitations on android
  * @param {string} stringDate
  */
-export const getFormattedDateTime = (stringDate: string = null) => {
-  const locale = getLocaleByPlatform()
-  moment.locale(locale())
-
-  const localeData = moment.localeData()
-  const format = localeData.longDateFormat('L')
-
-  const momentTime = moment(...filter([stringDate]))
-  return `${momentTime.format(format)} ${momentTime.format('HH:mm')}`
-}
-
-const getLocaleByPlatform = () =>
-  Platform.select({
-    web: () => window.navigator.userLanguage || window.navigator.language,
-    ios: () =>
-      NativeModules.SettingsManager.settings.AppleLocale || NativeModules.SettingsManager.settings.AppleLanguages[0],
-    android: () => NativeModules.I18nManager.localeIdentifier,
-  })
+export const getFormattedDateTime = (stringDate: string = null) =>
+   new Date(...filter([stringDate]))
+     .toLocaleString([], {
+       day: '2-digit',
+       month: '2-digit',
+       year: '2-digit',
+       hour: '2-digit',
+       minute: '2-digit',
+       hour12: false,
+     })
+     .replace(',', '')

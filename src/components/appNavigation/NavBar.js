b361ef64d1f19518dbb0ff17229e889b1382f947
sirpy <hadarr@gmail.com>
2020-12-02 14:55:30 +0200

    add: material design back button effect

--- src/components/appNavigation/NavBar.js
+++ src/components/appNavigation/NavBar.js
@@ -1,11 +1,12 @@
 //@flow
 import React from 'react'
+import { Platform } from 'react-native'
 import { Appbar } from 'react-native-paper'
 import { withStyles } from '../../lib/styles'
 import normalize from '../../lib/utils/normalizeText'
 import { getShadowStyles } from '../../lib/utils/getStyles'
-import { Icon } from '../common'
 import { isMobile } from '../../lib/utils/platform'
+import { IconButton } from '../common'
 
 /**
  * @type
@@ -30,12 +31,28 @@ class NavBar extends React.Component<NavBarProps> {
     return (
       <NavigationBar dark style={styles.topbarStyles}>
         {!backToWallet && this.props.goBack && (
-          <Icon name="arrow-back" onPress={this.props.goBack} color="white" size={22} style={styles.backButton} />
+          <IconButton
+            name="arrow-back"
+            onPress={this.props.goBack}
+            color="white"
+            reverse={false}
+            reverseColor={'transparent'}
+            size={22}
+            style={styles.backButton}
+          />
         )}
         <Appbar.Content title={this.props.title} titleStyle={styles.titleStyle} />
         {!backToWallet && this.props.goBack && <Appbar.Action color="white" />}
         {backToWallet && this.props.goBack && (
-          <Icon name="wallet" onPress={this.props.goBack} color="white" size={36} style={styles.walletButton} />
+          <IconButton
+            name="wallet"
+            reverse={false}
+            reverseColor={'transparent'}
+            onPress={this.props.goBack}
+            color="white"
+            size={36}
+            style={styles.walletButton}
+          />
         )}
       </NavigationBar>
     )
@@ -58,6 +75,8 @@ const getStylesFromProps = ({ theme }) => ({
   backButton: {
     marginLeft: 15,
     width: 33,
+    flex: Platform.select({ web: 'inherit', default: 0 }),
+    justifyContent: 'center',
   },
   walletButton: {
     position: 'absolute',

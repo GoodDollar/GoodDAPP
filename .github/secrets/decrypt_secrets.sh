804aa709d545f31cb944b068f3a090f4529549be
Mauricio Guzman <mauricio.guzman@dixtra.co>
2020-12-15 15:32:53 -0300

    add: build flow support for store distribution

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Decrypting files"
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/b29b41c9-e82a-4aab-96a8-e5d00e55756e.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+echo "Decrypting files - using IS_PROD value of: $IS_PROD"
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+if $IS_PROD; then
+  gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/21f28d4b-7641-4420-875f-989a0bcc3d52.mobileprovision ./.github/secrets/profile_store.mobileprovision.gpg
+else
+  gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/b29b41c9-e82a-4aab-96a8-e5d00e55756e.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+fi
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 

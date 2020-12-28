21044a59d065b80528fec0ea64de1d3cb342180f
Mariano Amado <28148619+MarianoAmado@users.noreply.github.com>
2020-12-28 17:52:32 -0300

    ModalJaggedEdge for native: * Added JaggedEdge component for generating the edge's triangles * Rendering JaggedEdge component only on native

--- src/components/common/modal/ModalJaggedEdge.js
+++ src/components/common/modal/ModalJaggedEdge.js
@@ -2,9 +2,13 @@
 import React from 'react'
 import { Platform, View } from 'react-native'
 import { withStyles } from '../../../lib/styles'
+import { isMobileNative } from '../../../lib/utils/platform'
+import JaggedEdge from '../view/JaggedEdge'
 import { mediumZIndex } from './styles'
 
-const ModalJaggedEdge = ({ styles, style }: any) => <View style={[styles.jaggedEdge, style]} />
+const ModalJaggedEdge = ({ styles, style }: any) => (
+  <View style={[styles.jaggedEdge, style]}>{isMobileNative && <JaggedEdge />}</View>
+)
 
 const getStylesFromProps = ({ theme }) => ({
   jaggedEdge: {
@@ -16,6 +20,9 @@ const getStylesFromProps = ({ theme }) => ({
         backgroundPosition: `-${theme.modals.jaggedEdgeSize / 2}px 0`,
         backgroundRepeat: 'repeat-x',
         backgroundSize: `${theme.modals.jaggedEdgeSize}px ${theme.modals.jaggedEdgeSize}px`,
+      },
+      default: {
+        elevation: 24,
       },
     }),
     height: theme.modals.jaggedEdgeSize,

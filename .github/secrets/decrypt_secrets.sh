0bf5d89dad1fdb15aaf66fe4c5c45dd28fed8dbc
sirpy <hadarr@gmail.com>
2021-01-06 11:06:52 +0200

    refactor: ios certificates build

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
 echo "Decrypting files - using IS_PROD value of: $IS_PROD"
-echo $IOS_CERTIFICATE | base64 --decode > ./.github/secrets/Certificates.p12
-if $IS_PROD; then
-  echo $IOS_STORE_PROFILE | base64 --decode > ./.github/secrets/21f28d4b-7641-4420-875f-989a0bcc3d52.mobileprovision
-else
-  echo $IOS_ADHOC_PROFILE | base64 --decode > ./.github/secrets/b29b41c9-e82a-4aab-96a8-e5d00e55756e.mobileprovision
-fi
-
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-echo "Install profiles"
-cp ./.github/secrets/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo $IOS_CERTIFICATE | base64 --decode > ./Certificates.p12
+if $IS_PROD; then
+  echo $IOS_STORE_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/21f28d4b-7641-4420-875f-989a0bcc3d52.mobileprovision
+else
+  echo $IOS_ADHOC_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/b29b41c9-e82a-4aab-96a8-e5d00e55756e.mobileprovision
+fi
 
 echo "Creating keychain"
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PROFILE_KEY" -A
+security import ./Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PROFILE_KEY" -A
 
 echo "Installing in keychain"
 security list-keychains -s ~/Library/Keychains/build.keychain

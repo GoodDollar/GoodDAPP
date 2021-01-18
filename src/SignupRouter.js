23a6d019dc653dee36208bef61302e2f8bb09910
sirpy <hadarr@gmail.com>
2021-01-18 13:00:18 +0200

    add: show welcome intro to all users once

--- src/SignupRouter.js
+++ src/SignupRouter.js
@@ -4,8 +4,6 @@ import { once } from 'lodash'
 import Config from './config/config'
 import createAppContainer from './lib/utils/createAppContainer'
 import useNavigationStateHandler from './lib/hooks/useNavigationStateHandler'
-import DeepLinking from './lib/utils/deepLinking'
-
 import Signup from './components/signup/SignupState'
 import SigninInfo from './components/signin/SigninInfo'
 import Blurred from './components/common/view/Blurred'
@@ -13,8 +11,7 @@ import Welcome from './components/auth/login/WelcomeScreen'
 import InviteWelcome from './components/inviteWelcome/InviteWelcome'
 
 const generateRouter = () => {
-  const { params } = DeepLinking
-  const initialRouteName = params.inviteCode ? 'InviteWelcome' : 'Welcome'
+  const initialRouteName = 'InviteWelcome'
 
   const routes = {
     Welcome,

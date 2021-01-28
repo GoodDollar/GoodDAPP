d5025c024aa739d42c3827da10b9c4e385b438bc
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2021-01-29 00:11:35 +0200

    add comments

--- src/components/dashboard/FaceVerification/sdk/FaceTecGlobalState.js
+++ src/components/dashboard/FaceVerification/sdk/FaceTecGlobalState.js
@@ -5,10 +5,19 @@ import { FaceTecSDK } from './FaceTecSDK'
 // Like in the Zoom's demo app it contains also
 // initialization function with options preconfigured
 const FaceTecGlobalState = {
+  /**
+   * @var {Promise|null}
+   */
   faceTecSDKPreloading: null,
+
+  /**
+   * @var {Error|null}
+   */
   faceTecCriticalError: null,
 
-  // Convenience method to initialize the FaceTec SDK.
+  /**
+   * Convenience method to initialize the FaceTec SDK.
+   */
   async initialize() {
     const { faceTecLicenseKey, faceTecLicenseText, faceTecEncryptionKey } = Config
 

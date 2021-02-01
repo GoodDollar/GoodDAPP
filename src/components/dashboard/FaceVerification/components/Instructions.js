2c8db8c1b1d4752304ada7ef5dd6a342b9234d44
Aku <omerzamsky@gmail.com>
2021-02-01 14:18:11 +0200

    (Feature) 2913 new fv screens (#2933)
    * small typo fixes
    
    * typo fix
    
    * small trypo fixes
    
    * added FashionPhoshoot svg image
    
    * added design of new intro screen
    added random mechanism for AB test
    
    * instructions screen WIP
    
    * instructions screen WIP
    
    * fixed events for AB testing
    
    * added checkmark
    
    * changed AB test logic
    
    * removed AB util from analytics
    
    * fixed dark text box
    
    * refactor: keep buttons in theme design
    
    * revert anlytics class change
    
    * removex px from intro screen
    
    * changed back event names
    
    * imagebackround source imported on top of file
    
    * fit introscreen to small screen size
    
    * Merge branch '2913_new_FV_screens' of github.com:GoodDollar/GoodDAPP into 2913_new_FV_screens
    
    * fixed introscreen for small device
    
    * send ab pick as event param
    
    * hotfix promise lock on FaceTecGlobalState (need to cache promise itselft not it's result), less this usage
    
    * DRY code
    
    * moved ab test value to env var
    
    * lint
    
    * fixed event data not sent
    portrait image fixed size for web
    
    * fixed checkmark
    fixed button width
    
    * small fix
    
    * add useABTesting hook, create sub components for a/b intro screen
    
    * Update src/lib/hooks/useABTesting.js
    
    * when testing always show old screens
    
    * update snapshots
    
    Co-authored-by: sirpy <hadarr@gmail.com>
    Co-authored-by: Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
    Co-authored-by: serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>

--- src/components/dashboard/FaceVerification/components/Instructions.js
+++ src/components/dashboard/FaceVerification/components/Instructions.js
@@ -1,23 +1,41 @@
 // libraries
 import React from 'react'
-import { Image, Platform, View } from 'react-native'
+import { Image, ImageBackground, Platform, View } from 'react-native'
 import { noop } from 'lodash'
 
 // components
 import Text from '../../../common/view/Text'
 import { CustomButton, Section, Wrapper } from '../../../common'
 
+// hooks
+import createABTesting from '../../../../lib/hooks/useABTesting'
+
 // utils
 import { getDesignRelativeHeight, getDesignRelativeWidth, isLargeDevice } from '../../../../lib/utils/sizes'
 import normalize from '../../../../lib/utils/normalizeText'
 import { withStyles } from '../../../../lib/styles'
 import { isBrowser } from '../../../../lib/utils/platform'
+import { FV_INSTRUCTIONS } from '../../../../lib/analytics/analytics'
 
 // assets
 import illustration from '../../../../assets/FRInstructions.png'
 
+import QuestionMark from '../../../../assets/FaceVerification/FVQuestionMark.svg'
+import CheckMark from '../../../../assets/FaceVerification/CheckMark.svg'
+import { theme } from '../../../theme/styles'
+
+const { useABTesting } = createABTesting()
+
+const portrait = Platform.select({
+  native: () => require('../../../../assets/FaceVerification/FVPortrait.png'),
+  default: () => require('../../../../assets/FaceVerification/FVPortrait@2x.png'),
+})()
+
+// import portrait from '../../../../assets/FaceVerification/FVPortrait.png'
+
 if (Platform.OS === 'web') {
   Image.prefetch(illustration)
+  Image.prefetch(portrait)
 }
 
 const Dot = () => (
@@ -31,7 +49,7 @@ const Dot = () => (
   </Text>
 )
 
-const Instructions = ({ styles, onDismiss = noop, ready = false }) => (
+const InstructionsA = ({ styles, onDismiss = noop, ready }) => (
   <Wrapper>
     <Section style={styles.topContainer} grow>
       <View style={styles.mainContent}>
@@ -66,6 +84,47 @@ const Instructions = ({ styles, onDismiss = noop, ready = false }) => (
   </Wrapper>
 )
 
+const InstructionsB = ({ styles, onDismiss = noop, ready }) => (
+  <Wrapper>
+    <Section style={styles.topContainerB} grow>
+      <View style={styles.mainContentB}>
+        <ImageBackground source={portrait} style={{ flex: 1, position: 'static' }} imageStyle={{ borderRadius: 5 }} />
+        <View style={styles.descriptionContainerB}>
+          <View style={styles.descriptionWrapperB}>
+            <QuestionMark style={{ position: 'absolute', right: 0, marginTop: 9, marginRight: 10 }} />
+            <Text fontWeight="bold" style={[styles.textB, { paddingTop: 12 }]}>
+              Make sure you...
+            </Text>
+            <Text style={[styles.textB, { marginTop: 7 }]}>
+              <CheckMark style={{ marginRight: theme.sizes.default }} />
+              Hold Your Camera at Eye Level
+            </Text>
+            <Text style={[styles.textB, { marginTop: 3, marginBottom: 14 }]}>
+              <CheckMark style={{ marginRight: theme.sizes.default }} />
+              Light Your Face Evenly
+            </Text>
+          </View>
+        </View>
+        <CustomButton
+          style={[styles.button, { height: 53 }]}
+          onPress={onDismiss}
+          testID="dismiss_button"
+          loading={!ready}
+          enabled={ready}
+        >
+          GOT IT
+        </CustomButton>
+      </View>
+    </Section>
+  </Wrapper>
+)
+
+const Instructions = ({ styles, onDismiss = noop, ready = false }) => {
+  const [InstructionsComponent] = useABTesting(InstructionsA, InstructionsB, FV_INSTRUCTIONS)
+
+  return <InstructionsComponent styles={styles} onDismiss={onDismiss} ready={ready} />
+}
+
 const getStylesFromProps = ({ theme }) => ({
   topContainer: {
     alignItems: 'center',
@@ -80,10 +139,18 @@ const getStylesFromProps = ({ theme }) => ({
     paddingRight: getDesignRelativeWidth(theme.sizes.default),
     paddingTop: getDesignRelativeHeight(theme.sizes.defaultDouble),
   },
+  topContainerB: {
+    display: 'contents',
+  },
   mainContent: {
     flexGrow: 1,
     justifyContent: 'space-between',
     width: '100%',
+  },
+  mainContentB: {
+    flexGrow: 1,
+    paddingHorizontal: getDesignRelativeHeight(10),
+    paddingBottom: getDesignRelativeHeight(10),
   },
   illustration: {
     marginTop: getDesignRelativeHeight(18),
@@ -95,9 +162,16 @@ const getStylesFromProps = ({ theme }) => ({
     paddingVertical: getDesignRelativeHeight(isBrowser ? theme.sizes.defaultDouble : 14),
     alignItems: 'center',
   },
+  descriptionContainerB: {
+    paddingVertical: getDesignRelativeHeight(isBrowser ? 12 : 10),
+  },
   descriptionWrapper: {
     flexDirection: 'column',
     alignItems: 'flex-start',
+  },
+  descriptionWrapperB: {
+    backgroundColor: theme.colors.darkGray,
+    borderRadius: 8,
   },
   button: {
     width: '100%',
@@ -106,6 +180,13 @@ const getStylesFromProps = ({ theme }) => ({
     textAlign: 'left',
     fontSize: normalize(isLargeDevice ? 22 : 20),
     lineHeight: isLargeDevice ? 36 : 34,
+  },
+  textB: {
+    textAlign: 'left',
+    paddingLeft: 18,
+    fontSize: 16,
+    letterSpacing: 0.16,
+    color: theme.colors.white,
   },
 })
 

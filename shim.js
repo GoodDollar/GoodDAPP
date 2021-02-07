6f8183d3ac4d9ebc49510db9a6bbea0d0b557cf0
sirpy <hadarr@gmail.com>
2021-02-07 14:33:59 +0200

    fix: invalid semver for RN

--- shim.js
+++ shim.js
@@ -2,12 +2,11 @@ if (typeof __dirname === 'undefined') global.__dirname = '/'
 if (typeof __filename === 'undefined') global.__filename = ''
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
-    protocol: 'file:',
-  };
+  protocol: 'file:',
+}
 
-global.process = require('process');
-global.process.version = 'v0.10';
-global.process.browser = false;
+global.process = require('process')
+global.process.browser = false
 
 const isDev = typeof __DEV__ === 'boolean' && __DEV__
 
@@ -16,7 +15,7 @@ if (typeof localStorage !== 'undefined') {
   localStorage.debug = isDev ? '*' : ''
 }
 
-global.btoa = require('Base64').btoa;
-global.atob = require('Base64').atob;
-global.Buffer = require('buffer').Buffer;
+global.btoa = require('Base64').btoa
+global.atob = require('Base64').atob
+global.Buffer = require('buffer').Buffer
 // global.crypto = require('crypto')

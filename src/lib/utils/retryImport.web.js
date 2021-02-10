85c0d01f0b051fad1b47182aaa753d5759d736b4
Zhigamovsky <zhigamovsky@gmail.com>
2021-02-10 16:46:01 +0200

    Catching error while dynamic import then restarting app

--- src/lib/utils/retryImport.web.js
+++ src/lib/utils/retryImport.web.js
@@ -1,6 +1,7 @@
 // @flow
 import { retry } from './async'
+import restart from './restart'
 
-const retryImport = fn => retry(fn, 5, 1000)
+const retryImport = fn => retry(fn, 5, 1000).catch(() => restart())
 
 export default retryImport

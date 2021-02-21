3ea36b36bc2d87574821f4fa00da49130575ecb3
omerzam <omerzamsky@gmail.com>
2021-02-21 10:21:26 +0200

    fixed categoryBox rendering on RN

--- src/components/common/view/CategoryBox.js
+++ src/components/common/view/CategoryBox.js
@@ -7,44 +7,41 @@ import ModalContainer from '../modal/ModalContainer'
 import { Section } from '../index'
 import { getShadowStyles } from '../../../lib/utils/getStyles'
 
-export const CategoryBox = ({ style, primarycolor, children, title, contentStyle }) => {
-  return (
-    <ModalContainer style={[styles.box, styles.shadow, style]} fullHeight>
-      <ModalContents style={[styles.content, contentStyle]}>
-        {title && (
-          <>
-            <View style={styles.contentWrapper}>
-              <Section.Row style={styles.categoryImage}>{children}</Section.Row>
-              <Section.Row>
-                <Section.Text
-                  style={{
-                    textTransform: 'capitalize',
-                    width: '100%',
-                    minWidth: 'max-content',
-                    paddingTop: theme.paddings.defaultMargin,
-                  }}
-                  color={primarycolor}
-                  fontSize={12}
-                  textAlign={'center'}
-                  lineHeight={14}
-                  fontFamily="Roboto"
-                >
-                  {title}
-                </Section.Text>
-              </Section.Row>
-            </View>
-          </>
-        )}
-      </ModalContents>
-    </ModalContainer>
-  )
-}
+export const CategoryBox = ({ style, primarycolor, children, title, contentStyle }) => (
+  <ModalContainer style={[styles.box, styles.shadow, style]} fullHeight>
+    <ModalContents style={[styles.content, contentStyle]}>
+      {title && (
+        <>
+          <View>
+            <Section.Row style={styles.categoryImage}>{children}</Section.Row>
+            <Section.Row>
+              <Section.Text
+                style={{
+                  textTransform: 'capitalize',
+                  width: '100%',
+
+                  // minWidth: 'max-content',
+                  paddingTop: theme.paddings.defaultMargin,
+                }}
+                color={primarycolor}
+                fontSize={12}
+                textAlign={'center'}
+                lineHeight={14}
+                fontFamily="Roboto"
+              >
+                {title}
+              </Section.Text>
+            </Section.Row>
+          </View>
+        </>
+      )}
+    </ModalContents>
+  </ModalContainer>
+)
 
 const styles = {
-  contentWrapper: {
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
+  content: {
+    justifyContent: 'center',
   },
   shadow: getShadowStyles('0px 2px 4px #00000029', {
     shadowColor: '#00000029',

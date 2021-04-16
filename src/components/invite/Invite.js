de8281ff88a7bbf613f750f4fbbd587ab2159c66
Zhigamovsky <zhigamovsky@gmail.com>
2021-04-16 17:11:56 +0300

    hotfix: remove multiple scollbars for Ivite

--- src/components/invite/Invite.js
+++ src/components/invite/Invite.js
@@ -20,6 +20,8 @@ import { shareMessage, shareTitle } from './constants'
 
 const log = logger.child({ from: 'Invite' })
 
+const Divider = ({ size = 10 }) => <Section.Separator color="transparent" width={size} style={{ zIndex: -10 }} />
+
 const InvitedUser = ({ address, status }) => {
   const profile = useGunProfile(address)
   const isApproved = status === 'approved'
@@ -253,20 +255,12 @@ const InvitesHowTO = () => {
 
 const InvitesData = ({ invitees, refresh, level, totalEarned = 0 }) => (
   <View style={{ width: '100%' }}>
-    <Section.Stack
-      style={{
-        alignSelf: 'stretch',
-        marginTop: getDesignRelativeHeight(theme.paddings.defaultMargin * 3, false),
-      }}
-    >
-      <ShareBox level={level} />
-    </Section.Stack>
-    <Section.Stack style={{ alignSelf: 'stretch', marginTop: theme.paddings.defaultMargin * 1.5 }}>
-      <TotalEarnedBox totalEarned={totalEarned} />
-    </Section.Stack>
-    <Section.Stack style={{ alignSelf: 'stretch', marginTop: theme.paddings.defaultMargin * 1.5 }}>
-      <InvitesBox invitees={invitees} refresh={refresh} />
-    </Section.Stack>
+    <Divider size={getDesignRelativeHeight(theme.paddings.defaultMargin * 3, false)} />
+    <ShareBox level={level} />
+    <Divider size={theme.paddings.defaultMargin * 1.5} />
+    <TotalEarnedBox totalEarned={totalEarned} />
+    <Divider size={theme.paddings.defaultMargin * 1.5} />
+    <InvitesBox invitees={invitees} refresh={refresh} />
   </View>
 )
 
@@ -314,29 +308,23 @@ const Invite = () => {
           For Each Friend You Invite!
         </Section.Text>
       </Section.Stack>
-      <Section.Stack style={{ marginTop: theme.sizes.defaultDouble }}>
-        <Section.Text letterSpacing={-0.07} lineHeight={20} fontSize={15} color={theme.colors.darkBlue}>
-          {`Make sure they claim to get your reward`}
-        </Section.Text>
-      </Section.Stack>
-      <View
-        style={{
-          marginTop: getDesignRelativeHeight(theme.paddings.defaultMargin * 3, false),
-        }}
+      <Divider size={theme.sizes.defaultDouble} />
+      <Section.Text letterSpacing={-0.07} lineHeight={20} fontSize={15} color={theme.colors.darkBlue}>
+        {`Make sure they claim to get your reward`}
+      </Section.Text>
+      <Divider size={getDesignRelativeHeight(theme.paddings.defaultMargin * 3, false)} />
+      <CustomButton
+        color={theme.colors.darkBlue}
+        iconColor={theme.colors.darkBlue}
+        iconStyle={{ marginLeft: 10 }}
+        iconAlignment="right"
+        icon={showHowTo ? 'arrow-up' : 'arrow-down'}
+        mode="text"
+        textStyle={{ fontWeight: 'bold', letterSpacing: 0, textDecorationLine: 'underline' }}
+        onPress={toggleHowTo}
       >
-        <CustomButton
-          color={theme.colors.darkBlue}
-          iconColor={theme.colors.darkBlue}
-          iconStyle={{ marginLeft: 10 }}
-          iconAlignment="right"
-          icon={showHowTo ? 'arrow-up' : 'arrow-down'}
-          mode="text"
-          textStyle={{ fontWeight: 'bold', letterSpacing: 0, textDecorationLine: 'underline' }}
-          onPress={toggleHowTo}
-        >
-          {`How Do I Invite People?`}
-        </CustomButton>
-      </View>
+        {`How Do I Invite People?`}
+      </CustomButton>
       {showHowTo && <InvitesHowTO />}
       <InvitesData {...{ invitees, refresh, level, totalEarned }} />
     </Wrapper>
@@ -355,6 +343,7 @@ const styles = {
     justifyContent: 'flex-start',
     alignItems: 'center',
     flex: 1,
+    flexBasis: 1,
     height: '100%',
   },
   linkBoxStyle: {

654d1ffe32fd98f0c05e7502125347f955b9534d
omerzam <omerzamsky@gmail.com>
2021-05-25 15:53:03 +0300

    always use verifyEditCode

--- src/components/profile/VerifyEdit.js
+++ src/components/profile/VerifyEdit.js
@@ -56,9 +56,8 @@ const EditProfile = ({ screenProps, theme, styles, navigation }) => {
       if (data.alreadyVerified) {
         await userStorage.setProfileField(field, content)
         screenProps.pop()
-      } else {
-        screenProps.push('VerifyEditCode', { field, content })
       }
+      screenProps.push('VerifyEditCode', { field, content })
     } catch (e) {
       log.error('Failed to send code', e.message, e, { dialogShown: true })
 

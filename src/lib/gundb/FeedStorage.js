ea6abd34a93417ba5492f7a51e8f80ba2f3f416c
sirpy <hadar@gooddollar.org>
2021-05-26 08:57:25 +0300

    New feed improves (#3246)
    * fix: prevent reading uninitialized feed cache
    
    * fix: calling non existing method setUserProfileFields
    
    * fix: prevent double receipt callbacks

--- src/lib/gundb/FeedStorage.js
+++ src/lib/gundb/FeedStorage.js
@@ -120,10 +120,12 @@ export class FeedStorage {
     this.userStorage = userStorage
     this.walletAddress = wallet.account.toLowerCase()
     log.debug('initialized', { wallet: this.walletAddress })
+    this.ready = new Promise((resolve, reject) => {
+      this.setReady = resolve
+    })
   }
 
   async init() {
-    this.feedInitialized = true
     const { feed } = await this.gunuser
     const receiptEvents = ['receiptReceived', 'receiptUpdated', 'otplUpdated']
 
@@ -153,6 +155,10 @@ export class FeedStorage {
         log.warn('initfeed failed parsing feed from cache')
       })
       .then(ids => ids || {})
+
+    //mark as initialized, ie resolve ready promise
+    this.feedInitialized = true
+    this.setReady()
 
     //no need to block on this
     this._syncFeedCache()
@@ -604,6 +610,8 @@ export class FeedStorage {
   async enqueueTX(_event: FeedEvent): Promise<> {
     const event = delUndefValNested(_event)
 
+    await this.ready //wait before accessing feedIds cache
+
     //a race exists between enqueuing and receipt from websockets/polling
     const release = await this.feedMutex.lock()
     try {
@@ -737,7 +745,9 @@ export class FeedStorage {
       })
   }
 
-  writeFeedEvent(event): Promise<FeedEvent> {
+  async writeFeedEvent(event): Promise<FeedEvent> {
+    await this.ready //wait before accessing feedIds cache
+
     this.feedIds[event.id] = event
     AsyncStorage.setItem('GD_feed', this.feedIds)
     this.emitUpdate({ event })
@@ -779,7 +789,9 @@ export class FeedStorage {
    * @param {string} transactionHash - transaction identifier
    * @returns {object} feed item or null if it doesn't exist
    */
-  getFeedItemByTransactionHash(transactionHash: string): Promise<FeedEvent> {
+  async getFeedItemByTransactionHash(transactionHash: string): Promise<FeedEvent> {
+    await this.ready //wait before accessing feedIds cache
+
     const feedItem = this.feedIds[transactionHash]
     if (feedItem) {
       return feedItem
@@ -904,6 +916,8 @@ export class FeedStorage {
    * @returns {Promise} Promise with an array of feed events
    */
   async getFeedPage(numResults: number, reset?: boolean = false): Promise<Array<FeedEvent>> {
+    await this.ready //wait before accessing feedIds cache
+
     let { feedIndex, feedIds } = this
 
     if (!feedIndex) {
@@ -968,6 +982,7 @@ export class FeedStorage {
           })
 
           if (receipt) {
+            log.warn('getFeedPage: missing item in cache, processing receipt again', { id, receipt })
             item = await this.handleReceipt(receipt)
           } else {
             log.warn('getFeedPage no receipt found for undefined item id:', id)

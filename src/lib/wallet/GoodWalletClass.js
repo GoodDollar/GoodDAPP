f95de47a2a10b8450a82d408d9bb305f8cef719c
sirpy <hadarr@gmail.com>
2021-05-31 15:26:33 +0300

    fix: correctly read latestblock events

--- src/lib/wallet/GoodWalletClass.js
+++ src/lib/wallet/GoodWalletClass.js
@@ -275,7 +275,7 @@ export class GoodWallet {
         }
         const nextLastBlock = await this.wallet.eth.getBlockNumber()
 
-        fn()
+        await fn() //await callback to finish processing events before updating lastEventblock
         this.lastEventsBlock = nextLastBlock
         lastBlockCallback(nextLastBlock)
 
@@ -624,8 +624,14 @@ export class GoodWallet {
 
   sendReceiptWithLogsToSubscribers(receipt: any, subscriptions: Array<string>) {
     subscriptions.forEach(subscription => {
+      const subscribers = this.getSubscribers(subscription)
+      log.debug('sendReceiptWithLogsToSubscribers', { subscription, subscribers })
       this.getSubscribers(subscription).forEach(cb => {
-        log.debug('receiptCallback:', { subscription, hash: receipt.transactionHash, cb })
+        log.debug('sendReceiptWithLogsToSubscribers receiptCallback:', {
+          subscription,
+          hash: receipt.transactionHash,
+          cb,
+        })
         cb(receipt)
       })
     })
@@ -802,7 +808,6 @@ export class GoodWallet {
     const subscribers = this.subscribers[eventName]
     const id = Math.max(...Object.keys(subscribers).map(parseInt), 0) + 1 // Give next id in a raw to current subscriber
     this.subscribers[eventName][id] = cb
-
     return { id, eventName }
   }
 

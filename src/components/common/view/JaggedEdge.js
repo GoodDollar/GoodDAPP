ea99d4232b839014c9bc43421bfa69d4fa5e7db0
julianpolcode <julian.kobrynski@polcode.net>
2021-07-02 13:06:06 +0200

    Mobile jagged edge fixed

--- src/components/common/view/JaggedEdge.js
+++ src/components/common/view/JaggedEdge.js
@@ -2,23 +2,26 @@
 import React, { useMemo, useState } from 'react'
 import { View } from 'react-native'
 import { withStyles } from '../../../lib/styles'
-import { getDesignRelativeWidth } from '../../../lib/utils/sizes'
+
+// To match web version
+const TILES = 27
 
 const useJaggedEdge = (width, tileSize, styles) =>
   useMemo(() => {
-    const calcTiles = width / tileSize
-    const tiles = Math.ceil(calcTiles) // amount of triangles
+    const tileDiagonal = Math.sqrt(2) * tileSize
 
-    const leftOffset = ((tiles - calcTiles) * tileSize) / 2 // offset for aligning tiles to the left
-    const margin = (-tileSize * 1.5) / 2 // negative margin for vertical alignment
+    const marginLeft = tileDiagonal - tileSize
+    const marginTop = -tileDiagonal / 2 // negative margin for vertical alignment
 
     // Create array of tiles (triangles)
-    const tileItems = Array.from({ length: tiles }, (e, i) => {
-      const marginLeft = i === 0 ? leftOffset : 0
-      return <View key={`tile-${i}`} style={[styles.tile, { marginLeft }]} />
+    const tileItems = Array.from({ length: TILES }, (e, i) => {
+      const leftMargin = i === 0 ? marginLeft / 2 : marginLeft
+      return (
+        <View key={`tile-${i}`} style={[styles.tile, { width: tileSize, height: tileSize, marginLeft: leftMargin }]} />
+      )
     })
 
-    return { leftOffset, margin, tileItems }
+    return { marginTop, tileItems, tileDiagonal }
   }, [width, tileSize, styles])
 
 const JaggedEdge = ({ styles, theme }) => {
@@ -28,39 +31,28 @@ const JaggedEdge = ({ styles, theme }) => {
   const onLayout = ({ nativeEvent }) => setWidth(nativeEvent.layout.width)
 
   // size of the triangles
-  const tileSize = getDesignRelativeWidth(theme.modals.jaggedEdgeSize)
+  const tileSize = useMemo(() => width / ((TILES - 0.5) * Math.sqrt(2)), [width])
 
-  const { leftOffset, margin, tileItems } = useJaggedEdge(width, tileSize, styles)
+  const { marginTop, tileItems, tileDiagonal } = useJaggedEdge(width, tileSize, styles)
 
   return (
-    <View onLayout={onLayout} style={[{ marginTop: margin }, styles.container]}>
+    <View onLayout={onLayout} style={[{ marginTop, height: tileDiagonal }, styles.container]}>
       {tileItems}
-      <View style={[styles.end, { marginLeft: -leftOffset }]} />
     </View>
   )
 }
 
-const getStylesFromProps = ({ theme }) => {
-  const size = getDesignRelativeWidth(theme.modals.jaggedEdgeSize)
-
-  return {
-    container: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      overflow: 'hidden',
-      width: '100%',
-    },
-    tile: {
-      width: size,
-      height: size,
-      backgroundColor: theme.modals.backgroundColor,
-      transform: [{ rotate: '45deg' }],
-    },
-    end: {
-      width: size,
-      height: size * 1.5, // height is the tile's diagonal
-    },
-  }
-}
+const getStylesFromProps = ({ theme }) => ({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    overflow: 'hidden',
+    width: '100%',
+  },
+  tile: {
+    backgroundColor: theme.modals.backgroundColor,
+    transform: [{ rotate: '45deg' }],
+  },
+})
 
 export default withStyles(getStylesFromProps)(JaggedEdge)

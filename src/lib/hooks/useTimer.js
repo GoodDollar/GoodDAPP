cb00b00e47fef33a9e5a9be929bd7968fb5ad056
sirpy <hadar@gooddollar.org>
2021-07-06 13:13:35 +0300

    3170 unable to claim custom hooks (#3307)
    * claim component refactoring using custom hooks, add deps fixes
    
    * fix polling conditions
    
    * restore trigger getting stats if reached time to claim
    
    * Destructuring issue fixed
    
    * Updated snapshots
    
    * Claim tests updated
    
    * fix datetime formats in snapshots
    
    Co-authored-by: Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
    Co-authored-by: julianpolcode <julian.kobrynski@polcode.net>

---
+++ src/lib/hooks/useTimer.js
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+
+import moment from 'moment'
+import 'moment-duration-format'
+
+import useInterval from './useInterval'
+
+const getTimerState = targetTime => {
+  const duration = moment.duration(moment(targetTime).diff(moment()))
+  const isReachedZero = duration.asSeconds() <= 0
+  const countdown = isReachedZero ? '00:00:00' : duration.format('HH:mm:ss')
+
+  return [countdown, isReachedZero]
+}
+
+const useTimer = tillTime => {
+  const [targetTime, setTargetTime] = useState(tillTime)
+  const [timerState, setTimerState] = useState(() => getTimerState(targetTime))
+  const [countdown, isReachedZero] = timerState
+
+  const onTick = useCallback(() => {
+    setTimerState(getTimerState(targetTime))
+  }, [targetTime, setTimerState])
+
+  const [start, stop] = useInterval(onTick, 1000, false)
+
+  useEffect(() => {
+    start()
+
+    return stop
+  }, [start, stop, targetTime])
+
+  useEffect(() => {
+    if (isReachedZero) {
+      stop()
+    }
+  }, [isReachedZero, stop])
+
+  return [countdown, isReachedZero, setTargetTime]
+}
+
+export default useTimer

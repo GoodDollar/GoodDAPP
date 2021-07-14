ae0de7485eaa117a455d7bae5c4e4970ef5ac734
sirpy <hadarr@gmail.com>
2021-07-14 12:29:29 +0300

    refactor: remove heavy image log

--- src/lib/hooks/useImageSource.js
+++ src/lib/hooks/useImageSource.js
@@ -1,11 +1,11 @@
-import { useEffect, useMemo } from 'react'
-import { first, last } from 'lodash'
+import { useMemo } from 'react'
 
 import { isGoodDollarImage, isValidLocalImage, isValidRootImage } from '../utils/image'
-import logger from '../logger/pino-logger'
+
+// import logger from '../logger/pino-logger'
 import useProfileAvatar from './useProfileAvatar'
 
-const log = logger.child({ from: 'useImageSource' })
+// const log = logger.child({ from: 'useImageSource' })
 
 export default (source, skipCache = false) => {
   // firstly, trying for image sources could be checked syncronously
@@ -43,12 +43,12 @@ export default (source, skipCache = false) => {
     return [false, null]
   }, [cachedState, base64])
 
-  useEffect(() => {
-    const resolved = last(sourceState)
-    const isGDLogo = first(sourceState) === true
+  // useEffect(() => {
+  //   const resolved = last(sourceState)
+  //   const isGDLogo = first(sourceState) === true
 
-    log.debug('image source:', { source, isGDLogo, resolved })
-  }, [source, sourceState])
+  //   log.debug('image source:', { isGDLogo, source  })
+  // }, [source, sourceState])
 
   return sourceState
 }

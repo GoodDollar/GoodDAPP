58d7317cceef3deaf91fb44729f4e0d2b47d52c7
sirpy <hadarr@gmail.com>
2021-07-22 13:55:48 +0300

    add: adapt tests to v2

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-pushd node_modules/@gooddollar/goodcontracts
-npm ci
-cd stakingModel
-npm ci
-npm run ganache:test &
-npm run wait
-npm run start:withmain
+pushd node_modules/@gooddollar/goodprotocol
+yarn install --frozen-lockfile 
+yarn deployTest
 popd

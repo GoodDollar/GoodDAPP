57b3f46e5a72eb98684f876310bdd611c575c0a0
julianpolcode <julian.kobrynski@polcode.net>
2021-07-22 10:10:23 +0200

    Version checking  hook

---
+++ src/lib/hooks/useVersionCheck.js
@@ -0,0 +1,41 @@
+import { Linking, StyleSheet } from 'react-native'
+import VersionCheck from 'react-native-version-check'
+import { useDialog } from '../undux/utils/dialog'
+
+export default () => {
+  const [showDialog, hideDialog] = useDialog()
+
+  const checkVersion = async () => {
+    const currentVersion = VersionCheck.getCurrentVersion()
+    const latestVersion = await VersionCheck.getLatestVersion()
+
+    const res = await VersionCheck.needUpdate({ currentVersion, latestVersion, depth: 3 })
+
+    if (res.isNeeded) {
+      const onUpdate = () => {
+        Linking.openURL(res.storeUrl)
+      }
+
+      showDialog({
+        title: 'UPDATE AVAILABLE',
+        message: 'There is a new version of the app available, please update for better experience',
+        buttons: [
+          {
+            text: 'Later',
+            onPress: hideDialog,
+            style: styles.laterButton,
+          },
+          { text: 'Update', onPress: onUpdate },
+        ],
+      })
+    }
+  }
+
+  return [checkVersion]
+}
+
+const styles = StyleSheet.create({
+  laterButton: {
+    backgroundColor: '#CBCBCB',
+  },
+})

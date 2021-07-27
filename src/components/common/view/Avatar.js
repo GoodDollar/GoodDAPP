GoodDollar/GoodDAPP

commit 1387ce7218118b541f8959ada3687f61933ebc13

Improve avatar edit (#3345)
* refactor: simplify useImageSource _linkAvatarsToDatabase

* add: remove auto crop

* add: on web save avatar only after crop. prevent double save

src/components/common/view/Avatar.js
@@ -33,7 +33,8 @@ const CustomAvatar = ({
   ...avatarProps
 }) => {
   const _onPress = useOnPress(onPress)
-  const [isGDLogo, imgSource] = useImageSource(source)
+  const isGDLogo = source === -1
+  const imgSource = useImageSource(source)
   const ImageComponent = plain ? Image : Avatar.Image
 
   const calculatedStyles = useMemo(() => {

src/lib/hooks/useImageSource.js
@@ -1,26 +1,15 @@
-import { useEffect, useMemo } from 'react'
-import { first, last } from 'lodash'
+import { useMemo } from 'react'
 
 import { isGoodDollarImage, isValidLocalImage, isValidRootImage } from '../utils/image'
-import logger from '../logger/pino-logger'
 import useProfileAvatar from './useProfileAvatar'
-
-const log = logger.child({ from: 'useImageSource' })
 
 export default (source, skipCache = false) => {
   // firstly, trying for image sources could be checked syncronously
   const cachedState = useMemo(() => {
     // GD logo (-1)
-    if (isGoodDollarImage(source)) {
-      return [true, null]
+    if (isGoodDollarImage(source) || isValidLocalImage(source) || isValidRootImage(source)) {
+      return source
     }
-
-    // local (require()) image (numbers > 0) or relative url (starts with /)
-    if (isValidLocalImage(source) || isValidRootImage(source)) {
-      return [false, source]
-    }
-
-    // if no match - return null
     return null
   }, [source])
 
@@ -29,26 +18,14 @@ export default (source, skipCache = false) => {
 
   // aggregating memo
   const sourceState = useMemo(() => {
-    // if GD, local or root image - return cached value
-    if (cachedState) {
-      return cachedState
-    }
-
     // if was base64 or was loaded form ipfs - return data url
     if (base64) {
-      return [false, { uri: base64 }]
+      return { uri: base64 }
     }
 
     // otherwise return unknown profile image
-    return [false, null]
-  }, [cachedState, base64])
-
-  useEffect(() => {
-    const resolved = last(sourceState)
-    const isGDLogo = first(sourceState) === true
-
-    log.debug('image source:', { source, isGDLogo, resolved })
-  }, [source, sourceState])
+    return source
+  }, [source, base64])
 
   return sourceState
 }

src/components/common/form/ImageCropper.web.js
@@ -15,7 +15,7 @@ const ImageCropper = props => {
       dragMode="move"
       aspectRatio={1}
       guides={false}
-      autoCropArea={9 / 10}
+      autoCropArea={1}
       restore={false}
       highlight={false}
       center={false}

src/components/profile/EditAvatar.js
@@ -16,15 +16,17 @@ const TITLE = 'Edit Avatar'
 
 const EditAvatar = ({ theme, screenProps }) => {
   const store = GDStore.useStore()
+  const passedAvatar = screenProps.screenState.avatar
   const [showErrorDialog] = useErrorDialog()
 
   const user = useWrappedUserStorage()
   const profile = store.get('profile')
 
-  const [isDirty, markAsDirty] = useState(false)
+  const [isDirty, markAsDirty] = useState(false || passedAvatar !== undefined) //if passed avatar mark as dirty so we save it by default
   const [processing, setProcessing] = useState(false)
 
-  const avatar = useProfileAvatar(profile.avatar, true)
+  const storedAvatar = useProfileAvatar(profile.avatar, true)
+  const avatar = passedAvatar || storedAvatar
   const croppedRef = useRef(avatar)
 
   const updateAvatar = useCallback(async () => {

src/components/profile/ViewOrUploadAvatar/index.js
@@ -62,13 +62,9 @@ const ViewOrUploadAvatar = props => {
   const handleAddAvatar = useCallback(
     avatar => {
       fireEvent(PROFILE_IMAGE)
-      wrappedUserStorage.setAvatar(avatar).catch(e => {
-        log.error('save image failed:', e.message, e, { dialogShown: true })
-        showErrorDialog('Could not save image. Please try again.')
-      })
 
       if (Platform.OS === 'web') {
-        screenProps.push('EditAvatar')
+        screenProps.push('EditAvatar', { avatar })
       }
     },
     [screenProps, wrappedUserStorage],

commit d8572d403738efa98c80ea5fe35530d0ee845f37

fix: avatar not displayed caused by #3345

src/components/common/view/Avatar.js
@@ -33,8 +33,7 @@ const CustomAvatar = ({
   ...avatarProps
 }) => {
   const _onPress = useOnPress(onPress)
-  const isGDLogo = source === -1
-  const imgSource = useImageSource(source)
+  const [isGDLogo, imgSource] = useImageSource(source)
   const ImageComponent = plain ? Image : Avatar.Image
 
   const calculatedStyles = useMemo(() => {

src/lib/hooks/useImageSource.js
@@ -7,9 +7,16 @@ export default (source, skipCache = false) => {
   // firstly, trying for image sources could be checked syncronously
   const cachedState = useMemo(() => {
     // GD logo (-1)
-    if (isGoodDollarImage(source) || isValidLocalImage(source) || isValidRootImage(source)) {
-      return source
+    if (isGoodDollarImage(source)) {
+      return [true, null]
     }
+
+    // local (require()) image (numbers > 0) or relative url (starts with /)
+    if (isValidLocalImage(source) || isValidRootImage(source)) {
+      return [false, source]
+    }
+
+    // if no match - return null
     return null
   }, [source])
 
@@ -18,14 +25,19 @@ export default (source, skipCache = false) => {
 
   // aggregating memo
   const sourceState = useMemo(() => {
+    // if GD, local or root image - return cached value
+    if (cachedState) {
+      return cachedState
+    }
+
     // if was base64 or was loaded form ipfs - return data url
     if (base64) {
-      return { uri: base64 }
+      return [false, { uri: base64 }]
     }
 
     // otherwise return unknown profile image
-    return source
-  }, [source, base64])
+    return [false, null]
+  }, [cachedState, base64])
 
   return sourceState
 }
bb15dac2a6a1088eafcb5752ae79520aeb8eb11e
sirpy <hadarr@gmail.com>
2021-08-08 12:38:32 +0300

    fix: wait for properties sync correctly for registred users

--- src/lib/userStorage/UserStorageClass.js
+++ src/lib/userStorage/UserStorageClass.js
@@ -517,6 +517,8 @@ export class UserStorage {
 
     const seed = this.wallet.wallet.eth.accounts.wallet[this.wallet.getAccountForType('gundb')].privateKey.slice(2)
     await this.feedDB.init(seed, this.wallet.getAccountForType('gundb')) //only once user is registered he has access to realmdb via signed jwt
+    //after we initialize the database wait for user properties which depands on database
+    await this.userProperties.ready
     await this.initFeed()
 
     // get trusted GoodDollar indexes and pub key
@@ -544,7 +546,6 @@ export class UserStorage {
       try {
         // firstly, awaiting for wallet is ready
         await wallet.ready
-        await this.userProperties.ready
         const isReady = await retry(() => this.initGun(), 1) // init user storage, if exception thrown, retry init one more times
 
         logger.debug('userStorage initialized.')

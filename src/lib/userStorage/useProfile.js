d3b4ff467b9958151328864aacb833ca16afd911
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2021-08-13 18:19:06 +0300

    adjust useProfile

--- src/lib/userStorage/useProfile.js
+++ src/lib/userStorage/useProfile.js
@@ -1,24 +1,24 @@
-import { pick } from 'lodash'
+import { mapValues, pick } from 'lodash'
 import { useEffect, useMemo, useState } from 'react'
 
 import userStorage from './UserStorage'
 
 const defaultFields = ['fullName', 'avatar']
 
-const useProfile = (fields = defaultFields) => {
-  return useMemo(() => {
+const useProfile = (fields = defaultFields) =>
+  useMemo(() => {
     const rawProfile = userStorage.getProfile()
+
     return pick(rawProfile, fields)
   }, [fields])
-}
 
 export const useUserProfile = (walletAddress, fields = defaultFields) => {
   const [profile, setProfile] = useState(null)
 
   useEffect(() => {
-    userStorage.getProfileByWalletAddress(walletAddress).then(rawProfile => {
-      setProfile(pick(rawProfile, fields))
-    })
+    userStorage
+      .getProfileByWalletAddress(walletAddress)
+      .then(rawProfile => setProfile(mapValues(pick(rawProfile, fields), 'display')))
   }, [walletAddress, fields, setProfile])
 
   return profile

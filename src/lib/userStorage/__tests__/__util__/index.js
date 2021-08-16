2129fe0ab571f7ae88dd484dfbcba78c23a11e4e
serdiukov-o-nordwhale <62208067+serdiukov-o-nordwhale@users.noreply.github.com>
2021-08-16 10:48:03 +0300

    Update index.js

--- src/lib/userStorage/__tests__/__util__/index.js
+++ src/lib/userStorage/__tests__/__util__/index.js
@@ -11,10 +11,11 @@ export const initUserStorage = async () => {
     'GD_jwt',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiMHg1YjliNDlmZjM1ZmE4OWZkMWZiOWNmNGJmNTNkNmI1MDA5ZmVjNjgxIiwiZ2RBZGRyZXNzIjoiMHg3NDBlMjIxNjFkZWVhYTYwYjhiMGI1Y2RhYWEwOTE1MzRmZjIxNjQ5IiwicHJvZmlsZVB1YmxpY2tleSI6IjlZdFNlSXdELVN3Z080UVIxaHBobGt4dFhleUdESjFIX01PQ3pncWcwWEkuTDN3RTJZUkpOT3c0cUo1UFVST0lRNTk3OVR3RFlCcmFmZGUwTlFkXzFSUSIsImV4cCI6MjIzMzU3MzQzNiwiYXVkIjoicmVhbG1kYl93YWxsZXRfZGV2ZWxvcG1lbnQiLCJzdWIiOiIweDViOWI0OWZmMzVmYTg5ZmQxZmI5Y2Y0YmY1M2Q2YjUwMDlmZWM2ODEiLCJpYXQiOjE2Mjg3NzM0MzZ9.y4EJ6Ban0MJL0TORh_kaO_9CKbGouI9FmuRo9iBgUCo',
   )
+  
+  // user storage awaits for the wallet during initialization
+  await userStorage.ready
 
   const { feedDB, profilePrivateKey } = userStorage
 
-  // user storage awaits for the wallet during initialization
-  await userStorage.ready
   await feedDB.init(profilePrivateKey)
 }

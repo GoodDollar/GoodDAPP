b68983c480cb7700221eba77a6eac87c1d31f5ff
sirpy <hadarr@gmail.com>
2021-08-19 10:00:37 +0300

    add: finally polyfill

--- src/lib/shim.js
+++ src/lib/shim.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-extend-native */
 if (!Object.fromEntries) {
   Object.defineProperty(Object, 'fromEntries', {
     value(entries) {
@@ -17,3 +18,12 @@ if (!Object.fromEntries) {
     },
   })
 }
+
+Promise.prototype.finally =
+  Promise.prototype.finally ||
+  {
+    finally(fn) {
+      const onFinally = callback => Promise.resolve(fn()).then(callback)
+      return this.then(result => onFinally(() => result), reason => onFinally(() => Promise.reject(reason)))
+    },
+  }.finally

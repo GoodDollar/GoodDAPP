1618842b3cb1c7132b3e80d9fb09161b0aa22a16
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2021-08-19 10:48:38 +0300

    remove bloated polyfill packages

--- src/lib/shim.js
+++ src/lib/shim.js
@@ -1,5 +1,24 @@
-import fromEntries from 'object.fromentries'
-import promiseFinally from 'promise.prototype.finally'
+import { fromPairs } from 'lodash'
 
-fromEntries.shim()
-promiseFinally.shim()
+const shim = (object, method, implementation) => {
+  if ('function' === typeof object[method]) {
+    return
+  }
+
+  Object.defineProperty(object, method, { value: implementation })
+}
+
+shim(Object, 'fromEntries', entries => {
+  if (!entries || !entries[Symbol.iterator]) {
+    throw new Error('Object.fromEntries() requires a single iterable argument')
+  }
+
+  return fromPairs(entries)
+})
+
+// shouldn't be arrow to access 'this'
+shim(Promise.prototype, 'finally', function(fn) {
+  const onFinally = callback => Promise.resolve(fn()).then(callback)
+
+  return this.then(result => onFinally(() => result), reason => onFinally(() => Promise.reject(reason)))
+})

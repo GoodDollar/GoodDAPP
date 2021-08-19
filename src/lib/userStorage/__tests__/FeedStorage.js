47ef3e992fbcf32963e09edfa9f0ab62f73d72ea
julianpolcode <julian.kobrynski@polcode.net>
2021-08-19 11:52:52 +0200

    Saving feedEvent to db in test

--- src/lib/userStorage/__tests__/FeedStorage.js
+++ src/lib/userStorage/__tests__/FeedStorage.js
@@ -11,6 +11,24 @@ fromEntries.shim()
 
 jest.setTimeout(10000)
 
+const feedEvent = {
+  // id: '0x2eb0e2cdd80b2f61e81d3b0d7eea84276f1831e2dff4c49d2bc8f9cee673b73a',
+  id: '0x2eb0e2cdd80b2f61e81d3b0d7eea84276f1831e2dff4c49d2bc8f9cee673b73b',
+  createdDate: '2021-08-19T06:21:09.324Z',
+  date: '2021-08-19T06:21:09.324Z',
+  type: 'senddirect',
+  data: {
+    to: '0x77ceeadfb6b852de370f94e395dd61ca7ad5b30f',
+    reason: null,
+    category: 'Product',
+    amount: '5500',
+  },
+  status: 'pending',
+
+  // _id: '0x2eb0e2cdd80b2f61e81d3b0d7eea84276f1831e2dff4c49d2bc8f9cee673b73a',
+  _id: '0x2eb0e2cdd80b2f61e81d3b0d7eea84276f1831e2dff4c49d2bc8f9cee673b73b',
+}
+
 describe('FeedStorage', () => {
   let feedStorage
 
@@ -29,5 +47,31 @@ describe('FeedStorage', () => {
 
   it('should pass', () => {
     expect(true).toBeTruthy()
+  })
+
+  it('should add new event to outbox', async () => {
+    await feedStorage.addToOutbox(feedEvent)
+    const savedItem = await feedStorage.storage.inboxes.findOne({ txHash: feedEvent.id })
+    const userId = feedStorage.storage.user.id
+    const recipientPubkey = await feedStorage.userStorage.getUserProfilePublickey(feedEvent.data.to)
+    expect(savedItem).toHaveProperty('user_id', userId)
+    expect(savedItem).toHaveProperty('txHash', feedEvent.id)
+    expect(savedItem).toHaveProperty('recipientPublicKey', recipientPubkey)
+  })
+
+  // it('should get event from outbox and decrypt', async () => {
+  //   const decrypted = await feedStorage.getFromOutbox(feedEvent)
+  //   console.log('DECRYPTED', decrypted)
+  // })
+
+  it('should delete record', async () => {
+    try {
+      // const result = await feedStorage.userStorage.inboxes.findOneAndDelete({ txHash: feedEvent.id })
+      await feedStorage.userStorage.inboxes.findOneAndDelete({ txHash: feedEvent.id })
+
+      // console.log('RESULT', result)
+    } catch (e) {
+      // console.log('EXCEPTION', e)
+    }
   })
 })

50d4ef0542d55602e82d7717098a53f4215621a6
John Smith <alexey@gooddollar.org>
2021-09-09 18:46:19 +0300

    hotfix user name is not visible at all

--- src/lib/updates/index.js
+++ src/lib/updates/index.js
@@ -4,12 +4,15 @@ import userStorage from '../userStorage/UserStorage'
 import Config from '../../config/config'
 import { fireEvent } from '../analytics/analytics'
 import logger from '../logger/js-logger'
+
 import uploadAvatars from './avatar'
 import upgradeRealmDB from './upgradeRealmdb'
 import upgradeProfileRealmDB from './upgradeProfileRealmdb'
+import upgradeProfile from './restoreProfile'
 
 const log = logger.child({ from: 'updates' })
-const updates = [uploadAvatars, upgradeRealmDB, upgradeProfileRealmDB]
+
+const updates = [upgradeRealmDB, upgradeProfileRealmDB, upgradeProfile, uploadAvatars]
 
 const update = async () => {
   const updatesData = (await userStorage.userProperties.get('updates')) || {
@@ -25,32 +28,38 @@ const update = async () => {
   log.debug('starting updates:', { prevVersion, lastUpdate, doneUpdates })
 
   if (prevVersion) {
-    const promises = updates.map(upd => {
+    const updatesTasks = updates.reduce((acc, upd) => {
       const updateKey = `${upd.key}_${new Date(upd.fromDate).toISOString()}`
 
       if (upd.fromDate > lastUpdate || !doneUpdates[updateKey]) {
-        return upd
-          .update(lastUpdate, prevVersion, log)
-          .then(_ => {
-            doneUpdates[updateKey] = true
-            log.info('update done:', updateKey)
-            fireEvent('UPDATE_SUCCESS', { key: upd.key })
-          })
-          .catch(e => {
-            doneUpdates[updateKey] = false
-            fireEvent('UPDATE_FAILED', { key: upd.key, error: e.message })
-            log.error('update failed:', e.message, e, { updKey: upd.key })
-          })
-          .then(_ => true)
+        acc.push(() =>
+          upd
+            .update(lastUpdate, prevVersion, log)
+            .then(_ => {
+              doneUpdates[updateKey] = true
+              log.info('update done:', updateKey)
+              fireEvent('UPDATE_SUCCESS', { key: upd.key })
+            })
+            .catch(e => {
+              doneUpdates[updateKey] = false
+              fireEvent('UPDATE_FAILED', { key: upd.key, error: e.message })
+              log.error('update failed:', e.message, e, { updKey: upd.key })
+            })
+            .then(_ => true),
+        )
+      } else {
+        log.info('update skipped:', { updateKey })
       }
 
-      log.info('updated skipped:', { updateKey })
-      return false
-    })
+      return acc
+    }, [])
 
-    log.debug('waiting update promises:', promises.length)
+    log.debug('waiting update tasks:', updatesTasks.length)
 
-    const results = await Promise.all(promises).then(filter)
+    // eslint-disable-next-line prettier/prettier
+    const results = await updatesTasks.reduce((promise, upd) =>
+      promise.then(acc => upd().then(res => [...acc, res])), Promise.resolve([]))
+      .then(filter)
 
     log.debug('done updates:', { results }, results.length)
   } else {

b8bd46e799d21070837e9b85c392f67b6b8400f0
John Smith <alexey@gooddollar.org>
2021-09-15 14:33:05 +0300

    hotfix: use ref instead of state var for SDK in useTorus

--- src/components/auth/torus/hooks/useTorus.js
+++ src/components/auth/torus/hooks/useTorus.js
@@ -10,16 +10,28 @@ import logger from '../../../../lib/logger/js-logger'
 const log = logger.child({ from: 'AuthTorus' })
 
 export default (onInitialized = noop) => {
-  const sdk = useState(() => TorusSDK.factory({ uxMode: isMobileNative ? 'popup' : 'redirect' }))
+  const sdkRef = useRef(null)
   const [initialized, setInitialized] = useState(false)
   const onInitializedRef = useRef(onInitialized)
   const mountedState = useMountedState()
+
+  // inline functions outside effects are allowed if we're accessing refs only
+  // https://reactjs.org/docs/hooks-faq.html#how-to-create-expensive-objects-lazily
+  ;(() => {
+    if (sdkRef.current) {
+      return
+    }
+
+    sdkRef.current = TorusSDK.factory({ uxMode: isMobileNative ? 'popup' : 'redirect' })
+  })()
 
   useEffect(() => {
     onInitializedRef.current = onInitialized
   }, [onInitialized])
 
   useEffect(() => {
+    const { current: sdk } = sdkRef
+
     const registerTorusWorker = async () => {
       try {
         const result = await sdk.initialize()
@@ -39,7 +51,7 @@ export default (onInitialized = noop) => {
     }
 
     registerTorusWorker()
-  }, [sdk])
+  }, [])
 
-  return [sdk, initialized]
+  return [sdkRef.current, initialized]
 }

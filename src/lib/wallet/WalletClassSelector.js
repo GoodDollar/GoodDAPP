98499a157343814d4278c620a74f38a800c626f6
John Smith <alexey@gooddollar.org>
2021-10-27 19:13:15 +0300

    hotfix: Invalid call at line

--- src/lib/wallet/WalletClassSelector.js
+++ src/lib/wallet/WalletClassSelector.js
@@ -2,13 +2,18 @@ import Config from '../../config/config'
 import logger from '../../lib/logger/js-logger'
 
 const { env } = Config
+const log = logger.child({ from: 'WalletClassSelector' })
+const logSelected = className => log.debug('Selected wallet class for env', { [env]: className })
 
-let walletModule = './GoodWalletClass'
-if ('production' === env) {
-  walletModule += 'Old'
-}
+const { GoodWallet } = (() => {
+  switch (env) {
+    case 'production':
+      logSelected('GoodWalletClassOld')
+      return require('./GoodWalletClassOld')
+    default:
+      logSelected('GoodWalletClass')
+      return require('./GoodWalletClass')
+  }
+})()
 
-const { GoodWallet } = require(walletModule)
-
-logger.debug('WalletClassSelector:', { env, walletModule })
 export { GoodWallet }

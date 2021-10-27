d2e86b4b90fba894abc18664f24e8c0c0d240aac
John Smith <alexey@gooddollar.org>
2021-10-27 18:58:56 +0300

    add debug to WalletClassSelector

--- src/lib/wallet/WalletClassSelector.js
+++ src/lib/wallet/WalletClassSelector.js
@@ -1,9 +1,14 @@
 import Config from '../../config/config'
+import logger from '../../lib/logger/js-logger'
 
-let GoodWallet
-if (['production'].includes(Config.env) === false) {
-  GoodWallet = require('./GoodWalletClass').GoodWallet
-} else {
-  GoodWallet = require('./GoodWalletClassOld').GoodWallet
+const { env } = Config
+
+let walletModule = './GoodWalletClass'
+if ('production' === env) {
+  walletModule += 'Old'
 }
+
+const { GoodWallet } = require(walletModule)
+
+logger.debug('WalletClassSelector:', { env, walletModule })
 export { GoodWallet }

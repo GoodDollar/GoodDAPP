602bc3268f60e0a7967c4d4bef1ea5e6f686532a
Mukul Shakya <muklshakya123@gmail.com>
2021-11-02 18:08:04 +0530

    enhancements

--- src/pages/DatastudioDashboard/index.tsx
+++ src/pages/DatastudioDashboard/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const DASHBOARD_URL =
+    process.env.DATASTUDIO_DASHBOARD_URL ||
+    'https://datastudio.google.com/embed/reporting/f1ce8f56-058c-4e31-bfd4-1a741482642a/page/p_miuw33ljnc'
+
 const Wrapper = styled.div`
     width: 100%;
     margin-top: -2.5rem;
@@ -20,10 +24,10 @@ export default function DatastudioDashboard(): JSX.Element {
             <iframe
                 title="Datastudio Dashboard"
                 width="100%"
-                height="2000px"
-                src="https://datastudio.google.com/embed/reporting/f1ce8f56-058c-4e31-bfd4-1a741482642a/page/p_miuw33ljnc"
+                height="924"
+                src={DASHBOARD_URL}
                 frameBorder="0"
-                style={{ border: 0 }}
+                style={{ border: 'none' }}
                 allowFullScreen
             ></iframe>
         </Wrapper>

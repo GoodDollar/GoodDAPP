755328898072d5ec660077e2f2b858003c636f9d
John Smith <alexey@gooddollar.org>
2021-11-08 16:45:36 +0200

    fix bundle analyzer

--- config/paths.js
+++ config/paths.js
@@ -30,7 +30,7 @@ const getPublicUrl = appPackageJson =>
 // Webpack needs to know it to put the right <script> hrefs into HTML even in
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
+// like /todos/42/src/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =

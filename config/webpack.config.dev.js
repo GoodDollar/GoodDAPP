cb89ea52c1cb236683c827a17ea6460f8c2bae9d
John Smith <alexey@gooddollar.org>
2021-11-08 16:35:07 +0200

    fix js bundles location

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -103,9 +103,9 @@ module.exports = {
     // This does not produce a real file. It's just the virtual path that is
     // served by WebpackDevServer in development. This is the JS bundle
     // containing code from all our entry points, and the Webpack runtime.
-    filename: 'static/js/bundle.js',
+    filename: 'src/bundle.js',
     // There are also additional JS chunk files if you use code splitting.
-    chunkFilename: 'static/js/[name].chunk.js',
+    chunkFilename: 'src/[name].chunk.js',
     // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)

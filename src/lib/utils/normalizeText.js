67fd354e35ae495b72c764a2dcbee68d1400d8d4
John Smith <alexey@gooddollar.org>
2021-11-10 13:24:14 +0200

    fix: normalizeText codestyle

--- src/lib/utils/normalizeText.js
+++ src/lib/utils/normalizeText.js
@@ -10,29 +10,17 @@ const DESIGN_WIDTH = 360
 const DESIGN_HEIGHT = 640 - 24
 
 const MAX_WIDTH = 475
-const MAX_HEIGHT = 788 //without topbar which is 56
+const MAX_HEIGHT = 788 // without topbar which is 56
 
 const isDesktopOrTablet = isBrowser || isTablet
 
 const windowHeight = isDesktopOrTablet && height > MAX_HEIGHT ? MAX_HEIGHT : height
 const windowWidth = isDesktopOrTablet && width > MAX_WIDTH ? MAX_WIDTH : width
 
-const CURRENT_RESOLUTION = Math.sqrt(windowHeight * windowHeight + windowWidth * windowWidth)
-const DESIGN_RESOLUTION = Math.sqrt(DESIGN_HEIGHT * DESIGN_HEIGHT + DESIGN_WIDTH * DESIGN_WIDTH)
-
-const RESOLUTIONS_PROPORTION = CURRENT_RESOLUTION / DESIGN_RESOLUTION
-
-function normalizeText(size) {
-  let normalizedSize = size
-
-  if (RESOLUTIONS_PROPORTION < 1 && size > 16) {
-    normalizedSize *= RESOLUTIONS_PROPORTION
-  }
-
-  return normalizedSize
-}
-
-export default normalizeText
+const RESOLUTIONS_PROPORTION = Math.sqrt(
+  (windowHeight * windowHeight + windowWidth * windowWidth) /
+    (DESIGN_HEIGHT * DESIGN_HEIGHT + DESIGN_WIDTH * DESIGN_WIDTH),
+)
 
 /*
  * Decreases font-size of string based on the string lenght
@@ -48,7 +36,20 @@ export default normalizeText
 
 export const normalizeByLength = (text, fontBaseSize, decreaseThreshold, decreaseRate = 2, minFontSize = 2) => {
   const characterAmount = text.toString().length
+
   return characterAmount > decreaseThreshold
     ? Math.max(fontBaseSize - decreaseRate * (characterAmount - decreaseThreshold), minFontSize)
     : fontBaseSize
 }
+
+const normalizeText = size => {
+  let normalizedSize = size
+
+  if (RESOLUTIONS_PROPORTION < 1 && size > 16) {
+    normalizedSize *= RESOLUTIONS_PROPORTION
+  }
+
+  return normalizedSize
+}
+
+export default normalizeText

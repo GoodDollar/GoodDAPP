ec5a7a4b1ebc2c5646f574ab42869cf2c133090d
TomasGonzalez <tomasgp14@gmail.com>
2021-11-17 13:16:09 -0400

    (Chore) update the delete screen to pats specifications

--- src/lib/hooks/useDeleteAccountDialog.js
+++ src/lib/hooks/useDeleteAccountDialog.js
@@ -1,21 +1,45 @@
 import React, { useCallback } from 'react'
+import { Text } from 'react-native'
+
 import AsyncStorage from '../utils/asyncStorage'
 
 import logger from '../logger/js-logger'
 import IconWrapper from '../../components/common/modal/IconWrapper'
 import LoadingIcon from '../../components/common/modal/LoadingIcon'
+import ExplanationDialog from '../../components/common/dialogs/ExplanationDialog'
+import { theme } from '../../components/theme/styles'
 import retryImport from '../utils/retryImport'
 import restart from '../utils/restart'
+import normalizeText from '../../lib/utils/normalizeText'
 
 const log = logger.child({ from: 'useDeleteAccountDialog' })
+
+const TrashIcon = () => <IconWrapper iconName="trash" color={theme.colors.error} size={50} />
+
+const MessageTextComponent = () => (
+  <Text style={{ color: theme.colors.error, fontSize: normalizeText(18) }}>
+    If you delete your account <br /> <Text style={{ fontWeight: 'bold' }}> you might lose access to your G$!</Text>
+  </Text>
+)
 
 export default ({ API, showErrorDialog, theme }) => {
   const deleteHandler = useCallback(async () => {
     showErrorDialog('', '', {
       title: 'ARE YOU SURE?',
-      message: 'If you delete your account',
-      boldMessage: 'you might lose access to your G$!',
-      image: <LoadingIcon />,
+      content: (
+        <ExplanationDialog
+          image={LoadingIcon}
+          label={<MessageTextComponent />}
+          text={'For security reasons, it might take up to 48 hours for your data to be completely removed.'}
+          textStyle={{
+            fontSize: normalizeText(16),
+            color: theme.colors.lighterGray,
+            lineHeight: normalizeText(18),
+            textAlign: 'center',
+          }}
+          labelStyle={{ textAlign: 'center', lineHeight: normalizeText(18) }}
+        />
+      ),
       showButtons: false,
     })
 
@@ -50,9 +74,20 @@ export default ({ API, showErrorDialog, theme }) => {
     () =>
       showErrorDialog('', '', {
         title: 'ARE YOU SURE?',
-        message: 'If you delete your account',
-        boldMessage: 'you might lose access to your G$!',
-        image: <IconWrapper iconName="trash" color={theme.colors.error} size={50} />,
+        content: (
+          <ExplanationDialog
+            image={TrashIcon}
+            label={<MessageTextComponent />}
+            text={'For security reasons, it might take up to 48 hours for your data to be completely removed.'}
+            textStyle={{
+              fontSize: normalizeText(16),
+              color: theme.colors.lighterGray,
+              lineHeight: normalizeText(18),
+              textAlign: 'center',
+            }}
+            labelStyle={{ textAlign: 'center', lineHeight: normalizeText(18) }}
+          />
+        ),
         buttons: [
           { text: 'Cancel', onPress: dismiss => dismiss(), mode: 'text', color: theme.colors.lighterGray },
           {

97087ba6777d6204c71251bab0d7626507426100
John Smith <alexey@gooddollar.org>
2021-11-22 19:06:33 +0200

    fix duplicate symbols during linking

--- shim.js
+++ shim.js
@@ -1,22 +1,23 @@
-if (typeof __dirname === 'undefined') global.__dirname = '/'
-if (typeof __filename === 'undefined') global.__filename = ''
-// Needed so that 'stream-http' chooses the right default protocol.
-global.location = {
-  protocol: 'file:',
-}
+require('node-libs-react-native/globals')
 
-global.process = require('process')
-// global.process.version = 'v0.10';
-global.process.browser = false
-global.process.release = { name: 'gooddollar-wallet' }
 const isDev = typeof __DEV__ === 'boolean' && __DEV__
+const base64 = require('Base64')
 
+global.btoa = base64.btoa
+global.atob = base64.atob
+
+process.browser = false
+process.release = { name: 'gooddollar-wallet' }
 process.env['NODE_ENV'] = isDev ? 'development' : 'production'
+
 if (typeof localStorage !== 'undefined') {
   localStorage.debug = isDev ? '*' : ''
 }
 
-global.btoa = require('Base64').btoa
-global.atob = require('Base64').atob
-global.Buffer = require('buffer').Buffer
-// global.crypto = require('crypto')
+if (typeof __dirname === 'undefined') {
+  global.__dirname = '/'
+}
+
+if (typeof __filename === 'undefined') {
+  global.__filename = ''
+}

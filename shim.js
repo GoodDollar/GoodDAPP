41785f2f7c9a975d3c3e9d729de4a1665c4767ec
John Smith <alexey@gooddollar.org>
2021-11-25 16:19:28 +0200

    suggesiton: remove extra base64 polyfill

--- shim.js
+++ shim.js
@@ -1,10 +1,6 @@
 require('node-libs-react-native/globals')
 
 const isDev = typeof __DEV__ === 'boolean' && __DEV__
-const base64 = require('Base64')
-
-global.btoa = base64.btoa
-global.atob = base64.atob
 
 process.browser = false
 process.release = { name: 'gooddollar-wallet' }

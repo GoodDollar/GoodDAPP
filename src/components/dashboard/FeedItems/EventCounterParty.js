35d41ab4cef7ea353b5a1344a585505f15db506e
Tomas Gonzalez <tomasgp14@hotmail.com>
2021-12-15 09:25:17 -0400

    Fixed profile name not showing

--- src/components/dashboard/FeedItems/EventCounterParty.js
+++ src/components/dashboard/FeedItems/EventCounterParty.js
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react'
 import { get } from 'lodash'
 import { Text } from '../../common'
 import { withStyles } from '../../../lib/styles'
+import useProfile from '../../../lib/userStorage/useProfile'
 import { getEventDirection } from '../../../lib/userStorage/FeedStorage'
 
 const getStylesFromProps = () => ({
@@ -43,10 +44,17 @@ const EventContent = withStyles(getStylesFromProps)(
 
 export const EventSelfParty = ({ feedItem, styles, style, textStyle, subtitle, isSmallDevice }) => {
   const direction = useMemo(() => getEventDirection(feedItem, true), [feedItem])
+  const { fullName } = useProfile()
 
-  let hasSubtitle = get(feedItem, 'data.readMore') !== false
+  const hasSubtitle = get(feedItem, 'data.readMore') !== false
 
-  return <EventContent description={feedItem?.data?.senderName} hasSubtitle={hasSubtitle} direction={direction} />
+  return (
+    <EventContent
+      description={feedItem?.data?.senderName || fullName}
+      hasSubtitle={hasSubtitle}
+      direction={direction}
+    />
+  )
 }
 
 const EventCounterParty = ({ feedItem, styles, style, textStyle, subtitle, isSmallDevice }) => {

7a5b7347d93a8e79b7e0c29709bdaec5fc3cfbba
sirpy <hadarr@gmail.com>
2022-01-02 14:42:17 +0200

    fix: protocol integration test

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pushd node_modules/@gooddollar/goodprotocol
 export CI=false
+export MNEMONIC='test test test test test test test test test test test junk'
+export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
 yarn --frozen-lockfile
 yarn deployTest
 popd

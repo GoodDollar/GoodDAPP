8b2fdf31de7750677213da8b82356799fbd1f4ac
Sonkeng Maldini <sdmg15@pm.me>
2022-01-19 15:09:48 +0100

    fix missing identifier

--- src/lib/updates/updateSeed.js
+++ src/lib/updates/updateSeed.js
@@ -1,5 +1,3 @@
-// import API from '../API/api'
-
 import { CeramicSDK } from '@gooddollar/ceramic-seed-sdk'
 import AsyncStorage from '../../lib/utils/asyncStorage'
 import { default as API } from '../API/api'
@@ -8,9 +6,10 @@ import wallet from '../wallet/GoodWallet'
 import config from '../../config/config'
 const fromDate = new Date('2022/01/20')
 
-const upadateWalletSeed = async userIdentifier => {
+const upadateWalletSeed = async (lastUpdate, prevVersion, log) => {
+  const identifier = wallet.getAccountForType('login')
   const ceramic = new CeramicSDK(config.cermaicNodeUrl)
-  const data = await API.userExistsCheck({ userIdentifier })
+  const data = await API.userExistsCheck({ identifier })
   const privateAccountKey = await AsyncStorage.getItem('GD_masterSeed')
   const publicAccountKey = wallet.wallet.eth.accounts.privateKeyToAccount(privateAccountKey)
   ceramic.initialize(privateAccountKey, publicAccountKey.address, data.provider)

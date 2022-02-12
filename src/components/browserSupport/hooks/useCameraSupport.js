5b98a2149d974db1d2d01eabcc0ba7658de0bb96
sirpy <hadarr@gmail.com>
2022-02-12 21:45:51 +0200

    wip: log ios details for useCameraSupport

--- src/components/browserSupport/hooks/useCameraSupport.js
+++ src/components/browserSupport/hooks/useCameraSupport.js
@@ -1,9 +1,10 @@
 import { isChrome, isFirefox, isIOSWeb, isSafari, osVersionInfo } from '../../../lib/utils/platform'
 import useBrowserSupport from './useBrowserSupport'
 
-const iosSupported = isSafari || (osVersionInfo.major >= 14 && osVersionInfo.minor >= 4 && (isChrome || isFirefox))
-
 // if non-ios camera support is present on all browsers
 // for IOS camera is supported omnly in Safari or ios>=14.4 chrome+firefox
-export default (options = {}) =>
-  useBrowserSupport({ ...options, checkOutdated: false, onCheck: () => !isIOSWeb || iosSupported })
+export default (options = {}) => {
+  const iosSupported = isSafari || (osVersionInfo.major >= 14 && osVersionInfo.minor >= 4 && (isChrome || isFirefox))
+  console.log('iosSupported', { isSafari, osVersionInfo, isChrome, isFirefox })
+  return useBrowserSupport({ ...options, checkOutdated: false, onCheck: () => !isIOSWeb || iosSupported })
+}

e44cc02b513ef2ac42f8d4ff6413f4c5a18a65f4
John Smith <alexey@gooddollar.org>
2022-02-15 12:43:41 +0200

    fix GD price

--- src/lib/wallet/GoodWalletClass.js
+++ src/lib/wallet/GoodWalletClass.js
@@ -611,8 +611,9 @@ export class GoodWallet {
     )
 
     const price = await reserve.methods.currentPriceDAI().call()
+    const koeff = (Config.env || '').startsWith('prod') ? 1 : 100
 
-    return wallet.utils.fromWei(price) * 100
+    return wallet.utils.fromWei(price) * koeff
   }
 
   async getClaimScreenStatsMainnet() {

8472e62e2b4d82a1b7f162819a9f4209dcaa10cf
roman_hladilka <felixhack02@gmail.com>
2022-04-04 23:35:32 +0300

    replace for inline svg native lib

--- src/components/dashboard/FeedItems/SvgImage.native.js
+++ src/components/dashboard/FeedItems/SvgImage.native.js
@@ -1,5 +1,5 @@
 import React from 'react'
 // eslint-disable-next-line import/default
-import SvgUri from 'react-native-svg-uri'
+import SvgUri from 'react-native-svg-uri-updated'
 
 export const SvgImage = ({ src, width, height }) => <SvgUri width={width} height={height} svgXmlData={src} />

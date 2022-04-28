d37d537a895c9195af68a598565a4d452024c52b
John Smith <alexey@gooddollar.org>
2022-04-28 17:35:34 +0300

    fix

--- src/lib/login/useUserExists.js
+++ src/lib/login/useUserExists.js
@@ -6,20 +6,23 @@ import { useWallet } from '../wallet/GoodWalletProvider'
 const useUserExists = () => {
   const goodWallet = useWallet()
 
-  // eslint-disable-next-line require-await
-  const userExists = useCallback(async ({ mnemonics, privateKey, email, mobile }) => {
-    let identifier
+  const userExists = useCallback(
+    // eslint-disable-next-line require-await
+    async ({ mnemonics, privateKey, email, mobile }) => {
+      let identifier
 
-    if (goodWallet) {
-      identifier = goodWallet.getAccountForType('login')
-    }
+      if (goodWallet) {
+        identifier = goodWallet.getAccountForType('login')
+      }
 
-    if (![identifier, email, mobile].find(_ => _)) {
-      return { exists: false }
-    }
+      if (![identifier, email, mobile].find(_ => _)) {
+        return { exists: false }
+      }
 
-    return API.userExistsCheck({ identifier, email, mobile }).then(({ data }) => data)
-  })
+      return API.userExistsCheck({ identifier, email, mobile }).then(({ data }) => data)
+    },
+    [goodWallet],
+  )
 
   return userExists
 }

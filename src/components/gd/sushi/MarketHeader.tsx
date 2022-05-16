bf785ead1b7243ef94ec3f9969acd3680ca898bd
1ucius (Vadym Orlov) <take.eat.ezzy@gmail.com>
2022-05-16 05:43:36 +0300

    update mibile UI

--- src/components/gd/sushi/MarketHeader.tsx
+++ src/components/gd/sushi/MarketHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { useWindowSize } from 'hooks/useWindowSize'
+
 const Title = styled.div`
     font-style: normal;
     font-weight: bold;
@@ -34,9 +36,13 @@ const Search = styled.div`
         top: 50%;
         transform: translateY(-50%);
     }
+
+    @media screen and (max-width: 768px) {
+        width: 100%;
+    }
 `
 
-function MarketHeader({ title, lists, noSearch = false }: any) {
+function MarketHeader({ title, titleClass, lists, noSearch = false }: any) {
     if (lists.setTerm) {
         lists = [lists]
     }
@@ -47,30 +53,41 @@ function MarketHeader({ title, lists, noSearch = false }: any) {
         })
     }
 
-    return (
-        <div className="flex flex-col md:flex-row items-center justify-between w-full mb-6">
-            <div className="flex items-center w-1/2">
-                <Title className="pl-4">{title}</Title>
-            </div>
+    const { width } = useWindowSize()
 
-            <div className="flex justify-end w-full py-4 md:py-0">
-                <Search className="relative w-full max-w-md" hidden={noSearch}>
-                    <input
-                        className={`py-2 pl-4 pr-10 rounded w-full focus:outline-none focus:ring focus:ring-blue`}
-                        onChange={e => onSearch(e.target.value)}
-                        value={lists[0].term}
-                        placeholder="Search by symbol"
-                    />
-                    <div className="icon pointer-events-none">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
-                                fill="#00B0FF"
-                            />
-                        </svg>
-                    </div>
-                </Search>
+    const isMobile = width ? width <= 768 : undefined
+
+    return (
+        <div className="flex flex-col md:flex-row md:items-center justify-between w-full md:mb-6">
+            <div className="flex items-center w-1/2">
+                <Title className={`md:pl-4 ${titleClass}`}>{title}</Title>
             </div>
+            {!noSearch && (
+                <div className="flex md:justify-end w-full py-4 md:py-0">
+                    <Search className="relative w-full max-w-md" hidden={noSearch}>
+                        <input
+                            className={`py-2 pl-4 pr-10 rounded w-full focus:outline-none focus:ring focus:ring-blue`}
+                            onChange={e => onSearch(e.target.value)}
+                            value={lists[0].term}
+                            placeholder="Search by symbol"
+                        />
+                        <div className="icon pointer-events-none">
+                            <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
+                                    fill="#00B0FF"
+                                />
+                            </svg>
+                        </div>
+                    </Search>
+                </div>
+            )}
         </div>
     )
 }

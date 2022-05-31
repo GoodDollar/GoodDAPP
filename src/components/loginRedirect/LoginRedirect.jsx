17c8db99596f608696da39f55d539d0b23208dfa
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2022-06-01 03:35:30 +0530

    login-integration-ui

--- src/components/loginRedirect/LoginRedirect.jsx
+++ src/components/loginRedirect/LoginRedirect.jsx
@@ -1,5 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react'
+<<<<<<< HEAD
 import { BackHandler, View } from 'react-native'
+=======
+import { BackHandler, CheckBox, View } from 'react-native'
+>>>>>>> 522f65f6e (login-integration-ui)
 import GooddollarImage from '../../assets/gooddollarLogin.svg'
 
 import { CustomButton, Section, Text } from '../common'
@@ -152,45 +156,96 @@ const LoginRedirect = ({
                 </View>
               </View>
               <Text style={{ fontWeight: 'bold' }}>is requesting to view the following information:</Text>
+<<<<<<< HEAD
               <View
                 style={{
                   marginTop: 20,
+=======
+              <Text>(please note that you’re free to hide certain info)</Text>
+              <View
+                style={{
+                  width: '100%',
+                  alignItems: 'center',
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+>>>>>>> 522f65f6e (login-integration-ui)
                 }}
               >
                 <View style={{ alignItems: 'flex-start' }}>
                   <Text style={{ color: '#8499BB' }}>Name</Text>
                   <Text>{fullName}</Text>
                 </View>
+<<<<<<< HEAD
               </View>
               <View
                 style={{
                   marginTop: 20,
+=======
+                <CheckBox />
+              </View>
+              <View
+                style={{
+                  width: '100%',
+                  alignItems: 'center',
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+>>>>>>> 522f65f6e (login-integration-ui)
                 }}
               >
                 <View style={{ alignItems: 'flex-start' }}>
                   <Text style={{ color: '#8499BB' }}>Mobile</Text>
                   <Text>{mobile}</Text>
                 </View>
+<<<<<<< HEAD
               </View>
               <View
                 style={{
                   marginTop: 20,
+=======
+                <CheckBox />
+              </View>
+              <View
+                style={{
+                  width: '100%',
+                  alignItems: 'center',
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+>>>>>>> 522f65f6e (login-integration-ui)
                 }}
               >
                 <View style={{ alignItems: 'flex-start' }}>
                   <Text style={{ color: '#8499BB' }}>Email</Text>
                   <Text>{email}</Text>
                 </View>
+<<<<<<< HEAD
               </View>
               <View
                 style={{
                   marginTop: 20,
+=======
+                <CheckBox />
+              </View>
+              <View
+                style={{
+                  width: '100%',
+                  alignItems: 'center',
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+>>>>>>> 522f65f6e (login-integration-ui)
                 }}
               >
                 <View style={{ alignItems: 'flex-start' }}>
                   <Text style={{ color: '#8499BB' }}>Location</Text>
                   <Text>{country}</Text>
                 </View>
+<<<<<<< HEAD
+=======
+                <CheckBox />
+>>>>>>> 522f65f6e (login-integration-ui)
               </View>
               <View style={{ width: '100%', alignItems: 'flex-start', marginTop: 10 }}>
                 <Text style={{ color: '#8499BB' }}>Wallet Address</Text>
@@ -227,6 +282,61 @@ const LoginRedirect = ({
                   Allow
                 </CustomButton>
               </View>
+<<<<<<< HEAD
+=======
+              {/* <Text style={{ textAlign: 'flex-start', fontSize: 18, marginBottom: 10 }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{urlDetails.vendorName}</Text> is asking for the
+              following details
+            </Text>
+            {urlDetails.requestedDetails.includes('name') && (
+        <Text style={{ textAlign: 'flex-start', fontSize: 14, lineHeight: 18 }}>Name : {fullName}</Text>
+            )}
+            {urlDetails.requestedDetails.includes('mobile') && (
+              <Text style={{ textAlign: 'flex-start', fontSize: 14, lineHeight: 18 }}>Mobile : {mobile}</Text>
+            )}
+            {urlDetails.requestedDetails.includes('location') && (
+              <Text style={{ textAlign: 'flex-start', fontSize: 14, lineHeight: 18 }}>Location : {country}</Text>
+            )}
+            {urlDetails.requestedDetails.includes('email') && (
+              <Text style={{ textAlign: 'flex-start', fontSize: 14, lineHeight: 18 }}>Email : {email}</Text>
+            )}
+            <Text style={{ textAlign: 'flex-start', fontSize: 14, lineHeight: 18 }}>
+              Wallet Address : {walletAddress}
+            </Text>
+            <Text style={{ textAlign: 'flex-start', fontSize: 15, marginTop: 10 }}>
+              Vendor&apos;s URL : <Text style={{ fontWeight: '500' }}>{urlDetails?.vendorURL}</Text>
+            </Text>
+            <Text style={{ marginBottom: 10, textAlign: 'flex-start', fontSize: 15 }}>
+              Vendor&apos;s Wallet Address : <Text style={{ fontWeight: '500' }}>{urlDetails?.vendorAddress}</Text>
+            </Text>
+            <Text style={{ textAlign: 'flex-start' }}>
+              Do you want to allow {urlDetails?.vendorName} to access this information?
+            </Text>
+            {isWebDomainDifferent && (
+              <View style={{ padding: 10, backgroundColor: 'lightyellow', marginTop: 5 }}>
+                <Text style={{ textAlign: 'flex-start' }}>
+                  Warning ! The web url of vendor is different than the redirect/callback URL
+                </Text>
+              </View>
+            )}
+            {!isVendorWalletWhitelisted && (
+              <View style={{ padding: 10, backgroundColor: 'lightyellow', marginTop: 5 }}>
+                <Text style={{ textAlign: 'flex-start' }}>Warning ! The vendor wallet address is not verified !</Text>
+              </View>
+            )}
+
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+              <CustomButton
+                onPress={() => submit({ isRejected: true })}
+                style={{ width: 150, backgroundColor: 'lightgrey', marginRight: 20, borderRadius: 5 }}
+              >
+                Deny
+              </CustomButton>
+              <CustomButton onPress={submit} style={{ width: 150, borderRadius: 5 }}>
+                Allow
+              </CustomButton>
+            </View> */}
+>>>>>>> 522f65f6e (login-integration-ui)
             </View>
           </View>
         </Section>

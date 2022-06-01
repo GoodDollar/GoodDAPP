7463f311dd839a4d126ae79f2fd86d9f58b6e98b
johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
2022-06-01 18:54:29 +0300

    add read new feed index, optimize sync (#3739)
    * add read new feed index, optimize sync
    
    * add small refactoring
    
    * add comments
    
    * add lint fixes, updated code push
    
    * add fixes
    
    * add update indexes in all envs

--- src/lib/hoc/withHotCodePush.native.js
+++ src/lib/hoc/withHotCodePush.native.js
@@ -1,7 +1,7 @@
-import codePush, { CheckFrequency } from 'react-native-code-push'
+import codePush from 'react-native-code-push' // eslint-disable-line import/default
 
+const { CheckFrequency } = codePush
 const defaultOptions = { checkFrequency: CheckFrequency.MANUAL }
-
 const withHotCodePush = (component, options = defaultOptions) => codePush(options)(component)
 
 export default withHotCodePush

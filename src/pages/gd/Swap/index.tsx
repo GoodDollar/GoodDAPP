c0dc651b067f1d25940e7b514e16c1b6c61513fa
Lewis B <lewis@ikigaistudios.eu>
2022-06-07 00:45:58 +0200

    add calculating loader for both sell/buy

--- src/pages/gd/Swap/index.tsx
+++ src/pages/gd/Swap/index.tsx
@@ -72,7 +72,8 @@ function Swap() {
     const web3 = useWeb3()
     const [lastEdited, setLastEdited] = useState<{ field: 'external' | 'internal' }>()
 
-    const [calculating, setCalculating] = useState(false)
+    const [calcSell, setCalcSell] = useState(false)
+    const [calcBuy, setCalcBuy] = useState(false)
 
     const metaTimer = useRef<any>()
     useEffect(() => {
@@ -98,7 +99,13 @@ function Swap() {
         }
 
         const timer = (metaTimer.current = setTimeout(async () => {
-          setCalculating(true)
+          
+          buying ?
+            field === 'external' 
+            ? setCalcSell(true) : setCalcBuy(true)
+            : field === 'external'
+            ? setCalcBuy(true) : setCalcSell(true)
+
             const meta = await getMeta(web3, symbol, value, parseFloat(slippageTolerance.value)).catch(e => {
                 console.error(e)
                 return null
@@ -115,7 +122,13 @@ function Swap() {
                     : meta.outputAmount.toExact()
             )
             setMeta(meta)
-            setCalculating(false)
+
+            buying ?
+              field === 'external' 
+              ? setCalcSell(false) : setCalcBuy(false)
+              : field === 'external'
+              ? setCalcBuy(false) : setCalcSell(false)
+            
         }, 400))
     }, [account, chainId, lastEdited, buying, web3, slippageTolerance.value])
     const [approving, setApproving] = useState(false)
@@ -304,6 +317,7 @@ function Swap() {
                                 setMeta(undefined)
                             }}
                             tokenList={tokenList ?? []}
+                            isCalculating={calcBuy}
                         />
                         <div className="switch">
                             {cloneElement(SwitchSVG, {
@@ -322,7 +336,7 @@ function Swap() {
                                 setSwapValue(value)
                                 setLastEdited({ field: 'internal' })
                             }}
-                            isCalculating={calculating}
+                            isCalculating={calcSell}
                             style={{ marginTop: buying ? 13 : 0, marginBottom: buying ? 0 : 13, order: buying ? 3 : 1 }}
                         />
                         <div style={{ marginTop: 14, padding: '0 4px' }}>

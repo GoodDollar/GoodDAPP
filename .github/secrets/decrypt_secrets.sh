c7651dc84e7cc7a2a5f09123ba1d5fe2bf53172b
alt-a1r <95229834+alt-a1r@users.noreply.github.com>
2022-06-17 20:11:39 +0300

    Chore: update ios ci workflow (#3742)
    * chore: update ios ci workflow
    
    * fix comments
    
    * fix
    
    * update ios ci workflow; uncomment codepush job fow ios
    
    * fix indentation
    
    * fix indentation
    
    * added cocoadpods installation
    
    * pods install fix
    
    * pods install fix
    
    * pods install fix
    
    * added c-compiler install
    
    * added xcode switch
    
    * update pods install
    
    * indent fix
    
    * added cocoapods install
    
    * commented codepush job; updated build job wo caching; updated xcode version;
    
    * hotfix
    
    * fix checkout
    
    * fix max_user_watches for osx
    
    * upgrade node version
    
    * added jdk install
    
    * Apply suggestions from code review
    
    * adjust ios flow
    
    * changed checkout version; removed xcode select
    
    * echo target branch
    
    * check android actions/checkout call
    
    * remove checkout ref param
    
    * fix
    
    * added network timeout
    
    * fix
    
    * restored pods and npm deps caching
    
    * remove node modules caching; added checkout ref;
    
    * echo target branch
    
    * add root user config
    
    * temporary comment triggering by pull request
    
    * fixes: android build, workflow dispatch inputs;
    
    * add animation assets to ios build
    
    * change branch name
    
    * name hotfix
    
    * added iphoneos deployment target version to podfile
    
    * added profile provisioning set
    
    * add codesign
    
    * update
    
    * check identities
    
    * update
    
    * codesign refactoring
    
    * fix
    
    * provision profile flow update
    
    * fix
    
    * build app action draft
    
    * secrets update; build flow update
    
    * update
    
    * pbxproj
    
    * update
    
    * update
    
    * update
    
    * certificate and key separation approach
    
    * update
    
    * update
    
    * update
    
    * add scheme
    
    * add update-targets
    
    * echo certificate
    
    * manual team-id set
    
    * fix appbuild & archive
    
    * trigger workflow
    
    * update
    
    * revert xcodebuild flow
    
    * trigger workflow
    
    * echo certificate
    
    * pbxproj update
    
    * trigger workflow
    
    * signing flow refactor
    
    * add PP cat
    
    * remove cat
    
    * trigger workflow
    
    * update signing flow
    
    * temporary switch deps install to yarn
    
    * revert bahmutov/npm-install@v1 action
    
    * clean trash; update workflow
    
    * update
    
    * update
    
    * check
    
    * ci.plist update
    
    * fix
    
    * clean trash; restore codepush workflow
    
    * add codepush trigger condition
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * trigger workflow
    
    * update
    
    * update
    
    * echo target branch
    
    Co-authored-by: Artem_Maslakov <a.maslakov@artjoker.team>
    Co-authored-by: johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
    Co-authored-by: John Smith <alexey@gooddollar.org>

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,20 +4,16 @@ echo "Decrypting files - using IS_PROD value of: $IS_PROD"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 echo $IOS_CERTIFICATE | base64 --decode > ./Certificates.p12
-if $IS_PROD; then
-  echo $IOS_STORE_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/21f28d4b-7641-4420-875f-989a0bcc3d52.mobileprovision
-else
-  echo $IOS_ADHOC_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/b29b41c9-e82a-4aab-96a8-e5d00e55756e.mobileprovision
-fi
+echo $IOS_ADHOC_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/build_pp.mobileprovision
 
 echo "Creating keychain"
 security create-keychain -p "" build.keychain
-security import ./Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PROFILE_KEY" -A
+security import ./Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_CERTIFICATE_PASSWORD" -A
 
 echo "Installing in keychain"
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security set-keychain-settings -lut 3000
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
+security find-identity -v
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain

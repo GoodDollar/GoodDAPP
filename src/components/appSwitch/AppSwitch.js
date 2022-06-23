df42e3df4f3425d9188d6879e2f4bbf15e7befa3
sirpy <hadarr@gmail.com>
2022-06-23 23:44:42 +0300

    fix: init registered usertorage only if logged in

--- src/components/appSwitch/AppSwitch.js
+++ src/components/appSwitch/AppSwitch.js
@@ -224,8 +224,9 @@ const AppSwitch = (props: LoadingProps) => {
   useAppState({ onForeground: recheck })
 
   useEffect(() => {
-    initWalletAndStorage(undefined, 'SEED', true).then(() => log.debug('storage and wallet ready'))
-  }, [initWalletAndStorage])
+    //initialize with initRegistered = true only if user is loggedin correctly
+    initWalletAndStorage(undefined, 'SEED', isLoggedIn).then(() => log.debug('storage and wallet ready'))
+  }, [initWalletAndStorage, isLoggedIn])
 
   useEffect(() => {
     const { initializedRegistered } = userStorage || {}

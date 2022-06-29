53ba2e8bde8e954a0e0448cf2ae5163c1a34ce52
Roman Hladilka <felixhack02@gmail.com>
2022-06-29 18:37:35 +0300

    fix: pass identifier to api

--- src/components/auth/hooks/useCheckExisting.js
+++ src/components/auth/hooks/useCheckExisting.js
@@ -1,7 +1,7 @@
 import { useCallback, useContext } from 'react'
 
 import logger from '../../../lib/logger/js-logger'
-import useUserExists from '../../../lib/login/useUserExists'
+import userExists from '../../../lib/login/userExists'
 import AuthContext from '../context/AuthContext'
 
 const log = logger.child({ from: 'useCheckExisting' })
@@ -9,11 +9,10 @@ const log = logger.child({ from: 'useCheckExisting' })
 // check if email/mobile was used to register before and offer user to login instead
 const useCheckExisting = () => {
   const { activeStep, setAlreadySignedUp } = useContext(AuthContext)
-  const userExists = useUserExists()
 
   const checkExisting = useCallback(
     async (torusProvider, torusUser, goodWallet, eventVars = {}) => {
-      const checkResult = await userExists(torusUser).catch(e => {
+      const checkResult = await userExists(torusUser, goodWallet).catch(e => {
         log.warn('userExists check failed:', e.message, e)
         return { exists: false }
       })
@@ -27,7 +26,7 @@ const useCheckExisting = () => {
       }
 
       // User exists, it is not the number check and it is the correct login
-      if (provider?.includes(torusProvider) && !activeStep) {
+      if (provider?.includes(torusProvider) && activeStep === 1) {
         return 'login'
       }
 
@@ -35,7 +34,7 @@ const useCheckExisting = () => {
 
       return new Promise(resolve => setAlreadySignedUp(checkResult, analyticsVars, resolve))
     },
-    [userExists],
+    [activeStep, setAlreadySignedUp],
   )
 
   return checkExisting

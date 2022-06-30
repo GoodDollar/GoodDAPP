b6ec165011181f60b4403f175a8eaf0018625b7d
Artem_Maslakov <a.maslakov@artjoker.team>
2022-07-01 02:57:54 +0300

    extend codepush options

--- android/app/src/main/java/org/gooddollar/MainApplication.java
+++ android/app/src/main/java/org/gooddollar/MainApplication.java
@@ -14,7 +14,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import io.branch.rnbranch.RNBranchModule;
-import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 

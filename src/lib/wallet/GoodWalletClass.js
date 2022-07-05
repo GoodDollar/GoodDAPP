8095087d77331abc051e104ecb6f35850edaec8b
John Smith <alexey@gooddollar.org>
2022-07-05 20:14:30 +0300

    fix

--- src/lib/wallet/GoodWalletClass.js
+++ src/lib/wallet/GoodWalletClass.js
@@ -20,6 +20,7 @@ import {
   chunk,
   filter,
   findKey,
+  first,
   flatten,
   get,
   identity,
@@ -1431,7 +1432,7 @@ export class GoodWallet {
     const checksum = this.wallet.utils.toChecksumAddress(address)
     const findByKey = contracts => findKey(contracts, key => [lcAddress, checksum].includes(key))
 
-    return filter(values(ContractsAddress).map(findByKey))
+    return first(filter(values(ContractsAddress).map(findByKey)))
   }
 }
 

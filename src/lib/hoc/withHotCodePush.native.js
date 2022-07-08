29f7f965097c335306a1dd2e765b4d818c49ea9c
John Smith <alexey@gooddollar.org>
2022-07-08 19:18:11 +0300

    import code push options from dev/qa

--- src/lib/hoc/withHotCodePush.native.js
+++ src/lib/hoc/withHotCodePush.native.js
@@ -1,7 +1,14 @@
 import codePush from 'react-native-code-push' // eslint-disable-line import/default
 
-const { CheckFrequency } = codePush
-const defaultOptions = { checkFrequency: CheckFrequency.MANUAL }
+const { CheckFrequency, InstallMode } = codePush
+const defaultOptions = {
+  checkFrequency: CheckFrequency.ON_APP_RESUME,
+  mandatoryInstallMode: InstallMode.IMMEDIATE,
+  updateDialog: {
+    appendReleaseDescription: true,
+    title: 'a new update is available!',
+  },
+}
 const withHotCodePush = (component, options = defaultOptions) => codePush(options)(component)
 
 export default withHotCodePush

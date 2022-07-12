c3235e2d4b61e340d0f30d3503850ffdc0bda7d8
John Smith <alexey@gooddollar.org>
2022-07-12 15:12:15 +0300

    add fix refund flag, modify update

--- src/lib/updates/resetRefund.js
+++ src/lib/updates/resetRefund.js
@@ -1,21 +1,18 @@
 import { REFUNDED_FLAG } from '../../components/refund/hooks/useRefund'
 
-const fromDate = new Date('2022/07/12')
+const fromDate = new Date('2022/07/13')
 
 /**
  * @returns {Promise<void>}
  */
+// eslint-disable-next-line require-await
 const resetRefund = async (lastUpdate, prevVersion, log, goodWallet, userStorage) => {
   const { userProperties } = userStorage
 
   log.debug('ready to reset refund flag, updating user props')
 
-  try {
-    await userProperties.set(REFUNDED_FLAG, false)
-    log.debug('refund flag have been reset')
-  } catch (e) {
-    log.error('failed to reset refund flag', e.message, e)
-  }
+  userProperties.setLocal(REFUNDED_FLAG, false)
+  log.debug('refund flag have been reset')
 }
 
 export default { fromDate, update: resetRefund, key: 'resetRefund' }

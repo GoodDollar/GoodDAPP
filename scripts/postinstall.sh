41422d78f9a8b281e31b8ac6b3e554a774d3cbba
sirpy <hadarr@gmail.com>
2022-07-19 13:16:24 +0300

    fix: revert some changes

--- scripts/postinstall.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-rm -rf node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg
-rm -rf node_modules/@walletconnect/react-native-dapp/node_modules/react-native-qrcode-svg/node_modules/react-native-svg

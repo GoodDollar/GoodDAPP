c856df249d882c85caf0291eac78f8e6905eecd1
johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
2022-07-26 12:41:50 +0300

    Apply suggestions from code review

--- src/lib/utils/linking.js
+++ src/lib/utils/linking.js
@@ -53,7 +53,7 @@ export const openLink = async (uri: string, target: '_blank' | '_self' = '_blank
 }
 
 export const handleLinks = async (logger = log) => {
-  const { params } = DeepLinking
+  const { params, pathname } = DeepLinking
 
   try {
     const { inviteCode } = params
@@ -63,7 +63,7 @@ export const handleLinks = async (logger = log) => {
       await AsyncStorage.setItem(INVITE_CODE, inviteCode)
     }
 
-    let path = get(DeepLinking, 'pathname', '').slice(1)
+    let path =(pathname || '').slice(1)
     path = path.length === 0 ? 'AppNavigation/Dashboard/Home' : path
 
     if (params && Object.keys(params).length > 0) {

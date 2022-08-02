be9308412aaeb513e41b91b4c1f3e50ddee6d524
sirpy <hadarr@gmail.com>
2022-08-02 09:36:50 +0300

    refactor: add async to afterscan

--- src/lib/wallet/WalletConnectClient.js
+++ src/lib/wallet/WalletConnectClient.js
@@ -513,46 +513,48 @@ export const useWalletConnectSession = () => {
     log.debug('setting chain:', { chainDetails })
     setChain(chainDetails)
 
-    // if (activeConnector && chains.length > 0) {
-    // const payload = {
-    //   id: 1657446841779151,
-    //   jsonrpc: '2.0',
-    //   method: 'eth_sendTransaction',
-    //   params: [
-    //     {
-    //       from: '0x1379510d8b1dd389d4cf1b9c6c3c8cc3136d8e56',
-    //       to: '0xe3f85aad0c8dd7337427b9df5d0fb741d65eeeb5',
-    //       gasPrice: 1e9,
-    //       gas: '0x3b90d',
-    //       value: '0x2d79883d2000',
-    //       data:
-    //         '0x7ff36ab5000000000000000000000000000000000000000000000000003221e606b24f2900000000000000000000000000000000000000000000000000000000000000800000000000000000000000001379510d8b1dd389d4cf1b9c6c3c8cc3136d8e560000000000000000000000000000000000000000000000000000000062caa66500000000000000000000000000000000000000000000000000000000000000030000000000000000000000000be9e53fd7edac9f859882afdda116645287c629000000000000000000000000620fd5fa44be6af63715ef4e65ddfa0387ad13f500000000000000000000000034ef2cc892a88415e9f02b91bfa9c91fc0be6bd4',
-    //     },
-    //   ],
-    // }
-    // handleTxRequest(payload.params[0], payload, activeConnector)
-    // const payload = {
-    //   id: 1657446841779151,
-    //   jsonrpc: '2.0',
-    //   method: 'wallet_addEthereumChain',
-    //   params: [
-    //     {
-    //       chainId: '0x' + (122).toString(16),
-    //       chainName: 'fuse',
-    //       nativeCurrency: {
-    //         name: 'Fuse',
-    //         symbol: 'fuse',
-    //         decimals: 18,
-    //       },
-    //       rpcUrls: ['https://rpc.fuse.io'],
-    //       blockExplorerUrls: ['https://explorer.fuse.io'],
-    //       iconUrls: [],
-    //     },
-    //   ],
-    // }
-    // // handleSwitchChainRequest(payload, activeConnector)
-    // handleUnsupportedRequest(payload, activeConnector)
-    // }
+    /**
+    if (activeConnector && chains.length > 0) {
+    const payload = {
+      id: 1657446841779151,
+      jsonrpc: '2.0',
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: '0x1379510d8b1dd389d4cf1b9c6c3c8cc3136d8e56',
+          to: '0xe3f85aad0c8dd7337427b9df5d0fb741d65eeeb5',
+          gasPrice: 1e9,
+          gas: '0x3b90d',
+          value: '0x2d79883d2000',
+          data:
+            '0x7ff36ab5000000000000000000000000000000000000000000000000003221e606b24f2900000000000000000000000000000000000000000000000000000000000000800000000000000000000000001379510d8b1dd389d4cf1b9c6c3c8cc3136d8e560000000000000000000000000000000000000000000000000000000062caa66500000000000000000000000000000000000000000000000000000000000000030000000000000000000000000be9e53fd7edac9f859882afdda116645287c629000000000000000000000000620fd5fa44be6af63715ef4e65ddfa0387ad13f500000000000000000000000034ef2cc892a88415e9f02b91bfa9c91fc0be6bd4',
+        },
+      ],
+    }
+    handleTxRequest(payload.params[0], payload, activeConnector)
+    const payload = {
+      id: 1657446841779151,
+      jsonrpc: '2.0',
+      method: 'wallet_addEthereumChain',
+      params: [
+        {
+          chainId: '0x' + (122).toString(16),
+          chainName: 'fuse',
+          nativeCurrency: {
+            name: 'Fuse',
+            symbol: 'fuse',
+            decimals: 18,
+          },
+          rpcUrls: ['https://rpc.fuse.io'],
+          blockExplorerUrls: ['https://explorer.fuse.io'],
+          iconUrls: [],
+        },
+      ],
+    }
+    // handleSwitchChainRequest(payload, activeConnector)
+    handleUnsupportedRequest(payload, activeConnector)
+    }
+    **/
   }, [activeConnector, chains, setChain, handleTxRequest])
 
   return {

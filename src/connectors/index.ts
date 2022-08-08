ee10d419c0475d9a65aa2ff5e801eaec17ef2e25
Lewis B <lewis@ikigaistudios.eu>
2022-08-08 16:36:28 +0200

    (fix): remove resolution for onboard/core and update to 2.6

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -64,7 +64,7 @@ const injectedBN = injectedModule({
   filter: {
     ["Binance Smart Wallet"]: false,
     ["MetaMask"]: true,
-    ["Coinbase Wallet"]: ['desktop'], // both injected / wallet-module enabled for coinbase in-app browser.. filter out on desktop to avoid duplicate buttons
+    ["Coinbase Wallet"]: true,
     ["detected"]: true,
     ["trust"]: false,
     ["opera"]: false,
@@ -111,6 +111,7 @@ const zenGoBN = zenGoModule({
 
 // const walletLink = walletLinkModule({ darkMode: true })
 
+
 export const onboard = init({
   wallets: [injectedBN, walletConnectBN, zenGoBN, coinbaseWalletSdk],
   chains: [
@@ -149,6 +150,9 @@ export const onboard = init({
   },
   accountCenter: {
     desktop: {
+      enabled: false,
+    },
+    mobile: {
       enabled: false,
     }
   },
@@ -225,7 +229,40 @@ export const onboard = init({
         "poweredBy": "powered by",
         "addAccount": "Add Account",
         "setPrimaryAccount": "Set Primary Account",
-        "disconnectWallet": "Disconnect Wallet"
+        "disconnectWallet": "Disconnect Wallet",
+        "copyAddress": "Copy Wallet address"
+      },
+      "notify": {
+        "transaction": {
+          "txRequest": "Your transaction is waiting for you to confirm",
+          "nsfFail": "You have insufficient funds for this transaction",
+          "txUnderpriced": "The gas price for your transaction is too low, try a higher gas price",
+          "txRepeat": "This could be a repeat transaction",
+          "txAwaitingApproval": "You have a previous transaction waiting for you to confirm",
+          "txConfirmReminder": "Please confirm your transaction to continue",
+          "txSendFail": "You rejected the transaction",
+          "txSent": "Your transaction has been sent to the network",
+          "txStallPending": "Your transaction has stalled before it was sent, please try again",
+          "txStuck": "Your transaction is stuck due to a nonce gap",
+          "txPool": "Your transaction has started",
+          "txStallConfirmed": "Your transaction has stalled and hasn't been confirmed",
+          "txSpeedUp": "Your transaction has been sped up",
+          "txCancel": "Your transaction is being canceled",
+          "txFailed": "Your transaction has failed",
+          "txConfirmed": "Your transaction has succeeded",
+          "txError": "Oops something went wrong, please try again"
+        },
+        "watched": {
+          "txPool": "Your account is {verb} {formattedValue} {asset} {preposition} {counterpartyShortened}",
+          "txSpeedUp": "Transaction for {formattedValue} {asset} {preposition} {counterpartyShortened} has been sped up",
+          "txCancel": "Transaction for {formattedValue} {asset} {preposition} {counterpartyShortened} has been canceled",
+          "txConfirmed": "Your account successfully {verb} {formattedValue} {asset} {preposition} {counterpartyShortened}",
+          "txFailed": "Your account failed to {verb} {formattedValue} {asset} {preposition} {counterpartyShortened}"
+        },
+        "time": {
+          "minutes": "min",
+          "seconds": "sec"
+        }
       }
     }
   }

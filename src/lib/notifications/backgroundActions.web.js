18602156f8d5e17ef671c4d82d9738d241f17c8f
John Smith <alexey@gooddollar.org>
2022-08-09 20:51:34 +0300

    remove extra stubs

--- src/lib/notifications/backgroundActions.web.js
+++ src/lib/notifications/backgroundActions.web.js
@@ -1,13 +1,3 @@
 import { noop } from 'lodash'
 
-export const CLAIM_NOTIFICATION = 'claim-notification'
-
-export const dailyClaimTime = new Date().setUTCHours(12)
-
-export const notificationsCategories = {
-  CLAIM: 'CLAIM',
-}
-
-export const dailyClaimNotification = noop
-
 export const useNotifications = noop

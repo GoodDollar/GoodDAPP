6c7815db1dfc10750b56ff0a16f35afcae0db1c5
Roman Hladilka <felixhack02@gmail.com>
2022-08-11 15:54:46 +0300

    replace old market link

--- src/config/config.js
+++ src/config/config.js
@@ -105,7 +105,7 @@ const Config = {
   infuraKey: env.REACT_APP_INFURA_KEY,
   interestCollectedInterval: env.REACT_APP_INTEREST_BLOCKS_INTERVAL || 5760 * 8, // default is 1Week, add 1 day because its not exact
   goodDollarPriceInfoUrl: env.REACT_APP_PRICE_INFO_URL || 'https://datastudio.google.com/u/0/reporting/f1ce8f56-058c-4e31-bfd4-1a741482642a/page/p_97jwocmrmc',
-  marketUrl: env.REACT_APP_MARKET_URL || 'https://gooddollarmarketplace.sharetribe.com/en',
+  marketUrl: env.REACT_APP_MARKET_URL || 'https://goodmarkets.xyz/',
   torusEnabled: env.REACT_APP_USE_TORUS === 'true',
   torusNetwork: env.REACT_APP_TORUS_NETWORK || 'testnet',
   torusProxyContract: env.REACT_APP_TORUS_PROXY_CONTRACT || '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183',

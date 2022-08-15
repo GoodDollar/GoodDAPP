935e4a19c4498fe229e6a08e3b49e5b88cad5b37
alt-a1r <a.maslakov@artjoker.team>
2022-08-15 12:57:43 +0300

    PP separation workflow change

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -7,12 +7,14 @@ echo $IOS_CERTIFICATE | base64 --decode > ./Certificates.p12
 
 if [ "$IS_PROD" ]; then
     echo $IOS_STORE_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/build_pp.mobileprovision
-    cat ./ios/appstore_ci.plist > ./ios/ci.plist
-    cat ./ios/GoodDollar.xcodeproj/appstore.pbxproj > ./ios/GoodDollar.xcodeproj/project.pbxproj
+    /usr/libexec/PlistBuddy -c 'set objects:13B07F951A680F5B00A75B9A:buildSettings:PROVISIONING_PROFILE_SPECIFIER AppStore (iOS Distribution)' ./ios/GoodDollar.xcodeproj/project.pbxproj
+    /usr/libexec/PlistBuddy -c 'set method app-store' ./ios/ci.plist
+    /usr/libexec/PlistBuddy -c 'set provisioningProfiles:org.gooddollar AppStore (iOS Distribution)' ./ios/ci.plist
   else
     echo $IOS_ADHOC_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/build_pp.mobileprovision
-    cat ./ios/adhoc_ci.plist > ./ios/ci.plist
-    cat ./ios/GoodDollar.xcodeproj/adhoc.pbxproj > ./ios/GoodDollar.xcodeproj/project.pbxproj
+    /usr/libexec/PlistBuddy -c 'set objects:13B07F951A680F5B00A75B9A:buildSettings:PROVISIONING_PROFILE_SPECIFIER GoodDollar AdHoc (iOS Distribution)' ./ios/GoodDollar.xcodeproj/project.pbxproj
+    /usr/libexec/PlistBuddy -c 'set method ad-hoc' ./ios/ci.plist
+    /usr/libexec/PlistBuddy -c 'set provisioningProfiles:org.gooddollar GoodDollar AdHoc (iOS Distribution)' ./ios/ci.plist
 fi
 
 echo "Creating keychain"

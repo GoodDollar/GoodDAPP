3d0ff94f4856574e75ec66412470d437116910e4
John Smith <alexey@gooddollar.org>
2022-08-17 14:14:19 +0300

    add restore patch package call at BC unit tests setup

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -4,6 +4,7 @@ export CI=false
 export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
 yarn --immutable
+npx patch-package
 yarn deployTest
 yarn minimize
 popd

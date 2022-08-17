b103aa13cb8e4bd736abc9bec50aeb34b689d6b3
John Smith <alexey@gooddollar.org>
2022-08-17 19:59:16 +0300

    remove hot code push update dialog

--- src/lib/hoc/withHotCodePush.native.js
+++ src/lib/hoc/withHotCodePush.native.js
@@ -1,15 +1,9 @@
 import codePush from 'react-native-code-push' // eslint-disable-line import/default
-import { t } from '@lingui/macro'
 
-const { CheckFrequency, InstallMode } = codePush
+const { CheckFrequency } = codePush
 
 const defaultOptions = {
   checkFrequency: CheckFrequency.MANUAL,
-  mandatoryInstallMode: InstallMode.IMMEDIATE,
-  updateDialog: {
-    appendReleaseDescription: true,
-    title: t`A new update is available!`,
-  },
 }
 
 const withHotCodePush = (component, options = defaultOptions) => codePush(options)(component)

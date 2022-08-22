a4688b33807e96067d8e186e751e6fce45802d5b
Roman Hladilka <felixhack02@gmail.com>
2022-08-22 17:21:21 +0300

    remove wrapper prop

--- src/components/common/buttons/CustomIcon.js
+++ src/components/common/buttons/CustomIcon.js
@@ -4,12 +4,12 @@ import { View } from 'react-native'
 import Icon from '../view/Icon'
 import { withStyles } from '../../../lib/styles'
 
-const CustomIcon = ({ styles, theme, name, color, size, circle = 16, wrapper = true, reverse, reverseColor }) => {
+const CustomIcon = ({ styles, theme, name, color, size, circle = 16, reverse, reverseColor }) => {
   const [_circle, borderRadius] = useMemo(() => {
     const _circle = circle !== false
 
     return [_circle, _circle ? circle / 2 : null]
-  }, [circle, wrapper])
+  }, [circle])
 
   return (
     <View

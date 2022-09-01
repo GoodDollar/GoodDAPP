0526b639cc70954bcd7f30603825ec2d0717b1e5
John Smith <alexey@gooddollar.org>
2022-09-01 13:27:37 +0300

    add adjust background actions

--- src/lib/notifications/backgroundActions.native.js
+++ src/lib/notifications/backgroundActions.native.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { Notifications } from 'react-native-notifications'
 import { t } from '@lingui/macro'
 import { assign, invokeMap, pickBy } from 'lodash'
@@ -9,7 +9,9 @@ import { fireEvent, NOTIFICATION_ERROR, NOTIFICATION_RECEIVED, NOTIFICATION_TAPP
 import Config from '../../config/config'
 import { useUserStorage } from '../wallet/GoodWalletProvider'
 
+const { testClaimNotification } = Config
 const log = logger.child({ from: 'backgroundFetch' })
+const notificationsEnabled = userStorage => userStorage?.userProperties?.getLocal('shouldRemindClaims') || false
 
 export const dailyClaimTime = new Date().setUTCHours(12)
 
@@ -21,9 +23,8 @@ export const dailyClaimNotification = async (userStorage, goodWallet) => {
   const { userProperties } = userStorage
   const dateNow = new Date()
   const now = dateNow.getTime()
-  const shouldRemindClaims = userProperties.getLocal('shouldRemindClaims')
 
-  if (!shouldRemindClaims) {
+  if (!notificationsEnabled(userStorage)) {
     return
   }
 
@@ -41,8 +42,6 @@ export const dailyClaimNotification = async (userStorage, goodWallet) => {
     if (!dailyUBI || (lastClaimNotification && now <= lastClaimNotification)) {
       return
     }
-
-    const { testClaimNotification } = Config
 
     // notify if current time is dailyClaimTime or later OR test notifications are enabled
     const needToNotify = now >= dailyClaimTime || testClaimNotification
@@ -64,25 +63,17 @@ export const dailyClaimNotification = async (userStorage, goodWallet) => {
 }
 
 export const useNotifications = navigation => {
-  const [getNavigation] = usePropsRefs([navigation])
   const userStorage = useUserStorage()
-  const { userProperties } = userStorage || {}
+  const [getNavigation] = usePropsRefs([navigation])
+  const shouldRemindClaims = useMemo(() => notificationsEnabled(userStorage), [userStorage])
 
   useEffect(() => {
-    if (!userProperties) {
-      return
-    }
-
-    const shouldRemindClaims = userProperties.getLocal('shouldRemindClaims')
-
     if (!shouldRemindClaims) {
       return
     }
 
     // eslint-disable-next-line require-await
     const onClaimNotification = async navigation => navigation.navigate('Claim')
-    Notifications.registerRemoteNotifications()
-    const events = Notifications.events()
 
     const onReceived = (notification, completion) => {
       const { payload, title, body } = notification || {}
@@ -119,6 +110,9 @@ export const useNotifications = navigation => {
       completion()
     }
 
+    Notifications.registerRemoteNotifications()
+
+    const events = Notifications.events()
     const subscriptions = [
       events.registerNotificationReceivedForeground(onReceived),
       events.registerNotificationReceivedBackground(onReceived),
@@ -128,5 +122,5 @@ export const useNotifications = navigation => {
     return () => {
       invokeMap(subscriptions, 'remove')
     }
-  }, [userProperties])
+  }, [shouldRemindClaims])
 }

cbe577032fce723a18b3db0a946596f03f3a7087
Lewis B <lewis@ikigaistudios.eu>
2022-11-16 15:19:39 +0100

    fix unsupported network bug & add disconnect/switch account from unsupported

--- src/components/Web3Network/index.tsx
+++ src/components/Web3Network/index.tsx
@@ -1,37 +1,93 @@
 import React from 'react'
+import { darken } from 'polished'
+import { Activity } from 'react-feather'
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React'
 import { useNetworkModalToggle } from '../../state/application/hooks'
 import { NETWORK_ICON, NETWORK_LABEL } from '../../constants/networks'
 import NetworkModal from '../NetworkModal'
 import { ButtonOutlined } from '../gd/Button'
+import styled from 'styled-components'
+import { ButtonSecondary } from '../ButtonLegacy'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+
+const Web3StatusGeneric = styled(ButtonSecondary)`
+    ${({ theme }) => theme.flexRowNoWrap}
+    width: 100%;
+    align-items: center;
+    padding: 0.5rem;
+    border-radius: ${({ theme }) => theme.borderRadius};
+    cursor: pointer;
+    user-select: none;
+    :focus {
+        outline: none;
+    }
+`
+
+const Web3StatusError = styled(Web3StatusGeneric)`
+    background-color: ${({ theme }) => theme.red1};
+    border: 1px solid ${({ theme }) => theme.red1};
+    color: ${({ theme }) => theme.white};
+    font-weight: 500;
+    :hover,
+    :focus {
+        background-color: ${({ theme }) => darken(0.1, theme.red1)};
+    }
+`
+
+const NetworkIcon = styled(Activity)`
+    margin-left: 0.25rem;
+    margin-right: 0.5rem;
+    width: 16px;
+    height: 16px;
+`
+
+const Text = styled.p`
+    flex: 1 1 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 0 0.5rem 0 0.25rem;
+    font-size: 1rem;
+    width: fit-content;
+    font-weight: 500;
+`
 
 function Web3Network(): JSX.Element | null {
     const { chainId, error, active } = useActiveWeb3React()
+    const { i18n } = useLingui()
 
     const toggleNetworkModal = useNetworkModalToggle()
 
-    if (!chainId || error || !active) return null
+    if (!chainId || !active) return null
 
     return (
-      <>      
-        <div
-          className="flex items-center rounded p-0.5 whitespace-nowrap   cursor-pointer select-none pointer-events-auto"
-          onClick={() => toggleNetworkModal()}
-        >
-          <ButtonOutlined style={{padding: "0"}}>
-              <div className="grid items-center grid-flow-col px-3 py-2 rounded-lg pointer-events-auto auto-cols-max">
-                  <img
-                      src={NETWORK_ICON[chainId]}
-                      alt="Switch Network"
-                      className="mr-2 rounded-md"
-                      style={{ width: 22, height: 22 }}
-                  />
-                  <div className="">{NETWORK_LABEL[chainId]}</div>
-              </div>
-          </ButtonOutlined>
-          <NetworkModal />
-        </div>
-      </>
+        <>
+            {error ? (
+                <Web3StatusError onClick={toggleNetworkModal}>
+                    <NetworkIcon />
+                    <Text>{i18n._(t`Unsupported network`)}</Text>
+                </Web3StatusError>
+            ) : (
+                <div
+                    className="flex items-center rounded p-0.5 whitespace-nowrap   cursor-pointer select-none pointer-events-auto"
+                    onClick={() => toggleNetworkModal()}
+                >
+                    <ButtonOutlined style={{ padding: '0' }}>
+                        <div className="grid items-center grid-flow-col px-3 py-2 rounded-lg pointer-events-auto auto-cols-max">
+                            <img
+                                src={NETWORK_ICON[chainId]}
+                                alt="Switch Network"
+                                className="mr-2 rounded-md"
+                                style={{ width: 22, height: 22 }}
+                            />
+                            <div className="">{NETWORK_LABEL[chainId]}</div>
+                        </div>
+                    </ButtonOutlined>
+                </div>
+            )}
+            <NetworkModal />
+        </>
     )
 }
 
--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -1,94 +1,16 @@
-import { darken, lighten } from 'polished'
 import React, { useMemo } from 'react'
-import { Activity } from 'react-feather'
 import styled from 'styled-components'
 import useENSName from '../../hooks/useENSName'
 import { useWalletModalToggle } from '../../state/application/hooks'
 import { isTransactionRecent, useAllTransactions } from '../../state/transactions/hooks'
 import { TransactionDetails } from '../../state/transactions/reducer'
 import { shortenAddress } from '../../utils'
-import { ButtonSecondary } from '../ButtonLegacy'
 import Loader from '../Loader'
-import WalletModal from '../WalletModal' 
+import WalletModal from '../WalletModal'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { OnboardConnectButton } from '../BlockNativeOnboard'
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React'
-import { UnsupportedChainId } from '@gooddollar/web3sdk'
-
-const IconWrapper = styled.div<{ size?: number }>`
-    ${({ theme }) => theme.flexColumnNoWrap};
-    align-items: center;
-    justify-content: center;
-    & > * {
-        height: ${({ size }) => (size ? size + 'px' : '32px')};
-        width: ${({ size }) => (size ? size + 'px' : '32px')};
-    }
-`
-
-const Web3StatusGeneric = styled(ButtonSecondary)`
-    ${({ theme }) => theme.flexRowNoWrap}
-    width: 100%;
-    align-items: center;
-    padding: 0.5rem;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    cursor: pointer;
-    user-select: none;
-    :focus {
-        outline: none;
-    }
-`
-const Web3StatusError = styled(Web3StatusGeneric)`
-    background-color: ${({ theme }) => theme.red1};
-    border: 1px solid ${({ theme }) => theme.red1};
-    color: ${({ theme }) => theme.white};
-    font-weight: 500;
-    :hover,
-    :focus {
-        background-color: ${({ theme }) => darken(0.1, theme.red1)};
-    }
-`
-
-const Web3StatusConnect = styled(Web3StatusGeneric)<{ faded?: boolean }>`
-    background-color: ${({ theme }) => theme.color.text2};
-    border: none;
-    border-radius: 6px;
-
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 19px;
-    color: ${({ theme }) => theme.color.main};
-    padding-left: 17px;
-    padding-right: 17px;
-    height: 42px;
-    transition: background 0.25s;
-
-    &:hover,
-    &:focus {
-        border: none;
-        background-color: ${({ theme }) => theme.color.text2hover};
-        transition: background 0.25s;
-    }
-`
-
-const Text = styled.p`
-    flex: 1 1 auto;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    margin: 0 0.5rem 0 0.25rem;
-    font-size: 1rem;
-    width: fit-content;
-    font-weight: 500;
-`
-
-const NetworkIcon = styled(Activity)`
-    margin-left: 0.25rem;
-    margin-right: 0.5rem;
-    width: 16px;
-    height: 16px;
-`
 
 // we want the latest one to come first, so return negative if a is after b
 function newTransactionsFirst(a: TransactionDetails, b: TransactionDetails) {
@@ -97,7 +19,7 @@ function newTransactionsFirst(a: TransactionDetails, b: TransactionDetails) {
 
 const Web3StatusInnerSC = styled.div`
     background: ${({ theme }) => theme.color.bg1};
-    color: ${({ theme }) => theme.color.input}; 
+    color: ${({ theme }) => theme.color.input};
     box-shadow: ${({ theme }) => theme.shadow.settings};
     border-radius: 3px;
 `
@@ -115,52 +37,40 @@ function Web3StatusInner() {
         return txs.filter(isTransactionRecent).sort(newTransactionsFirst)
     }, [allTransactions])
 
-    const pending = sortedRecentTransactions.filter(tx => !tx.receipt).map(tx => tx.hash)
+    const pending = sortedRecentTransactions.filter((tx) => !tx.receipt).map((tx) => tx.hash)
 
     const hasPendingTransactions = !!pending.length
 
     const toggleWalletModal = useWalletModalToggle()
 
-    if (account && !error) {
+    if (account) {
         return (
-            <Web3StatusInnerSC
-                id="web3-status-connected"
-                className="flex items-center px-3 py-2 rounded-lg"
-                onClick={toggleWalletModal}
-            >
-                {hasPendingTransactions ? (
-                    <div className="flex items-center justify-between">
-                        <div className="pr-2">
-                            {pending?.length} {i18n._(t`Pending`)}
-                        </div>{' '}
-                        <Loader stroke="#173046" />
-                    </div>
-                ) : (
-                    <div className="mr-2">{ENSName || shortenAddress(account)}</div>
-                )}
-            </Web3StatusInnerSC>
-        )
-    } else if (error) {
-        return (
-            <Web3StatusError onClick={toggleWalletModal}>
-                <NetworkIcon />
-                <Text>
-                    {
-                    error instanceof UnsupportedChainId
-                        ? i18n._(t`You are on the wrong network`)
-                        : i18n._(t`Error`)}
-                </Text>
-            </Web3StatusError>
+            <div className="flex flex-row">
+                <Web3StatusInnerSC
+                    id="web3-status-connected"
+                    className="flex items-center px-3 py-2 ml-2 rounded-lg"
+                    onClick={toggleWalletModal}
+                >
+                    {hasPendingTransactions ? (
+                        <div className="flex items-center justify-between">
+                            <div className="pr-2">
+                                {pending?.length} {i18n._(t`Pending`)}
+                            </div>{' '}
+                            <Loader stroke="#173046" />
+                        </div>
+                    ) : (
+                        <div className="mr-2">{ENSName || shortenAddress(account)}</div>
+                    )}
+                </Web3StatusInnerSC>
+            </div>
         )
     } else {
-        return (
-          <OnboardConnectButton />
-        )
+        return <OnboardConnectButton />
     }
 }
 
-export default function Web3Status() {
-    const { active, account } = useActiveWeb3React()
+export default function Web3Status(): JSX.Element {
+    const { account } = useActiveWeb3React()
 
     const { ENSName } = useENSName(account ?? undefined)
 

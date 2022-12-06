b72733c9f99a523843d32db972c12580869f9d66
sirpy <hadar@gooddollar.org>
2022-12-06 13:51:04 +0200

    add: hcaptcha (#3874)
    * add: hcaptcha
    
    * add: generic recaptcha web
    
    * add: fingerprint

--- src/components/auth/components/Recaptcha/index.js
+++ src/components/auth/components/Recaptcha/index.js
@@ -1,5 +1,6 @@
 import { get, noop } from 'lodash'
 import React, { useCallback, useImperativeHandle, useRef, useState } from 'react'
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 import Config from '../../../../config/config'
 import logger from '../../../../lib/logger/js-logger'
 import API from '../../../../lib/API'
@@ -7,21 +8,28 @@ import Captcha from './Recaptcha'
 
 const log = logger.child({ from: 'init' })
 
-const { recaptchaSiteKey, publicUrl } = Config
+const { recaptchaSiteKey, publicUrl, fpSiteKey } = Config
+const fpPromise = FingerprintJS.load({ apiKey: fpSiteKey })
+const getFingerprintId = () => {
+  // Initialize an agent at application startup.
 
+  // Get the visitor identifier when you need it.
+  return fpPromise.then(fp => fp.get())
+}
 const Recaptcha = React.forwardRef(({ onSuccess = noop, onFailure = noop, children }, ref) => {
   const captchaRef = useRef()
   const [isPassed, setIsPassed] = useState(false)
 
   const onVerify = useCallback(
-    async payload => {
+    async (payload, ekey) => {
       let hasPassed = false
-      log.debug('Recaptcha payload', payload)
-
       try {
-        const result = await API.verifyCaptcha(payload)
-
-        log.debug('Recaptcha verify result', result)
+        const captchaType = captchaRef.current.type?.() || 'recaptcha'
+        const fingerprint = await getFingerprintId()
+        log.debug('Recaptcha payload', { payload, ekey, captchaType, fingerprint })
+        const result = await API.verifyCaptcha({ payload, captchaType, fingerprint })
+        captchaRef.current.reset()
+        log.debug('Recaptcha verify result', { result })
         hasPassed = get(result, 'data.success', false)
       } catch (exception) {
         log.error('recaptcha verification failed', exception.message, exception, payload)

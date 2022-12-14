9517e621b025a416af9dda723b74261b555456fe
johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
2022-12-14 17:02:10 +0200

    263 lint staged tsc (#266)
    * Delete .husky directory
    
    * add husky, lint staged
    
    * terst lint
    
    * final adjustments
    
    * -
    
    * fix husky install
    
    * chore: update lint-staged, partial eslint fix
    
    Co-authored-by: Alex Serdukov <alexcyberneticist@gmail.com>
    Co-authored-by: L03TJ3 <laurence@gooddollar.org>

---
+++ lint-staged.config.js
@@ -0,0 +1,32 @@
+const { ESLint } = require('eslint')
+const { writeFile } = require('fs')
+const { promisify } = require('util')
+
+const eslint = new ESLint()
+const writeFileAsync = promisify(writeFile)
+const tsConfigPath = './tsconfig.lint-staged.json'
+
+const tsConfig = (files) => `
+{
+  "extends": "./tsconfig.json",
+  "include": [
+    "./src/**/*.d.ts",
+    "./types",
+    ${files.join(',\n')}
+  ],
+  "compilerOptions": {
+    "noEmit": true
+  }
+}    
+`
+
+module.exports = {
+    '**/*.{js,jsx,ts,tsx}': async (files) => {
+        const ignored = await Promise.all(files.map(async (file) => eslint.isPathIgnored(file)))
+        const filtered = files.filter((_, index) => !ignored[index]).map((path) => `"${path}"`)
+
+        await writeFileAsync(tsConfigPath, tsConfig(filtered))
+
+        return [`eslint --no-ignore --max-warnings=0 --fix ${filtered.join(' ')}`, `tsc --p ${tsConfigPath}`]
+    },
+}

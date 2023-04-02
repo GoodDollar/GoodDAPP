467f23f55bd72ab3f1311fc767c6b216f2f672cb
sirpy <hadarr@gmail.com>
2023-04-02 22:03:35 +0300

    fix: workbox config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -545,7 +545,7 @@ module.exports = {
       skipWaiting: false,
       exclude: [/\.map$/, /asset-manifest\.json$/],
       navigateFallback: publicUrl + '/index.html',
-      navigateFallbackBlacklist: [
+      navigateFallbackDenylist: [
         // Exclude URLs starting with /_, as they're likely an API call
         new RegExp('^/_'),
         // Exclude URLs containing a dot, as they're likely a resource in

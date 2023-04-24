27d1b37aa393c15c2942d4caa0e33fbf01305b7e
John Smith <alexey@gooddollar.org>
2023-04-24 19:09:16 +0300

    add set new goodid branches for vercel

--- vercel-goodid-ignore-build.sh
+++ vercel-goodid-ignore-build.sh
@@ -3,7 +3,7 @@
 # this is for vercel to prevent builds of feature branches. it is used in settings/git/ignored build step
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  || "$VERCEL_GIT_COMMIT_REF" == "next" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "goodid/dev" || "$VERCEL_GIT_COMMIT_REF" == "goodid/qa"  || "$VERCEL_GIT_COMMIT_REF" == "goodid/prod" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;

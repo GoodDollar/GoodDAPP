d2b712ab43b78b988267af7c6b3bce17e43128f1
John Smith <alexey@gooddollar.org>
2023-04-24 14:02:44 +0300

    add rewrite send() via switch() statement

--- src/lib/wallet/JsonRpcWithSigner.js
+++ src/lib/wallet/JsonRpcWithSigner.js
@@ -5,42 +5,52 @@ import { Wallet } from '@ethersproject/wallet'
 export class JsonRpcProviderWithSigner extends JsonRpcProvider {
   constructor(jsonRpcProvider, privateKey: string) {
     super()
+
     this.signer = new Wallet(privateKey, this)
     this.jsonRpcProvider = jsonRpcProvider
   }
 
   // eslint-disable-next-line require-await
   async request(request): Promise<any> {
-    const res = await this.send(request.method, request.params)
-    return res
+    return this.send(request.method, request.params)
   }
 
   async send(method: string, params: []): Promise<any> {
-    if (method === 'eth_sendTransaction') {
-      const transaction = params[0]
-      const signedTransaction = await this.request({ method: 'eth_signTransaction', params: [transaction] })
-      return this.jsonRpcProvider.send('eth_sendRawTransaction', [signedTransaction])
-    }
-    if (method === 'eth_signTransaction') {
-      const transaction = params[0]
-      if (transaction.gas) {
-        transaction.gasLimit = transaction.gas
-        delete transaction.gas
+    const { jsonRpcProvider, signer } = this
+    const [transaction, data] = params || []
+
+    switch (method) {
+      case 'eth_sendTransaction': {
+        const signedTransaction = await this.send('eth_signTransaction', [transaction])
+
+        return jsonRpcProvider.send('eth_sendRawTransaction', [signedTransaction])
       }
-      if (!transaction.nonce) {
-        transaction.nonce = await this.signer.getTransactionCount().then(_ => `0x${_.toString(16)}`)
+      case 'eth_signTransaction': {
+        let { gas, gasLimit, nonce, ...txData } = transaction || {}
+
+        if (!nonce) {
+          nonce = await signer.getTransactionCount()
+          nonce = `0x${nonce.toString(16)}`
+        }
+
+        return signer.signTransaction({ ...txData, nonce, gasLimit: gas || gasLimit })
       }
-      const signedTransaction = await this.signer.signTransaction(transaction)
-      return signedTransaction
-    } else if (method === 'eth_sign' || method === 'personal_sign') {
-      const message = params[1]
-      return this.signer.signMessage(message)
-    } else if (method.startsWith('eth_signTypedData')) {
-      const typedData = params[1]
-      return this.signer._signTypedData(typedData.domain, typedData.types, typedData.message)
-    } else if (method === 'eth_accounts') {
-      return [this.signer.address]
+      case 'personal_sign':
+      case 'eth_sign': {
+        return signer.signMessage(data)
+      }
+      case 'eth_accounts': {
+        return [signer.address]
+      }
+      default: {
+        if (method.startsWith('eth_signTypedData')) {
+          const { domain, types, message } = data || {}
+
+          return signer._signTypedData(domain, types, message)
+        }
+      }
     }
-    return this.jsonRpcProvider.send(method, params)
+
+    return jsonRpcProvider.send(method, params)
   }
 }

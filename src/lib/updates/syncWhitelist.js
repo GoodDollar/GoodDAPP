797df9d8577d6cb62b956b68e08dfc08a263bc12
sirpy <hadarr@gmail.com>
2023-04-25 15:44:49 +0300

    fix: missing syncwhitelist

---
+++ src/lib/updates/syncWhitelist.js
@@ -0,0 +1,16 @@
+import API from '../API'
+
+const fromDate = new Date('2023/04/25')
+
+/**
+ * @returns {Promise<void>}
+ */
+// eslint-disable-next-line require-await
+const syncWhitelist = async (lastUpdate, prevVersion, log, goodWallet, userStorage) => {
+  log.debug('syncwhitelist update')
+
+  const result = await API.syncWhitelist(goodWallet.account)
+  log.debug('syncwhitelist update success', { result })
+}
+
+export default { fromDate, update: syncWhitelist, key: 'syncWhitelist' }

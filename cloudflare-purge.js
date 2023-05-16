3af2f3e2e0decda9a20d589fc4dffca96f420f4f
sirpy <hadarr@gmail.com>
2023-05-16 14:09:45 +0300

    fix: check for cf key

--- cloudflare-purge.js
+++ cloudflare-purge.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch")
 
 // purge cache on production deploy
 console.log({vercel: process.env.VERCEL, branch: process.env.VERCEL_GIT_COMMIT_REF, token: process.env.CLOUDFLARE_KEY, zone: process.env.CLOUDFLARE_ZONE})
-if (process.env.VERCEL && ["next", "production"].find(_ => _ === process.env.VERCEL_GIT_COMMIT_REF)) {
+if (process.env.CLOUDFLARE_KEY && process.env.VERCEL && ["next", "production"].find(_ => _ === process.env.VERCEL_GIT_COMMIT_REF)) {
 
     fetch(`https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE}/purge_cache`,
      {body: JSON.stringify({purge_everything: true}), headers: [["Authorization", `Bearer ${process.env.CLOUDFLARE_KEY}`],["Content-Type", "application/json"]], method: "POST"})

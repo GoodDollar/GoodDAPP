327bcbc8b7d8a272dcbefe1bdf3168d7faf4e6f2
sirpy <hadarr@gmail.com>
2023-06-08 15:27:25 +0300

    wip: nodejq

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -6,7 +6,9 @@ export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
 yarn set version berry
 yarn --immutable
-ls -la node_modules/node-jq
+ls -la node_modules
+mkdir node_modules && true
+mkdir node_modules/node-jq && true
 mkdir node_modules/node-jq/bin && true
 curl -LO https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o ./node_modules/node-jq/bin/jq
 chmod +x ./node_modules/node-jq/bin/jq

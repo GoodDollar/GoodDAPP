b6a55fbee2223d17229926a46c0ae303905113b4
sirpy <hadarr@gmail.com>
2023-06-08 10:48:19 +0300

    wip: goodprotocol yarn3

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cp -R node_modules/@gooddollar/goodprotocol /tmp
 pushd /tmp/goodprotocol
-export CI=false
 export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
+yarn set version berry
 yarn --immutable
 npx patch-package
 yarn runNode &

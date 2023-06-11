0271c7fd513ca70dc96269fb67fb039a7d031dbd
sirpy <hadarr@gmail.com>
2023-06-11 13:13:14 +0300

    fix: failing test snapshot

--- scripts/blockchainTestSetup.sh
+++ scripts/blockchainTestSetup.sh
@@ -6,7 +6,6 @@ export MNEMONIC='test test test test test test test test test test test junk'
 export ADMIN_MNEMONIC='test test test test test test test test test test test junk'
 yarn set version berry
 yarn --immutable
-ls -la node_modules
 mkdir node_modules && true
 mkdir node_modules/node-jq && true
 mkdir node_modules/node-jq/bin && true

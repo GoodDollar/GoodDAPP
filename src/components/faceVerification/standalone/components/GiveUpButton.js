1228005e21715a35cc3b9c97581c02edd880881c
LewisB <lewis@gooddollar.org>
2023-08-01 08:15:14 +0200

    wip: giveup redirect fallback for requestIdleCallback

--- src/components/faceVerification/standalone/components/GiveUpButton.js
+++ src/components/faceVerification/standalone/components/GiveUpButton.js
@@ -8,6 +8,23 @@ import useFVRedirect from '../hooks/useFVRedirect'
 import { fireEvent, FV_GIVEUP } from '../../../../lib/analytics/analytics'
 import GiveUpDialog from './GiveUpDialog'
 
+// ref: https://gist.github.com/paullewis/55efe5d6f05434a96c36
+// requestIdleCallback not supported on safari
+// todo: properly test if this works
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function(cb) {
+    return setTimeout(function() {
+      var start = Date.now()
+      cb({
+        didTimeout: false,
+        timeRemaining: function() {
+          return Math.max(0, 50 - (Date.now() - start))
+        },
+      })
+    }, 1)
+  }
+
 const GiveUpButton = () => {
   const { showDialog } = useDialog()
   const fvRedirect = useFVRedirect()
@@ -19,7 +36,13 @@ const GiveUpButton = () => {
       fireEvent(FV_GIVEUP, data)
 
       // await before analytics scripts will perform some activity
+
       window.requestIdleCallback(() => fvRedirect(false, reason))
+
+      // Tried, but does not redirect it seems
+      // window.requestIdleCallback
+      //   ? window.requestIdleCallback(() => fvRedirect(false, reason))
+      //   : fvRedirect(false, reason)
     },
     [fvRedirect],
   )

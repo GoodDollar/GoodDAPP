4a366000f8087f6941cce5a97ca2ac39692c92b0
sirpy <hadarr@gmail.com>
2023-08-20 12:20:06 +0300

    add: reduce errors

--- src/components/dashboard/QR/QRScanner.web.js
+++ src/components/dashboard/QR/QRScanner.web.js
@@ -2,7 +2,7 @@
 
 // libraries
 import React from 'react'
-import QrReader from 'react-qr-reader'
+import QrReader from 'react-qr-reader-es6'
 
 export default ({ delay, onError, onScan }) => (
   <QrReader delay={delay} onError={onError} onScan={onScan} style={{ width: '100%' }} />

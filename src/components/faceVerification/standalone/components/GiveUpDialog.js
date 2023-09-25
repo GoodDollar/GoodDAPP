e79c808952e240f9d7e7ecfa67b7709ec12fea89
LewisB <lewis@gooddollar.org>
2023-09-25 19:26:49 +0200

    fix: simply giveup options map

--- src/components/faceVerification/standalone/components/GiveUpDialog.js
+++ src/components/faceVerification/standalone/components/GiveUpDialog.js
@@ -6,7 +6,7 @@ import { useDialog } from '../../../../lib/dialog/useDialog'
 import { withStyles } from '../../../../lib/styles'
 import { Section, Text } from '../../../common'
 import ExplanationDialog from '../../../common/dialogs/ExplanationDialog'
-import { GiveUpReason } from '../utils/giveupReason'
+import { GiveUpCancelled, GiveUpFailed } from '../utils/giveupReason'
 
 const OptionsRow = ({ styles, theme, reason, text }) => (
   <View style={styles.optionsRowContainer}>
@@ -31,12 +31,12 @@ const GiveUpDialog = ({ styles, theme, onReasonChosen, type }) => {
   )
 
   const title = type === 'cancelled' ? t`Why didn't you complete the GoodDollar-verification?` : t`What happened?`
-
+  const GiveUpReason = type === 'cancelled' ? GiveUpCancelled : GiveUpFailed
   return (
     <ExplanationDialog title={title}>
       <Section.Stack justifyContent="flex-start" style={styles.optionsRowWrapper}>
         <RadioButton.Group onValueChange={onSelected}>
-          {GiveUpReason[type].map(([reason, text]) => (
+          {Object.entries(GiveUpReason).map(([reason, text]) => (
             <OptionsRow key={reason} {...{ reason, text, theme, styles }} />
           ))}
         </RadioButton.Group>
--- src/components/faceVerification/standalone/utils/giveupReason.js
+++ src/components/faceVerification/standalone/utils/giveupReason.js
@@ -1,15 +1,15 @@
 import { t } from '@lingui/macro'
 
-const cancelledOptions = {
-  noScan: t`I don't want my face scanned`,
-  noTime: t`I don't have time for this`,
-  noComprendo: t`I don't understand what I'm doing`,
-  doLater: t`I will do it later`,
-  techIssues: t`I'm having technical issues`,
-  noVerify: t`I don't want to GoodDollar-verify anymore`,
+const GiveUpCancelled = {
+  NoScan: t`I don't want my face scanned`,
+  NoTime: t`I don't have time for this`,
+  NoComprendo: t`I don't understand what I'm doing`,
+  DoLater: t`I will do it later`,
+  TechIssues: t`I'm having technical issues`,
+  NoVerify: t`I don't want to GoodDollar-verify anymore`,
 }
 
-const failedOptions = {
+const GiveUpFailed = {
   NotRecognizeFace: t`My face is not recognized on the camera`,
   LowCameraQuali: t`I think my camera quality is too low`,
   OvalIssue: t`Unable to put my face in the Oval`,
@@ -19,16 +19,4 @@ const failedOptions = {
   DoItLater: t`I will do it later`,
 }
 
-export const GiveUpReason = new class {
-  failedSurvey = failedOptions
-
-  cancelledSurvey = cancelledOptions
-
-  get cancelled() {
-    return Object.entries(this.cancelledSurvey)
-  }
-
-  get failed() {
-    return Object.entries(this.failedSurvey)
-  }
-}()
+export { GiveUpCancelled, GiveUpFailed }

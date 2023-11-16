36e92f9ad1df9af9f3b18129920ba4867301dd36
LewisB <lewis@gooddollar.org>
2023-11-16 10:52:21 +0700

    fix: remove unused prop

--- src/components/dashboard/FeedList.js
+++ src/components/dashboard/FeedList.js
@@ -60,7 +60,6 @@ const FeedList = ({
   listHeaderComponent,
   listFooterComponent,
   headerLarge,
-  isScrolling,
   windowSize,
 }: FeedListProps) => {
   const { showErrorDialog } = useDialog()

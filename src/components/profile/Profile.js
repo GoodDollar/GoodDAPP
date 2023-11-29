4c7a81b9c6afaeaa30a942caae8baa7bcaf76dc7
LewisB <lewis@gooddollar.org>
2023-11-29 21:38:50 +0700

    update profile page with wallet-address and refac UI

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,17 +2,20 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { View } from 'react-native'
 import { t } from '@lingui/macro'
+
 import { createStackNavigator } from '../appNavigation/stackNavigation'
 import { Section, Text, Wrapper } from '../common'
+
 import UserAvatar from '../common/view/UserAvatar'
 import { withStyles } from '../../lib/styles'
-import { getDesignRelativeWidth } from '../../lib/utils/sizes'
+import { getDesignRelativeHeight, getDesignRelativeWidth } from '../../lib/utils/sizes'
 import RoundIconButton from '../common/buttons/RoundIconButton'
 import useProfile, { usePublicProfile } from '../../lib/userStorage/useProfile'
 import { theme } from '../theme/styles'
-import BorderedBox from '../common/view/BorderedBox'
 import Avatar from '../common/view/Avatar'
-import { useUserStorage } from '../../lib/wallet/GoodWalletProvider'
+import { useUserStorage, useWallet } from '../../lib/wallet/GoodWalletProvider'
+import { truncateMiddle } from '../../lib/utils/string'
+import { useClipboardCopy } from '../../lib/hooks/useClipboard'
 
 import EditProfile from './EditProfile'
 import ProfileDataTable from './ProfileDataTable'
@@ -21,6 +24,8 @@ import VerifyEdit from './VerifyEdit'
 import VerifyEditCode from './VerifyEditCode'
 
 const avatarSize = getDesignRelativeWidth(136)
+
+const copiedActionTimeout = 2000 // time during which the copy success message is displayed
 
 const ProfileAvatar = withStyles(() => ({
   avatar: {
@@ -33,9 +38,55 @@ const ProfileAvatar = withStyles(() => ({
   return <Avatar source={avatar} style={[styles.avatar, style]} imageStyle={style} unknownStyle={style} plain />
 })
 
+const AddressRow = ({ title, address = undefined, styles }) => {
+  const [performed, setPerformed] = useState(false)
+
+  const _onCopied = useCallback(() => {
+    setPerformed(true)
+    setTimeout(() => setPerformed(false), copiedActionTimeout)
+  }, [setPerformed])
+
+  const copyToClipboard = useClipboardCopy(address, _onCopied)
+
+  const handleCopy = useCallback(() => {
+    copyToClipboard()
+  }, [copyToClipboard])
+
+  const headerCopy = title === 'Wallet' ? t`My Wallet Address` : t`My Face Record ID`
+
+  const truncAddress = truncateMiddle(address, 29)
+
+  return (
+    <View style={styles.addressRow}>
+      {title === 'Wallet' ? (
+        <RoundIconButton
+          onPress={handleCopy}
+          iconSize={24}
+          iconName="wallet_alt-1"
+          style={[styles.iconContainer, { backgroundColor: theme.colors.lessDarkGray }]}
+        />
+      ) : (
+        <ProfileAvatar style={{ width: 42, height: 42 }} />
+      )}
+
+      <View style={{ flexDirection: 'column', width: 232 }}>
+        <Text style={{ textAlign: 'left', fontSize: 16 }}>{headerCopy}</Text>
+        <Text style={{ textAlign: 'left', fontSize: 14 }}>{truncAddress}</Text>
+      </View>
+      <RoundIconButton
+        onPress={handleCopy}
+        iconSize={performed ? 16 : 24}
+        iconName={performed ? 'success' : 'copy'}
+        style={styles.iconContainer}
+      />
+    </View>
+  )
+}
+
 const ProfileWrapper = ({ screenProps, styles }) => {
   const profile = usePublicProfile()
   const userStorage = useUserStorage()
+  const goodWallet = useWallet()
   const [faceRecordId, setRecordId] = useState()
 
   const { fullName } = profile
@@ -70,14 +121,12 @@ const ProfileWrapper = ({ screenProps, styles }) => {
         <ProfileDataTable profile={profile} showCustomFlag />
 
         <Section grow justifyContent="flex-end" style={{ marginBottom: 16 }}>
-          <BorderedBox
-            image={ProfileAvatar}
-            title="My Face Record ID"
-            content={faceRecordId}
-            truncateContent
-            copyButtonText="Copy ID"
-            enableIndicateAction
-          />
+          <Section.Row>
+            <AddressRow title="Wallet" address={goodWallet.account} styles={styles} />
+          </Section.Row>
+          <Section.Row>
+            <AddressRow title="FaceId" address={faceRecordId} styles={styles} />
+          </Section.Row>
         </Section>
       </Section>
       <View style={styles.userDataWrapper}>
@@ -105,6 +154,7 @@ ProfileWrapper.navigationOptions = {
 
 const getStylesFromProps = ({ theme }) => {
   const halfAvatarSize = avatarSize / 2
+
   return {
     emptySpace: {
       height: 75,
@@ -149,6 +199,28 @@ const getStylesFromProps = ({ theme }) => {
     },
     userName: {
       marginTop: theme.sizes.default,
+    },
+    iconContainer: {
+      height: 42,
+      width: 42,
+      backgroundColor: theme.colors.primary,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: getDesignRelativeHeight(4, false),
+      marginRight: 'auto',
+      marginLeft: 'auto',
+    },
+    addressRow: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      gap: 8,
+      width: '100%',
+      padding: 8,
+      backgroundColor: theme.colors.lightestGray,
+      borderRadius: 5,
+      marginBottom: 16,
     },
   }
 }

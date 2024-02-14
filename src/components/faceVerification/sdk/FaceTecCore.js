73ecbc1b3fbf8c13272e94ac02393fbeb17bc297
sirpy <hadar@gooddollar.org>
2024-02-14 09:52:55 +0200

    refactor: use once instead of let

--- src/components/faceVerification/sdk/FaceTecCore.js
+++ src/components/faceVerification/sdk/FaceTecCore.js
@@ -1,4 +1,6 @@
-// this is a hack for vite.
+import { once } from 'lodash'
+
+// this is a hack for vite. facetec sdk doesnt seem to work if not injectd via script tag but imported.
 const injectScript = src => {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script')
@@ -9,11 +11,6 @@ const injectScript = src => {
   })
 }
 
-let injected = false
-export const importSDK = async () => {
-  if (injected) {
-    return
-  }
+export const importSDK = once(async () => {
   await injectScript('/facetec/FaceTecSDK.web.js')
-  injected = true
-}
+})

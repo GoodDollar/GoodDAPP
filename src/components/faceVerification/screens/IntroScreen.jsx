c816aad1460f7800d20e866f0f29cd672190f710
LewisB <lewis@gooddollar.org>
2024-04-23 18:40:28 +0900

    Revert "Add isDelta to fv introscreen as compatible webview"
    This reverts commit 4fd1ff73c5529daa65bb95a02f651d95b8af71fd.

--- src/components/faceVerification/screens/IntroScreen.jsx
+++ src/components/faceVerification/screens/IntroScreen.jsx
@@ -145,7 +145,7 @@ const IntroScreen = ({ styles, screenProps, navigation }) => {
       requestCameraPermissions({ ignoreMountedState: true }) // we let the user try anyways. we add ignoreMOuntedState because when showing the unsupportedbrowser popup it unmounts
     },
     unsupportedPopup: UnsupportedWebview,
-    onCheck: () => (!isWebView && (!isIOSWeb || iosSupportedWeb)) || Config.isDeltaApp,
+    onCheck: () => !isWebView && (!isIOSWeb || iosSupportedWeb),
   })
 
   const handleVerifyClick = useCallback(async () => {

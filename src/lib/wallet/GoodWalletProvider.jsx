78647bf57f7d0cdc1719e9422877a91c1f3892cc
LewisB <lewis@gooddollar.org>
2024-06-19 22:18:55 +0900

    temp: wait on userproperties when seed exists

--- src/lib/wallet/GoodWalletProvider.jsx
+++ src/lib/wallet/GoodWalletProvider.jsx
@@ -222,7 +222,7 @@ export const GoodWalletProvider = ({ children, disableLoginAndWatch = false }) =
           await doLogin(wallet, storage, false)
         }
 
-        if (isLoggedInRouter) {
+        if (isLoggedInRouter || (seedOrWeb3 && Config.env !== 'test')) {
           await storage.initRegistered()
 
           if (loginAndWatch) {

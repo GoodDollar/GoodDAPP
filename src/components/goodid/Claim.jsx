ff0fe44dd33289247be7f6e1b7fd38e9f3a9547d
LewisB <lewis@gooddollar.org>
2024-09-19 00:44:15 +0900

    add: claim success modal

--- src/components/goodid/Claim.jsx
+++ src/components/goodid/Claim.jsx
@@ -2,18 +2,61 @@ import React, { useCallback, useContext } from 'react'
 import { ClaimProvider, ClaimWizard, GoodIdProvider } from '@gooddollar/good-design'
 import { NewsFeedProvider, SupportedChains } from '@gooddollar/web3sdk-v2'
 import { noop } from 'lodash'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
+import { t } from '@lingui/macro'
+import moment from 'moment'
 
+import AsyncStorage from '../../lib/utils/asyncStorage'
 import Config from '../../config/config'
 import { GoodWalletContext, useSwitchNetwork } from '../../lib/wallet/GoodWalletProvider'
+import { useFlagWithPayload } from '../../lib/hooks/useFeatureFlags'
+import { useDialog } from '../../lib/dialog/useDialog'
+import TaskDialog from '../common/dialogs/TaskDialog'
 
 import { withStyles } from '../../lib/styles'
 import { getDesignRelativeHeight } from '../../lib/utils/sizes'
+import SpinnerCheckMark from '../common/animations/SpinnerCheckMark/SpinnerCheckMark'
+
+export const LoadingAnimation = ({ success, speed = 3 }) => (
+  <View style={{ alignItems: 'center' }}>
+    <SpinnerCheckMark
+      successSpeed={speed}
+      success={success}
+      width={145}
+      marginTop={Platform.select({ web: undefined, default: 5 })}
+    />
+  </View>
+)
 
 const ClaimPageWrapper = ({ screenProps, styles }) => {
   const { goodWallet } = useContext(GoodWalletContext)
   const { navigateTo } = screenProps
   const { switchNetwork } = useSwitchNetwork()
+  const { showDialog } = useDialog()
+
+  const payload = useFlagWithPayload('next-tasks')
+
+  const { tasks } = payload
+
+  const onClaimSuccess = useCallback(async () => {
+    const today = moment().format('YYYY-MM-DD')
+    const shownTasksToday = await AsyncStorage.getItem('shownTasksToday')
+
+    if (shownTasksToday === today) {
+      return
+    }
+
+    await AsyncStorage.setItem('shownTasksToday', today)
+
+    await showDialog({
+      image: <LoadingAnimation success speed={2} />,
+      content: <TaskDialog tasks={tasks} />,
+      title: t`You've claimed today!`,
+      titleStyle: { paddingTop: 0, marginTop: 0, minHeight: 'auto' },
+      onDismiss: noop,
+      showButtons: false,
+    })
+  }, [showDialog])
 
   const onUpgrade = useCallback(() => {
     navigateTo('GoodIdOnboard')
@@ -22,7 +65,12 @@ const ClaimPageWrapper = ({ screenProps, styles }) => {
   return (
     <View style={styles.wrapper}>
       <GoodIdProvider>
-        <ClaimProvider explorerEndPoints={Config.goodIdExplorerUrls} onSwitchChain={switchNetwork} withNewsFeed={false}>
+        <ClaimProvider
+          explorerEndPoints={Config.goodIdExplorerUrls}
+          onSwitchChain={switchNetwork}
+          withNewsFeed={false}
+          onSuccess={onClaimSuccess}
+        >
           <NewsFeedProvider env={'qa'} limit={1}>
             <ClaimWizard
               account={goodWallet.account}

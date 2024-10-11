4231b01cf565e66b8f8737c04f186c410ff714bf
LewisB <lewis@gooddollar.org>
2024-10-12 01:32:42 +0700

    fix: align date formatting with web

--- src/lib/utils/FormatDate.android.js
+++ src/lib/utils/FormatDate.android.js
@@ -3,18 +3,18 @@ import { filter } from 'lodash'
 import moment from 'moment'
 import 'moment/min/locales.min'
 
-import { locale } from './i18n.js'
+// const shortDateTimeFormat = (() => {
+//   // do not change locale globally just for particular instance
+//   const now = moment().locale(locale)
+//   const localeData = now.localeData()
 
-const shortDateTimeFormat = (() => {
-  // do not change locale globally just for particular instance
-  const now = moment().locale(locale)
-  const localeData = now.localeData()
+//   // detect date format
+//   const dateFormat = localeData.longDateFormat('L')
 
-  // detect date format
-  const dateFormat = localeData.longDateFormat('L')
+//   return `${dateFormat} HH:mm`
+// })()
 
-  return `${dateFormat} HH:mm`
-})()
+const shortDateTimeFormat = 'DD.MM.YYYY HH:mm'
 
 /**
  * format date util function

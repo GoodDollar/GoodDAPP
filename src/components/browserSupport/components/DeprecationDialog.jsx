76644a8ae5ed730ae8e3331250679f743389e063
LewisB <lewis@gooddollar.org>
2024-10-23 15:50:50 +0700

    fix: update deprecationDialog to redirect NGW modal

--- src/components/browserSupport/components/DeprecationDialog.jsx
+++ src/components/browserSupport/components/DeprecationDialog.jsx
@@ -1,7 +1,8 @@
 // libraries
 import React, { useCallback } from 'react'
+import { Platform, Text } from 'react-native'
+import { t } from '@lingui/macro'
 
-import { retry } from '../../../lib/utils/async'
 import { isAndroidNative } from '../../../lib/utils/platform'
 import ExplanationDialog from '../../common/dialogs/ExplanationDialog'
 import { useDialog } from '../../../lib/dialog/useDialog'
@@ -20,42 +21,53 @@ export const shouldShowDeprecationDialog = async () => {
     return false
   }
 
-  const { count, last, country } = (await AsyncStorage.getItem(STORAGE_KEY)) || {
+  const { count, last } = (await AsyncStorage.getItem(STORAGE_KEY)) || {
     count: 0,
     last: 0,
-    country: undefined,
   }
 
-  let userCountry = country
-  if (!userCountry) {
-    userCountry = await retry(
-      async () => (await fetch('https://get.geojs.io/v1/ip/country.json')).json(),
-      3,
-      2000,
-    ).then(data => data.country)
-    AsyncStorage.setItem(STORAGE_KEY, { count, last, country: userCountry })
-  }
-
-  if (userCountry === 'GB' && Date.now() > last + BACKOFF * 2 ** count) {
-    AsyncStorage.setItem(STORAGE_KEY, { count: count === 5 ? 1 : count + 1, last: Date.now(), country: userCountry })
+  if (Date.now() > last + BACKOFF * 2 ** count) {
+    AsyncStorage.setItem(STORAGE_KEY, { count: count === 5 ? 1 : count + 1, last: Date.now() })
     return true
   }
 
   return false
 }
 
+const DeprecationCopy = () => (
+  <Text
+    style={{
+      fontSize: normalizeText(18),
+      textAlign: 'center',
+      fontWeight: 'bold',
+      marginBottom: 12,
+    }}
+    {...Platform.select({ android: { marginVertical: getDesignRelativeHeight(25, false) } })}
+  >
+    {t`To continue using GoodWallet, please access 
+the New GoodWallet using a web browser https://goodwallet.xyz/`}
+  </Text>
+)
+
 const DeprecationDialog = () => {
   const goToWallet = () => {
-    openLink('https://wallet.gooddollar.org', '_blank')
+    openLink('https://goodwallet.xyz', '_blank')
   }
 
   return (
     <ExplanationDialog
-      title={'UK Users!'}
-      text={`Following September 12, 2024, \n support for the GoodWallet on Android in the UK will be limited. \n As all GoodWallet users, UK-based users can use the web-app version of GoodWallet through a compatible web browser at https://wallet.gooddollar.org`}
+      title={'The New GoodWallet!'}
+      customText={<DeprecationCopy />}
+      containerStyle={{ height: '80%' }}
+      titleStyle={{ fontWeight: 'normal', margin: 0 }}
       textStyle={{
         fontSize: normalizeText(16),
-        marginVertical: getDesignRelativeHeight(25, false),
+
+        marginVertical: {
+          ...Platform.select({
+            android: getDesignRelativeHeight(25, false),
+          }),
+        },
       }}
       buttons={[
         {
@@ -64,7 +76,7 @@ const DeprecationDialog = () => {
         },
       ]}
       buttonsContainerStyle={{ justifyContent: 'center' }}
-      buttonText={{ fontSize: normalizeText(10) }}
+      buttonText={{ fontSize: 14, fontWeight: 'bold' }}
     />
   )
 }

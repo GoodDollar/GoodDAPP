7210a3c5edb185aaedb25d5f4b9ffa358d65eb81
sirpy <hadar@gooddollar.org>
2024-11-24 12:40:00 +0200

    fix: unit tests with languageprovider

--- src/mainApp/AppHolder.jsx
+++ src/mainApp/AppHolder.jsx
@@ -1,20 +1,17 @@
 // @flow
 import { ActionSheetProvider } from '@expo/react-native-action-sheet'
 import React from 'react'
-import { GoodUIi18nProvider } from '@gooddollar/good-design'
 import '../lib/shim'
 
 import LanguageProvider from '../language/i18n'
 import AppHot from './AppHot'
 
 const AppHolder = () => (
-  <GoodUIi18nProvider>
-    <LanguageProvider>
-      <ActionSheetProvider>
-        <AppHot />
-      </ActionSheetProvider>
-    </LanguageProvider>
-  </GoodUIi18nProvider>
+  <LanguageProvider>
+    <ActionSheetProvider>
+      <AppHot />
+    </ActionSheetProvider>
+  </LanguageProvider>
 )
 
 export default AppHolder

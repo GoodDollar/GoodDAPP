92a73676915ebe3e6946dc056d947006f63742fb
LewisB <lewis@gooddollar.org>
2024-12-13 14:38:30 +0100

    fixes: - ProfileID shown twice - catch geojs error

--- src/lib/utils/supportedCountries.js
+++ src/lib/utils/supportedCountries.js
@@ -1,13 +1,11 @@
 import { retry } from './async'
 
 export const supportedCountries = async (supportedCountries = '', whitelist = [], account) => {
-  const country = await retry(
-    async () => (await fetch('https://get.geojs.io/v1/ip/country.json')).json(),
-    3,
-    2000,
-  ).then(data => data.country)
+  const country = await retry(async () => (await fetch('https://get.geojs.io/v1/ip/country.json')).json(), 3, 2000)
+    .then(data => data.country)
+    .catch(() => false)
 
-  const isEligible = supportedCountries?.split(',')?.includes(country) || whitelist?.includes(account)
+  const isEligible = supportedCountries?.split(',')?.includes(country) || whitelist?.toLowerCase().includes(account)
 
   return isEligible
 }
